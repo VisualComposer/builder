3304e7f0170a0d23e8bc2c8d80406e6c435e431d
Vjaceslav Vasenin <time2type@gmail.com>
2016-01-04 16:17:04 +0200

    add button in wp

--- index.php
+++ index.php
@@ -39,5 +39,31 @@ if ( is_admin() ) {
 
 	// 3. load page
 } else {
+	$jsScriptRendered = false;
+	$rowActionControl = function() use (&$jsScriptRendered) {
+		$post = get_post();
+		if ( $this->showButton( $post->ID ) ) {
+			$actions['edit_vc_v'] = '<a
+		href="#" onClick="vcvLoadInline">' . __( 'Edit with VC 5', 'vc5' ) . '</a>';
+		}
+		if(!$jsScriptRendered) {
+			ob_start();
+			?>
+			<script>
+				function vcvLoadInline() {
+					var g = d.createElement('script'),
+        	            s = d.getElementsByTagName('script')[0]; // find the first script tag in the document
+					g.src = ' <?php echo preg_replace( '/\s/', '%20', plugins_url( 'public/wp.bundle.js', __FILE__ ) ) ?>''
+                    s.parentNode.insertBefore(g, s);
+				};
+			</script>
+			<?php
+			$jsScriptRendered = true;
+			$actions['edit_vc_v'] .= $ob_get_flush();
+		}
+		return $actions;
+	};
+	add_filter( 'page_row_actions', $rowActionControl);
+	add_filter( 'post_row_actions', $rowActionControl);
 	// add button for frontend editor near edit
 }
