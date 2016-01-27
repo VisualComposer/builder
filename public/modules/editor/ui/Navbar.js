1c42b52a1cb99d20ee3f5e1b4c253010e95a6592
Vjaceslav Vasenin <time2type@gmail.com>
2016-01-27 16:19:15 +0200

    Navbar vertical position

--- public/modules/editor/ui/Navbar.js
+++ public/modules/editor/ui/Navbar.js
@@ -4,34 +4,70 @@ var TreeElement = require('../layouts/tree/TreeLayout');
 var AddElementModal = require('./add-element/AddElement.js');
 var classNames = require('classnames');
 require('./less/navbar/navbar-init.less');
+var mouseDown = true;
 module.exports = React.createClass(Mediator.installTo({
 	getInitialState: function() {
 		return {
-			menuExpand: false
+      startMove: false,
+			menuExpand: false,
+      vertical: false,
+      position: 0
 		}
 	},
-    componentDidMount: function() {
+  componentDidMount: function() {
       this.subscribe('layout:tree', function(){
         this.setState({menuExpand: true});
       }.bind(this));
-    },
-    openAddElement: function (e) {
+  },
+  openAddElement: function (e) {
         e && e.preventDefault();
         this.publish('app:add', 'vc-v-root-element');
-    },
+  },
 	clickMenuExpand: function() {
 		this.setState({menuExpand: !this.state.menuExpand});
 	},
-    clickSaveData: function() {
+  clickSaveData: function() {
       this.publish('app:save', true);
-    },
-    render: function () {
+  },
+  changePosition: function(e) {
+    if(this.setState({startMove: true}));
+    document.body.addEventListener('mouseup', this.cancelPositionChange);
+  },
+  checkMouseMove: function() {
+    if(this.state.startMove) {
+      if(this.state.position > 10) {
+        this.cancelPositionChange();
+        this.setState({vertical: !this.state.vertical});
+      } else {
+        this.setState({position: this.state.position+1})
+      }
+    }
+  },
+  cancelPositionChange: function() {
+    document.body.removeEventListener('mouseup', this.cancelPositionChange);
+    if(this.setState({startMove: false}));
+    this.setState({position: 0});
+  },
+  render: function () {
 		var menuExpandClass = classNames({
 			'dropdown': true,
 			'open': this.state.menuExpand
 		});
+    var mainCssClasses = classNames({
+      'navbar': true,
+      'navbar-vc': true,
+      'navbar-fixed-top': true,
+      'vcv-navbar-vertical': this.state.vertical
+    });
+    var navStyle = {
+      top: this.state.position > 5 ?  this.state.position + 'px' : 0,
+      left: this.state.position > 5 ? this.state.position + 'px' : 0
+    };
         return (
-            <nav className="navbar navbar-vc navbar-fixed-top">
+            <nav className={mainCssClasses}
+                 onMouseDown={this.changePosition}
+                 onMouseMove={this.state.startMove ? this.checkMouseMove : null}
+                style={navStyle}>
                 <div className="navbar-header">
                     <a className="navbar-brand"><span className="vcv-logo"></span></a>
                 </div>
@@ -44,9 +80,11 @@ module.exports = React.createClass(Mediator.installTo({
                         <TreeElement/>
                     </li>
                 </ul>
-                <ul className="nav navbar-nav pull-right">
-                    <li><button type="button" className="btn btn-default navbar-btn" onClick={this.clickSaveData}>Update</button></li>
-                </ul>
+                <div className="vcv-navbar-right-block">
+                  <ul className="nav navbar-nav pull-right" style={{marginRight: this.state.vertical ? null : 10 + 'px'}}>
+                    <li><button type="button" className="btn btn-success navbar-btn" onClick={this.clickSaveData}>Update</button></li>
+                  </ul>
+                </div>
 				<div className="vc_ui-inline-editor-container"></div>
                 <AddElementModal/>
             </nav>
