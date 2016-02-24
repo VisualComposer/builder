93667605a76600f7919361cf9c46a2f0672a5263
Vjaceslav Vasenin <time2type@gmail.com>
2016-02-24 16:06:55 +0200

    Hide inline when not needed

--- public/editor/modules/ui/inline-editor/lib/navbar-controls.js
+++ public/editor/modules/ui/inline-editor/lib/navbar-controls.js
@@ -2,8 +2,17 @@ var vcCake = require('vc-cake');
 vcCake.add('ui-inline-editor', function(api){
   var React = require('react');
   var Control = React.createClass({
+    getInitialState: function() {
+      return {
+        show: false
+      };
+    },
     render: function() {
-      return  <div className="vc-ui-navbar-controls-group vc-ui-navbar-hidden-sm">
+      var style = {};
+      if(true !== this.state.show) {
+        style.display = 'none';
+      }
+      return  <div className="vc-ui-navbar-controls-group vc-ui-navbar-hidden-sm" style={style}>
         <a className="vc-ui-navbar-control" href="#" title="Undo"><span className="vc-ui-navbar-control-content"><i className="vc-ui-navbar-control-icon vc-ui-icon vc-ui-icon-undo"></i><span>Undo</span></span></a>
         <a className="vc-ui-navbar-control" href="#" title="Redo" disabled><span className="vc-ui-navbar-control-content"><i className="vc-ui-navbar-control-icon vc-ui-icon vc-ui-icon-redo"></i><span>Redo</span></span></a>
       </div>;
