e6074dcdd9c94e9f73821a9d649f3865c4cbcdbc
Raitis G <raitisg@gmail.com>
2016-03-03 11:29:39 +0200

    Added (commented out) old implementation for some functions in Todo helper

--- visualcomposer/Helpers/Generic/Todo.php
+++ visualcomposer/Helpers/Generic/Todo.php
@@ -20,6 +20,12 @@ abstract class Todo {
 	 * @todo Implementation
 	 */
 	public static function isNetworkPlugin() {
+		//		if ( is_null( $this->isNetworkPlugin ) ) {
+		//			$isNetworkPlugin = is_multisite() && ( is_plugin_active_for_network( $this->pluginName() ) || is_network_only_plugin( $this->pluginName() ) );
+		//			$this->setAsNetworkPlugin( $isNetworkPlugin );
+		//		}
+		//
+		//		return $this->isNetworkPlugin;
 		return false;
 	}
 
@@ -27,6 +33,13 @@ abstract class Todo {
 	 * @todo Implementation
 	 */
 	public static function pointersAreDismissed() {
+		//		global $vc_default_pointers;
+		//
+		//		$pointers = (array) apply_filters( 'vc_pointers_list', $vc_default_pointers );
+		//		$prev_meta_value = get_user_meta( get_current_user_id(), 'dismissed_wp_pointers', true );
+		//		$dismissed = explode( ',', (string) $prev_meta_value );
+		//
+		//		return count( array_diff( $dismissed, $pointers ) ) < count( $dismissed );
 		return true;
 	}
 
