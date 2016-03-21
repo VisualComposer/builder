55902b34e521443a4a50341cde84b41a17fedf31
Pavel Ivanov <reactoranime@gmail.com>
2016-03-21 15:34:53 +0200

    Re-add to fix github folder name conflict.

---
+++ visualcomposer/Framework/Illuminate/container/ContextualBindingBuilder.php
@@ -0,0 +1,57 @@
+<?php namespace VisualComposer\Framework\Illuminate\Container;
+
+use VisualComposer\Framework\Illuminate\Contracts\Container\ContextualBindingBuilder as Contract;
+
+class ContextualBindingBuilder implements Contract
+{
+    /**
+     * The underlying container instance.
+     *
+     * @var \VisualComposer\Framework\Illuminate\Container\Container
+     */
+    protected $container;
+    /**
+     * The concrete instance.
+     *
+     * @var string
+     */
+    protected $concrete;
+    protected $needs;
+
+    /**
+     * Create a new contextual binding builder.
+     *
+     * @param  \VisualComposer\Framework\Illuminate\Container\Container $container
+     * @param  string $concrete
+     * @return void
+     */
+    public function __construct(Container $container, $concrete)
+    {
+        $this->concrete = $concrete;
+        $this->container = $container;
+    }
+
+    /**
+     * Define the abstract target that depends on the context.
+     *
+     * @param  string $abstract
+     * @return $this
+     */
+    public function needs($abstract)
+    {
+        $this->needs = $abstract;
+
+        return $this;
+    }
+
+    /**
+     * Define the implementation for the contextual binding.
+     *
+     * @param  \Closure|string $implementation
+     * @return void
+     */
+    public function give($implementation)
+    {
+        $this->container->addContextualBinding($this->concrete, $this->needs, $implementation);
+    }
+}
