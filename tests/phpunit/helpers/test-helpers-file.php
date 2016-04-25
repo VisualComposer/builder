54d0a93140b94a5ab6dbdab47e07d6984938df47
Raitis G <raitisg@gmail.com>
2016-04-25 17:05:08 +0300

    Add tests for File and Options helpers

---
+++ tests/phpunit/helpers/test-helpers-file.php
@@ -0,0 +1,35 @@
+<?php
+
+class HelpersFileTest extends WP_UnitTestCase
+{
+    public function testFileHelper()
+    {
+        /**
+         * @var $helper VisualComposer\Helpers\File
+         */
+        $helper = vcapp('VisualComposer\Helpers\File');
+
+        $this->assertTrue(is_object($helper), 'File helper should be an object');
+    }
+
+    public function testGetSetContents()
+    {
+        /**
+         * @var $helper VisualComposer\Helpers\File
+         */
+        $helper = vcapp('VisualComposer\Helpers\File');
+
+        $dir = wp_upload_dir();
+
+        $filepath = $dir['path'] . '/test-' . md5(microtime());
+
+        $this->assertEquals(6, $helper->setContents($filepath, 'foobar'));
+
+        $this->assertEquals('foobar', $helper->getContents($filepath));
+
+        unlink($filepath);
+
+        $this->assertFalse($helper->getContents($filepath));
+    }
+
+}
