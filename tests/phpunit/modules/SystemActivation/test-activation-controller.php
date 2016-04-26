7becd9a202d9e4b1c27e4b942c86dd1ef4a79640
Pavel Ivanov <reactoranime@gmail.com>
2016-04-26 11:22:45 +0300

    Improved phpdocs for variables

--- tests/phpunit/modules/SystemActivation/test-activation-controller.php
+++ tests/phpunit/modules/SystemActivation/test-activation-controller.php
@@ -7,10 +7,12 @@ class ActivationControllerTest extends WP_UnitTestCase
         /** @var $mock \VisualComposer\Application */
         $mock = vc_create_module_mock('\VisualComposer\Modules\System\Activation\Controller');
 
+        /** @var \VisualComposer\Helpers\Options $optionsHelper */
         $optionsHelper = vchelper('Options');
         $optionsHelper->set('version', 111);
         $this->assertEquals(111, $optionsHelper->get('version'));
 
+        /** @see \VisualComposer\Modules\System\Activation\Controller::setVersion */
         $mock->call('setVersion');
         $this->assertEquals(VCV_VERSION, $optionsHelper->get('version'));
     }
