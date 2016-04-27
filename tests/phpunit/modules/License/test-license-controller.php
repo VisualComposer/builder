0adee34e7445f86f31ea2b7f6bea73483098a9e3
Raitis G <raitisg@gmail.com>
2016-04-27 16:52:12 +0300

    Add tests for License/Controller module

---
+++ tests/phpunit/modules/License/test-license-controller.php
@@ -0,0 +1,41 @@
+<?php
+
+class LicenseControllerTest extends WP_UnitTestCase
+{
+    /**
+     * By changing these properties we can break BC.
+     */
+    public function testProperties()
+    {
+        /** @var $module \VisualComposer\Modules\License\Controller */
+        $module = vcapp('LicenseController');
+
+        $prop = new ReflectionProperty($module, 'licenseKeyOption');
+        $prop->setAccessible(true);
+        $this->assertEquals('license_key', $prop->getvalue($module));
+
+        $prop = new ReflectionProperty($module, 'licenseKeyTokenOption');
+        $prop->setAccessible(true);
+        $this->assertEquals('license_key_token', $prop->getvalue($module));
+
+        $prop = new ReflectionProperty($module, 'activationHost');
+        $prop->setAccessible(true);
+        $this->assertEquals('https://account.visualcomposer.io', $prop->getvalue($module));
+    }
+
+    public function testIsActivated()
+    {
+        /** @var $module \VisualComposer\Modules\License\Controller */
+        $module = vcapp('LicenseController');
+
+        /** @var \VisualComposer\Helpers\Options $optionsHelper */
+        $optionsHelper = vchelper('Options');
+
+        $this->assertFalse($module->isActivated());
+
+        $optionsHelper->set('license_key', 'foo-bar');
+
+        $this->assertTrue($module->isActivated());
+    }
+
+}
