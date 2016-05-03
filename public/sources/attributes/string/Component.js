9f76290490fe5dbae226bd45b3acca4b28346975
Pavel Ivanov <reactoranime@gmail.com>
2016-05-03 17:34:30 +0300

    Moved unused attributes to another folder

--- public/sources/attributes/string/Component.js
+++ public/sources/attributes/string/Component.js
@@ -1,17 +1,19 @@
 import React from 'react';
 
-module.exports = React.createClass({
+export class Component extends React.Component {
   render() {
     console.log('render string', this.props);
+    var {settings, name} = this.props;
+    var {value} = this.state;
     return (
       <div className="vc_ui-form-group">
-        <label className="vc_ui-form-group-heading">{this.props.settings.getTitle()}</label>
+        <label className="vc_ui-form-group-heading">{settings.getTitle()}</label>
         <input
           className="vc_ui-form-input"
           type="text"
           onChange={this.handleChange}
-          ref={this.props.name + 'Component'}
-          value={this.state.value}/>
+          ref={name + 'Component'}
+          value={value}/>
       </div>);
   }
-});
+}
