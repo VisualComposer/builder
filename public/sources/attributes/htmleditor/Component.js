faca0737f63acd8c4efda0a47d838a8528d4b7af
Pavel Ivanov <reactoranime@gmail.com>
2016-05-19 17:18:24 +0300

    changed tinymce default css

--- public/sources/attributes/htmleditor/Component.js
+++ public/sources/attributes/htmleditor/Component.js
@@ -3,8 +3,8 @@ import React from 'react'
 import tinymce from 'tinymce/tinymce'
 import Editor from 'react-tinymce'
 import 'tinymce/themes/modern/theme'
-import 'tinymce/skins/lightgray/skin.min.css'
-import 'tinymce/skins/lightgray/content.min.css'
+import './css/skin.css'
+import './css/content.css'
 import Attribute from '../attribute'
 
 export default class Component extends Attribute {
