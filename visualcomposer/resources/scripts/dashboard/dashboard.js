b79910c864c17c638f9e8da9f3934d21b8d301f4
Pavel Ivanov <reactoranime@gmail.com>
2016-05-19 19:13:50 +0300

    remove dashboard files

--- visualcomposer/resources/scripts/dashboard/dashboard.js
+++
@@ -1,8 +0,0 @@
-(function ($) {
-  $(function () {
-    $('#toplevel_page_vcv-dashboard a').each(function () {
-      var $el = $(this)
-      $el.attr('target', '_blank')
-    })
-  })
-})(window.jQuery)
