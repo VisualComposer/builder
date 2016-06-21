89b9ad0aa503a9e4ecfe9b8844d0919c04834ad2
Vjaceslav Vasenin <time2type@gmail.com>
2016-06-21 12:23:50 +0300

    Fix not loading editor on reload

--- public/node-main.js
+++ public/node-main.js
@@ -1,18 +1,19 @@
-var vcCake = require('vc-cake')
+import vcCake from 'vc-cake'
 require('./sources/less/bootstrap/init.less')
 require('./config/node-services')
 require('./config/node-attributes')
-var $ = require('expose?$!jquery')
+const $ = require('expose?$!jquery')
 
 $(document).ready(function () {
   require('./sources/css/wordpress.less')
   $('#vcv-editor-iframe').load(function () {
-    var iframeDocument = $('#vcv-editor-iframe').get(0).contentWindow.document
+    let iframeDocument = $('#vcv-editor-iframe').get(0).contentWindow.document
     $('[data-vcv="edit-fe-editor"]', iframeDocument).remove()
     vcCake.env('platform', 'node').start(function () {
       require('./config/node-modules')
     })
   })
+  $('#vcv-editor-iframe').attr('src', '/page.html')
   $(window).on('load resize', function () {
     $('#vcv-editor-iframe').height($(window).height() - 61)
   })
