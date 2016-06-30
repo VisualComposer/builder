950a6020f13fa8741acc8c63c04ea6630e2e51d0
Pavel I <reactoranime@gmail.com>
2016-06-30 17:36:22 +0300

    Disabled brandlogo link while it is not needed

--- public/editor/modules/ui/brand-logo/module.js
+++ public/editor/modules/ui/brand-logo/module.js
@@ -6,9 +6,9 @@ vcCake.add('ui-brand-logo', function (api) {
   var React = require('react')
   var Control = React.createClass({
     render: function () {
+      // TODO: Make correct link&target http://alpha.visualcomposer.io/wp-admin/?amputm_medium=frontend_editor
       return <a className="vcv-ui-navbar-logo" title="Visual Composer"
-        href="http://alpha.visualcomposer.io/wp-admin/?amputm_medium=frontend_editor"
-        target="_blank">
+        href="#">
         <span className="vcv-ui-navbar-logo-title">Visual Composer</span>
       </a>
     }
