9aa41f944974c7f45e69a2687d3c8bebd282913a
Vasilij <vasilij@wpbakery.com>
2016-07-01 13:49:08 +0300

    Add tab categories for all elements  #149007119970915

--- public/sources/elements-2/row/element.js
+++ public/sources/elements-2/row/element.js
@@ -1,5 +1,5 @@
 window.vcvAddElement(
-  {"name":{"type":"string","access":"protected","value":"Row"},"category":{"type":"string","access":"protected","value":"Content"},"background":{"type":"color","access":"public","value":"","options":{"label":"Background color"}},"relatedTo":{"type":"group","access":"protected","value":["General"]},"containerFor":{"type":"group","access":"protected","value":["Column"]},"tag":{"access":"protected","type":"string","value":"08bde8ca-0faf-4274-a496-f4c5c59bc76a"},"type":{"access":"protected","type":"string","value":"container"}},
+  {"name":{"type":"string","access":"protected","value":"Row"},"category":{"type":"string","access":"protected","value":"Content"},"background":{"type":"color","access":"public","value":"","options":{"label":"Background color"}},"editFormTab1":{"type":"group","access":"protected","value":["background"],"options":{"label":"Options"}},"editFormTabs":{"type":"group","access":"protected","value":["editFormTab1"]},"relatedTo":{"type":"group","access":"protected","value":["General"]},"containerFor":{"type":"group","access":"protected","value":["Column"]},"tag":{"access":"protected","type":"string","value":"515a34d9-497f-4f64-84f5-60ed6a1ca8d8"},"type":{"access":"protected","type":"string","value":"container"}},
   // Component callback
   function(component) {
 	require( './styles.css' )
