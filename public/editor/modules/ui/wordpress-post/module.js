789a434f27c5fb6da11dbb07db8a476765b2541b
Pavel I <reactoranime@gmail.com>
2016-07-01 12:31:52 +0300

    Allow saving in WP and Getting content when page loads #150146091928537

--- public/editor/modules/ui/wordpress-post/module.js
+++ public/editor/modules/ui/wordpress-post/module.js
@@ -1,21 +1,22 @@
 const vcCake = require('vc-cake')
-const cook = vcCake.getService('cook')
+// const cook = vcCake.getService('cook')
 const storage = vcCake.getService('wordpress-storage')
 const documentData = vcCake.getService('document')
-const reWrapDefaultContent = function (data) {
+/* const reWrapDefaultContent = function (data) {
   let newData = {}
   Object.keys(data).forEach((k) => {
     newData[ k ] = data[ k ]
     newData[ k ].tag = cook.getTagByName(newData[ k ].name)
   })
   return newData
-}
+} */
 vcCake.add('ui-wp-load', function (api) {
   api.reply('start', function () {
     storage.get(function (request) {
       var data = JSON.parse(request.responseText || '{}')
       if (data) {
-        data = reWrapDefaultContent(data)
+        // Todo fix saving ( empty Name, params all undefined toJS function)
+        // data = reWrapDefaultContent(data)
         var timeMachine = vcCake.getService('time-machine')
         timeMachine.setZeroState(data)
         api.request('data:reset', data)
@@ -23,24 +24,11 @@ vcCake.add('ui-wp-load', function (api) {
     })
   })
 })
-if (false) {
-  // FEATURE TOGGLE.
-  vcCake.add('ui-save-data', function (api) {
-    api.on('save', function () {
-      storage.save(documentData.all())
-    })
-    api.reply('start', function () {
-      storage.get(function (request) {
-        var data = JSON.parse(request.responseText || '{}')
-        if (data) {
-          data = reWrapDefaultContent(data)
-          var timeMachine = vcCake.getService('time-machine')
-          timeMachine.setZeroState(data)
-          api.request('data:reset', data)
-        }
-      })
-    })
+// FEATURE TOGGLE.
+vcCake.add('ui-save-data', function (api) {
+  api.on('save', function () {
+    storage.save(documentData.all())
   })
-  require('./lib/navbar-dropdown-control')
-  require('./lib/navbar-save-button')
-}
+})
+// require('./lib/navbar-dropdown-control')
+require('./lib/navbar-save-button')
