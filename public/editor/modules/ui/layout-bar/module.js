e1ec8abed1b7585fc4dd65b3db128d5d4cb6943e
Pavel I <reactoranime@gmail.com>
2016-07-04 16:55:56 +0300

    Resizer for tree-view #140856541006953

--- public/editor/modules/ui/layout-bar/module.js
+++ public/editor/modules/ui/layout-bar/module.js
@@ -3,6 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import BarContent from './lib/content'
 import BarHeader from './lib/header'
+import Resizer from '../../content/resizer/component/resizer'
 
 vcCake.add('ui-layout-bar', (api) => {
   class LayoutBar extends React.Component {
@@ -11,6 +12,15 @@ vcCake.add('ui-layout-bar', (api) => {
         <div className='vcv-layout-bar'>
           <BarHeader api={api} />
           <BarContent api={api} />
+          <Resizer params={{
+            resizeY: false,
+            resizerTarget: '.vcv-layout-bar-content',
+            resizerClasses: 'vcv-ui-resizer vcv-ui-resizer-x vcv-ui-resizer-tree-view-detached-x'
+          }} />
+          <Resizer params={{
+            resizerTarget: '.vcv-layout-bar-content',
+            resizerClasses: 'vcv-ui-resizer vcv-ui-resizer-tree-view-detached-xy'
+          }} />
         </div>
       )
     }
