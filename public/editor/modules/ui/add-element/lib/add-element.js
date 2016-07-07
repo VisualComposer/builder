dc7895d85873f73b6d5b42ec6d191ef3d6a89186
Vjaceslav Vasenin <time2type@gmail.com>
2016-07-07 11:19:32 +0300

    Add relations to add element for elements

--- public/editor/modules/ui/add-element/lib/add-element.js
+++ public/editor/modules/ui/add-element/lib/add-element.js
@@ -2,12 +2,25 @@
 import vcCake from 'vc-cake'
 import React from 'react'
 import {default as Categories} from './categories'
-
-var cook = vcCake.getService('cook')
+const cook = vcCake.getService('cook')
+const documentManager = vcCake.getService('document')
 
 class AddElement extends React.Component {
+  getElementList () {
+    let allElements = cook.list.settings()
+    let parentId = this.props.api.actions.getParent()
+    if (parentId) {
+      let data = documentManager.get(parentId)
+      let parent = cook.get(data)
+      return allElements.filter((elementData) => {
+        let element = cook.get(elementData)
+        return element.relatedTo(parent.containerFor())
+      })
+    }
+    return allElements
+  }
   render () {
-    var elements = cook.list.settings() || []
+    var elements = this.getElementList()
     let content = <Categories elements={elements} api={this.props.api} />
 
     return (
