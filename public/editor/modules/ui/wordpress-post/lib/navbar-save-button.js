9e19ca844bff6a6ac7001363e13cce1f67bf96a5
Pavel I <reactoranime@gmail.com>
2016-07-21 15:04:03 +0300

    Save button make pinned in wordpress #156874246804752

--- public/editor/modules/ui/wordpress-post/lib/navbar-save-button.js
+++ public/editor/modules/ui/wordpress-post/lib/navbar-save-button.js
@@ -39,5 +39,5 @@ vcCake.add('ui-save-data', function (api) {
       </div>)
     }
   })
-  api.module('ui-navbar').do('addElement', 'Save post', Control)
+  api.module('ui-navbar').do('addElement', 'Save post', Control, { pin: 'visible' })
 })
