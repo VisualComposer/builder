454ae752f9e260d5679a325cdb9c96887511aa1f
Pavel I <reactoranime@gmail.com>
2016-08-12 13:07:35 +0300

    Make default colors for colorpicker

--- public/sources/attributes/color/Component.js
+++ public/sources/attributes/color/Component.js
@@ -55,7 +55,7 @@ class Color extends Attribute {
       colorPicker = (
         <div className='vcv-ui-form-input-color--popover'>
           <div className='vcv-ui-form-input-color--cover' onClick={this.handleClose} />
-          <SketchPicker color={color} onChange={this.handleChange} />
+          <SketchPicker color={color} presetColors={this.props.presetColors} onChange={this.handleChange} />
         </div>
       )
     }
@@ -68,6 +68,9 @@ class Color extends Attribute {
       </div>
     )
   }
+}
+Color.defaultProps = {
+  presetColors: [ '#ffffff', '#ededed', '#dadada', '#c6c6c6', '#555555', '#3e3d3d', '#2f2f2f', '#212121', '#ff827b', '#ff3f3b', '#e11612', '#b82e24', '#f88749', '#f96c31', '#ec5418', '#bc4826', '#ffcd58', '#e7b460', '#cc8b4a', '#a78461', '#fff7a2', '#ffed47', '#ffde00', '#ffc000', '#c8db39', '#a8d228', '#8ac60a', '#579202', '#40c651', '#119944', '#0a8136', '#056a39', '#4dd1ab', '#16b095', '#0c9c86', '#088382', '#4dc5cc', '#1da0c5', '#0b6e8f', '#0b556e', '#4d8fcc', '#1d64c5', '#0b4c8f', '#103c6a', '#6567df', '#484bc7', '#4530c2', '#263382', '#9461d3', '#9d41d1', '#841fbe', '#6c258a', '#d85bd3', '#cf33af', '#a12c87', '#811e6c', '#d46094', '#d6456e', '#c11a4a', '#911e37' ]
 }
 
 export default Color
