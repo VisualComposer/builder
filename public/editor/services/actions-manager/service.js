a6d54effa3079921c89303920518f62347fb6a78
Pavel I <reactoranime@gmail.com>
2016-08-22 13:34:57 +0300

    Fix button dependencies #170229833090827

--- public/editor/services/actions-manager/service.js
+++ public/editor/services/actions-manager/service.js
@@ -13,16 +13,17 @@ const ActionsManager = {
     alert: (state, target, options) => {
     },
     toggleVisibility: (state, target, options) => {
+      // TODO: Same for tabs
       let $el = ReactDOM.findDOMNode(target.ref)
       $el.classList.toggle('vcv-ui-state--visible', state)
       $el.classList.toggle('vcv-ui-state--hidden', !state)
 
-      let $plate = $el.closest('.vcv-ui-editor-plate')
+      /* let $plate = $el.closest('.vcv-ui-editor-plate')
       let hideTab = $plate.querySelectorAll('.vcv-ui-form-dependency').length ===
         $plate.querySelectorAll('.vcv-ui-form-dependency.vcv-ui-state--hidden').length
       let index = Array.prototype.indexOf.call($plate.parentElement.children, $plate)
       let $tab = ReactDOM.findDOMNode(target.getRefTab(index))
-      $tab.classList.toggle('vcv-ui-state--hidden', hideTab)
+      $tab.classList.toggle('vcv-ui-state--hidden', hideTab) */
     },
     preset: (state, target, options) => {
     }
--- public/sources/elements/button/scripts.js
+++ public/sources/elements/button/scripts.js
@@ -1,25 +1,40 @@
-var classes = 'vce-button';
+let classes = 'vce-button'
+let buttonHtml = buttonText
+
+if (addUrl) {
+  let { url, title, targetBlank, relNofollow } = buttonUrl
+  buttonHtml = (
+    <a
+      href={url}
+      title={title}
+      target={targetBlank ? '_BLANK' : undefined}
+      rel={relNofollow ? 'nofollow' : undefined}
+    >
+      {buttonText}
+    </a>
+  )
+}
 
 if (shape && shape !== 'square') {
-  classes += ' vce-button--border-' + shape;
+  classes += ` vce-button--border-${shape}`
 }
 
 if (style) {
-  classes += ' vce-button--style-' + style;
+  classes += ` vce-button--style-${style}`
 }
 
 if (textColor) {
-  classes += ' vce-button--text-color-' + textColor;
+  classes += ` vce-button--text-color-${textColor}`
 }
 
 if (borderColor) {
-  classes += ' vce-button--border-color-' + borderColor;
+  classes += ` vce-button--border-color-${borderColor}`
 }
 
 if (backgroundColor) {
-  classes += ' vce-button--background-color-' + backgroundColor;
+  classes += ` vce-button--background-color-${backgroundColor}`
 }
 
-if (showIcon) {
-  classes += ' vce-button--icon-state-visible';
+if (showArrow) {
+  classes += ' vce-button--icon-state-visible'
 }
