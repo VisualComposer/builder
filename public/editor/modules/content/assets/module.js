903b41450a85c123b6e5a99f5602553b35ddc3e6
Vasilij <vasilij@wpbakery.com>
2016-08-25 22:05:23 +0300

    Update media query for design  options and output design options in to body #161699449240223

--- public/editor/modules/content/assets/module.js
+++ public/editor/modules/content/assets/module.js
@@ -4,22 +4,34 @@ const documentService = vcCake.getService('document')
 const assetManager = vcCake.getService('assets-manager')
 
 vcCake.add('assets', (api) => {
+  api.reply('data:changed', () => {
+    let iframeDocument = window.document.querySelector('.vcv-layout-iframe').contentWindow.document
+    let doElement = iframeDocument.querySelector('#do-styles')
+    let styleElement = iframeDocument.querySelector('#css-styles')
+    if (!styleElement) {
+      styleElement = iframeDocument.createElement('style')
+      styleElement.id = 'css-styles'
+      iframeDocument.body.appendChild(styleElement)
+    }
+    if (!doElement) {
+      doElement = iframeDocument.createElement('style')
+      doElement.id = 'do-styles'
+      iframeDocument.body.appendChild(doElement)
+    }
+    assetManager.getCompiledCss().then((result) => {
+      styleElement.innerHTML = result
+    })
+    assetManager.getCompiledDesignOptions().then((result) => {
+      doElement.innerHTML = result
+    })
+  })
+
   api.reply('data:afterAdd', (ids) => {
     assetManager.add(ids)
-    // assetManager.getCompiledCss().then((result) => {
-    //   console.log(result)
-    // })
   })
 
   api.reply('data:afterUpdate', (id, element) => {
     assetManager.update(id)
-    // assetManager.getCompiledDesignOptions().then((result) => {
-    //   console.log(result)
-    // })
-    // console.log(assetManager.get())
-    // assetManager.getCompiledCss().then((result) => {
-    //   console.log(result)
-    // })
   })
 
   api.reply('data:beforeRemove', (id) => {
