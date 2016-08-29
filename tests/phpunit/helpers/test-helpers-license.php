16fabeb09be9e0c4ce6d5e5a8c6f0523f431fef6
Pavel I <reactoranime@gmail.com>
2016-08-29 17:04:57 +0300

    Add licenseHelper tests

--- tests/phpunit/helpers/test-helpers-license.php
+++ tests/phpunit/helpers/test-helpers-license.php
@@ -15,6 +15,35 @@ class HelpersLicenseTest extends WP_UnitTestCase
         $this->assertEquals('license-key-token', $helper->getKeyTokenOptionName());
     }
 
+    public function testKey()
+    {
+        /** @var \VisualComposer\Helpers\License $helper */
+        $helper = vchelper('License');
+
+        vcapp()->call([$helper, 'setKey'], ['test']);
+        $this->assertEquals('test', vcapp()->call([$helper, 'getKey']));
+        $this->assertTrue(vcapp()->call([$helper, 'isValid'], ['test']));
+        $this->assertFalse(vcapp()->call([$helper, 'isValid'], ['testFalse']));
+    }
+
+    public function testKeyToken()
+    {
+        /** @var \VisualComposer\Helpers\License $helper */
+        $helper = vchelper('License');
+
+        vcapp()->call([$helper, 'setKeyToken'], ['testToken']);
+        $this->assertEquals('testToken', vcapp()->call([$helper, 'getKeyToken']));
+    }
+
+    public function testType()
+    {
+        /** @var \VisualComposer\Helpers\License $helper */
+        $helper = vchelper('License');
+
+        vcapp()->call([$helper, 'setType'], ['mega']);
+        $this->assertEquals('mega', vcapp()->call([$helper, 'getType']));
+    }
+
     public function testIsActivated()
     {
         /** @var \VisualComposer\Helpers\License $helper */
@@ -33,5 +62,11 @@ class HelpersLicenseTest extends WP_UnitTestCase
         //reset
         $helper->setKey(false, $optionsHelper);
 
+    }
+
+    public function testIsValidFormat()
+    {
+        $this->assertTrue(vchelper('License')->isValidFormat('c634fef7-6195-4a77-a3c9-d888b90d235e'));
+        $this->assertFalse(vchelper('License')->isValidFormat('xxxx-xxxx'));
     }
 }
