44fc5eab009b8e5da1e9e2c420596604128afb40
Pavel I <reactoranime@gmail.com>
2016-08-31 17:36:41 +0300

    Feature toggle edit-form redesign

---
+++ public/editor/modules/ui/edit-element-redesign/lib/form-wrapper.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import EditElementResizeTabs from './tabs-extenstion-resizer'
+
+export default class EditFormTabs extends React.Component {
+  static propTypes = {
+    api: React.PropTypes.object.isRequired,
+    element: React.PropTypes.object.isRequired
+  }
+  state = {
+    activeTabIndex: 0
+  }
+  allTabs = []
+
+  componentWillMount () {
+    this.allTabs = this.updateTabs(this.props)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    this.allTabs = this.updateTabs(nextProps)
+    this.setState({
+      activeTabIndex: 0
+    })
+  }
+
+  updateTabs (props) {
+    let tabs = []
+    EditFormTabs.editFormTabs(props).map((tab, index) => {
+      let tabsData = {
+        id: tab.key,
+        fieldKey: tab.key,
+        index: index,
+        data: tab.data,
+        isVisible: true,
+        pinned: tab.data.settings.options && tab.data.settings.options.pinned ? tab.data.settings.options.pinned : false,
+        params: EditFormTabs.editFormTabParams(props, tab.key),
+        key: `edit-form-tab-${tab.key}`,
+        changeTab: this.onChangeActiveTab.bind(this, index),
+        ref: (ref) => {
+          if (this.allTabs[ index ]) {
+            this.allTabs[ index ].ref = ref
+          }
+        }
+      }
+      tabs.push(tabsData)
+    }, EditFormTabs)
+    return tabs
+  }
+
+  static editFormTabs (props) {
+    const group = props.element.get('metaEditFormTabs')
+    if (group && group.each) {
+      return group.each(EditFormTabs.editFormTabsIterator.bind(this, props))
+    }
+    return []
+  }
+
+  static editFormTabsIterator (props, item) {
+    return {
+      key: item,
+      value: props.element.get(item),
+      data: props.element.settings(item)
+    }
+  }
+
+  static editFormTabParams (props, tabName) {
+    const value = props.element.get(tabName)
+    const settings = props.element.settings(tabName)
+    if (settings.settings.type === 'group' && value && value.each) {
+      return value.each(EditFormTabs.editFormTabsIterator.bind(this, props))
+    }
+    // In case if tab is single param holder
+    return [ {
+      key: tabName,
+      value: value,
+      data: settings
+    } ]
+  }
+
+  onChangeActiveTab (tabIndex) {
+    this.setState({
+      activeTabIndex: tabIndex
+    })
+  }
+
+  setTabs = (tabs) => {
+    this.allTabs = tabs
+  }
+
+  render () {
+    return (
+      <EditElementResizeTabs
+        {...this.props}
+        allTabs={this.allTabs}
+        activeTabIndex={this.state.activeTabIndex}
+        activeTab={this.allTabs[ this.state.activeTabIndex ]}
+        updateTabs={this.setTabs}
+      />
+    )
+  }
+}
---
+++ public/editor/modules/ui/edit-element-redesign/lib/form.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import classNames from 'classnames'
+import EditFormHeaderTabs from './form-header-tabs'
+import EditFormFooter from './form-footer'
+import EditFormContent from './form-content'
+
+export default class EditForm extends React.Component {
+  static propTypes = {
+    api: React.PropTypes.object.isRequired,
+    element: React.PropTypes.object.isRequired
+  }
+
+  render () {
+    let treeContentClasses = classNames({
+      'vcv-ui-tree-content': true
+    })
+
+    return (
+      <div className='vcv-ui-tree-view-content'>
+        <div className={treeContentClasses}>
+          <EditFormHeaderTabs {...this.props} />
+          <EditFormContent {...this.props} />
+          <EditFormFooter {...this.props} />
+        </div>
+      </div>
+    )
+  }
+}
