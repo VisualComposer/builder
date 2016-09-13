c29bcce3ce2d0bbf57a42209b5f1946acebaecaf
Konutis <janis.k@wpbakery.com>
2016-09-13 11:48:06 +0300

    Completed active state for control #179745270412448

--- public/editor/modules/ui/layout-control/module.js
+++ public/editor/modules/ui/layout-control/module.js
@@ -2,5 +2,5 @@ import vcCake from 'vc-cake'
 import LayoutNavbarControl from './lib/navbar-control'
 
 vcCake.add('ui-layout-control', (api) => {
-  api.module('ui-navbar').do('addElement', 'Layout control', LayoutNavbarControl)
+  api.module('ui-navbar').do('addElement', 'Layout control', LayoutNavbarControl, { pin: 'visible' })
 })
