a185e0127741ccc75d58ba2d4df49519de2ecc7f
Vasilij <vasilij@wpbakery.com>
2016-09-14 19:43:26 +0300

    Add smart background fill for row and column  #177643257500661

--- public/sources/elements/row/scripts.js
+++ public/sources/elements/row/scripts.js
@@ -4,4 +4,24 @@ const classNames = require('classnames')
 if (typeof customClass === 'string' && customClass) {
   classes.push(customClass)
 }
+
+if (!(Object.keys(designOptions).length === 0 && designOptions.constructor === Object) && designOptions.used) {
+  if (designOptions.deviceTypes === 'all' && (designOptions.all.backgroundColor !== '' || designOptions.all.backgroundImage.urls.length)) {
+    classes.push('vce-row--has-background')
+  } else {
+    let devices = {
+      'desktop': 'xl',
+      'tablet-landscape': 'lg',
+      'tablet-portrait': 'md',
+      'mobile-landscape': 'sm',
+      'mobile-portrait': 'xs'
+    }
+    for (let device in devices) {
+      if ((designOptions[ device ].backgroundColor !== '' || designOptions[ device ].backgroundImage.urls.length)) {
+        classes.push('vce-row--' + devices[ device ] + '--has-background')
+      }
+    }
+  }
+}
+
 let className = classNames(classes)
