da7c1063d470c6ba699e8c851791f4af8820dcec
Vjaceslav Vasenin <time2type@gmail.com>
2016-09-20 18:14:31 +0300

    Edit form tabs expand from sandwitch  #179726735242222

--- public/editor/modules/ui/edit-element/lib/tabs-extenstion-resizer.js
+++ public/editor/modules/ui/edit-element/lib/tabs-extenstion-resizer.js
@@ -35,13 +35,12 @@ export default class EditFormResizeTabs extends React.Component {
   onTabsUnmount = (freespace, options) => {
     this.options = options
     this.freeSpace = freespace
-    Utils.removeResizeListener(ReactDOM.findDOMNode(freespace), options, this.refreshTabs)
+    // Utils.removeResizeListener(ReactDOM.findDOMNode(freespace), options, this.refreshTabs)
   }
 
   refreshTabs = ($freeSpace, options) => {
     // get tabs line width
     let freeSpace = $freeSpace.offsetWidth
-
     // If there is no space move tab from visible to hidden tabs.
     let visibleAndUnpinnedTabs = this.getVisibleAndUnpinnedTabs()
     if (freeSpace === 0 && visibleAndUnpinnedTabs.length > 0) {
@@ -64,7 +63,7 @@ export default class EditFormResizeTabs extends React.Component {
       }
       while (freeSpace > 0 && hiddenTabs.length) {
         let lastTab = hiddenTabs.pop()
-        if (lastTab.realref) {
+        if (lastTab.realRef) {
           freeSpace -= lastTab.realRef.getRealWidth()
           if (freeSpace > 0) {
             this.allTabs[ lastTab.index ].isVisible = true
