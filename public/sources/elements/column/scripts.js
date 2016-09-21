58758f20642af5c927ff19c6efc06a8c2fcb85ac
Vjaceslav Vasenin <time2type@gmail.com>
2016-09-21 09:43:16 +0300

    Enhance designOptions to return special css classes for element for column #184247466041059

--- public/sources/elements/column/scripts.js
+++ public/sources/elements/column/scripts.js
@@ -7,23 +7,7 @@ if (typeof customClass === 'string' && customClass.length) {
   classes.push(customClass)
 }
 
-if (!(Object.keys(designOptions).length === 0 && designOptions.constructor === Object) && designOptions.used) {
-  if (designOptions.deviceTypes === 'all' && (designOptions.all.backgroundColor !== '' || designOptions.all.backgroundImage.urls.length)) {
-    classes.push('vce-col--has-background')
-  } else {
-    let devices = {
-      'desktop': 'xl',
-      'tablet-landscape': 'lg',
-      'tablet-portrait': 'md',
-      'mobile-landscape': 'sm',
-      'mobile-portrait': 'xs'
-    }
-    for (let device in devices) {
-      if ((designOptions[ device ].backgroundColor !== '' || designOptions[ device ].backgroundImage.urls.length)) {
-        classes.push('vce-col--' + devices[ device ] + '--has-background')
-      }
-    }
-  }
-}
+classes = classes.concat(vcvAPI.getDesignOptionsCssClasses(designOptions))
+
 
 let className = classNames(classes)
