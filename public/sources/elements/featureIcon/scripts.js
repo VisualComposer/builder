b7d0d9491921ffa2405ed32c8640508420c49d16
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2016-10-18 11:31:40 +0300

    Update feature icon markup #193002491096334

--- public/sources/elements/featureIcon/scripts.js
+++ public/sources/elements/featureIcon/scripts.js
@@ -1,6 +1,8 @@
 let classes = 'vce-features'
 let customProps = {}
 let CustomTag = 'i'
+let tinycolor = require('react-color/modules/tinycolor2')
+let colorValue = tinycolor(iconColor).toRgbString()
 
 if (addUrl) {
   CustomTag = 'a'
--- public/sources/elements/featureIcon/template.jsx
+++ public/sources/elements/featureIcon/template.jsx
@@ -1,8 +1,8 @@
 <div className={classes}>
-  <div className='vce-features__icon'>
+  <div className='vce-features__icon vce-icon vce' id={'el-' + id}>
     <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 769 769'>
       <path strokeWidth='40' d='M565.755 696.27h-360l-180-311.77 180-311.77h360l180 311.77z'/>
     </svg>
-    <span className='glyphicon glyphicon-asterisk'></span>
+    <span className={iconPicker} style={{ iconColor: colorValue }}></span>
   </div>
 </div>
