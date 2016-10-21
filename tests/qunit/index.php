3f9815cd8a3f77023b1524b6d600eef2ca745c01
Pavel I <reactoranime@gmail.com>
2016-10-21 23:12:06 +0300

    Make few tests for qunit

--- tests/qunit/index.php
+++ tests/qunit/index.php
@@ -6,12 +6,13 @@
     <link type="text/css" rel="stylesheet" href="../../node_modules/qunitjs/qunit/qunit.css"/>
     <script src="//code.jquery.com/jquery-1.12.1.js"
         integrity="sha256-VuhDpmsr9xiKwvTIHfYWCIQ84US9WqZsLfR4P7qF6O8=" crossorigin="anonymous"></script>
+    <script src="http://underscorejs.org/underscore-min.js"></script>
 </head>
 <body>
 <div id="qunit"></div>
 <div id="qunit-fixture"></div>
 <script src="../../node_modules/qunitjs/qunit/qunit.js"></script>
-<script src="../../resources/assets/js/dist/framework.min.js"></script>
+<script src="../../public/dist/wp.bundle.js"></script>
 <?php
 $data = glob('tests/test-*.js');
 foreach ($data as $file) {
