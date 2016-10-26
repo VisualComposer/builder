daae21ffc9ec6795bfab6f08831c60c38f422b84
Vasilij <vasilij@wpbakery.com>
2016-10-26 14:03:03 +0300

    Create elemnt API method to get mixin data #193620322540753

--- public/editor/services/api/lib/elementComponent.js
+++ public/editor/services/api/lib/elementComponent.js
@@ -6,6 +6,21 @@ export default class ElementComponent extends Component {
     atts: PropTypes.object,
     editor: PropTypes.object
   }
+  getMixinData (mixinName) {
+    const vcCake = require('vc-cake')
+    const assetsManager = vcCake.getService('assets-manager')
+    let returnData = null
+    let mixinData = assetsManager.getCssMixinsByElement(this.props.atts)
+    var { tag } = this.props.atts
+    if (mixinData[tag][mixinName]) {
+      let mixin = Object.keys(mixinData[tag][mixinName])
+      mixin = mixin.length ? mixin.pop() : null
+      if (mixin) {
+        returnData = mixinData[tag][mixinName][mixin]
+      }
+    }
+    return returnData
+  }
   render () {
     return null
   }
