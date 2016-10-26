37270136391267c63b7bd916728adfb55ea6c6e1
Vjaceslav Vasenin <time2type@gmail.com>
2016-10-26 17:26:29 +0300

    Html inline render for elements

--- public/editor/services/api/lib/elementComponent.js
+++ public/editor/services/api/lib/elementComponent.js
@@ -1,10 +1,18 @@
 import {Component, PropTypes} from 'react'
-
+import ReactDOM from 'react-dom'
 export default class ElementComponent extends Component {
   static propTypes = {
     id: PropTypes.string,
     atts: PropTypes.object,
     editor: PropTypes.object
+  }
+  updateInlineHtml (tagString = '') {
+    const component = ReactDOM.findDOMNode(this)
+    let range = document.createRange()
+    let documentFragment = range.createContextualFragment(tagString)
+    component.innerHTML = ''
+    console.log(documentFragment.querySelector('a'))
+    component.appendChild(documentFragment)
   }
   render () {
     return null
