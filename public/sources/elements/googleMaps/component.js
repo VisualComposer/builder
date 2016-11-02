47268b80391779a557dda8badb4808694f17f060
Janis Konutis <janis.k@wpbakery.com>
2016-11-02 17:44:27 +0200

    Google maps: size edit #202399040322616

--- public/sources/elements/googleMaps/component.js
+++ public/sources/elements/googleMaps/component.js
@@ -21,53 +21,6 @@ class Component extends vcvAPI.elementComponent {
     }
   }
 
-  render () {
-    let { id, atts, editor } = this.props
-    let { embed, designOptions, customClass, size, alignment } = atts
-    let classes = 'vce-google-maps vce'
-    let innerClasses = 'vce-google-maps-inner'
-    let wrapperClasses = 'vce-google-maps-wrapper'
-    let customProps = {}
-    let innerProps = {}
-    let wrapperProps = {}
-
-    if (typeof customClass === 'string' && customClass) {
-      classes += ' ' + customClass
-    }
-
-    if (typeof size === 'string' && size) {
-      wrapperProps.style = { maxWidth: this.state ? (this.state.imgSize ? this.state.imgSize.maxWidth : null) : null }
-      innerProps.style = { paddingBottom: this.state ? (this.state.imgSize ? this.state.imgSize.paddingBottom : null) : null }
-      wrapperClasses += ` vce-google-maps--size-custom`
-    }
-
-    if (alignment) {
-      classes += ` vce-google-maps--align-${alignment}`
-    }
-
-    customProps.key = `customProps:${id}`
-
-    let devices = designOptions.visibleDevices ? Object.keys(designOptions.visibleDevices) : []
-    let animations = []
-    devices.forEach((device) => {
-      let prefix = designOptions.visibleDevices[ device ]
-      if (designOptions[ device ].animation) {
-        if (prefix) {
-          prefix = `-${prefix}`
-        }
-        animations.push(`vce-o-animate--${designOptions[ device ].animation}${prefix}`)
-      }
-    })
-    if (animations.length) {
-      customProps[ 'data-vce-animate' ] = animations.join(' ')
-    }
-    return <div {...customProps} className={classes} id={'el-' + id} {...editor}>
-      <div {...wrapperProps} className={wrapperClasses}>
-        <div className={innerClasses} {...innerProps} dangerouslySetInnerHTML={{__html:embed}} />
-      </div>
-  </div>
-  }
-
   checkCustomSize (size) {
     size = size.toLowerCase().split(' ').join('')
 
@@ -117,5 +70,52 @@ class Component extends vcvAPI.elementComponent {
         paddingBottom: size ? size.height / size.width * 100 + '%' : null
       }
     })
+  }
+
+  render () {
+    let { id, atts, editor } = this.props
+    let { embed, designOptions, customClass, size, alignment } = atts
+    let classes = 'vce-google-maps vce'
+    let innerClasses = 'vce-google-maps-inner'
+    let wrapperClasses = 'vce-google-maps-wrapper'
+    let customProps = {}
+    let innerProps = {}
+    let wrapperProps = {}
+
+    if (typeof customClass === 'string' && customClass) {
+      classes += ' ' + customClass
+    }
+
+    if (typeof size === 'string' && size) {
+      wrapperProps.style = { maxWidth: this.state ? (this.state.imgSize ? this.state.imgSize.maxWidth : null) : null }
+      innerProps.style = { paddingBottom: this.state ? (this.state.imgSize ? this.state.imgSize.paddingBottom : null) : null }
+      wrapperClasses += ` vce-google-maps--size-custom`
+    }
+
+    if (alignment) {
+      classes += ` vce-google-maps--align-${alignment}`
+    }
+
+    customProps.key = `customProps:${id}-${size}`
+
+    let devices = designOptions.visibleDevices ? Object.keys(designOptions.visibleDevices) : []
+    let animations = []
+    devices.forEach((device) => {
+      let prefix = designOptions.visibleDevices[ device ]
+      if (designOptions[ device ].animation) {
+        if (prefix) {
+          prefix = `-${prefix}`
+        }
+        animations.push(`vce-o-animate--${designOptions[ device ].animation}${prefix}`)
+      }
+    })
+    if (animations.length) {
+      customProps[ 'data-vce-animate' ] = animations.join(' ')
+    }
+    return <div {...customProps} className={classes} id={'el-' + id} {...editor}>
+      <div {...wrapperProps} className={wrapperClasses}>
+        <div className={innerClasses} {...innerProps} dangerouslySetInnerHTML={{__html:embed}} />
+      </div>
+    </div>
   }
 }
