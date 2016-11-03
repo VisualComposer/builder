14ccad42842b8ba64b2807bac77af963062bed07
Janis Konutis <janis.k@wpbakery.com>
2016-11-03 10:50:01 +0200

    Flickr Image: embed to url change #198166371507347

--- public/sources/elements/flickrImage/public/js/flickrImage.js
+++
@@ -1,7 +0,0 @@
-/* global vcv */
-vcv.ready(() => {
-  console && console.log('flickr image public javascript called once')
-});
-vcv.on('ready', () => {
-  console && console.log('flickr image public javascript will be called everytime you need react on ready')
-});
