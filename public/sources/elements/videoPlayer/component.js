fb70f693ad185cd9cdf2cdbe9edc4ce90a972903
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2016-11-09 14:43:38 +0200

    Add time check, url check, alignment to video player element #191501963504076

--- public/sources/elements/videoPlayer/component.js
+++ public/sources/elements/videoPlayer/component.js
@@ -2,37 +2,48 @@
 class Component extends vcvAPI.elementComponent {
   render () {
     let { id, atts, editor } = this.props
-    let { customClass, videoPlayer } = atts
-    let classes = 'vce-video-player'
-    let source, videoId
-    let yrx = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/
+    let { customClass, videoPlayer, alignment } = atts
+    let classes = 'vce-video-player vce'
+    let wrapperClasses = 'vce-video-player-wrapper'
+    let innerClasses = 'vce-video-player-inner'
+    let source, url
     let vrx = /https?:\/\/(?:www\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/([^\/]*)\/videos\/|album\/(\d+)\/video\/|)(\d+)(?:$|\/|\?)/
-
+    let ytrx = /^.*((youtu\.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*)(?:(\?t|&start)=(?:(\d+)h)?(?:(\d+)m)?(\d+)s)?.*/
     if (typeof customClass === 'string' && customClass) {
       classes = classes.concat(' ' + customClass)
     }
-    console.log(this.props)
-    console.log('vimeo: ', videoPlayer.match(vrx))
-    console.log('youtube: ', videoPlayer.match(yrx))
-
+    if (editor) {
+      innerClasses += ' vce-video-player-disabled'
+    }
+    if (alignment) {
+      classes += ` vce-video-player--align-${alignment}`
+    }
     if (videoPlayer.match(vrx)) {
-      videoId = videoPlayer.match(vrx)
-      source = `//player.vimeo.com/video/${videoId[3]}`
-    } else if (videoPlayer.match(yrx)) {
-      videoId = videoPlayer.match(yrx)
-      source = `https://www.youtube.com/embed/${videoId[7]}`
+      url = videoPlayer.match(vrx)
+      source = `//player.vimeo.com/video/${url[3]}?loop=0`
+    } else if (videoPlayer.match(ytrx)) {
+      url = videoPlayer.match(ytrx)
+      let seconds = 0
+      if (url[8]) {
+        seconds += url[9] === undefined ? 0 : (Number(url[9]) * 60 * 60)
+        seconds += url[10] === undefined ? 0 : (Number(url[10]) * 60)
+        seconds += url[11] === undefined ? 0 : (Number(url[11]))
+      }
+      source = `https://www.youtube.com/embed/${url[7]}?start=${seconds}&loop=0&cc_load_policy=0&iv_load_policy=3`
     }
 
     return <div className={classes} id={'el-' + id} {...editor}>
-      <div className='vce-video-player-iframe-container'>
-        <iframe
-          className='vce-video-player-iframe'
-          src={source}
-          width='640'
-          height='390'
-          frameBorder='0'
-          allowFullScreen='true'
-        />
+      <div className={wrapperClasses}>
+        <div className={innerClasses}>
+          <iframe
+            className='vce-video-player-iframe'
+            src={source}
+            width='640'
+            height='390'
+            frameBorder='0'
+            allowFullScreen='true'
+          />
+        </div>
       </div>
     </div>
   }
