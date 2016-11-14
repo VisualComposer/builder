169e74b0f537b02d0fb0406dc03ec3f2e5f93c37
Pavel I <reactoranime@gmail.com>
2016-11-14 16:56:23 +0200

    Improve attributes for woocommerce recent products

--- visualcomposer/Modules/Elements/WooCommerce/WooCommerceRecentProducts.php
+++ visualcomposer/Modules/Elements/WooCommerce/WooCommerceRecentProducts.php
@@ -14,36 +14,4 @@ class WooCommerceRecentProducts extends Container implements Module
     private $shortcodeTag = 'recent_products';
 
     private $shortcodeNs = 'woocommerce:';
-
-    /**
-     * @param \VisualComposer\Helpers\Request $request
-     *
-     * @return string
-     */
-    protected function renderEditor(Request $request)
-    {
-        ob_start();
-        $atts = $request->input('vcv-atts');
-        $per_page = isset($atts['per_page']) ? $atts['per_page'] : 12;
-        $columns = isset($atts['columns']) ? $atts['columns'] : 4;
-        $orderby = isset($atts['orderby']) ? $atts['orderby'] : 'date';
-        $order = isset($atts['order']) ? $atts['order'] : 'desc';
-        // TODO: Check \WC_Shortcodes::recent_products category and operator attributes (not documented)
-        echo do_shortcode(
-            sprintf(
-                '[recent_products per_page="%d" columns="%d" orderby="%s" order="%s"]',
-                (int)$per_page,
-                (int)$columns,
-                $orderby,
-                $order
-            )
-        );
-        wp_print_styles();
-        print_late_styles();
-        wp_print_head_scripts();
-        wp_print_footer_scripts();
-        $response = ob_get_clean();
-
-        return $response;
-    }
 }
