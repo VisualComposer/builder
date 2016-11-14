f574aaf27a5fd68cbbf265765351a98cb4a8024e
Pavel I <reactoranime@gmail.com>
2016-11-14 17:16:14 +0200

    Fix woocommerce products element

--- visualcomposer/Modules/Elements/WooCommerce/WooCommerceProducts.php
+++ visualcomposer/Modules/Elements/WooCommerce/WooCommerceProducts.php
@@ -3,7 +3,6 @@
 namespace VisualComposer\Modules\Elements\WooCommerce;
 
 use VisualComposer\Framework\Illuminate\Support\Module;
-use VisualComposer\Helpers\Request;
 use VisualComposer\Framework\Container;
 use VisualComposer\Modules\Elements\Traits\ShortcodesTrait;
 
@@ -14,45 +13,4 @@ class WooCommerceProducts extends Container implements Module
     private $shortcodeTag = 'products';
 
     private $shortcodeNs = 'woocommerce:';
-
-    /**
-     * @param \VisualComposer\Helpers\Request $request
-     *
-     * @return string
-     */
-    protected function renderEditor(Request $request)
-    {
-        ob_start();
-        $atts = $request->input('vcv-atts');
-        $selector = isset($atts['selector']) && in_array($atts['selector'], ['ids', 'skus']) ? $atts['selector']
-            : 'ids';
-        $ids = isset($atts['productIds']) ? $atts['productIds'] : '';
-        $skus = isset($atts['productSkus']) ? $atts['productSkus'] : '';
-        $columns = isset($atts['columns']) ? $atts['columns'] : 4;
-        $orderby = isset($atts['orderby']) ? $atts['orderby'] : 'title';
-        $order = isset($atts['productsOrder']) ? $atts['productsOrder'] : 'asc';
-        $queryData = [];
-        if ($selector === 'ids') {
-            $queryData[] = sprintf('ids="%d"', $ids);
-        } else {
-            $queryData[] = sprintf('skus="%s"', $skus);
-        }
-        $query = implode(' ', $queryData);
-        echo do_shortcode(
-            sprintf(
-                '[products %s columns="%d" orderby="%s" order="%s"]',
-                $query,
-                $columns,
-                $orderby,
-                $order
-            )
-        );
-        wp_print_styles();
-        print_late_styles();
-        wp_print_head_scripts();
-        wp_print_footer_scripts();
-        $response = ob_get_clean();
-
-        return $response;
-    }
 }
