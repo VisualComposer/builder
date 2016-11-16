7f83393de5aae97e6e68fe3aec939c7e6035eb52
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2016-11-16 15:49:53 +0200

    Add start/end time calculation for youtube player element #211959731793370

--- public/sources/elements/youtubePlayer/component.js
+++ public/sources/elements/youtubePlayer/component.js
@@ -1,36 +1,113 @@
 /* global React, vcvAPI */
 class Component extends vcvAPI.elementComponent {
+  parseTime (timeString) {
+    let time = {
+      h: 0,
+      m: 0,
+      s: 0,
+      toSeconds() {
+        return this.h * 60 * 60 + this.m * 60 + this.s
+      },
+      toString () {
+        return this.h + 'h' + this.m + 'm' + this.s + 's'
+      }
+    }
+
+    // replace all spaces
+    timeString = timeString.replace(/\s+/g, '')
+    // get matched vars
+    let re = /([0-9]+[hms])|([0-9]+(?=:|$))/gi
+    let timeData = timeString.match(re)
+    if (timeData) {
+      function findByLetter (char, value) {
+        if (value.slice(-1) === char) {
+          return true
+        }
+        return false
+      }
+
+      // search for hours
+      let hi = timeData.findIndex(findByLetter.bind(null, 'h'))
+      if (hi !== -1) {
+        time.h = parseInt(timeData[ hi ])
+        timeData.splice(hi, 1)
+      }
+      // search for minutes
+      let mi = timeData.findIndex(findByLetter.bind(null, 'm'))
+      if (mi !== -1) {
+        time.m = parseInt(timeData[ mi ])
+        timeData.splice(mi, 1)
+      }
+      // search for seconds
+      let si = timeData.findIndex(findByLetter.bind(null, 's'))
+      if (si !== -1) {
+        time.s = parseInt(timeData[ si ])
+        timeData.splice(si, 1)
+      }
+    }
+
+    // get simple vars
+    // filter data and remove last matched elements
+    timeData = timeData.filter((value) => {
+      let re = /^\d+$/
+      if (re.test(value)) {
+        return true
+      }
+      return false
+    })
+    timeData.splice(3)
+    // get seconds
+    if (timeData.length) {
+      if (!time.s) {
+        time.s = parseInt(timeData.pop())
+      }
+    }
+    // get minutes
+    if (timeData.length) {
+      if (!time.m) {
+        time.m = parseInt(timeData.pop())
+      }
+    }
+    // get hours
+    if (timeData.length) {
+      if (!time.h) {
+        time.h = parseInt(timeData.pop())
+      }
+    }
+    return time.toSeconds()
+  }
+  validUnits (src) {
+    switch (src) {
+      case 'px':
+        return src
+      case 'em':
+        return src
+      case 'rem':
+        return src
+      case 'pt':
+        return src
+      case 'pc':
+        return src
+      case '%':
+        return src
+      default:
+        return 'px'
+    }
+  }
+
   render () {
     let { id, atts, editor } = this.props
     let { customClass, videoPlayer, alignment, size, customSize, color, controls } = atts
     let classes = 'vce-yt-video-player vce'
     let wrapperClasses = 'vce-yt-video-player-wrapper'
     let innerClasses = 'vce-yt-video-player-inner'
-    let source, videoWidth, videoId
-    let autoplay = 0
-    let loop = `&loop=0`
-    let rel = 0
+    let source, videoWidth, videoId, loop
+    let autoplay = atts.autoplay ? 1 : 0
+    let rel = atts.rel ? 1 : 0
     let start = 0
-    let end = ''
+    let end = atts.end ? `&end=${this.parseTime(atts.end)}` : ''
     let ytrx = /^.*((youtu\.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*)(?:(\?t|&start)=(?:(\d+)h)?(?:(\d+)m)?(\d+)s)?.*/
-    let validUnits = (src) => {
-      switch (src) {
-        case 'px':
-          return src
-        case 'em':
-          return src
-        case 'rem':
-          return src
-        case 'pt':
-          return src
-        case 'pc':
-          return src
-        case '%':
-          return src
-        default:
-          return 'px'
-      }
-    }
+
     if (typeof customClass === 'string' && customClass) {
       classes = classes.concat(` ${customClass}`)
     }
@@ -41,44 +118,33 @@ class Component extends vcvAPI.elementComponent {
       classes += ` vce-yt-video-player--align-${alignment}`
     }
     if (size) {
+      classes += ` vce-yt-video-player--size-${size}`
       if (size === 'custom') {
         if (/^[0-9.]+$/.test(customSize)) {
           videoWidth = `${parseFloat(customSize)}px`
         } else if (parseFloat(customSize)) {
           let chars = customSize.slice(parseFloat(customSize).toString().length)
-          videoWidth = `${parseFloat(customSize)}${validUnits(chars)}`
+          videoWidth = `${parseFloat(customSize)}${this.validUnits(chars)}`
         } else {
           videoWidth = '560px'
         }
       } else {
         videoWidth = `${size.slice(0, size.indexOf('x'))}px`
       }
-      classes += ` vce-yt-video-player--size-${size}`
     }
     if (videoPlayer.match(ytrx)) {
       let url
       url = videoPlayer.match(ytrx)
-      videoId = url[7]
-      if (url[8]) {
-        start += url[9] === undefined ? 0 : (Number(url[9]) * 60 * 60)
-        start += url[10] === undefined ? 0 : (Number(url[10]) * 60)
-        start += url[11] === undefined ? 0 : (Number(url[11]))
+      videoId = url[ 7 ]
+      loop = atts.loop ? `&loop=1&playlist=${videoId}` : `&loop=0`
+      if (url[ 8 ]) {
+        start += url[ 9 ] === undefined ? 0 : (Number(url[ 9 ]) * 60 * 60)
+        start += url[ 10 ] === undefined ? 0 : (Number(url[ 10 ]) * 60)
+        start += url[ 11 ] === undefined ? 0 : (Number(url[ 11 ]))
       }
     }
-    if (atts.autoplay) {
-      autoplay = 1
-    }
-    if (atts.loop) {
-      loop = `&loop=1&playlist=${videoId}`
-    }
-    if (atts.rel) {
-      rel = 1
-    }
     if (atts.start) {
-      start = atts.start
-    }
-    if (atts.end) {
-      end += `&end=${atts.end}`
+      start = this.parseTime(atts.start)
     }
 
     source = `https://www.youtube.com/embed/${videoId}?autoplay=${autoplay}&color=${color}&controls=${controls}${loop}&rel=${rel}&start=${start}${end}&cc_load_policy=0&iv_load_policy=3`
