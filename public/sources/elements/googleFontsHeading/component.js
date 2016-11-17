46b8d518ea1118e599c4ff6a2925621b2c0002f7
Janis Konutis <janis.k@wpbakery.com>
2016-11-17 09:35:54 +0200

    Google fonts heading: font size, line height, font add #197137122189041

--- public/sources/elements/googleFontsHeading/component.js
+++ public/sources/elements/googleFontsHeading/component.js
@@ -1,12 +1,22 @@
 /* global React, vcvAPI */
 /*eslint no-unused-vars: 0*/
 class Component extends vcvAPI.elementComponent {
-  componentDidMount () {
-
+  validateSize (value) {
+    let units = [ 'px', 'em', 'rem', '%', 'vw', 'vh' ]
+    let re = new RegExp('^-?\\d*(\\.\\d{0,9})?(' + units.join('|') + ')?$')
+    if (value === '' || value.match(re)) {
+      return value
+    } else {
+      return null
+    }
   }
 
-  componentWillReceiveProps (nextProps) {
-
+  getFontVariant (variant) {
+    let number = variant.match(/\d+/g)
+    let word = variant.match(/[a-z]+$/i)
+    let fontWeight = number ? number[ 0 ] : '400'
+    let fontStyle = word && word[ 0 ] === 'italic' ? 'italic' : ''
+    return { weight: fontWeight, style: fontStyle }
   }
 
   render () {
@@ -16,18 +26,25 @@ class Component extends vcvAPI.elementComponent {
     let customProps = {}
     let innerClasses = 'vce-google-fonts-heading-inner'
     let innerCustomProps = {}
+    innerCustomProps.style = {}
     let CustomTag = elementTag
     let headingHtml = text
+    let googleFontLink = ''
 
     if (link && link.url) {
-      CustomTag = 'a'
       let { url, title, targetBlank, relNofollow } = link
-      customProps = {
+      let linkProps = {
         'href': url,
         'title': title,
         'target': targetBlank ? '_blank' : undefined,
         'rel': relNofollow ? 'nofollow' : undefined
       }
+
+      headingHtml = (
+        <a className='vce-google-fonts-heading-link' {...linkProps}>
+          {headingHtml}
+        </a>
+      )
     }
 
     if (typeof customClass === 'string' && customClass) {
@@ -35,7 +52,21 @@ class Component extends vcvAPI.elementComponent {
     }
 
     if (fontSize) {
+      fontSize = this.validateSize(fontSize)
 
+      if (fontSize) {
+        fontSize = /^\d+$/.test(fontSize) ? fontSize + 'px' : fontSize
+        innerCustomProps.style.fontSize = fontSize
+      }
+    }
+
+    if (lineHeight) {
+      lineHeight = this.validateSize(lineHeight)
+
+      if (lineHeight) {
+        lineHeight = /^\d+$/.test(lineHeight) ? lineHeight + 'px' : lineHeight
+        innerCustomProps.style.lineHeight = lineHeight
+      }
     }
 
     if (alignment) {
@@ -46,6 +77,17 @@ class Component extends vcvAPI.elementComponent {
 
     if (mixinData) {
       classes += ` vce-google-fonts-heading--color-${mixinData.selector}`
+    }
+
+    if (font) {
+      let fontHref = `https://fonts.googleapis.com/css?family=${font.fontFamily}:${font.fontStyle}`
+      googleFontLink = (
+        <link href={fontHref} rel='stylesheet' />
+      )
+
+      innerCustomProps.style.fontFamily = font.fontFamily
+      innerCustomProps.style.fontWeight = this.getFontVariant(font.fontStyle).weight
+      innerCustomProps.style.fontStyle = this.getFontVariant(font.fontStyle).style
     }
 
     let devices = designOptions.visibleDevices ? Object.keys(designOptions.visibleDevices) : []
@@ -64,6 +106,7 @@ class Component extends vcvAPI.elementComponent {
     }
 
     return <div {...customProps} className={classes} id={'el-' + id} {...editor}>
+      {googleFontLink}
       <CustomTag className={innerClasses} {...innerCustomProps}>
         {headingHtml}
       </CustomTag>
