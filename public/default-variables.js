58f2511b8bd519ff89d7a31ad9a2a7039743f7e8
Janis Konutis <janis.k@wpbakery.com>
2016-12-02 10:23:08 +0200

    Flickr image: remove feature toggle from default variables #195214668286866

--- public/default-variables.js
+++ public/default-variables.js
@@ -1,7 +1,6 @@
 import vcCake from 'vc-cake'
 
 vcCake.env('FEATURE_XO_WORK', false)
-vcCake.env('FEATURE_FLICKR_IMAGE', false)
 vcCake.env('FEATURE_TWITTER_PUBLISHER', false)
 vcCake.env('FEATURE_ASSETS_MANAGER', false)
 vcCake.env('FEATURE_SINGLE_IMAGE', false)
