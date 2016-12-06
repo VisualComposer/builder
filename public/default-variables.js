5690c15e7052377115b3dc0b6e6cfb7fb1c2d940
Vasilij <vasilij@wpbakery.com>
2016-12-06 11:56:16 +0200

    Enable features

--- public/default-variables.js
+++ public/default-variables.js
@@ -1,12 +1,12 @@
 import vcCake from 'vc-cake'
 
 vcCake.env('FEATURE_XO_WORK', false)
-vcCake.env('FEATURE_TWITTER_PUBLISHER', false)
+vcCake.env('FEATURE_TWITTER_PUBLISHER', true)
 vcCake.env('FEATURE_ASSETS_MANAGER', true)
-vcCake.env('FEATURE_SINGLE_IMAGE', false)
+vcCake.env('FEATURE_SINGLE_IMAGE', true)
 vcCake.env('FEATURE_WOOCOMMERCE', false)
 vcCake.env('FEATURE_GOOGLE_FONTS_HEADING', false)
-vcCake.env('FEATURE_IMAGE_GALLERY', false)
+vcCake.env('FEATURE_IMAGE_GALLERY', true)
 vcCake.env('FEATURE_SHORTCODES_SERVER_RENDER', true)
 vcCake.env('FEATURE_WEBPACK', false)
 vcCake.env('FEATURE_HTML_EDITOR_WP_VERSION', false)
