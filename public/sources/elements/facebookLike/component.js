d3bb910cfbc40ec6798cf67523e5ee5e103822e2
Janis Konutis <janis.k@wpbakery.com>
2016-12-15 14:10:24 +0200

    Facebook like: app id remove #195284230173781

--- public/sources/elements/facebookLike/component.js
+++ public/sources/elements/facebookLike/component.js
@@ -76,7 +76,7 @@ class Component extends vcvAPI.elementComponent {
 	var js, fjs = d.getElementsByTagName(s)[0];
 	if (d.getElementById(id)) return;
 	js = d.createElement(s); js.id = id;
-	js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8&appId=1165810236784781";
+	js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
 	fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>`
 
