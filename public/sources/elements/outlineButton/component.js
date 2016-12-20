aa1840e8d1ce99794b91a6a4e3e55696ba5b492a
Janis Konutis <janis.k@wpbakery.com>
2016-12-20 08:58:54 +0200

    Outline button: remove mixins, use arrays to create classes #141501573050851

--- public/sources/elements/outlineButton/component.js
+++ public/sources/elements/outlineButton/component.js
@@ -1,13 +1,14 @@
 /* global React, vcvAPI */
-/*eslint no-unused-vars: 0*/
+/* eslint no-unused-vars: 0 */
 class Component extends vcvAPI.elementComponent {
   render () {
     let { id, atts, editor } = this.props
     let { buttonUrl, buttonText, shape, color, designOptions, alignment, customClass, buttonType } = atts
 
-    let containerClasses = 'vce-button-container'
-    let wrapperClasses = 'vce-button-wrapper vce'
-    let classes = ''
+    let containerClasses = ['vce-button-container']
+
+    let classes = []
+
     let buttonHtml = buttonText
     let customProps = {}
     let CustomTag = 'button'
@@ -24,35 +25,34 @@ class Component extends vcvAPI.elementComponent {
     }
 
     if (typeof customClass === 'string' && customClass) {
-      containerClasses += ' ' + customClass
+      containerClasses.push(customClass)
     }
 
     if (shape && shape !== 'square') {
-      classes += ` vce-button--border-${shape}`
+      classes.push(`vce-button--border-${shape}`)
     }
 
     if (alignment) {
-      containerClasses += ` vce-button-container--align-${alignment}`
+      containerClasses.push(` vce-button-container--align-${alignment}`)
     }
 
     if (buttonType) {
-      classes += ` vce-button vce-button--style-${buttonType}`
+      classes.push(`vce-button vce-button--style-${buttonType}`)
     } else {
-      classes += ' vce-button vce-button--style-outline'
+      classes.push('vce-button vce-button--style-outline')
     }
 
     let mixinData = this.getMixinData('color')
 
     if (mixinData) {
-      classes += ` vce-button--style-${buttonType}--color-${mixinData.selector}`
+      classes.push(`vce-button--style-${buttonType}--color-${mixinData.selector}`)
     }
 
     mixinData = this.getMixinData('hoverColor')
 
     if (mixinData) {
-      classes += ` vce-button--style-${buttonType}--hover-color-${mixinData.selector}`
+      classes.push(`vce-button--style-${buttonType}--hover-color-${mixinData.selector}`)
     }
-
 
     let devices = designOptions.visibleDevices ? Object.keys(designOptions.visibleDevices) : []
     let animations = []
@@ -68,9 +68,9 @@ class Component extends vcvAPI.elementComponent {
     if (animations.length) {
       customProps[ 'data-vce-animate' ] = animations.join(' ')
     }
-    return <div className={containerClasses} {...editor}>
-      <span className={wrapperClasses} id={'el-' + id} >
-        <CustomTag className={classes} {...customProps}>
+    return <div className={containerClasses.join(' ')} {...editor}>
+      <span className='vce-button-wrapper vce' id={'el-' + id} >
+        <CustomTag className={classes.join(' ')} {...customProps}>
           {buttonHtml}
         </CustomTag>
       </span>
