d8e80a82713b94c89067abf1dca2a9e911351f70
Janis Konutis <janis.k@wpbakery.com>
2017-01-11 12:50:08 +0200

    Row placeholder control split as new component #238766304544616

--- public/editor/modules/content/layout/lib/helpers/rowPlaceholder/component.js
+++ public/editor/modules/content/layout/lib/helpers/rowPlaceholder/component.js
@@ -1,54 +1,32 @@
 import React from 'react'
+import RowPlaceholderControl from './lib/rowPlaceholderControl'
 
 export default class RowPlaceholder extends React.Component {
   static propTypes = {
     api: React.PropTypes.object.isRequired
   }
 
-  constructor (props) {
-    super(props)
-    this.handleAddElementClick = this.handleAddElementClick.bind(this)
-    this.handleAddTemplateClick = this.handleAddTemplateClick.bind(this)
-  }
-
-  handleAddElementClick (e) {
-    e && e.preventDefault()
-    this.props.api.request('app:add')
-  }
-
-  handleAddTemplateClick (e) {
-    e && e.preventDefault()
-    this.props.api.request('app:templates', true)
-  }
-
   render () {
     let addElementTitle = 'Add Element'
-    let addTemplateTitle = 'Add Element'
+    let addTemplateTitle = 'Add Template'
 
     return <vcvhelper>
       <div className='vcv-row-placeholder vcv-row-control-container vcv-row-control-container-hide-labels'>
-        <a
-          className='vcv-row-control'
-          href='#'
+        <RowPlaceholderControl
+          api={this.props.api}
           title={addElementTitle}
-          onClick={this.handleAddElementClick}
-        >
-          <span className='vcv-row-control-content'>
-            <i className='vcv-row-control-icon vcv-ui-icon vcv-ui-icon-add' />
-            <span>{addElementTitle}</span>
-          </span>
-        </a>
-        <a
-          className='vcv-row-control'
-          href='#'
+          text={addElementTitle}
+          icon='add'
+          action='app:add'
+        />
+        <RowPlaceholderControl
+          api={this.props.api}
           title={addTemplateTitle}
-          onClick={this.handleAddTemplateClick}
-        >
-          <span className='vcv-row-control-content'>
-            <i className='vcv-row-control-icon vcv-ui-icon vcv-ui-icon-template' />
-            <span>{addTemplateTitle}</span>
-          </span>
-        </a>
+          text={addTemplateTitle}
+          icon='template'
+          action='app:templates'
+          options='true'
+        />
       </div>
     </vcvhelper>
   }
