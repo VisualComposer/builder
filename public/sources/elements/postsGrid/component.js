1ca2c7cf3795078974a42155ee91fedf61544382
Pavel Ivanov <reactoranime@gmail.com>
2017-01-12 14:46:33 +0200

    Create element for posts grid #141889735080262

---
+++ public/sources/elements/postsGrid/component.js
@@ -0,0 +1,82 @@
+/* global React, vcvAPI, vcCake */
+/* eslint no-unused-vars: 0 */
+class Component extends vcvAPI.elementComponent {
+  state = {
+    shortcode: { __html: '' },
+    shortcodeContent: { __html: '' }
+  }
+
+  componentDidMount () {
+    this.requestToServer()
+  }
+
+  componentDidUpdate (prevProps) {
+    let isEqual = require('lodash').isEqual
+    if (!isEqual(this.props.atts, prevProps.atts)) {
+      this.requestToServer()
+    }
+  }
+
+  requestToServer () {
+    let ajax = (data, successCallback, failureCallback) => {
+      let request
+      request = new window.XMLHttpRequest()
+      request.open('POST', window.vcvAjaxUrl, true)
+      request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded')
+      request.onload = () => {
+        if (request.status >= 200 && request.status < 400) {
+          successCallback(request)
+        } else {
+          if (typeof failureCallback === 'function') {
+            failureCallback(request)
+          }
+        }
+      }
+      request.send(window.$.param(data))
+
+      return request
+    }
+
+    if (this.serverRequest) {
+      this.serverRequest.abort()
+    }
+    this.serverRequest = ajax({
+      'vcv-action': 'elements:posts_grid:adminNonce',
+      'vcv-nonce': window.vcvNonce,
+      'vcv-atts': this.props.atts // TODO: Pass correct grid atts
+    }, (result) => {
+      let response = JSON.parse(result.response)
+      this.setState({
+        shortcode: response.shortcode,
+        shortcodeContent: { __html: response.shortcodeContent }
+      })
+    })
+  }
+
+  render () {
+    let { id, atts, editor } = this.props
+    let { designOptions } = atts
+
+    let customProps = {}
+    let devices = designOptions.visibleDevices ? Object.keys(designOptions.visibleDevices) : []
+    let animations = []
+    devices.forEach((device) => {
+      let prefix = designOptions.visibleDevices[ device ]
+      if (designOptions[ device ].animation) {
+        if (prefix) {
+          prefix = `-${prefix}`
+        }
+        animations.push(`vce-o-animate--${designOptions[ device ].animation}${prefix}`)
+      }
+    })
+    if (animations.length) {
+      customProps[ 'data-vce-animate' ] = animations.join(' ')
+    }
+
+    return (
+      <div className='vce vce-posts-grid-wrapper' {...customProps} id={'el-' + id} {...editor}>
+        <vcvhelper data-vcvs-html={this.state.shortcode || ''} dangerouslySetInnerHTML={this.state.shortcodeContent || { __html: '' }} />
+      </div>
+    )
+  }
+}
