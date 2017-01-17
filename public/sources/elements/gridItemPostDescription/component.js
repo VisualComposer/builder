fce6ae0a4f1e62874fa925288cc43a0a0a976cb8
Janis Konutis <janis.k@wpbakery.com>
2017-01-17 13:12:44 +0200

    Post description item background edit #247236427962529

--- public/sources/elements/gridItemPostDescription/component.js
+++ public/sources/elements/gridItemPostDescription/component.js
@@ -4,15 +4,17 @@ class Component extends vcvAPI.elementComponent {
   render () {
     // TODO: Style
     return (
-      <article className='vce-posts-grid-item vce-post-description vce-post-description--with-background'>
-        <div className='vce-post-description--background-wrapper-box'>
-          <div className='vce-post-description--background-wrapper'>
-            {`{{custom_post_description_featured_image}}`}
+      <article className='vce-posts-grid-item'>
+        <div className='vce-post-description vce-post-description--with-background'>
+          <div className='vce-post-description--background-wrapper-box'>
+            <div className='vce-post-description--background-wrapper'>
+              {`{{custom_post_description_featured_image}}`}
+            </div>
           </div>
-        </div>
-        <div className='vce-post-description--content'>
-          <div className='vce-post-description--title'>{`{{post_title}}`}</div>
-          <div className='vce-post-description--excerpt'>{`{{post_teaser}}`}</div>
+          <div className='vce-post-description--content'>
+            <div className='vce-post-description--title'>{`{{post_title}}`}</div>
+            <div className='vce-post-description--excerpt'>{`{{post_teaser}}`}</div>
+          </div>
         </div>
       </article>
     )
