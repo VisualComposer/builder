94267521e3ed06c82b532ddae26310998885e4ad
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2017-01-18 08:09:40 +0200

    Add representer dependency to defaultElement #247236427962539

--- public/editor/modules/content/backendLayout/lib/defaultElement.js
+++ public/editor/modules/content/backendLayout/lib/defaultElement.js
@@ -65,10 +65,27 @@ export default class DefaultElement extends React.Component {
 
   // Getters
 
+  getDependency (label, element, cookElement) {
+    let isDependency, isRuleTrue
+    let options = cookElement.settings('metaBackendLabels').settings.options
+    if (options && options.onChange) {
+      isDependency = options.onChange.find((option) => {
+        return option.dependency === label
+      })
+      if (isDependency) {
+        isRuleTrue = isDependency.rule.value === element[isDependency.rule.attribute]
+      }
+    }
+    return isRuleTrue
+  }
+
   getRepresenter (element) {
     let cookElement = cook.get({tag: element.tag})
     let backendLabels = cookElement.get('metaBackendLabels').value
     return backendLabels.map((label) => {
+      if (this.getDependency(label, element, cookElement)) {
+        return null
+      }
       let RepresenterComponent = cookElement.settings(label).type.getRepresenter('Backend')
       return <RepresenterComponent
         key={`representer-${label}-${cookElement.get('id')}`}
@@ -97,9 +114,11 @@ export default class DefaultElement extends React.Component {
       return <div className='vce-wpbackend-element-container'>
         <div className={headerClasses} onClick={this.handleClick}>
           <div className='vce-wpbackend-element-header-icon'>
-            <img src={icon} alt={element.name} />
+            <img src={icon} alt={element.name} title={element.name} />
           </div>
-          <div className='vce-wpbackend-element-header-name'>{element.name}</div>
+          <div className='vce-wpbackend-element-header-name-wrapper'>
+            <span className='vce-wpbackend-element-header-name'>{element.name}</span>
+          </div>
         </div>
         <div className={attributesClasses}>
           {this.getRepresenter(element)}
@@ -109,9 +128,11 @@ export default class DefaultElement extends React.Component {
     return <div className='vce-wpbackend-element-container'>
       <div className='vce-wpbackend-element-header'>
         <div className='vce-wpbackend-element-header-icon'>
-          <img src={icon} alt={element.name} />
+          <img src={icon} alt={element.name} title={element.name} />
         </div>
-        <div className='vce-wpbackend-element-header-name'>{element.name}</div>
+        <div className='vce-wpbackend-element-header-name-wrapper'>
+          <span className='vce-wpbackend-element-header-name'>{element.name}</span>
+        </div>
       </div>
     </div>
   }
