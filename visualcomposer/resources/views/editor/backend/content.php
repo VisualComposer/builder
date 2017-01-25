cb135506c1bf8c7bf846a7751894e8278a648d3b
Janis Konutis <janis.k@wpbakery.com>
2017-01-25 14:40:05 +0200

    Backend editor: switcher add #206767557185249

--- visualcomposer/resources/views/editor/backend/content.php
+++ visualcomposer/resources/views/editor/backend/content.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @var $editableLink - link to editable content
+ * @var $frontendEditorLink - link to frontend editor
  */
 /** @var \VisualComposer\Helpers\Url $urlHelper */
 $urlHelper = vchelper('Url');
@@ -16,6 +17,7 @@ $postTypeHelper = vchelper('PostType');
     window.vcvPluginUrl = '<?php echo VCV_PLUGIN_URL; ?>';
     window.vcvPostData = <?php echo json_encode($postTypeHelper->getPostData()); ?>;
     window.vcvPostPermanentLink = '<?php echo get_permalink(get_the_ID()) ?>';
+    window.vcvFrontendEditorLink = '<?php echo $frontendEditorLink; ?>';
 </script>
 
 <div id="vcv-editor">
