e76e216717af6457ee61f4f0859e0d5f577310f1
Pavel Ivanov <reactoranime@gmail.com>
2017-01-25 12:58:39 +0200

    Custom post types controller #247236427962530

---
+++ public/sources/elements/postsGridDataSourceCustomPostType/component.js
@@ -0,0 +1,20 @@
+/* global React, vcvAPI, vcCake */
+/* eslint no-unused-vars: 0 */
+class Component extends vcvAPI.elementComponent {
+  render () {
+    let { attsPostType, attsLimit, attsOffset } = this.props.atts
+    let query = `post_type=${attsPostType}`
+    if (attsLimit && attsLimit.length) {
+      attsLimit = attsLimit > 0 ? (attsLimit > 1000 ? 1000 : attsLimit) : 1000
+      query += `&numberposts=${attsLimit}`
+    }
+    if (attsOffset && attsOffset.length) {
+      attsOffset = attsOffset > 0 ? attsOffset : 0
+      query += `&offset=${attsOffset}`
+    }
+
+    return (
+      <div>{query}</div>
+    )
+  }
+}
