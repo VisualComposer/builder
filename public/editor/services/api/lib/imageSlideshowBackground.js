9f30548b6e16ec737c79edbf4182256adccafb86
vaslij <vasilij@wpbakery.com>
2017-02-03 08:57:47 +0200

    Move background types from elementComponent #228828165948600

---
+++ public/editor/services/api/lib/imageSlideshowBackground.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import classNames from 'classnames'
+const { Component, PropTypes } = React
+export default class ImageSlideshowBackground extends Component {
+  static propTypes = {
+    id: PropTypes.string,
+    atts: PropTypes.object,
+    editor: PropTypes.object
+  }
+
+  render () {
+    const { reactKey, deviceKey, deviceData } = this.props
+    const { images, backgroundStyle, sliderTimeout, parallax } = deviceData
+    let timeout = sliderTimeout
+    if (!timeout) {
+      timeout = 5
+    }
+    if (images && images.urls && images.urls.length) {
+      let customProps = {}
+      let imagesJSX = []
+      images.urls.forEach((imgData) => {
+        let styles = {
+          backgroundImage: `url(${imgData.full})`
+        }
+        let imgKey = `${reactKey}-${imgData.id}`
+        imagesJSX.push((
+          <div className='vce-asset-background-slider-item' style={styles} key={imgKey} />
+        ))
+      })
+      let containerClasses = [
+        `vce-asset-background-slider-container`,
+        `vce-visible-${deviceKey}-only`
+      ]
+      if (backgroundStyle) {
+        containerClasses.push(`vce-asset-background-slider--style-${backgroundStyle}`)
+      }
+      let slideshowClasses = [
+        `vce-asset-background-slider`
+      ]
+
+      if (parallax) {
+        customProps[ 'data-vce-assets-parallax' ] = '.vce-asset-background-slider'
+      }
+
+      return <div className={classNames(containerClasses)} {...customProps} key={reactKey}>
+        <div className={classNames(slideshowClasses)} data-vce-assets-slider={timeout}
+          data-vce-assets-slider-slide='.vce-asset-background-slider-item'>
+          {imagesJSX}
+        </div>
+      </div>
+    }
+    return null
+  }
+}
