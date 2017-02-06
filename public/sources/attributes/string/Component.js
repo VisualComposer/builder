5fedb74afd7fe1e6ff7dc8b63de5b748e0bcf8bf
Pavel Ivanov <reactoranime@gmail.com>
2017-02-06 14:46:38 +0200

    Add description and fix placeholder #247236427962529

--- public/sources/attributes/string/Component.js
+++ public/sources/attributes/string/Component.js
@@ -5,13 +5,18 @@ class StringAttribute extends Attribute {
   render () {
     let { value } = this.state
     let { placeholder } = this.props
+    if (!placeholder && this.props.options && this.props.options.placeholder) {
+      placeholder = this.props.options.placeholder
+    }
+
     return (
       <input
         className='vcv-ui-form-input'
         type='text'
         onChange={this.handleChange}
         placeholder={placeholder}
-        value={value} />
+        value={value}
+      />
     )
   }
 }
