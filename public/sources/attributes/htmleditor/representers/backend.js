a28427885123adbf0124312b3e4159ddbb2a6d36
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2017-02-15 14:15:12 +0200

    Add target attribute to links in htlmEditor content in backendView #247236427962539

--- public/sources/attributes/htmleditor/representers/backend.js
+++ public/sources/attributes/htmleditor/representers/backend.js
@@ -3,12 +3,23 @@ import classNames from 'classnames'
 import Representer from '../../representer'
 
 export default class Backend extends Representer {
+  htmlEditor = null
+
+  componentDidMount () {
+    let anchors = [].slice.call(this.htmlEditor.getElementsByTagName('a'))
+    anchors.forEach((a) => { a.target = '_blank' })
+  }
+
   render () {
     let classes = classNames({
       'vcv-wpbackend-attributes-content': true,
       'vcv-wpbackend-attributes-content-block': true,
       'vcv-wpbackend-attr-representer-htmleditor': true
     })
-    return <div className={classes} dangerouslySetInnerHTML={{__html: this.state.value}} />
+    return <div
+      className={classes}
+      dangerouslySetInnerHTML={{__html: this.state.value}}
+      ref={(htmlEditor) => { this.htmlEditor = htmlEditor }}
+    />
   }
 }
