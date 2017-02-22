1c90675ba0525b799b3206507659c5c9714ce1f0
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2017-02-22 17:31:12 +0200

    Refactor backend element resize event #276186285084110

--- public/editor/modules/content/backendLayout/lib/defaultElement.js
+++ public/editor/modules/content/backendLayout/lib/defaultElement.js
@@ -57,12 +57,11 @@ export default class DefaultElement extends React.Component {
     if (window.getComputedStyle(element).position === 'static') {
       element.style.position = 'relative'
     }
-    let obj = element.__resizeTrigger__ = document.createElement('object')
+    let obj = element.__resizeTrigger__ = document.createElement('iframe')
     obj.setAttribute('style', 'display: block; position: absolute; top: 0; left: 0; height: 100%; width: 100%; overflow: hidden; opacity: 0; pointer-events: none; z-index: -1;')
     obj.__resizeElement__ = element
     obj.onload = function (e) {
-      console.log('test')
-      this.contentDocument.addEventListener('resize', fn)
+      this.contentDocument.defaultView.addEventListener('resize', fn)
     }
     obj.type = 'text/html'
     if (isIE) {
@@ -75,7 +74,7 @@ export default class DefaultElement extends React.Component {
   }
 
   removeResizeListener (element, fn) {
-    element.__resizeTrigger__.contentDocument.removeEventListener('resize', fn)
+    element.__resizeTrigger__.contentDocument.defaultView.removeEventListener('resize', fn)
     element.__resizeTrigger__ = !element.removeChild(element.__resizeTrigger__)
   }
 
