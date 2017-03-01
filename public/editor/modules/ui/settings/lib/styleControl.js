aaf9f6721d10455ea37fb0279d17eb76d45a081a
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2017-03-01 13:00:35 +0200

    Add preventDefault to buttons, fix link selector modal window z-index in backendView #247236427962539

--- public/editor/modules/ui/settings/lib/styleControl.js
+++ public/editor/modules/ui/settings/lib/styleControl.js
@@ -2,12 +2,20 @@ import React from 'react'
 import classNames from 'classnames'
 
 export default class StyleControl extends React.Component {
+  static propTypes = {
+    changeActive: React.PropTypes.func,
+    index: React.PropTypes.number.isRequired,
+    title: React.PropTypes.string,
+    active: React.PropTypes.bool
+  }
+
   constructor (props) {
     super(props)
     this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick () {
+  handleClick (e) {
+    e && e.preventDefault()
     this.props.changeActive(this.props.index)
   }
 
@@ -24,10 +32,4 @@ export default class StyleControl extends React.Component {
       </button>
     )
   }
-}
-StyleControl.propTypes = {
-  changeActive: React.PropTypes.func,
-  index: React.PropTypes.number.isRequired,
-  title: React.PropTypes.string,
-  active: React.PropTypes.bool
 }
