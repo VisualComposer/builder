fc56e65f5b45ddb6d2430052da960e786bb821c1
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2017-03-09 13:41:13 +0200

    Remove classic editor button, add link to frontend editor control #206767557185249

--- public/editor/modules/content/backendSwitcher/lib/helpers/backendSwitcher/component.js
+++ public/editor/modules/content/backendSwitcher/lib/helpers/backendSwitcher/component.js
@@ -1,70 +1,15 @@
 import React from 'react'
-import vcCake from 'vc-cake'
 import '../../../../../../../sources/less/wpbackend-switcher/init.less'
 
 export default class BackendSwitcher extends React.Component {
-  constructor (props) {
-    super(props)
-    this.setEditor = this.setEditor.bind(this)
-  }
-
-  setEditor (e) {
-    e && e.preventDefault && e.preventDefault()
-    if (typeof e.currentTarget.dataset.switch !== 'undefined') {
-      window.setUserSetting('vcvEditorsBackendLayoutSwitcher', e.currentTarget.dataset.switch)
-    }
-    window.location.href = e.currentTarget.dataset.href
-  }
-
   render () {
-    let backendEnabled = window.getUserSetting('vcvEditorsBackendLayoutSwitcher') !== '0'
-    let output = (
-      <div className='vcv-wpbackend-switcher-wrapper'>
-        <div className='vcv-wpbackend-switcher'>
-          <span className='vcv-wpbackend-switcher-logo' />
-          <button
-            type='button'
-            data-href={window.vcvFrontendEditorLink}
-            onClick={this.setEditor}
-            className='vcv-wpbackend-switcher-option'>Frontend Editor
-          </button>
-          <button
-            type='button'
-            data-href={window.location.href}
-            data-switch='1'
-            onClick={this.setEditor}
-            disabled={!vcCake.env('FEATURE_WPBACKEND')}
-            className='vcv-wpbackend-switcher-option'>Backend Editor
-          </button>
-        </div>
+    return <div className='vcv-wpbackend-switcher-wrapper'>
+      <div className='vcv-wpbackend-switcher'>
+        <span className='vcv-wpbackend-switcher-logo' />
+        <a className='vcv-wpbackend-switcher-option' href={window.vcvFrontendEditorLink}>
+          Frontend Editor
+        </a>
       </div>
-    )
-
-    if (backendEnabled) {
-      output = (
-        <div className='vcv-wpbackend-switcher-wrapper'>
-          <div className='vcv-wpbackend-switcher'>
-            <span className='vcv-wpbackend-switcher-logo' />
-            <button
-              type='button'
-              data-href={window.vcvFrontendEditorLink}
-              onClick={this.setEditor}
-              className='vcv-wpbackend-switcher-option'>Frontend Editor
-            </button>
-          </div>
-          <div className='vcv-wpbackend-switcher--type-classic'>
-            <button
-              type='button'
-              data-href={window.location.href}
-              data-switch='0'
-              onClick={this.setEditor}
-              className='vcv-wpbackend-switcher-option'>Classic Editor
-            </button>
-          </div>
-        </div>
-      )
-    }
-
-    return output
+    </div>
   }
 }
