0af488538a7a5435373b409ea98018fa17b6825c
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2017-03-09 10:13:11 +0200

    Remove treeView feature toggle #276878098803537

--- public/editor/modules/ui/treeViewBackend/module.js
+++ public/editor/modules/ui/treeViewBackend/module.js
@@ -2,12 +2,10 @@ import vcCake from 'vc-cake'
 import TreeView from './lib/treeViewLayout'
 import TreeViewNavbarControl from './lib/navbarControl'
 
-if (vcCake.env('FEATURE_BACKEND_TREEVIEW')) {
-  vcCake.add('ui-tree-view-backend', (api) => {
-    api.module('ui-layout-bar').do('setStartContent', TreeView, {
-      api: api
-    })
-
-    api.module('ui-navbar').do('addElement', 'Tree layout', TreeViewNavbarControl, { pin: 'visible', api: api })
+vcCake.add('ui-tree-view-backend', (api) => {
+  api.module('ui-layout-bar').do('setStartContent', TreeView, {
+    api: api
   })
-}
+
+  api.module('ui-navbar').do('addElement', 'Tree layout', TreeViewNavbarControl, { pin: 'visible', api: api })
+})
