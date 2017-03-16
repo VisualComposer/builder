efe9ba5b530f5e793e29343b8f249f2ddf6a02b0
Janis Konutis <janis.k@wpbakery.com>
2017-03-16 09:02:35 +0200

    Edit resizer labels #281048355291052

--- public/resources/columnResizer/columnResizer.js
+++ public/resources/columnResizer/columnResizer.js
@@ -170,7 +170,7 @@ class ColumnResizer extends React.Component {
     for (let i = 0; i < sizes.length; i++) {
       let percentage = parseFloat(sizes[ i ]) / 100
       let mixinName = `customColumnMixinResize${i}`
-      newMixins[ mixinName ] = lodash.defaultsDeep({}, rowData.layout.attributeMixins[Object.keys(rowData.layout.attributeMixins)[0]])
+      newMixins[ mixinName ] = lodash.defaultsDeep({}, rowData.layout.attributeMixins[ Object.keys(rowData.layout.attributeMixins)[ 0 ] ])
       newMixins[ mixinName ].variables.percentageSelector.value = sizes[ i ]
       newMixins[ mixinName ].variables.percentage.value = percentage.toString()
       rowData.layout.attributeMixins[ mixinName ] = newMixins[ mixinName ]
@@ -199,29 +199,44 @@ class ColumnResizer extends React.Component {
       resizerLabels = (
         <div className='vce-column-resizer-label-container' {...labelProps}>
           <div className='vce-column-resizer-label vce-column-resizer-label-left'>
-            <svg width='41px' height='23px' viewBox='0 0 41 23' version='1.1' xmlns='http://www.w3.org/2000/svg'>
-              <g id='Page-1' stroke='none' strokeWidth='1' fill='none' fillRule='evenodd' opacity='0.9'>
-                <g id='Left' fill='#282828'>
-                  <path
-                    d='M9.67660985,2.33058017e-12 L35.1786526,0 C37.9367983,0 40.1727172,2.24721412 40.1727172,4.99065745 L40.1727172,18.0093426 C40.1727172,20.7656066 37.9304373,23 35.1786526,23 L9.67660985,23 C9.12217523,23 8.35313873,22.6804216 7.97065195,22.2979348 L0.582842894,12.9101257 C-0.195948043,12.1313348 -0.192612096,10.8653293 0.582842894,10.0898743 L7.97065195,0.702065207 C8.35839186,0.3143253 9.12167598,2.33058017e-12 9.67660985,2.33058017e-12 Z'
-                    id='Combined-Shape'
-                    transform='translate(20.086359, 11.500000) scale(-1, 1) translate(-20.086359, -11.500000)' />
-                </g>
+            <svg className='vce-column-resizer-label-left-svg' width='5px' height='23px' viewBox='0 0 5 23'
+              version='1.1'
+              xmlns='http://www.w3.org/2000/svg'>
+              <g id='Page-1' stroke='none' strokeWidth='1' fill='#282828' fillRule='evenodd' opacity='0.9'>
+                <path
+                  d='M9.67660985,2.33058017e-12 L35.1786526,0 C37.9367983,0 40.1727172,2.24721412 40.1727172,4.99065745 L40.1727172,18.0093426 C40.1727172,20.7656066 37.9304373,23 35.1786526,23 L9.67660985,23 C9.12217523,23 8.35313873,22.6804216 7.97065195,22.2979348 L0.582842894,12.9101257 C-0.195948043,12.1313348 -0.192612096,10.8653293 0.582842894,10.0898743 L7.97065195,0.702065207 C8.35839186,0.3143253 9.12167598,2.33058017e-12 9.67660985,2.33058017e-12 Z'
+                  transform='translate(20.086359, 11.500000) scale(-1, 1) translate(-20.086359, -11.500000)' />
               </g>
             </svg>
-            <span className='vce-column-resizer-label-percentage'>{this.state.leftColPercentage + '%'}</span>
+            <div className='vce-column-resizer-label-background'>
+              <span className='vce-column-resizer-label-percentage'>{this.state.leftColPercentage + '%'}</span>
+            </div>
+            <svg className='vce-column-resizer-label-right-svg' width='11px' height='23px' viewBox='0 0 11 23'
+              version='1.1' xmlns='http://www.w3.org/2000/svg'>
+              <g id='Page-1' stroke='none' strokeWidth='1' fillRule='evenodd' opacity='0.9'
+                transform='translate(-30.000000, 0.000000)' fill='#282828'>
+                <path
+                  d='M9.67660985,2.33058017e-12 L35.1786526,0 C37.9367983,0 40.1727172,2.24721412 40.1727172,4.99065745 L40.1727172,18.0093426 C40.1727172,20.7656066 37.9304373,23 35.1786526,23 L9.67660985,23 C9.12217523,23 8.35313873,22.6804216 7.97065195,22.2979348 L0.582842894,12.9101257 C-0.195948043,12.1313348 -0.192612096,10.8653293 0.582842894,10.0898743 L7.97065195,0.702065207 C8.35839186,0.3143253 9.12167598,2.33058017e-12 9.67660985,2.33058017e-12 Z'
+                  transform='translate(20.086359, 11.500000) scale(-1, 1) translate(-20.086359, -11.500000)' />
+              </g>
+            </svg>
           </div>
           <div className='vce-column-resizer-label vce-column-resizer-label-right'>
-            <svg width='41px' height='23px' viewBox='0 0 41 23' version='1.1' xmlns='http://www.w3.org/2000/svg'>
-              <g id='Page-1' stroke='none' strokeWidth='1' fill='none' fillRule='evenodd' opacity='0.9'>
-                <g id='Right' fill='#282828'>
-                  <path
-                    d='M9.67660985,2.33058017e-12 L35.1786526,0 C37.9367983,0 40.1727172,2.24721412 40.1727172,4.99065745 L40.1727172,18.0093426 C40.1727172,20.7656066 37.9304373,23 35.1786526,23 L9.67660985,23 C9.12217523,23 8.35313873,22.6804216 7.97065195,22.2979348 L0.582842894,12.9101257 C-0.195948043,12.1313348 -0.192612096,10.8653293 0.582842894,10.0898743 L7.97065195,0.702065207 C8.35839186,0.3143253 9.12167598,2.33058017e-12 9.67660985,2.33058017e-12 Z'
-                    id='Combined-Shape' />
-                </g>
+            <svg width='10px' height='23px' viewBox='0 0 10 23' version='1.1' xmlns='http://www.w3.org/2000/svg'>
+              <g id='Page-1' stroke='none' strokeWidth='1' fillRule='evenodd' fill='#282828' opacity='0.9'>
+                <path
+                  d='M9.67660985,2.33058017e-12 L35.1786526,0 C37.9367983,0 40.1727172,2.24721412 40.1727172,4.99065745 L40.1727172,18.0093426 C40.1727172,20.7656066 37.9304373,23 35.1786526,23 L9.67660985,23 C9.12217523,23 8.35313873,22.6804216 7.97065195,22.2979348 L0.582842894,12.9101257 C-0.195948043,12.1313348 -0.192612096,10.8653293 0.582842894,10.0898743 L7.97065195,0.702065207 C8.35839186,0.3143253 9.12167598,2.33058017e-12 9.67660985,2.33058017e-12 Z' />
               </g>
             </svg>
-            <span className='vce-column-resizer-label-percentage'>{this.state.rightColPercentage + '%'}</span>
+            <div className='vce-column-resizer-label-background'>
+              <span className='vce-column-resizer-label-percentage'>{this.state.rightColPercentage + '%'}</span>
+            </div>
+            <svg width='6px' height='23px' viewBox='0 0 6 23' version='1.1' xmlns='http://www.w3.org/2000/svg'>
+              <g id='Page-1' stroke='none' strokeWidth='1' fillRule='evenodd' fill='#282828' opacity='0.9'>
+                <path
+                  d='M-25.3233902,2.33058017e-12 L0.178652594,0 C2.93679829,0 5.17271716,2.24721412 5.17271716,4.99065745 L5.17271716,18.0093426 C5.17271716,20.7656066 2.93043732,23 0.178652594,23 L-25.3233902,23 C-25.8778248,23 -26.6468613,22.6804216 -27.029348,22.2979348 L-34.4171571,12.9101257 C-35.195948,12.1313348 -35.1926121,10.8653293 -34.4171571,10.0898743 L-27.029348,0.702065207 C-26.6416081,0.3143253 -25.878324,2.33058017e-12 -25.3233902,2.33058017e-12 Z' />
+              </g>
+            </svg>
           </div>
         </div>
       )
