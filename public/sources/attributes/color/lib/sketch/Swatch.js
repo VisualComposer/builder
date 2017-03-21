f6fe02b1c3c674b0d11bab2d1de48794f209a631
Xopoc <vasilij@wpbakery.com>
2017-03-21 19:21:47 +0200

    Add trigger to clear colorPicker value #244499587954464

--- public/sources/attributes/color/lib/sketch/Swatch.js
+++ public/sources/attributes/color/lib/sketch/Swatch.js
@@ -9,7 +9,7 @@ export const Swatch = (props) => {
       cursor: 'pointer',
       backgroundColor: color
     }
-    if (color === 'transparent') {
+    if (color === 'rgba(186, 218, 85, 0)') {
       color = 'url("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAAXNSR0IArs4c6QAAAG5JREFUOBFjZCATvI+MtP//9+9aRnL0wzUzMweTbACyZsHlyw+SZAC6ZpDriTYAm2aiDcClmSgD8GkmaAAhzXgNIEYzTgOI1YzVAFI0YxhAqmYUA8jRDDeAXM1gAyjRDDKACZwlgbkKlDFAAqQCAB5beZgTNEIdAAAAAElFTkSuQmCC")'
       styles.backgroundColor = ''
       styles.backgroundImage = color
