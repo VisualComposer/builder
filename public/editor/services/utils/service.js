41d8fb636e3ae9e12b8718a44cb50ec867767343
Pavel Ivanov <reactoranime@gmail.com>
2017-03-31 16:13:48 +0300

    Create slugify function #298041844173124 for assets files store

--- public/editor/services/utils/service.js
+++ public/editor/services/utils/service.js
@@ -127,6 +127,12 @@ const API = {
     }
 
     return html
+  },
+  slugify (str) {
+    return str.toString().toLowerCase()
+      .replace(/[^\w\s-]/g, '') // remove non-word [a-z0-9_], non-whitespace, non-hyphen characters
+      .replace(/[\s_-]+/g, '-') // swap any length of whitespace, underscore, hyphen characters with a single -
+      .replace(/^-+|-+$/g, '').trim() // remove leading, trailing -
   }
 }
 vcCake.addService('utils', API)
