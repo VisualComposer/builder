be966c7124821ff784eed3147aebfa611f1a7430
Janis Konutis <janis.k@wpbakery.com>
2017-04-07 10:52:14 +0300

    Add show/hide logic #291369665889257

--- public/editor/modules/ui/start-blank/module.js
+++ public/editor/modules/ui/start-blank/module.js
@@ -5,10 +5,19 @@ import StartBlank from '../../../../resources/components/startBlank/component'
 
 if (vcCake.env('FEATURE_START_BLANK')) {
   vcCake.add('ui-start-blank', (api) => {
-    let startBlankOverlay = document.getElementById('vcv-layout-iframe-start-blank')
-    ReactDOM.render(
-      <StartBlank api={api} />,
-      startBlankOverlay
+    api.reply(
+      'data:changed',
+      (data) => {
+        let startBlankOverlay = document.getElementById('vcv-layout-iframe-start-blank')
+        if (data.length === 0) {
+          ReactDOM.render(
+            <StartBlank api={api} />,
+            startBlankOverlay
+          )
+        } else {
+          ReactDOM.unmountComponentAtNode(startBlankOverlay)
+        }
+      }
     )
   })
 }
