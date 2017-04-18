9677576bc7069118e3ea7297c23acdc350be44d3
Pavel Ivanov <reactoranime@gmail.com>
2017-04-18 16:18:17 +0300

    Fix single Image overlay

--- visualcomposer/Modules/Assets/VendorBundleController.php
+++ visualcomposer/Modules/Assets/VendorBundleController.php
@@ -1,20 +1,24 @@
 <?php
 
-namespace VisualComposer\Modules\Editors;
+namespace VisualComposer\Modules\Assets;
 
 use VisualComposer\Framework\Container;
 use VisualComposer\Framework\Illuminate\Support\Module;
 use VisualComposer\Helpers\Traits\EventsFilters;
+use VisualComposer\Helpers\Traits\WpFiltersActions;
 use VisualComposer\Helpers\Url;
 
 class VendorBundleController extends Container implements Module
 {
     use EventsFilters;
+    use WpFiltersActions;
 
     public function __construct()
     {
-        /** @see \VisualComposer\Modules\Editors\VendorBundleController::addVendorScript */
+        /** @see \VisualComposer\Modules\Assets\VendorBundleController::addVendorScript */
         $this->addFilter('vcv:backend:extraOutput vcv:frontend:head:extraOutput', 'addVendorScript', 1);
+
+        $this->wpAddAction('wp_enqueue_scripts', 'enqueueVendorFrontScripts', 1);
     }
 
     protected function addVendorScript($response, $payload, Url $urlHelper)
@@ -33,5 +37,25 @@ class VendorBundleController extends Container implements Module
         );
 
         return $response;
+    }
+
+    protected function enqueueVendorFrontScripts(Url $urlHelper)
+    {
+        wp_enqueue_script('jquery'); // Required for 3-rd elements libraries
+        wp_enqueue_script(
+            'vcv:assets:vendor:script',
+            $urlHelper->to(
+                'public/dist/vendor.bundle.js?' . uniqid()
+            )
+        );
+        wp_enqueue_script(
+            'vcv:assets:front:script',
+            $urlHelper->to(
+                'public/dist/front.bundle.js?' . uniqid()
+            ),
+            [
+                'vcv:assets:vendor:script',
+            ]
+        );
     }
 }
