5aeb53140b6defcfbb40d9a2f10377867cfdbeb4
Pavel Ivanov <reactoranime@gmail.com>
2017-04-21 14:46:56 +0300

    Fix account url #286390964180416

--- plugin-wordpress.php
+++ plugin-wordpress.php
@@ -86,7 +86,7 @@ if (!defined('VCV_LAZY_LOAD')) {
     define('VCV_LAZY_LOAD', false);
 }
 
-define('VCV_ACCOUNT_URL', 'http://localhost:8000');
+define('VCV_ACCOUNT_URL', 'http://test.account.visualcomposer.io');
 
 /**
  * Check PHP version.
