a3ccbde0896aef427259c4d1b01570777003e532
Pavel Ivanov <reactoranime@gmail.com>
2017-04-28 14:40:48 +0300

    Update animate js #280294763420491

--- public/sources/assetsLibrary/animate/src/animate.js
+++ public/sources/assetsLibrary/animate/src/animate.js
@@ -1,53 +1,49 @@
-/* global Waypoint */
-window.vcv.on('ready', function (action, id) {
-  if (action !== 'merge') {
-    vceAnimate.enableAnimate(action && id ? id : '');
-  }
-});
-
-var vceAnimate = {
-  enableAnimate: function enableAnimate (id) {
-    window.Waypoint.destroyAll();
-    var waypoints = [];
-    var selector = id ? '[data-vcv-element="' + id + '"]' : '[data-vce-animate]';
-    var elements = document.querySelectorAll(selector);
-    elements = [].slice.call(elements);
-    elements.forEach(function (element) {
-      var _element$classList;
+window.vcv.on('ready', (action, id) => {
+  let enableAnimate = (id) => {
+    window.Waypoint.destroyAll()
+    let waypoints = []
+    let selector = id ? '[data-vcv-element="' + id + '"]' : '[data-vce-animate]'
+    let elements = document.querySelectorAll(selector)
+    elements = [].slice.call(elements)
+    elements.forEach((element) => {
       if (id && !element.getAttribute('data-vce-animate')) {
-        element = element.querySelector('[data-vce-animate]');
+        element = element.querySelector('[data-vce-animate]')
         if (!element) {
-          return;
+          return
         }
       }
       // remove old classes
-      var oldClasses = [];
-      var re = /^vce-o-animate--/;
-      element.classList.forEach(function (className) {
+      let oldClasses = []
+      let re = /^vce-o-animate--/
+      element.classList.forEach((className) => {
         if (className.search(re) !== -1) {
-          oldClasses.push(className);
+          oldClasses.push(className)
         }
-      });
-      (_element$classList = element.classList).remove.apply(_element$classList, oldClasses);
-      var waypointObj = new window.Waypoint({
+      })
+      element.classList.remove.apply(element.classList, oldClasses)
+
+      let waypointObj = new window.Waypoint({
         element: element,
-        handler: function handler () {
-          var _this = this;
+        handler: () => {
           // add new classes
-          var newClasses = [];
-          if (_this.element.dataset[ 'vceAnimate' ]) {
-            newClasses = _this.element.dataset[ 'vceAnimate' ].split(' ');
+          let newClasses = []
+          if (element.dataset[ 'vceAnimate' ]) {
+            newClasses = element.dataset[ 'vceAnimate' ].split(' ')
           }
           // _this.element.setAttribute('data-vce-animated', 'true')
-          newClasses.push('vce-o-animate--animated');
-          newClasses.forEach(function (className) {
-            _this.element.classList.add(className);
-          });
-          _this.destroy();
+          newClasses.push('vce-o-animate--animated')
+          newClasses.forEach((className) => {
+            element.classList.add(className)
+          })
+          waypointObj.destroy()
         },
         offset: '85%'
-      });
-      waypoints.push(waypointObj);
-    });
+      })
+      waypoints.push(waypointObj)
+    })
   }
-};
+
+  if (action !== 'merge') {
+    enableAnimate(action && id ? id : '')
+  }
+})
