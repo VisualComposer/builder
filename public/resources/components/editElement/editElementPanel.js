2c1854bca015860bdc9142d601487ac822739d85
Janis Konutis <janis.k@wpbakery.com>
2017-05-11 14:48:33 +0300

    Edit shouldComponentUpdate function #339419775174398

--- public/resources/components/editElement/editElementPanel.js
+++ public/resources/components/editElement/editElementPanel.js
@@ -4,27 +4,38 @@ import ActivitiesManager from './lib/activitiesManager'
 import {getStorage, getService} from 'vc-cake'
 const elementsStorage = getStorage('elements')
 const cook = getService('cook')
+import lodash from 'lodash'
 export default class EditElementPanel extends ActivitiesManager {
   static propTypes = {
     element: React.PropTypes.object.isRequired,
     activeTabId: React.PropTypes.string
   }
+
   constructor (props) {
     super(props)
     this.updateElementOnChange = this.updateElementOnChange.bind(this)
   }
+
+  shouldComponentUpdate (nextProps, nextState) {
+    let isEqual = lodash.isEqual(this.props, nextProps)
+    let isEqualState = lodash.isEqual(this.state, nextState)
+    return !(isEqual && isEqualState)
+  }
+
   componentDidMount () {
-    const {element} = this.props
+    const { element } = this.props
     const id = element.get('id')
     elementsStorage.state(`element:${id}`).onChange(this.updateElementOnChange)
   }
+
   componentWillUnmount () {
-    const {element} = this.props
+    const { element } = this.props
     const id = element.get('id')
     elementsStorage.state(`element:${id}`).ignoreChange(this.updateElementOnChange)
   }
+
   updateElementOnChange (data, source) {
-    const {element} = this.props
+    const { element } = this.props
     const id = element.get('id')
     if (source !== 'editForm') {
       const cookElement = cook.get(data)
@@ -37,6 +48,7 @@ export default class EditElementPanel extends ActivitiesManager {
       })
     }
   }
+
   getActiveTabId () {
     let formWrapper = this.refs[ 'formWrapper' ]
     return formWrapper.allTabs[ formWrapper.state.activeTabIndex ].data.id
@@ -48,7 +60,7 @@ export default class EditElementPanel extends ActivitiesManager {
   }
 
   render () {
-    const {element, activeTabId} = this.props
+    const { element, activeTabId } = this.props
     return (
       <FormWrapper
         activeTabId={activeTabId}
