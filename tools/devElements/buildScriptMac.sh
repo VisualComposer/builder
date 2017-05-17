8c48c88a2f26f7e4f1503d8e05305ab8a4dd398c
Pavel Ivanov <reactoranime@gmail.com>
2017-05-17 15:48:18 +0300

    Change update scripts

--- tools/devElements/buildScriptMac.sh
+++ tools/devElements/buildScriptMac.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
  find devElements/ -maxdepth 1 -type d | tail -n +2 | xargs -I % sh -c 'cd % && npm run build && sed -i "" "s:../../node_modules/:./node_modules/:g" public/dist/element.bundle.js'
-
-#&& sed -i "s:../../../../node_modules/:./node_modules/:g" public/dist/element.bundle.js'
--- tools/devElements/updateScript.sh
+++ tools/devElements/updateScript.sh
@@ -39,20 +39,6 @@ do
    cp $EXECDIR/tools/devElements/webpack.config.js $EXECDIR/devElements/$i/webpack.config.js
    sed -i "s:\[element\]:$i:g" $EXECDIR/devElements/$i/webpack.config.js
    cd $EXECDIR/devElements/$i && git add . && git commit -m "update webpack config for $i" && git push
-   #echo $i && cd $EXECDIR/devElements/$i && git status
-   #cd /Users/Konutis/Sites/vcelements
-   #echo "Cloning element $i from git@gitlab.com:visualcomposer-hub/$i.git"
-   #git clone git@gitlab.com:visualcomposer-hub/$i.git
-   #echo "Copying element $i to /Users/Konutis/Sites/vcelements/$i"
-   #cp -r /Users/Konutis/Sites/vc-five/public/sources/newElements/$i/ /Users/Konutis/Sites/vcelements/$i
-   #cp ./.gitignore /Users/Konutis/Sites/vcelements/$i/.gitignore
-   #cp ./package.json /Users/Konutis/Sites/vcelements/$i/package.json
-   
-   #cd /Users/Konutis/Sites/vcelements/$i && git add . && git commit -m "Initial element commit $i #341404808166592"
-   #cd /Users/Konutis/Sites/vcelements/$i && git push
-   
-   # in case of remove
-   #cd /Users/Konutis/Sites/vcelements/$i && rm -rf $i
 done
 
 #git clone git@gitlab.com:visualcomposer-hub/featureSection.git
