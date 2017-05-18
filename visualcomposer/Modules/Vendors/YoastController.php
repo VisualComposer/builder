647806a5df07dbad6c6d1937be2360e127968da6
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2017-05-18 13:29:49 +0300

    Rename and move yoast.js file to resources/components #328272623701687

--- visualcomposer/Modules/Vendors/YoastController.php
+++ visualcomposer/Modules/Vendors/YoastController.php
@@ -5,14 +5,11 @@ namespace VisualComposer\Modules\Vendors;
 use VisualComposer\Framework\Container;
 use VisualComposer\Framework\Illuminate\Support\Module;
 use VisualComposer\Helpers\Frontend;
-use VisualComposer\Helpers\Traits\EventsFilters;
 use VisualComposer\Helpers\Traits\WpFiltersActions;
-use VisualComposer\Helpers\Url;
 
 class YoastController extends Container implements Module
 {
     use WpFiltersActions;
-    use EventsFilters;
 
     public function __construct(Frontend $frontendHelper)
     {
@@ -21,14 +18,8 @@ class YoastController extends Container implements Module
 
     protected function initializeYoast(Frontend $frontendHelper)
     {
-        if (isset($GLOBALS['wpseo_metabox'])) {
-            if ($frontendHelper->isFrontend()) {
-                $this->removeFeScript();
-            } else {
-                /** @see \VisualComposer\Modules\Vendors\YoastController::enqueueVendorBackend */
-                //  $this->wpAddAction('admin_enqueue_scripts', 'enqueueVendorBackend');
-                $this->addFilter('vcv:backend:extraOutput', 'enqueueVendorBackend', 3);
-            }
+        if (isset($GLOBALS['wpseo_metabox']) && $frontendHelper->isFrontend()) {
+            $this->removeFeScript();
         }
     }
 
@@ -37,23 +28,5 @@ class YoastController extends Container implements Module
         if (isset($GLOBALS['wpseo_metabox'])) {
             remove_action('admin_enqueue_scripts', [$GLOBALS['wpseo_metabox'], 'enqueue']);
         }
-    }
-
-    protected function enqueueVendorBackend($response, $payload, Url $urlHelper)
-    {
-        // Add Vendor JS
-        $response = array_merge(
-            (array)$response,
-            [
-                sprintf(
-                    '<script id="vcv-script-vendor-yoast" type="text/javascript" src="%s"></script>',
-                    $urlHelper->to(
-                        'public/dist/yoast.bundle.js?' . uniqid()
-                    )
-                ),
-            ]
-        );
-
-        return $response;
     }
 }
