b2ccd8c7ed64e1bf9565f8eece852b050cc1b271
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2017-05-22 17:29:57 +0300

    Refactor backend saveController #328272623701691

--- public/editor/stores/wordpressBackendData/lib/saveController.js
+++ public/editor/stores/wordpressBackendData/lib/saveController.js
@@ -20,7 +20,7 @@ export default class SaveController {
    * @param status
    * @private
    */
-  _saveByElements (data, status, callback) {
+  save (data, status, callback, action = '') {
     const iframe = document.getElementById('vcv-editor-iframe')
     const contentLayout = iframe ? iframe.contentWindow.document.querySelector('[data-vcv-module="content-layout"]') : false
     let content = contentLayout ? utils.normalizeHtml(contentLayout.innerHTML) : ''
@@ -77,7 +77,7 @@ export default class SaveController {
       }
       document.getElementById('content').value = content
       document.getElementById('vcv-ready').value = '1'
-      document.getElementById('vcv-action').value = 'setData:adminNonce'
+      document.getElementById('vcv-action').value = `set${action}Data:adminNonce`
       document.getElementById('vcv-data').value = encodeURIComponent(JSON.stringify(data))
       document.getElementById('vcv-global-elements-css').value = globalStyles
       document.getElementById('vcv-elements-css-data').value = encodeURIComponent(JSON.stringify(elementsCss))
@@ -92,107 +92,8 @@ export default class SaveController {
       status.set({
         status: 'success'
       })
-      // } else {
-      //   if (typeof callback === 'function') {
-      //     callback('failed')
-      //   }
-      //   status.set({
-      //     status: 'failed'
-      //   })
-      // }
     })
   }
-  save (data, status, callback) {
-    if (vcCake.env('FEATURE_NO_GLOBAL_CSS') === true) {
-      return this._saveByElements(data, status, callback)
-    }
-    const iframe = document.getElementById('vcv-editor-iframe')
-    const contentLayout = iframe ? iframe.contentWindow.document.querySelector('[data-vcv-module="content-layout"]') : false
-    let content = contentLayout ? utils.normalizeHtml(contentLayout.innerHTML) : ''
-    let globalStyles = ''
-    let pageStyles = ''
-    let promises = []
-    const globalAssetsStorageInstance = modernAssetsStorage.getGlobalInstance()
-    let globalElements = globalAssetsStorageInstance.getElements()
-    let globalStylesManager = stylesManager.create()
-    globalStylesManager.add(globalAssetsStorageInstance.getSiteCssData())
-    promises.push(globalStylesManager.compile().then((result) => {
-      globalStyles = result
-    }))
-    let localStylesManager = stylesManager.create()
-    localStylesManager.add(globalAssetsStorageInstance.getPageCssData())
-    promises.push(localStylesManager.compile().then((result) => {
-      pageStyles = result
-    }))
-    let assetsFiles = {
-      jsBundles: [],
-      cssBundles: []
-    }
-    Object.keys(data.elements).forEach((key) => {
-      let cookElement = cook.get(data.elements[key])
-      let elementAssetsFiles = elementAssetsLibrary.getAssetsFilesByElement(cookElement)
-      assetsFiles.cssBundles = assetsFiles.cssBundles.concat(elementAssetsFiles.cssBundles)
-      assetsFiles.jsBundles = assetsFiles.jsBundles.concat(elementAssetsFiles.jsBundles)
-    })
-    assetsFiles.cssBundles = [ ...new Set(assetsFiles.cssBundles) ]
-    assetsFiles.jsBundles = [ ...new Set(assetsFiles.jsBundles) ]
-    Promise.all(promises).then(() => {
-      // if (iframe && iframe.contentWindow && iframe.contentWindow.document.querySelector('[data-vcv-module="content-layout"]')) {
-      if (window.switchEditors && window.tinymce) {
-        window.switchEditors.go('content', 'html')
-      }
-      document.getElementById('content').value = content
-      document.getElementById('vcv-ready').value = '1'
-      document.getElementById('vcv-action').value = 'setData:adminNonce'
-      document.getElementById('vcv-data').value = encodeURIComponent(JSON.stringify(data))
-      document.getElementById('vcv-global-elements-css').value = globalStyles
-      document.getElementById('vcv-global-elements').value = encodeURIComponent(JSON.stringify(globalElements))
-      document.getElementById('vcv-source-css').value = pageStyles
-      document.getElementById('vcv-source-assets-files').value = encodeURIComponent(JSON.stringify(assetsFiles))
-      document.getElementById('vcv-settings-source-custom-css').value = settingsStorage.state('customCss').get() || ''
-      document.getElementById('vcv-settings-global-css').value = settingsStorage.state('globalCss').get() || ''
-      if (typeof callback === 'function') {
-        callback('success')
-      }
-      status.set({
-        status: 'success'
-      })
-      // } else {
-      //   if (typeof callback === 'function') {
-      //     callback('failed')
-      //   }
-      //   status.set({
-      //     status: 'failed'
-      //   })
-      // }
-    })
-  }
-
-  // saveSuccess (status, responseText) {
-  //   let data = JSON.parse(responseText || '{}')
-  //   if (data && data.postData) {
-  //     window.vcvPostData = data.postData
-  //   }
-  //   status.set({
-  //     status: 'success',
-  //     request: responseText
-  //   })
-  //   // this.props.api.request('wordpress:data:saved', {
-  //   //   status: 'success',
-  //   //   request: responseText
-  //   // })
-  // }
-  //
-  // saveFailed (status, request) {
-  //   status.set({
-  //     status: 'failed',
-  //     request: request
-  //   })
-  //   // this.props.api.request('wordpress:data:saved', {
-  //   //   status: 'failed',
-  //   //   request: request
-  //   // })
-  // }
 
   load = (data, status) => {
     this.ajax(
