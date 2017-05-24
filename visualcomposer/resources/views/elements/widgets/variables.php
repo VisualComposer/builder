dded9e1a1eae3b0cdcc4db824d904504875dea63
Pavel Ivanov <reactoranime@gmail.com>
2017-05-24 18:07:08 +0300

    Undisable widgets variables #346805432846834

--- visualcomposer/resources/views/elements/widgets/variables.php
+++ visualcomposer/resources/views/elements/widgets/variables.php
@@ -4,7 +4,7 @@ if (!defined('ABSPATH')) {
     header('HTTP/1.1 403 Forbidden');
     exit;
 }
-exit; // Disabled yet
+
 $wpWidgets = vchelper('WpWidgets');
 $groups = $wpWidgets->allGrouped();
 if (!empty($groups['wpWidgetsCustom'])) {
