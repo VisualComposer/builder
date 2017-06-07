8169b47060873ae809b0e7da8c695d3c4dbb77f8
Janis <mcjohn@inbox.lv>
2017-06-07 08:21:20 +0300

    Revert env.php file #355356909495394

--- env.php
+++ env.php
@@ -1,11 +1,9 @@
 <?php
 
-define('VCV_ENV_DEV_ELEMENTS', true);
-define('VCV_ENV_DEV_CATEGORIES', true);
-define('VCV_ENV_HUB_DOWNLOAD', false);
-define('VCV_ENV_TEMPLATES_DOWNLOAD', false);
-define('VCV_ENV_ELEMENT_DOWNLOAD', false);
+define('VCV_ENV_HUB_DOWNLOAD', true);
+define('VCV_ENV_TEMPLATES_DOWNLOAD', true);
+define('VCV_ENV_ELEMENT_DOWNLOAD', true);
 define('VCV_ENV_ELEMENT_DOWNLOAD_V', 1);
-define('VCV_ENV_EXTENSION_DOWNLOAD', false);
+define('VCV_ENV_EXTENSION_DOWNLOAD', true);
 define('VCV_ACCOUNT_URL', 'http://account.visualcomposer.io');
 define('VCV_HUB_URL', 'http://account.visualcomposer.io');
