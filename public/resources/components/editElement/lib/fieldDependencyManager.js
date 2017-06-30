2291f843fe5432d15bcb1c56a3bd869b163a1106
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2017-06-30 18:12:45 +0300

    Disable current dependency logic execution for accordion #342621641012416

--- public/resources/components/editElement/lib/fieldDependencyManager.js
+++ public/resources/components/editElement/lib/fieldDependencyManager.js
@@ -39,7 +39,7 @@ export default class FieldDependencyManager extends React.Component {
   }
 
   componentDidUpdate (prevProps, prevState) {
-    this.updateAccordionSectionDependency(prevState)
+    // this.updateAccordionSectionDependency(prevState)
   }
 
   /**
@@ -47,6 +47,7 @@ export default class FieldDependencyManager extends React.Component {
    * @param prevState
    */
   updateAccordionSectionDependency (prevState) {
+    console.log('prevState, this.state: ', prevState.dependenciesClasses, this.state.dependenciesClasses)
     if (this.state.dependenciesClasses.length &&
       this.state.dependenciesClasses.length !== prevState.dependenciesClasses.length &&
       this.state.dependenciesClasses.length === 1
