0f880c56eb13de01e40747afd29f0d05ce1abb65
Pavel Ivanov <reactoranime@gmail.com>
2017-07-06 16:07:30 +0300

    add woocommerce elements to cloneScript #194452744474984

---
+++ tools/devElements/cloneScriptWC.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+echo "My script is running."
+
+declare -a arr=(
+'row'
+'column'
+'textBlock'
+'singleImage'
+'basicButton'
+'featureSection'
+'flickrImage'
+'googleFontsHeading'
+'googleMaps'
+'googlePlusButton'
+'heroSection'
+'icon'
+'imageGallery'
+'imageMasonryGallery'
+'instagramImage'
+'outlineButton'
+'pinterestPinit'
+'rawHtml'
+'facebookLike'
+'feature'
+'rawJs'
+'separator'
+'shortcode'
+'twitterButton'
+'twitterGrid'
+'twitterTimeline'
+'twitterTweet'
+'vimeoPlayer'
+'wpWidgetsCustom'
+'wpWidgetsDefault'
+'youtubePlayer'
+'faqToggle'
+'woocommerceTopRatedProducts'
+'woocommerceSaleProducts'
+'woocommerceRelatedProducts'
+'woocommerceRecentProducts'
+'woocommerceProducts'
+'woocommerceProductPage'
+'woocommerceProductCategory'
+'woocommerceProductCategories'
+'woocommerceProductAttribute'
+'woocommerceProduct'
+'woocommerceOrderTracking'
+'woocommerceMyAccount'
+'woocommerceFeaturedProducts'
+'woocommerceCheckout'
+'woocommerceCart'
+'woocommerceBestSellingProducts'
+'woocommerceAddToCart'
+)
+
+EXECDIR=`pwd`
+
+for i in "${arr[@]}"
+do
+   if cd $EXECDIR/devElements/$i; then cd $EXECDIR/devElements/$i && git pull; else git clone git@gitlab.com:visualcomposer-hub/$i.git $EXECDIR/devElements/$i; fi
+done
+
+echo "Done!"
