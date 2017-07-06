ec0f4bc01c1ec511883b2855c07f41ad44f81baa
Pavel Ivanov <reactoranime@gmail.com>
2017-07-06 17:19:27 +0300

    Create WooCommerce element controller #194452744474984

---
+++ visualcomposer/Modules/Elements/WooCommerce/Elements.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace VisualComposer\Modules\Elements\WooCommerce;
+
+use VisualComposer\Framework\Illuminate\Support\Module;
+use VisualComposer\Framework\Container;
+use VisualComposer\Helpers\Access\CurrentUser;
+use VisualComposer\Helpers\PostType;
+use VisualComposer\Helpers\Request;
+use VisualComposer\Helpers\Str;
+use VisualComposer\Helpers\Traits\EventsFilters;
+
+class Elements extends Container implements Module
+{
+    use EventsFilters;
+
+    /**
+     * Controller constructor.
+     */
+    public function __construct()
+    {
+        /** @see ShortcodesFactory::renderEditor */
+        $this->addFilter(
+            'vcv:ajax:elements:woocommerce:*:adminNonce',
+            'renderEditor'
+        );
+    }
+
+    protected function renderEditor(
+        $response,
+        Request $request,
+        PostType $postTypeHelper,
+        CurrentUser $currentUserAccessHelper
+    ) {
+        $sourceId = (int)$request->input('vcv-source-id');
+        if ($sourceId && $currentUserAccessHelper->wpAll(['edit_posts', $sourceId])->get()) {
+            if (!is_array($response)) {
+                $response = [];
+            }
+
+            $postTypeHelper->setupPost($sourceId);
+
+            /** @see  \VisualComposer\Modules\Elements\Traits\ShortcodesTrait::renderEditorContent */
+            $response['shortcodeContent'] = $this->call('renderEditorContent');
+            /** @see \VisualComposer\Modules\Elements\Traits\ShortcodesTrait::renderEditorShortcode */
+            $response['shortcode'] = $this->call('renderEditorShortcode');
+            $response['status'] = true;
+        }
+
+        return $response;
+    }
+
+    /**
+     * @return string
+     */
+    protected function renderEditorContent()
+    {
+        ob_start();
+        /** @see \VisualComposer\Modules\Elements\Traits\ShortcodesTrait::renderEditorShortcode */
+        $shortcodeString = $this->call('renderEditorShortcode');
+        do_action('wp_loaded'); // Fix for WooCommerce
+        echo apply_filters(
+            'the_content',
+            $shortcodeString
+        );
+        wp_print_styles();
+        print_late_styles();
+        wp_print_head_scripts();
+        wp_print_footer_scripts();
+        $content = ob_get_clean();
+
+        return $content;
+    }
+
+    /**
+     * @param \VisualComposer\Helpers\Request $request
+     *
+     * @return mixed
+     */
+    protected function renderEditorShortcode(Request $request)
+    {
+        $atts = $request->input('vcv-atts');
+        $content = $request->input('vcv-content');
+        $tag = $request->input('vcv-tag');
+
+        /** @see \VisualComposer\Modules\Elements\Traits\ShortcodesTrait::getShortcodeString */
+        return $this->call(
+            'getShortcodeString',
+            [
+                'atts' => $atts,
+                'content' => $content,
+                'tag' => $tag,
+            ]
+        );
+    }
+
+    /**
+     * @param \VisualComposer\Helpers\Str $strHelper
+     * @param $atts
+     * @param string $content
+     *
+     * @return string
+     */
+    protected function getShortcodeString(Str $strHelper, $atts, $content = '', $tag)
+    {
+        $shortcodeString = sprintf(
+            '[%s %s]%s[/%s]',
+            $tag,
+            $strHelper->buildQueryString($atts),
+            rawurlencode(base64_encode($content)),
+            $tag
+        );
+
+        return $shortcodeString;
+    }
+}
