70ffffa628b3066a4763a5ae8a01ca97060ada4c
Pavel Ivanov <reactoranime@gmail.com>
2017-07-10 16:45:40 +0300

    Fix codestyle #380642303478512

--- visualcomposer/Requirements.php
+++ visualcomposer/Requirements.php
@@ -25,15 +25,21 @@ class VcvCoreRequirements
 
         if (!self::checkVersion(VCV_REQUIRED_PHP_VERSION, PHP_VERSION)) {
             $die = true;
-            $message .= '<li>' . sprintf(
-                'PHP version %s or greater (recommended 7 or greater)',
-                VCV_REQUIRED_PHP_VERSION) . '</li>';
+            $message .= '<li>' .
+                sprintf(
+                    'PHP version %s or greater (recommended 7 or greater)',
+                    VCV_REQUIRED_PHP_VERSION
+                ) .
+                '</li>';
         }
         if (!self::checkVersion(VCV_REQUIRED_BLOG_VERSION, get_bloginfo('version'))) {
             $die = true;
-            $message .= '<li>' . sprintf(
-                'WordPress version %s or greater',
-                VCV_REQUIRED_BLOG_VERSION) . '</li>';
+            $message .= '<li>' .
+                sprintf(
+                    'WordPress version %s or greater',
+                    VCV_REQUIRED_BLOG_VERSION
+                ) .
+                '</li>';
         }
         if ($die === true) {
             wp_die(
