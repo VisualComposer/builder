2d326305ff5a25a2c98653ef9b5c776c7a411cc5
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2017-07-19 16:08:55 +0300

    Add dependencies logic for accordion sections #141889735080262

--- public/resources/components/editElement/lib/fieldDependencyManager.js
+++ public/resources/components/editElement/lib/fieldDependencyManager.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import classNames from 'classnames'
 import Field from './field'
-import vcCake from 'vc-cake'
+// import vcCake from 'vc-cake'
 
 export default class FieldDependencyManager extends React.Component {
   static propTypes = {
     fieldKey: React.PropTypes.string.isRequired,
     updater: React.PropTypes.func.isRequired,
     setFieldMount: React.PropTypes.func.isRequired,
-    setFieldUnmount: React.PropTypes.func.isRequired,
-    updateDependencies: vcCake.env('EDIT_FORM_ACCORDION') ? React.PropTypes.func.isRequired : React.PropTypes.func
+    setFieldUnmount: React.PropTypes.func.isRequired
+    // updateDependencies: vcCake.env('EDIT_FORM_ACCORDION') ? React.PropTypes.func.isRequired : React.PropTypes.func
   }
 
   constructor (props) {
@@ -39,25 +39,25 @@ export default class FieldDependencyManager extends React.Component {
     })
   }
 
-  componentDidUpdate (prevProps, prevState) {
-    if (vcCake.env('EDIT_FORM_ACCORDION')) {
-      this.updateAccordionSectionDependency(prevState)
-    }
-  }
+  // componentDidUpdate (prevProps, prevState) {
+  //   if (vcCake.env('EDIT_FORM_ACCORDION')) {
+  //     this.updateAccordionSectionDependency(prevState)
+  //   }
+  // }
 
   /**
    * Check if dependency state differs from previous and update accordion section state
    * @param prevState
    */
-  updateAccordionSectionDependency (prevState) {
-    if (this.props.fieldKey === this.props.tab.fieldKey &&
-      this.state.dependenciesClasses.length &&
-      this.state.dependenciesClasses.length !== prevState.dependenciesClasses.length &&
-      this.state.dependenciesClasses.length === 1
-    ) {
-      this.props.updateDependencies(this.state.dependenciesClasses)
-    }
-  }
+  // updateAccordionSectionDependency (prevState) {
+  //   if (this.props.fieldKey === this.props.tab.fieldKey &&
+  //     this.state.dependenciesClasses.length &&
+  //     this.state.dependenciesClasses.length !== prevState.dependenciesClasses.length &&
+  //     this.state.dependenciesClasses.length === 1
+  //   ) {
+      // this.props.updateDependencies(this.state.dependenciesClasses)
+  //   }
+  // }
 
   render () {
     let classes = classNames({
