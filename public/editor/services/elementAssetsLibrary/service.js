f8f5ce680826fb6b01d730653f7957335e31b7be
Janis <mcjohn@inbox.lv>
2017-07-20 15:02:58 +0300

    Revert changes #389556824209027

--- public/editor/services/elementAssetsLibrary/service.js
+++ public/editor/services/elementAssetsLibrary/service.js
@@ -45,22 +45,21 @@ const innerApi = {
       jsBundles: []
     }
 
-    // const cook = vcCake.getService('cook')
+    const cook = vcCake.getService('cook')
     cookElement.filter((key, value, settings) => {
       if (settings.type === 'element') {
         // Element Public JS
-        let elementPublicAssetsFiles = publicApi.getAssetsFilesByTags([ value.tag ])
-        // let cookElement = cook.get(value)
-        // if (!cookElement) {
-        //   return
-        // }
-        // let elementPublicAssetsFiles = publicApi.getAssetsFilesByElement(cookElement)
+        let cookElement = cook.get(value)
+        if (!cookElement) {
+          return
+        }
+        let elementPublicAssetsFiles = publicApi.getAssetsFilesByElement(cookElement)
         files.cssBundles = files.cssBundles.concat(elementPublicAssetsFiles.cssBundles)
         files.jsBundles = files.jsBundles.concat(elementPublicAssetsFiles.jsBundles)
       }
     })
-    // files.cssBundles = [ ...new Set(files.cssBundles) ]
-    // files.jsBundles = [ ...new Set(files.jsBundles) ]
+    files.cssBundles = [ ...new Set(files.cssBundles) ]
+    files.jsBundles = [ ...new Set(files.jsBundles) ]
 
     return files
   }
