67b964671d1ff9990fd585f8439b16ab3b01a232
Pavel Ivanov <reactoranime@gmail.com>
2017-07-31 12:08:32 +0300

    Enchance vcwb #391186749757042

--- env.php
+++ env.php
@@ -1,10 +1,58 @@
 <?php
 
-define('VCV_ENV_HUB_DOWNLOAD', true);
-define('VCV_ENV_TEMPLATES_DOWNLOAD', true);
-define('VCV_ENV_ELEMENT_DOWNLOAD', true);
-define('VCV_ENV_ELEMENT_DOWNLOAD_V', 1);
-define('VCV_ENV_EXTENSION_DOWNLOAD', true);
-define('VCV_ACCOUNT_URL', 'http://account.visualcomposer.io');
-define('VCV_TOKEN_URL', 'http://account.visualcomposer.io/api/getToken');
-define('VCV_HUB_URL', 'http://account.visualcomposer.io');
+if (!defined('VCV_ENV_ADDONS_ID')) {
+    define(
+        'VCV_ENV_ADDONS_ID',
+        isset($_SERVER['ENV_VCV_ENV_ADDONS_ID']) ? $_SERVER['ENV_VCV_ENV_ADDONS_ID'] : 'account'
+    );
+}
+if (!defined('VCV_ENV_HUB_DOWNLOAD')) {
+    define(
+        'VCV_ENV_HUB_DOWNLOAD',
+        isset($_SERVER['ENV_VCV_ENV_HUB_DOWNLOAD']) ? $_SERVER['ENV_VCV_ENV_HUB_DOWNLOAD'] : true
+    );
+}
+if (!defined('VCV_ENV_TEMPLATES_DOWNLOAD')) {
+    define(
+        'VCV_ENV_TEMPLATES_DOWNLOAD',
+        isset($_SERVER['ENV_VCV_ENV_TEMPLATES_DOWNLOAD']) ? $_SERVER['ENV_VCV_ENV_TEMPLATES_DOWNLOAD'] : true
+    );
+}
+if (!defined('VCV_ENV_ELEMENT_DOWNLOAD')) {
+    define(
+        'VCV_ENV_ELEMENT_DOWNLOAD',
+        isset($_SERVER['ENV_VCV_ENV_ELEMENT_DOWNLOAD']) ? $_SERVER['ENV_VCV_ENV_ELEMENT_DOWNLOAD'] : true
+    );
+}
+if (!defined('VCV_ENV_ELEMENT_DOWNLOAD_V')) {
+    define(
+        'VCV_ENV_ELEMENT_DOWNLOAD_V',
+        isset($_SERVER['ENV_VCV_ENV_ELEMENT_DOWNLOAD_V']) ? $_SERVER['ENV_VCV_ENV_ELEMENT_DOWNLOAD_V'] : '2'
+    );
+}
+if (!defined('VCV_ENV_EXTENSION_DOWNLOAD')) {
+    define(
+        'VCV_ENV_EXTENSION_DOWNLOAD',
+        isset($_SERVER['ENV_VCV_ENV_EXTENSION_DOWNLOAD']) ? $_SERVER['ENV_VCV_ENV_EXTENSION_DOWNLOAD'] : true
+    );
+}
+if (!defined('VCV_ACCOUNT_URL')) {
+    define(
+        'VCV_ACCOUNT_URL',
+        isset($_SERVER['ENV_VCV_ACCOUNT_URL']) ? $_SERVER['ENV_VCV_ACCOUNT_URL']
+            : 'http://account.visualcomposer.io'
+    );
+}
+if (!defined('VCV_HUB_URL')) {
+    define(
+        'VCV_HUB_URL',
+        isset($_SERVER['ENV_VCV_HUB_URL']) ? $_SERVER['ENV_VCV_HUB_URL'] : 'http://account.visualcomposer.io'
+    );
+}
+if (!defined('VCV_TOKEN_URL')) {
+    define(
+        'VCV_TOKEN_URL',
+        isset($_SERVER['ENV_VCV_TOKEN_URL']) ? $_SERVER['ENV_VCV_TOKEN_URL']
+            : 'http://account.visualcomposer.io/api/getToken'
+    );
+}
