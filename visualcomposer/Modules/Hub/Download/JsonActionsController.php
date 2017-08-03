ab36c5138c5314f9e405e5737ca07ae91d1587cc
Pavel Ivanov <reactoranime@gmail.com>
2017-08-03 10:44:52 +0300

    Add version checks for each action #391186749757048

--- visualcomposer/Modules/Hub/Download/JsonActionsController.php
+++ visualcomposer/Modules/Hub/Download/JsonActionsController.php
@@ -24,18 +24,30 @@ class JsonActionsController extends Container implements Module
     protected function processJson($status, $payload)
     {
         if ($status && $payload['json'] && !empty($payload['json']['actions'])) {
+            $optionHelper = vchelper('Options');
             foreach ($payload['json']['actions'] as $key => $value) {
                 if (isset($value['action'])) {
                     $action = $value['action'];
                     $data = $value['data'];
-                    $status = vcfilter(
-                        'vcv:hub:process:json:' . $action,
-                        $status,
-                        [
-                            'action' => $action,
-                            'data' => $data,
-                        ]
-                    );
+                    $version = $value['version'];
+                    $previousVersion = $optionHelper->get('hubAction' . $action, '0');
+                    if ($version && version_compare($version, $previousVersion, '<') || !$version) {
+                        $actionResult = vcfilter(
+                            'vcv:hub:process:json:' . $action,
+                            $status,
+                            [
+                                'action' => $action,
+                                'data' => $data,
+                                'version' => $version,
+                            ]
+                        );
+                        if ($actionResult !== false) {
+                            $optionHelper->set('hubAction' . $action, $version);
+                        } else {
+                            $status = false;
+                            break; // we should not continue if some part was broken
+                        }
+                    }
                 }
             }
         } else {
