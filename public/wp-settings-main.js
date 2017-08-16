a4154fa5aec22e9f88b789ff6c8d922c4d50d9d8
Pavel I <pavel@wpbakery.com>
2017-08-16 17:20:03 +0300

    Fix codestyle

--- public/wp-settings-main.js
+++ public/wp-settings-main.js
@@ -4,18 +4,18 @@ import './editor/modules/backendSettings/deactivation'
 import './sources/less/wpsettings/init.less'
 
 window.jQuery(() => {
-    let $ = window.jQuery
-    let installationUrl = 'https://visualcomposer.io/article/installation/'
-    let faqUrl = 'https://visualcomposer.io/article/faq/'
-    let descriptionSection = $('#section-description')
-    let showDescription = (e) => {
-        e.preventDefault()
-        e.stopPropagation()
-        setTimeout(() => {
-            descriptionSection.show()
-        }, 100)
-        $('<a>').attr('href', e.currentTarget.href).attr('target', '_blank')[0].click()
-    }
-    $('[name*="Installation"]').attr('href', installationUrl).attr('target', '_blank').click(showDescription)
-    $('[name*="FAQ"]').attr('href', faqUrl).attr('target', '_blank').click(showDescription)
+  let $ = window.jQuery
+  let installationUrl = 'https://visualcomposer.io/article/installation/'
+  let faqUrl = 'https://visualcomposer.io/article/faq/'
+  let descriptionSection = $('#section-description')
+  let showDescription = (e) => {
+    e.preventDefault()
+    e.stopPropagation()
+    setTimeout(() => {
+      descriptionSection.show()
+    }, 100)
+    $('<a>').attr('href', e.currentTarget.href).attr('target', '_blank')[0].click()
+  }
+  $('[name*="Installation"]').attr('href', installationUrl).attr('target', '_blank').click(showDescription)
+  $('[name*="FAQ"]').attr('href', faqUrl).attr('target', '_blank').click(showDescription)
 })
