619c1da5f9bc43dd912b05c0f1f2ab65a4c50a2d
Alvis Alferovs <aui@Alviss-MacBook-Air.local>
2017-09-19 15:55:53 +0300

    Corrected tree view scroll on edit, tree view edit and element rename #413468257940918

--- public/resources/components/editElement/lib/editFormContent.js
+++ public/resources/components/editElement/lib/editFormContent.js
@@ -3,15 +3,27 @@ import EditFormFieldsForm from './editFormFieldsForm'
 import Scrollbar from '../../../scrollbar/scrollbar.js'
 
 export default class EditFormContent extends React.Component {
+
+  scrollbar = null
+
+  constructor (props) {
+    super(props)
+    this.getSectionContentScrollbar = this.getSectionContentScrollbar.bind(this)
+  }
+
+  getSectionContentScrollbar () {
+    return this.scrollbar
+  }
+
   render () {
     return (
       <div className='vcv-ui-tree-content-section'>
-        <Scrollbar>
+        <Scrollbar ref={(scrollbar) => { this.scrollbar = scrollbar }}>
           <div className='vcv-ui-tree-content-section-inner'>
             <div className='vcv-ui-editor-plates-container'>
               <div className='vcv-ui-editor-plates'>
                 <div className='vcv-ui-editor-plate vcv-ui-state--active'>
-                  <EditFormFieldsForm {...this.props} />
+                  <EditFormFieldsForm {...this.props} getSectionContentScrollbar={this.getSectionContentScrollbar} />
                 </div>
               </div>
             </div>
