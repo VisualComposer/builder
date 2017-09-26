b1612846cf38d9993d526d6b56663f40c1657b80
Alvis Alferovs <aui@Alviss-MacBook-Air.local>
2017-09-26 15:44:32 +0300

    Added front end part #434986540179678

--- public/editor/modules/backendSettings/deactivation.js
+++ public/editor/modules/backendSettings/deactivation.js
@@ -1,27 +1,27 @@
-import $ from 'jquery'
-
-$(() => {
-  let $pluginRow = $(`[data-plugin="${window.vcvPluginName}"]`).eq(0)
-  if ($pluginRow.length) {
-    let $deactivationLink = $pluginRow.find('.deactivate a')
-    let noticeShown = false
-    let template = $('#vcv-deactivation-notice-template').html()
-    let showNotice = () => {
-      if (!noticeShown) {
-        noticeShown = true
-        $(template).insertAfter($pluginRow)
+(($) => {
+  $(() => {
+    let $pluginRow = $(`[data-plugin="${window.vcvPluginName}"]`).eq(0)
+    if ($pluginRow.length) {
+      let $deactivationLink = $pluginRow.find('.deactivate a')
+      let noticeShown = false
+      let template = $('#vcv-deactivation-notice-template').html()
+      let showNotice = () => {
+        if (!noticeShown) {
+          noticeShown = true
+          $(template).insertAfter($pluginRow)
+        }
       }
-    }
 
-    $deactivationLink.click((e) => {
-      if (!noticeShown) {
+      $deactivationLink.click((e) => {
+        if (!noticeShown) {
+          e.preventDefault()
+          showNotice()
+        }
+      })
+      $(document).on('click', '.vcv-deactivation-submit-button', (e) => {
         e.preventDefault()
-        showNotice()
-      }
-    })
-    $(document).on('click', '.vcv-deactivation-submit-button', (e) => {
-      e.preventDefault()
-      $deactivationLink.get(0).click()
-    })
-  }
-})
+        $deactivationLink.get(0).click()
+      })
+    }
+  })
+})(window.jQuery)
