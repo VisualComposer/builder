da82633fe4439d0b78b907aafc5a8d3c9305fab0
edw <edgars.v@wpbakery.com>
2017-11-03 11:53:08 +0200

    Fixed codestyle #465588934026182

--- visualcomposer/Modules/Hub/ElementsAutoload.php
+++ visualcomposer/Modules/Hub/ElementsAutoload.php
@@ -123,15 +123,15 @@ class ElementsAutoload extends Autoload implements Module
     protected static function isHelper($implements)
     {
         return count(
-                array_intersect(
-                    (array)$implements,
-                    [
-                        'Helper',
-                        '\VisualComposer\Framework\Illuminate\Support\Helper',
-                        '\\VisualComposer\\Framework\\Illuminate\\Support\\Helper',
-                    ]
-                )
-            ) > 0;
+            array_intersect(
+                (array)$implements,
+                [
+                    'Helper',
+                    '\VisualComposer\Framework\Illuminate\Support\Helper',
+                    '\\VisualComposer\\Framework\\Illuminate\\Support\\Helper',
+                ]
+            )
+        ) > 0;
     }
 
     /**
@@ -142,15 +142,15 @@ class ElementsAutoload extends Autoload implements Module
     protected static function isImmutable($implements)
     {
         return count(
-                array_intersect(
-                    (array)$implements,
-                    [
-                        'Immutable',
-                        '\VisualComposer\Framework\Illuminate\Support\Immutable',
-                        '\\VisualComposer\\Framework\\Illuminate\\Support\\Immutable',
-                    ]
-                )
-            ) > 0;
+            array_intersect(
+                (array)$implements,
+                [
+                    'Immutable',
+                    '\VisualComposer\Framework\Illuminate\Support\Immutable',
+                    '\\VisualComposer\\Framework\\Illuminate\\Support\\Immutable',
+                ]
+            )
+        ) > 0;
     }
 
     /**
