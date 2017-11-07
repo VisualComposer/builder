61781afa6ddce1527ef3871fc9b4d273e49f36bc
Emils Plavenieks <emils@plavenieks.lv>
2017-11-07 07:50:02 +0200

    Add key prop for node #470123975519765

--- public/sources/attributes/iconpicker/Component.js
+++ public/sources/attributes/iconpicker/Component.js
@@ -124,7 +124,7 @@ class Iconpicker extends Attribute {
     let categories = this.categoriesContent()
     let iconsContent = this.iconsContent()
     if (!iconsContent.length) {
-      iconsContent.push(<div className='vcv-ui-form-iconpicker-error'>No icons found</div>)
+      iconsContent.push(<div className='vcv-ui-form-iconpicker-error' key='no-icon-found'>No icons found</div>)
     }
 
     let popupClasses = classNames({
