9d4e6882a2b71b1470c382d395bb371af982d056
Pavel I <pavel@wpbakery.com>
2017-12-07 16:32:17 +0200

    Fix wpml for 404 page #489336306298047

--- visualcomposer/Modules/Vendors/WpmlController.php
+++ visualcomposer/Modules/Vendors/WpmlController.php
@@ -4,6 +4,7 @@ namespace VisualComposer\Modules\Vendors;
 
 use VisualComposer\Framework\Container;
 use VisualComposer\Framework\Illuminate\Support\Module;
+use VisualComposer\Helpers\Request;
 use VisualComposer\Helpers\Traits\EventsFilters;
 use VisualComposer\Helpers\Traits\WpFiltersActions;
 
@@ -22,6 +23,11 @@ class WpmlController extends Container implements Module
         if (defined('ICL_SITEPRESS_VERSION')) {
             $this->addFilter('vcv:frontend:pageEditable:url', 'addLangToLink');
             $this->addFilter('vcv:frontend:url', 'addLangToLink');
+            $this->addFilter('vcv:ajax:setData:adminNonce', 'setDataTrid', -1);
+            $this->wpAddAction(
+                'save_post',
+                'insertTrid'
+            );
         }
     }
 
@@ -33,5 +39,30 @@ class WpmlController extends Container implements Module
         }
 
         return $url;
+    }
+
+    protected function insertTrid($id, $post, Request $requestHelper)
+    {
+        $trid = $requestHelper->input('trid');
+        if ($trid) {
+            update_post_meta($id, '_' . VCV_PREFIX . 'wpmlTrid', $trid);
+        }
+    }
+
+    protected function setDataTrid($response, $payload)
+    {
+        $this->wpAddFilter('wpml_save_post_trid_value', 'checkTrid');
+
+        return $response;
+    }
+
+    protected function checkTrid($trid, $payload, Request $requestHelper)
+    {
+        if (empty($trid)) {
+            $sourceId = $requestHelper->input('vcv-source-id');
+            $trid = get_post_meta($sourceId, '_' . VCV_PREFIX . 'wpmlTrid', true);
+        }
+
+        return $trid;
     }
 }
