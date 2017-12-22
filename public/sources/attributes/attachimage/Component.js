3593b1d81a6db7c8107dc4f742c4497deffbd525
Alvis Alferovs <aui@Alviss-MacBook-Air.local>
2017-12-22 11:28:19 +0200

    Patched issue with image preview after hiding element #506676428161243

--- public/sources/attributes/attachimage/Component.js
+++ public/sources/attributes/attachimage/Component.js
@@ -65,6 +65,13 @@ export default class AttachImage extends Attribute {
       } else {
         value = { ids: [], urls: [] }
       }
+    } else {
+      if (!value.ids && !value.urls) {
+        value = {
+          ids: [ value.id ],
+          urls: [ value ]
+        }
+      }
     }
 
     return {
