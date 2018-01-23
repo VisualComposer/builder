2e37bfca7293d85a2fe083e69a12825cf926e78e
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2018-01-23 15:56:46 +0200

    Add codemirror editor to components #472725964083287

--- public/resources/components/settings/lib/customJavascript/editor.js
+++ public/resources/components/settings/lib/customJavascript/editor.js
@@ -6,7 +6,31 @@ import 'brace'
 import AceEditor from 'react-ace'
 import 'brace/mode/javascript'
 import 'brace/theme/github'
+
+import { env } from 'vc-cake'
+import CodeMirror from 'codemirror'
+import jshint from 'jshint'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/addon/fold/foldgutter.css'
+import 'codemirror/addon/fold/foldgutter'
+import 'codemirror/addon/fold/brace-fold'
+import 'codemirror/addon/comment/comment'
+import 'codemirror/addon/comment/continuecomment'
+import 'codemirror/addon/hint/show-hint.css'
+import 'codemirror/addon/hint/show-hint'
+import 'codemirror/addon/hint/javascript-hint'
+import 'codemirror/addon/selection/active-line'
+import 'codemirror/addon/edit/closebrackets'
+import 'codemirror/addon/edit/matchbrackets'
+import 'codemirror/addon/lint/lint.css'
+import 'codemirror/addon/lint/lint'
+import 'codemirror/addon/lint/javascript-lint'
+
 export default class ScriptEditor extends React.Component {
+  editor = null
+  codemirror = null
+
   static propTypes = {
     index: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
@@ -16,18 +40,69 @@ export default class ScriptEditor extends React.Component {
     value: PropTypes.string,
     updater: PropTypes.func
   }
+
   constructor (props) {
     super(props)
     this.handleChange = this.handleChange.bind(this)
   }
+
+  componentDidMount () {
+    if (env('CODEMIRROR')) {
+      window.JSHINT = jshint.JSHINT
+      /* eslint-disable */
+      this.codemirror = CodeMirror(this.editor, {
+        value: this.props.value,
+        mode: 'javascript',
+        tabSize: 2,
+        lineNumbers: true,
+        extraKeys: {
+          'Ctrl-Space': 'autocomplete',
+          'Ctrl-\/': 'toggleComment',
+          'Cmd-\/': 'toggleComment'
+        },
+        foldGutter: true,
+        gutters: [
+          'CodeMirror-linenumbers',
+          'CodeMirror-foldgutter',
+          'CodeMirror-lint-markers'
+        ],
+        nonEmpty: true,
+        scrollbarStyle: 'overlay',
+        styleActiveLine: true,
+        continueComments: true,
+        autoCloseBrackets: true,
+        matchBrackets: true,
+        lint: true,
+        lintOnChange: true
+      })
+      /* eslint-enable */
+      this.codemirror.setSize('100%', '50vh')
+      this.codemirror.on('change', this.handleChange)
+    }
+  }
+
+  componentDidUpdate (prevProps, prevState) {
+    if (env('CODEMIRROR')) {
+      this.codemirror.refresh()
+    }
+  }
+
   handleChange (value) {
+    value = env('CODEMIRROR') ? value.getValue() : value
     this.props.updater(this.props.name, value)
   }
+
   render () {
     let controlClass = classNames({
       'vcv-ui-script-editor': true,
       'vcv-ui-state--active': (this.props.index === this.props.activeIndex)
     })
+    if (env('CODEMIRROR')) {
+      return <div className={controlClass}>
+        <div className='vcv-ui-script-ace-container' ref={editor => (this.editor = editor)} />
+        <p className='vcv-ui-form-helper'>{this.props.editorLabel}</p>
+      </div>
+    }
     return (
       <div className={controlClass}>
         <div className='vcv-ui-script-ace-container'>
@@ -38,7 +113,7 @@ export default class ScriptEditor extends React.Component {
             theme='github'
             tabSize={2}
             name={this.props.aceId}
-            editorProps={{$blockScrolling: true}}
+            editorProps={{ $blockScrolling: true }}
             showPrintMargin={false}
             value={this.props.value}
             onChange={this.handleChange}
--- public/sources/attributes/rawCode/Component.js
+++ public/sources/attributes/rawCode/Component.js
@@ -3,11 +3,39 @@ import Attribute from '../attribute'
 import PropTypes from 'prop-types'
 
 import AceEditor from 'react-ace'
-import '../../../../node_modules/brace/mode/html'
-import '../../../../node_modules/brace/mode/javascript'
-import '../../../../node_modules/brace/theme/github'
+import 'brace/mode/html'
+import 'brace/mode/javascript'
+import 'brace/theme/github'
+
+import { env } from 'vc-cake'
+import CodeMirror from 'codemirror'
+import jshint from 'jshint'
+import htmlhint from 'htmlhint'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/htmlmixed/htmlmixed'
+import 'codemirror/addon/fold/foldgutter.css'
+import 'codemirror/addon/fold/foldgutter'
+import 'codemirror/addon/fold/brace-fold'
+import 'codemirror/addon/fold/markdown-fold'
+import 'codemirror/addon/fold/xml-fold'
+import 'codemirror/addon/fold/comment-fold'
+import 'codemirror/addon/comment/comment'
+import 'codemirror/addon/comment/continuecomment'
+import 'codemirror/addon/hint/show-hint.css'
+import 'codemirror/addon/hint/show-hint'
+import 'codemirror/addon/hint/javascript-hint'
+import 'codemirror/addon/hint/html-hint'
+import 'codemirror/addon/selection/active-line'
+import 'codemirror/addon/edit/closebrackets'
+import 'codemirror/addon/edit/matchbrackets'
+import 'codemirror/addon/lint/lint.css'
+import 'codemirror/addon/lint/lint'
+import 'codemirror/addon/lint/javascript-lint'
+import 'codemirror/addon/lint/html-lint'
 
 export default class RawCode extends Attribute {
+  editor = null
   static propTypes = {
     fieldKey: PropTypes.string.isRequired,
     value: PropTypes.string.isRequired,
@@ -23,14 +51,72 @@ export default class RawCode extends Attribute {
     editorProps: { $blockScrolling: true }
   }
 
-  componentDidUpdate () {
-    // Make the resize in case if dependency hidden
-    if (this.refs.ace && this.refs.ace.editor) {
-      this.refs.ace.editor.resize()
+  constructor (props) {
+    super(props)
+    this.setValue = this.setValue.bind(this)
+  }
+
+  componentDidMount () {
+    if (env('CODEMIRROR')) {
+      let mode
+      if (this.props.options.mode === 'html') {
+        window.HTMLHint = htmlhint.HTMLHint
+        mode = 'htmlmixed'
+      } else if (this.props.options.mode === 'javascript') {
+        window.JSHINT = jshint.JSHINT
+        mode = this.props.options.mode
+      }
+      /* eslint-disable */
+      this.codemirror = CodeMirror(this.editor, {
+        value: this.props.value,
+        mode: mode,
+        tabSize: 2,
+        lineNumbers: true,
+        extraKeys: {
+          'Ctrl-Space': 'autocomplete',
+          'Ctrl-\/': 'toggleComment',
+          'Cmd-\/': 'toggleComment'
+        },
+        foldGutter: true,
+        gutters: [
+          'CodeMirror-linenumbers',
+          'CodeMirror-foldgutter',
+          'CodeMirror-lint-markers'
+        ],
+        nonEmpty: true,
+        scrollbarStyle: 'overlay',
+        styleActiveLine: true,
+        continueComments: true,
+        autoCloseBrackets: true,
+        matchBrackets: true,
+        lint: true,
+        lintOnChange: true
+      })
+      /* eslint-enable */
+      this.codemirror.setSize('100%', '50vh')
+      this.codemirror.on('change', this.setValue)
     }
   }
 
+  componentDidUpdate () {
+    if (env('CODEMIRROR')) {
+      this.codemirror.refresh()
+    } else {
+      // Make the resize in case if dependency hidden
+      if (this.refs.ace && this.refs.ace.editor) {
+        this.refs.ace.editor.resize()
+      }
+    }
+  }
+
+  setValue (value) {
+    this.setFieldValue(value.getValue())
+  }
+
   render () {
+    if (env('CODEMIRROR')) {
+      return <div className='vcv-row-html-editor-container' ref={editor => (this.editor = editor)} />
+    }
     return (
       <div className='vcv-row-html-editor-container'>
         <AceEditor
