fa7ab694d2428b500759f4c639083d557587dc47
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2018-01-25 14:55:25 +0200

    Remove ACE editor imports and occurrences #472725964083287

--- public/resources/components/settings/lib/customJavascript/editor.js
+++ public/resources/components/settings/lib/customJavascript/editor.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import classNames from 'classnames'
 import PropTypes from 'prop-types'
-
-import 'brace'
-import AceEditor from 'react-ace'
-import 'brace/mode/javascript'
-import 'brace/theme/github'
-
-import { env } from 'vc-cake'
 import CodeEditor from '../../../../codeEditor/codeEditor'
 
 export default class ScriptEditor extends React.Component {
@@ -19,7 +12,6 @@ export default class ScriptEditor extends React.Component {
     name: PropTypes.string.isRequired,
     editorLabel: PropTypes.string,
     activeIndex: PropTypes.number,
-    aceId: PropTypes.string,
     value: PropTypes.string,
     updater: PropTypes.func
   }
@@ -30,22 +22,17 @@ export default class ScriptEditor extends React.Component {
   }
 
   componentDidMount () {
-    if (env('CODEMIRROR')) {
-      this.codeEditor = CodeEditor.getEditor(this.editorWrapper, 'javascript', this.props.value)
-      this.codeEditor.setSize('100%', '50vh')
-      this.codeEditor.on('change', this.handleChange)
-    }
+    this.codeEditor = CodeEditor.getEditor(this.editorWrapper, 'javascript', this.props.value)
+    this.codeEditor.setSize('100%', '50vh')
+    this.codeEditor.on('change', this.handleChange)
   }
 
   componentDidUpdate (prevProps, prevState) {
-    if (env('CODEMIRROR')) {
-      this.codeEditor.refresh()
-    }
+    this.codeEditor.refresh()
   }
 
   handleChange (value) {
-    value = env('CODEMIRROR') ? value.getValue() : value
-    this.props.updater(this.props.name, value)
+    this.props.updater(this.props.name, value.getValue())
   }
 
   render () {
@@ -53,30 +40,9 @@ export default class ScriptEditor extends React.Component {
       'vcv-ui-script-editor': true,
       'vcv-ui-state--active': (this.props.index === this.props.activeIndex)
     })
-    if (env('CODEMIRROR')) {
-      return <div className={controlClass}>
-        <div className='vcv-ui-script-ace-container' ref={editor => (this.editorWrapper = editor)} />
-        <p className='vcv-ui-form-helper'>{this.props.editorLabel}</p>
-      </div>
-    }
-    return (
-      <div className={controlClass}>
-        <div className='vcv-ui-script-ace-container'>
-          <AceEditor
-            width='100%'
-            height='50vh'
-            mode='javascript'
-            theme='github'
-            tabSize={2}
-            name={this.props.aceId}
-            editorProps={{ $blockScrolling: true }}
-            showPrintMargin={false}
-            value={this.props.value}
-            onChange={this.handleChange}
-          />
-        </div>
-        <p className='vcv-ui-form-helper'>{this.props.editorLabel}</p>
-      </div>
-    )
+    return <div className={controlClass}>
+      <div className='vcv-ui-script-ace-container' ref={editor => (this.editorWrapper = editor)} />
+      <p className='vcv-ui-form-helper'>{this.props.editorLabel}</p>
+    </div>
   }
 }
--- public/sources/attributes/rawCode/Component.js
+++ public/sources/attributes/rawCode/Component.js
@@ -1,13 +1,6 @@
 import React from 'react'
 import Attribute from '../attribute'
 import PropTypes from 'prop-types'
-
-// import AceEditor from 'react-ace'
-// import 'brace/mode/html'
-// import 'brace/mode/javascript'
-// import 'brace/theme/github'
-
-import { env } from 'vc-cake'
 import CodeEditor from '../../../resources/codeEditor/codeEditor'
 
 export default class RawCode extends Attribute {
@@ -20,44 +13,25 @@ export default class RawCode extends Attribute {
     options: PropTypes.object.isRequired
   }
 
-  static defaultEditorOptions = {
-    mode: 'html',
-    width: '100%',
-    height: '50vh',
-    theme: 'github',
-    tabSize: 2,
-    showPrintMargin: false,
-    editorProps: { $blockScrolling: true }
-  }
-
   constructor (props) {
     super(props)
     this.setValue = this.setValue.bind(this)
   }
 
   componentDidMount () {
-    if (env('CODEMIRROR')) {
-      const { value } = this.props
-      if (!this.props.options.mode || this.props.options.mode === 'html') {
-        // TODO: change rawHTML, wpWidgetsCustom, wpWidgetsDefault element options.mode setting to htmlmixed
-        this.codeEditor = CodeEditor.getEditor(this.editorWrapper, 'htmlmixed', value)
-      } else if (this.props.options.mode === 'javascript') {
-        this.codeEditor = CodeEditor.getEditor(this.editorWrapper, 'javascript', value)
-      }
-      this.codeEditor.setSize('100%', this.props.options.height || '50vh')
-      this.codeEditor.on('change', this.setValue)
+    const { value } = this.props
+    if (!this.props.options.mode || this.props.options.mode === 'html') {
+      // TODO: change rawHTML, wpWidgetsCustom, wpWidgetsDefault element options.mode setting to htmlmixed
+      this.codeEditor = CodeEditor.getEditor(this.editorWrapper, 'htmlmixed', value)
+    } else if (this.props.options.mode === 'javascript') {
+      this.codeEditor = CodeEditor.getEditor(this.editorWrapper, 'javascript', value)
     }
+    this.codeEditor.setSize('100%', this.props.options.height || '50vh')
+    this.codeEditor.on('change', this.setValue)
   }
 
-  componentDidUpdate () {
-    if (env('CODEMIRROR')) {
-      this.codeEditor.refresh()
-    } else {
-      // Make the resize in case if dependency hidden
-      if (this.refs.ace && this.refs.ace.editor) {
-        this.refs.ace.editor.resize()
-      }
-    }
+  componentDidUpdate (prevProps, prevState) {
+    this.codeEditor.refresh()
   }
 
   setValue (value) {
@@ -65,26 +39,7 @@ export default class RawCode extends Attribute {
   }
 
   render () {
-    if (env('CODEMIRROR')) {
-      return <div className='vcv-row-html-editor-container' ref={editor => (this.editorWrapper = editor)} />
-    }
-    return (
-      <div className='vcv-row-html-editor-container'>
-        <AceEditor
-          ref='ace'
-          name={this.props.fieldKey}
-          value={this.state.value}
-          onChange={this.setFieldValue}
-          mode={typeof this.props.options.mode === 'undefined' ? RawCode.defaultEditorOptions.mode : this.props.options.mode}
-          width={typeof this.props.options.width === 'undefined' ? RawCode.defaultEditorOptions.width : this.props.options.width}
-          height={typeof this.props.options.height === 'undefined' ? RawCode.defaultEditorOptions.height : this.props.options.height}
-          theme={typeof this.props.options.theme === 'undefined' ? RawCode.defaultEditorOptions.theme : this.props.options.theme}
-          tabSize={typeof this.props.options.tabSize === 'undefined' ? RawCode.defaultEditorOptions.tabSize : this.props.options.tabSize}
-          showPrintMargin={typeof this.props.options.showPrintMargin === 'undefined' ? RawCode.defaultEditorOptions.showPrintMargin : this.props.options.showPrintMargin}
-          editorProps={typeof this.props.options.editorProps === 'undefined' ? RawCode.defaultEditorOptions.editorProps : this.props.options.editorProps}
-        />
-      </div>
-    )
+    return <div className='vcv-row-html-editor-container' ref={editor => (this.editorWrapper = editor)} />
   }
 }
 
