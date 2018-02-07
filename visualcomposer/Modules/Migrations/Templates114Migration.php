4674ee0148924d3936ae4aa856d3f4ee1f5558fd
Pavel I <pavel@wpbakery.com>
2018-02-08 00:35:36 +0200

    Fix templates downloaded images #552131304583394

---
+++ visualcomposer/Modules/Migrations/Templates114Migration.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace VisualComposer\Modules\Migrations;
+
+if (!defined('ABSPATH')) {
+    header('Status: 403 Forbidden');
+    header('HTTP/1.1 403 Forbidden');
+    exit;
+}
+
+use VisualComposer\Framework\Illuminate\Support\Module;
+
+/**
+ * Class Templates114Migration
+ *
+ * This migration fixes the template downloaded images
+ *
+ * @package VisualComposer\Modules\Migrations
+ */
+class Templates114Migration extends MigrationsController implements Module
+{
+    protected $migrationId = 'templates114Migration';
+
+    protected $migrationPriority = 2;
+
+    protected function run()
+    {
+        global $wpdb;
+        $wpdb->query(
+            $wpdb->prepare(
+                'UPDATE ' . $wpdb->options . ' SET option_value="0.0.1" WHERE option_name LIKE "%s"',
+                VCV_PREFIX . 'hubAction:predefinedTemplate/%'
+            )
+        );
+        $wpdb->query(
+            $wpdb->prepare(
+                'UPDATE ' . $wpdb->options . ' SET option_value="0.0.1" WHERE option_name LIKE "%s"',
+                VCV_PREFIX . 'hubAction:template/%'
+            )
+        );
+    }
+}
