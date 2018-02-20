bfd05ff48556ac63d698aa31d8ef93bce571e7a1
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2018-02-20 13:44:11 +0200

    Add Slider Revolution element to cloneScript and HubCategories #564911816215866

--- visualcomposer/Modules/Development/HubCategories.php
+++ visualcomposer/Modules/Development/HubCategories.php
@@ -308,7 +308,8 @@ class HubCategories extends Container implements Module
                         'wpWidgetsDefault',
                         'widgetizedSidebar',
                         'contactForm7',
-                        'ninjaForms'
+                        'ninjaForms',
+                        'sliderRevolution',
                     ],
                     'icon' => $urlHelper->to('devCategories/categories/icons/WordPress.svg'),
                     'iconDark' => $urlHelper->to('devCategories/categories/iconsDark/WordPress.svg'),
