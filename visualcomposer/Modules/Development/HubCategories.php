ae01c8eb893a82388430fb56d12bf87578f78656
Alvis Alferovs <aui@Alviss-MacBook-Air.local>
2018-02-21 13:47:29 +0200

    Added addtoanysharebtns to hubcategories #565156074916330

--- visualcomposer/Modules/Development/HubCategories.php
+++ visualcomposer/Modules/Development/HubCategories.php
@@ -313,6 +313,7 @@ class HubCategories extends Container implements Module
                         'sliderRevolution',
                         'layerSlider',
                         'essentialGrid',
+                        'addToAnyShareButtons'
                     ],
                     'icon' => $urlHelper->to('devCategories/categories/icons/WordPress.svg'),
                     'iconDark' => $urlHelper->to('devCategories/categories/iconsDark/WordPress.svg'),
