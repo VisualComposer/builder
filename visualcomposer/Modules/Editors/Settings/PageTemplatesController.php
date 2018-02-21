c514c2911f2d9000bd937ba67f8cc3b6510dbe93
Pavel I <pavel@wpbakery.com>
2018-02-21 13:05:06 +0200

    Add Current page template variable #563505377349368

--- visualcomposer/Modules/Editors/Settings/PageTemplatesController.php
+++ visualcomposer/Modules/Editors/Settings/PageTemplatesController.php
@@ -10,6 +10,7 @@ if (!defined('ABSPATH')) {
 
 use VisualComposer\Framework\Container;
 use VisualComposer\Framework\Illuminate\Support\Module;
+use VisualComposer\Helpers\PostType;
 use VisualComposer\Helpers\Traits\EventsFilters;
 use VisualComposer\Helpers\Traits\WpFiltersActions;
 
@@ -21,11 +22,83 @@ class PageTemplatesController extends Container implements Module
     public function __construct()
     {
         if (vcvenv('VCV_PAGE_TEMPLATES_LAYOUTS')) {
+            $this->addFilter('vcv:editor:variables', 'outputCurrentTemplatesLayouts');
             $this->addFilter('vcv:editor:variables', 'outputTemplatesLayouts');
+            $this->addFilter('vcv:editor:variables', 'outputThemeTemplates');
+            $this->addFilter('vcv:editor:settings:pageTemplatesLayouts:current', 'getCurrentTemplateLayout');
         }
     }
 
+    protected function getCurrentTemplateLayout($output, PostType $postTypeHelper)
+    {
+        $post = $postTypeHelper->get();
+        if ($post) {
+            $currentPostTemplate = $post->page_template;
+            $customTemplate = get_post_meta($post->ID, '_vcv-page-template', true);
+            $customTemplateType = get_post_meta($post->ID, '_vcv-page-template-type', true);
+            if (!empty($customTemplateType) && !empty($customTemplate)) {
+                $output = [
+                    'type' => $customTemplateType,
+                    'value' => $customTemplate,
+                ];
+            } else {
+                $output = [
+                    'type' => 'theme',
+                    'value' => !empty($currentPostTemplate) ? $currentPostTemplate : 'default',
+                ];
+            }
+        }
+
+        return $output;
+    }
+
+    protected function outputCurrentTemplatesLayouts($variables, $payload)
+    {
+        $variables[] = [
+            'key' => 'VCV_PAGE_TEMPLATES_LAYOUTS_CURRENT',
+            'value' => vcfilter(
+                'vcv:editor:settings:pageTemplatesLayouts:current',
+                [
+                    'type' => 'theme',
+                    'value' => 'default',
+                ]
+            ),
+            'type' => 'constant',
+        ];
+
+        return $variables;
+    }
+
     protected function outputTemplatesLayouts($variables, $payload)
+    {
+        $variables[] = [
+            'key' => 'VCV_PAGE_TEMPLATES_LAYOUTS',
+            'value' => vcfilter(
+                'vcv:editor:settings:pageTemplatesLayouts',
+                [
+                    [
+                        'type' => 'vc',
+                        'title' => __('Visual Composer', 'vcwb'),
+                        'values' => [
+                            [
+                                'label' => __('Blank Template', 'vcwb'),
+                                'value' => 'blank',
+                            ],
+                            [
+                                'label' => __('Boxed Template', 'vcwb'),
+                                'value' => 'boxed',
+                            ],
+                        ],
+                    ],
+                ]
+            ),
+            'type' => 'constant',
+        ];
+
+        return $variables;
+    }
+
+    protected function outputThemeTemplates($variables, $payload)
     {
         $pageTemplates = get_page_templates();
         $pageTemplatesList = [
@@ -44,46 +117,17 @@ class PageTemplatesController extends Container implements Module
         }
 
         $variables[] = [
-            'key' => 'VCV_PAGE_TEMPLATES_LAYOUTS',
-            'value' => [
-                'current' => '',
-                'all' => vcfilter(
-                    'vcv:editor:settings:pageTemplatesLayouts',
-                    [
-                        [
-                            'type' => 'vc',
-                            'title' => __('Visual Composer', 'vcwb'),
-                            'values' => [
-                                [
-                                    'label' => __('Blank Template', 'vcwb'),
-                                    'value' => 'blank',
-                                ],
-                                [
-                                    'label' => __('Boxed Template', 'vcwb'),
-                                    'value' => 'boxed',
-                                ],
-                            ],
-                        ],
-                    ]
-                ),
-            ],
-            'type' => 'constant',
-        ];
-        $variables[] = [
             'key' => 'VCV_PAGE_TEMPLATES_LAYOUTS_THEME',
-            'value' => [
-                'current' => '',
-                'all' => vcfilter(
-                    'vcv:editor:settings:pageTemplatesLayouts:theme',
+            'value' => vcfilter(
+                'vcv:editor:settings:pageTemplatesLayouts:theme',
+                [
                     [
-                        [
-                            'type' => 'theme',
-                            'title' => __('Theme Templates', 'vcwb'),
-                            'values' => $pageTemplatesList,
-                        ],
-                    ]
-                ),
-            ],
+                        'type' => 'theme',
+                        'title' => __('Theme Templates', 'vcwb'),
+                        'values' => $pageTemplatesList,
+                    ],
+                ]
+            ),
             'type' => 'constant',
         ];
 
