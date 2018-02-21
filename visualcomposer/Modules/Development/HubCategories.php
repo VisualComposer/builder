bd450fd0d485016adcfc9281a9a2e91b71ca602f
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2018-02-21 11:56:10 +0200

    Add WP Forms element to cloneScript and HubCategories #529496659778207

--- visualcomposer/Modules/Development/HubCategories.php
+++ visualcomposer/Modules/Development/HubCategories.php
@@ -314,6 +314,7 @@ class HubCategories extends Container implements Module
                         'layerSlider',
                         'essentialGrid',
                         'nextGenGallery',
+                        'wpForms',
                     ],
                     'icon' => $urlHelper->to('devCategories/categories/icons/WordPress.svg'),
                     'iconDark' => $urlHelper->to('devCategories/categories/iconsDark/WordPress.svg'),
