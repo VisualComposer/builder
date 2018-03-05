32771cfec88d3bc79d0fee0e9d066a8e9cd584f5
Nikita Hlopovs <nikita.h@Nikitas-MacBook-Air.local>
2018-03-05 13:24:48 +0200

    Create paramsGroup attribute #578837618387861

---
+++ public/sources/attributes/paramsGroup/Component.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Attribute from '../attribute'
+
+export default class ParamsGroupAttribute extends Attribute {
+  render () {
+    return (
+      <div>Params Group</div>
+    )
+  }
+}
