47098467fd2c49b4b4019a56415fc39cd2f4e999
Alvis Alferovs <aui@Alviss-MacBook-Air.local>
2018-03-06 10:53:26 +0200

    Added onclick actions #575295901000265

--- public/sources/attributes/paramsGroup/Component.js
+++ public/sources/attributes/paramsGroup/Component.js
@@ -4,21 +4,41 @@ import Attribute from '../attribute'
 export default class ParamsGroupAttribute extends Attribute {
   constructor (props) {
     super(props)
+    this.state = {
+      groups: this.props.options.groups
+    }
+    this.clickAdd = this.clickAdd.bind(this)
     this.clickClone = this.clickClone.bind(this)
     this.clickDelete = this.clickDelete.bind(this)
     this.clickEdit = this.clickEdit.bind(this)
+  }
+
+  clickAdd () {
+    let result = this.state.groups
+    result.push('Group title')
+    this.setState({
+      groups: result
+    })
   }
 
   clickEdit () {
 
   }
 
-  clickClone () {
-
+  clickClone (index) {
+    let result = this.state.groups
+    result.push(this.state.groups[index])
+    this.setState({
+      groups: result
+    })
   }
 
-  clickDelete () {
-
+  clickDelete (index) {
+    let result = this.state.groups
+    result.splice(index, 1)
+    this.setState({
+      groups: result
+    })
   }
 
   getGroupList () {
@@ -30,8 +50,8 @@ export default class ParamsGroupAttribute extends Attribute {
     }
     let result = []
 
-    if (this.props.options.groups.length) {
-      this.props.options.groups.forEach((group, index) => {
+    if (this.state.groups.length) {
+      this.state.groups.forEach((group, index) => {
         result.push(
           <div className='vcv-ui-form-params-group-item vcv-ui-tree-layout-control' key={`param-group-${group}-${index}`}>
             <div className={dragHelperClasses}>
@@ -45,7 +65,7 @@ export default class ParamsGroupAttribute extends Attribute {
                   {group}
                 </span>
               </span>
-              {this.getChildContols()}
+              {this.getChildContols(index)}
             </div>
           </div>
         )
@@ -57,19 +77,19 @@ export default class ParamsGroupAttribute extends Attribute {
     return result
   }
 
-  getChildContols () {
+  getChildContols (index) {
     const localizations = window.VCV_I18N && window.VCV_I18N()
     const cloneText = localizations ? localizations.clone : 'Clone'
     const removeText = localizations ? localizations.remove : 'Remove'
     const editText = localizations ? localizations.edit : 'Edit'
     return (<span className='vcv-ui-tree-layout-control-actions'>
-      <span className='vcv-ui-tree-layout-control-action' title={cloneText} onClick={this.clickClone}>
+      <span className='vcv-ui-tree-layout-control-action' title={cloneText} onClick={() => { this.clickClone(index) }}>
         <i className='vcv-ui-icon vcv-ui-icon-copy' />
       </span>
-      <span className='vcv-ui-tree-layout-control-action' title={removeText} onClick={this.clickDelete}>
+      <span className='vcv-ui-tree-layout-control-action' title={removeText} onClick={() => { this.clickDelete(index) }}>
         <i className='vcv-ui-icon vcv-ui-icon-trash' />
       </span>
-      <span className='vcv-ui-tree-layout-control-action' title={editText} onClick={this.clickEdit}>
+      <span className='vcv-ui-tree-layout-control-action' title={editText} onClick={() => { this.clickEdit(index) }}>
         <i className='vcv-ui-icon vcv-ui-icon-arrow-right' />
       </span>
     </span>)
@@ -77,10 +97,14 @@ export default class ParamsGroupAttribute extends Attribute {
 
   render () {
     return (
-      <div className='vcv-ui-form-params-group'>
-        {this.getGroupList()}
-        <div className='vcv-ui-form-params-group-add-item vcv-ui-icon vcv-ui-icon-add' />
-      </div>
+      <React.Fragment>
+        {this.state.groups.length ? null : (<div className='vcv-ui-form-group-heading'>{this.props.options.title}</div>
+        )}
+        <div className='vcv-ui-form-params-group'>
+          {this.getGroupList()}
+          <div className='vcv-ui-form-params-group-add-item vcv-ui-icon vcv-ui-icon-add' onClick={this.clickAdd} />
+        </div>
+      </React.Fragment>
     )
   }
 }
