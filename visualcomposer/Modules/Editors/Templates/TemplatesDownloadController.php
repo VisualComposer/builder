def6b6c57e5169939aca180819d0c1ab9445a932
Edgars V <edgars.v@wpbakery.com>
2018-03-09 16:38:15 +0200

    Improve single image parse #557507828857558

--- visualcomposer/Modules/Editors/Templates/TemplatesDownloadController.php
+++ visualcomposer/Modules/Editors/Templates/TemplatesDownloadController.php
@@ -331,13 +331,14 @@ class TemplatesDownloadController extends Container implements Module
             }
             // first level
             if (is_string($propValue) || $propKey === "image") {
-                if ($wpMediaHelper->checkIsImage($propValue)
-                    || ($propKey === "image"
-                        && $wpMediaHelper->checkIsImage(
-                            $propValue[0]
-                        ))) {
+                if ($propKey === "image" && is_array($propValue) && $wpMediaHelper->checkIsImage($propValue[0])) {
+                    $propValue = $propValue[0];
+                } else if($propKey === "image" && is_string($propValue) && $wpMediaHelper->checkIsImage($propValue)) {
+                    $propValue = $propValue;
+                }
+                if ($wpMediaHelper->checkIsImage($propValue)) {
                     $images[] = [
-                        'url' => $propKey === "image" ? $propValue[0] : $propValue,
+                        'url' => $propValue,
                         'key' => $propKey,
                     ];
                 }
