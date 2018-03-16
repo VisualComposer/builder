9fa1204142832d5df2839189e91ef9036da49b05
Pavel I <pavel@wpbakery.com>
2018-03-16 13:10:46 +0200

    Fix HFS shortcodes rendering

---
+++ visualcomposer/Modules/Elements/AssetShortcode/Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace VisualComposer\Modules\Elements\AssetShortcode;
+
+if (!defined('ABSPATH')) {
+    header('Status: 403 Forbidden');
+    header('HTTP/1.1 403 Forbidden');
+    exit;
+}
+
+use VisualComposer\Framework\Container;
+use VisualComposer\Framework\Illuminate\Support\Module;
+use VisualComposer\Helpers\Assets;
+use VisualComposer\Modules\Elements\Traits\AddShortcodeTrait;
+
+class Controller extends Container implements Module
+{
+    use AddShortcodeTrait;
+
+    public function __construct()
+    {
+        $this->addShortcode('vcvAssetsUploadUrl', 'renderAssetsUploadUrl');
+        $this->addShortcode('vcvUploadUrl', 'renderUploadUrl');
+    }
+
+    protected function renderAssetsUploadUrl(Assets $assetsHelper)
+    {
+        return $assetsHelper->getAssetUrl();
+    }
+}
