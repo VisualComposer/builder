f7d3d5b3a1c3b09c979f3dcda532db12a8e2f9c5
Pavel I <pavel@wpbakery.com>
2018-03-16 15:15:11 +0200

    Update tested up #594630443656026

--- plugin-wordpress.php
+++ plugin-wordpress.php
@@ -10,7 +10,7 @@
  * License: GNU General Public License v2.0
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Requires at least: 4.5
- * Tested up to: 4.8.3
+ * Tested up to: 4.9.4
  * Text Domain: vcwb
  */
 
