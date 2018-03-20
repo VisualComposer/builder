c86dacb62b880236c35d516cac3c8a6fbe5d0e30
Pavel I <pavel@wpbakery.com>
2018-03-20 17:35:33 +0200

    Moving assets migration improvements #597908950802585

--- visualcomposer/Modules/Migrations/Assets22Migration.php
+++ visualcomposer/Modules/Migrations/Assets22Migration.php
@@ -10,6 +10,7 @@ if (!defined('ABSPATH')) {
 
 use VisualComposer\Framework\Illuminate\Support\Module;
 use VisualComposer\Helpers\File;
+use VisualComposer\Helpers\Options;
 
 /**
  * Class Templates114Migration
@@ -22,9 +23,9 @@ class Assets22Migration extends MigrationsController implements Module
 {
     protected $migrationId = 'assets22Migration';
 
-    protected $migrationPriority = 1;
+    protected $migrationPriority = 11;
 
-    protected function run(File $fileHelper)
+    protected function run(File $fileHelper, Options $optionsHelper)
     {
         // check if folder doesnt exists in wp-content/uploads/visualcomposer-assets
         // check if folder exists in wp-content/visualcomposer-assets
@@ -34,18 +35,61 @@ class Assets22Migration extends MigrationsController implements Module
                 && $fileSystem->is_dir(
                     WP_CONTENT_DIR . '/' . VCV_PLUGIN_ASSETS_DIRNAME
                 )) {
-                $result = $fileHelper->copyDirectory(
-                    WP_CONTENT_DIR . '/' . VCV_PLUGIN_ASSETS_DIRNAME,
-                    VCV_PLUGIN_ASSETS_DIR_PATH . '-temp',
-                    true
-                );
-                if (!is_wp_error($result) && $result) {
-                    $resultMove = $fileHelper->getFileSystem()->move(
-                        VCV_PLUGIN_ASSETS_DIR_PATH . '-temp',
-                        VCV_PLUGIN_ASSETS_DIR_PATH
-                    );
+                usleep(500000);
+                if (!$optionsHelper->getTransient('vcv:migration:assets22:lock')) {
+                    $optionsHelper->setTransient('vcv:migration:assets22:lock', true, 20);
 
-                    return !is_wp_error($resultMove) && $result;
+                    $result = $fileHelper->copyDirectory(
+                        WP_CONTENT_DIR . '/' . VCV_PLUGIN_ASSETS_DIRNAME,
+                        VCV_PLUGIN_ASSETS_DIR_PATH . '-temp',
+                        true
+                    );
+                    if (!is_wp_error($result) && $result) {
+                        $resultMove = $fileHelper->getFileSystem()->move(
+                            VCV_PLUGIN_ASSETS_DIR_PATH . '-temp',
+                            VCV_PLUGIN_ASSETS_DIR_PATH
+                        );
+                        $responseMove = !is_wp_error($resultMove) && $result;
+                        if (vcvenv('VCV_DEBUG')) {
+                            if (!$responseMove) {
+                                error_log(
+                                    print_r(
+                                        [
+                                            'code' => 2,
+                                            'is_wp_error' => is_wp_error($resultMove),
+                                            /** @var $result \WP_Error */
+                                            'data' => is_wp_error($result) ? $result->get_error_messages() : $result,
+                                            'codes' => is_wp_error($result) ? $result->get_error_codes() : $result,
+                                        ],
+                                        true
+                                    ),
+                                    3,
+                                    VCV_PLUGIN_DIR_PATH . 'not-ok.log'
+                                );
+                            }
+                        }
+
+                        return $responseMove;
+
+                    } else {
+                        if (vcvenv('VCV_DEBUG')) {
+                            error_log(
+                                print_r(
+                                    [
+                                        'code' => 1,
+                                        'is_wp_error' => is_wp_error($result),
+                                        'data' => is_wp_error($result) ? $result->get_error_messages() : $result,
+                                        'codes' => is_wp_error($result) ? $result->get_error_codes() : $result,
+                                    ],
+                                    true
+                                ),
+                                3,
+                                VCV_PLUGIN_DIR_PATH . 'not-ok.log'
+                            );
+                        }
+
+                        return false;
+                    }
                 } else {
                     return false;
                 }
