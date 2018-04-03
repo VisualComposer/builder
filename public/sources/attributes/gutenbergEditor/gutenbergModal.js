76d0255d76da0e262adbf8ad9840d6b6c098c824
Vjaceslav Vasenin <time2type@gmail.com>
2018-04-03 18:24:30 +0300

    Add modal popup #609051300004234

---
+++ public/sources/attributes/gutenbergEditor/gutenbergModal.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/* Working prototype */
+export class GutenbergModalPopup extends React.Component {
+
+  render () {
+    const style = {
+      backgroundColor: '#ffffff',
+      padding: '20px'
+    }
+    return (
+      <div style={style}>Gutenberg</div>
+    )
+  }
+}
