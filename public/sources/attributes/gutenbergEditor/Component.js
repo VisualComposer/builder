3d7c75e455ac7eb25a59d420d9e2c239e01b1125
Vjaceslav Vasenin <time2type@gmail.com>
2018-04-04 08:28:29 +0300

    Gutenberg modal #609051300004234

--- public/sources/attributes/gutenbergEditor/Component.js
+++ public/sources/attributes/gutenbergEditor/Component.js
@@ -1,91 +1,45 @@
 import React from 'react'
-import { getService } from 'vc-cake'
+import classnames from 'classnames'
 import Attribute from '../attribute'
+import GutenbergModal from './gutenbergModal'
 
-const dataProcessor = getService('dataProcessor')
 /* Working prototype */
 export default class Component extends Attribute {
-  componentDidMount () {
-    this.getEditor()
+  openEditor (e) {
+    e.preventDefault()
+    this.setState({ showEditor: true })
   }
 
-  spinnerHTML () {
-    return '<span class="vcv-ui-content-editable-helper-loader vcv-ui-wp-spinner"></span>'
-  }
-
-  // Dry
-  loadFiles (data) {
-    const assetsWindow = window
-    if (data.domNodes && data.domNodes.length) {
-      Array.from(data.domNodes).forEach(domNode => {
-        let position = ''
-        if (domNode.href) {
-          position = 'head'
-        } else if (domNode.src) {
-          !data.ignoreCache && (position = 'body')
-        } else if (domNode.id && domNode.type && domNode.type.indexOf('template') >= 0) {
-          position = 'head'
-        } else if (data.cacheInnerHTML) {
-        }
-        if (data.addToDocument) {
-          if (position) {
-            assetsWindow.document[ position ] && assetsWindow.jQuery(assetsWindow.document[ position ]).append(domNode)
-          } else {
-            data.ref && assetsWindow.jQuery(data.ref) && assetsWindow.jQuery(data.ref).append(domNode)
-          }
-        }
-      })
-    }
-  }
-
-  getEditor () {
-    const { output } = this.state.value
-    this.editorWrapper && (this.editorWrapper.innerHTML = this.spinnerHTML())
-    this.ajax = dataProcessor.appAdminServerRequest({
-      'vcv-action': 'attributes:ajaxGutenberg:adminNonce',
-      'vcv-attribute-string': output,
-      'vcv-nonce': window.vcvNonce,
-      'vcv-source-id': window.vcvSourceID
-    }).then((data) => {
-      if (this.ajax && this.ajax.cancelled) {
-        this.ajax = null
-        return
-      }
-      let jsonData
-      try {
-        jsonData = JSON.parse(data)
-      } catch (e) {
-        console.warn('failed to parse json data', e, data)
-      }
-      try {
-        const ref = this.editorWrapper
-        const { headerContent, editorContent, footerContent } = jsonData
-        ref && (ref.innerHTML = '')
-
-        const headerDom = window.jQuery('<div>' + headerContent + '</div>', document)
-        this.loadFiles({ type: 'header', ref: ref, domNodes: headerDom.children(), cacheInnerHTML: true, addToDocument: true })
-
-        const editorDom = window.jQuery('<div>' + editorContent + '</div>', document)
-        if (editorDom.children().length) {
-          this.loadFiles({ type: 'shortcode', ref: ref, domNodes: editorDom.children(), addToDocument: true })
-        }
-
-        const footerDom = window.jQuery('<div>' + footerContent + '</div>', document)
-        this.loadFiles({ type: 'footer', ref: ref, domNodes: footerDom.children(), addToDocument: true, ignoreCache: true })
-      } catch (e) {
-        console.warn('failed to parse json', e, jsonData)
-      }
-      this.ajax = null
-    })
+  closeEditor () {
+    this.setState({ showEditor: false })
   }
 
   render () {
-    let { value } = this.state
+    let { value, showEditor } = this.state
+    const editor = () => {
+      if (showEditor) {
+        const closeClasses = classnames({
+          'vcv-layout-bar-content-hide-icon': true,
+          'vcv-ui-icon': true,
+          'vcv-ui-icon-close-thin': true
+        })
+        const iframeURL = window.ajaxurl
+        return (
+          <GutenbergModal>
+            <i onClick={this.closeEditor.bind(this)} className={closeClasses} style={{ color: '#000' }} />
+            <iframe src={iframeURL} />
+          </GutenbergModal>
+        )
+      }
+    }
     return (
-      <React.Fragment>
-        <div className='vcv-ui-form-gutenberg' ref={(node) => { this.editorWrapper = node }} />
+      <div>
+        <button className='vcv-ui-form-button vcv-ui-form-button--default' onClick={this.openEditor.bind(this)}>
+          Open Gutenberg
+        </button>
         <textarea className='vcv-ui-form-input' onChange={this.handleChange} value={value} />
-      </React.Fragment>
+        {editor()}
+      </div>
     )
   }
 }
--- public/sources/attributes/gutenbergEditor/gutenbergModal.js
+++ public/sources/attributes/gutenbergEditor/gutenbergModal.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 /* Working prototype */
-export class GutenbergModalPopup extends React.Component {
+export default class GutenbergModal extends React.Component {
+  constructor (props) {
+    super(props)
+    this.el = document.createElement('div')
+    this.el.style.cssText = 'position:fixed;overflow:hidden;left:0;top:0;right:0;bottom:0;background:#fff;z-index:10000;'
+  }
+
+  componentDidMount () {
+    const modalRoot = document.querySelector('.vcv-layout-container')
+    modalRoot.appendChild(this.el)
+  }
+
+  componentWillUnmount () {
+    const modalRoot = document.querySelector('.vcv-layout-container')
+    modalRoot.removeChild(this.el)
+  }
 
   render () {
-    const style = {
-      backgroundColor: '#ffffff',
-      padding: '20px'
-    }
-    return (
-      <div style={style}>Gutenberg</div>
+    return ReactDOM.createPortal(
+      this.props.children,
+      this.el,
     )
   }
 }
