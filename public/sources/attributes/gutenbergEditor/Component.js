bf347b2e2b601c49de18a910fea8a0a024fa3a6d
Vjaceslav Vasenin <time2type@gmail.com>
2018-04-04 13:06:57 +0300

    Get content from gutenberg editor #609051300004234

--- public/sources/attributes/gutenbergEditor/Component.js
+++ public/sources/attributes/gutenbergEditor/Component.js
@@ -5,6 +5,12 @@ import GutenbergModal from './gutenbergModal'
 
 /* Working prototype */
 export default class Component extends Attribute {
+  constructor (props) {
+    super(props)
+    this.openEditor = this.openEditor.bind(this)
+    this.iframeLoaded = this.iframeLoaded.bind(this)
+    this.updateValueFromIframe = this.updateValueFromIframe.bind(this)
+  }
   openEditor (e) {
     e.preventDefault()
     this.setState({ showEditor: true })
@@ -14,6 +20,15 @@ export default class Component extends Attribute {
     this.setState({ showEditor: false })
   }
 
+  iframeLoaded (e) {
+    const wpData = document.getElementById('vcv-gutenberg-attribute-modal-iframe').contentWindow.wp.data
+    wpData.subscribe(this.updateValueFromIframe.bind(this))
+  }
+  updateValueFromIframe () {
+    const wpData = document.getElementById('vcv-gutenberg-attribute-modal-iframe').contentWindow.wp.data
+    const value = wpData.select('core/editor').getEditedPostContent()
+    this.setFieldValue(value)
+  }
   render () {
     let { value, showEditor } = this.state
     const editor = () => {
@@ -23,23 +38,23 @@ export default class Component extends Attribute {
           'vcv-ui-icon': true,
           'vcv-ui-icon-close-thin': true
         })
-        const iframeURL = window.ajaxurl
+        const iframeURL = '/wp-admin/post-new.php?post_type=page' // change with action
         return (
           <GutenbergModal>
-            <i onClick={this.closeEditor.bind(this)} className={closeClasses} style={{ color: '#000' }} />
-            <iframe src={iframeURL} />
+            <i onClick={this.closeEditor.bind(this)} className={closeClasses} style={{ color: '#000', position: 'fixed', top: '12px', right: '12px' }} />
+            <iframe id='vcv-gutenberg-attribute-modal-iframe' src={iframeURL} style={{ width: '100%', height: '100%' }} onLoad={this.iframeLoaded} />
           </GutenbergModal>
         )
       }
     }
     return (
-      <div>
-        <button className='vcv-ui-form-button vcv-ui-form-button--default' onClick={this.openEditor.bind(this)}>
+      <React.Fragment>
+        <button className='vcv-ui-form-button vcv-ui-form-button--default' onClick={this.openEditor}>
           Open Gutenberg
         </button>
         <textarea className='vcv-ui-form-input' onChange={this.handleChange} value={value} />
         {editor()}
-      </div>
+      </React.Fragment>
     )
   }
 }
