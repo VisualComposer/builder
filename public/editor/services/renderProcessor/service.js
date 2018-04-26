52378356e6ee47d6f6566042ec159c23452668cc
Pavel I <pavel@wpbakery.com>
2018-04-26 17:47:20 +0300

    Add renderProcess time-out #651313123422100

--- public/editor/services/renderProcessor/service.js
+++ public/editor/services/renderProcessor/service.js
@@ -1,15 +1,36 @@
 import { addService } from 'vc-cake'
+
 let processes = []
 const Service = {
   add (promise) {
     processes.push(promise)
   },
   appAllDone () {
-    return Promise.all(processes).then(() => {
-      this.purge()
+    this.clearTimeout()
+    let renderPromise = new window.Promise((resolve, reject) => {
+      this.timeout = window.setTimeout(() => {
+        console.warn && console.warn('RenderProcess timed-out')
+        this.purge()
+        resolve()
+      }, 60 * 1000)
+
+      Promise.all(processes).then(() => {
+        this.purge()
+        resolve()
+      }).catch((e) => {
+        console.warn && console.warn('renderProcessor failed', e)
+        this.purge()
+        resolve()
+      })
     })
+    return renderPromise
+  },
+  clearTimeout () {
+    this.timeout && window.clearTimeout(this.timeout)
+    this.timeout = null
   },
   purge () {
+    this.clearTimeout()
     processes = []
   }
 }
