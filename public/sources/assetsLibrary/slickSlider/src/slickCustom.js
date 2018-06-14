a3d3686b94c4815a759cdd9f73e8e058b924a17d
Pavel I <pavel@wpbakery.com>
2018-06-14 11:27:11 +0300

    Update slickCustom #699879816636685

--- public/sources/assetsLibrary/slickSlider/src/slickCustom.js
+++ public/sources/assetsLibrary/slickSlider/src/slickCustom.js
@@ -15,17 +15,7 @@
 
  */
 /* global window, document, define, jQuery, setInterval, clearInterval */
-(function(factory) {
-  'use strict';
-  if (typeof define === 'function' && define.amd) {
-    define(['jquery'], factory);
-  } else if (typeof exports !== 'undefined') {
-    module.exports = factory(require('jquery'));
-  } else {
-    factory(jQuery);
-  }
-
-}(function($) {
+(function($) {
   'use strict';
   var Slick = window.Slick || {};
 
@@ -2900,4 +2890,4 @@
     return _;
   };
 
-}));
+})(window.jQuery)
