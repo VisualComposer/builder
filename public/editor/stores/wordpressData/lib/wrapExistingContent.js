1dd6e687212f096e6ab51c6fa84ef3acd7d84fff
Vjaceslav Vasenin <time2type@gmail.com>
2018-07-13 12:29:13 +0300

    Parse shortcode string

--- public/editor/stores/wordpressData/lib/wrapExistingContent.js
+++ public/editor/stores/wordpressData/lib/wrapExistingContent.js
@@ -1,11 +1,43 @@
+/* global wp */
 import { getStorage, getService } from 'vc-cake'
-const utils = getService('utils')
+
+// const utils = getService('utils')
 const cook = getService('cook')
 const elementsStorage = getStorage('elements')
+const multipleShortcodesRegex = wp.shortcode.regexp(window.VCV_API_WPBAKERY_VC_MAP().join('|'))
+const localShortcodesRegex = new RegExp(multipleShortcodesRegex.source)
+
+const parse = (content, parent = false) => {
+  const globalMatches = content.match(multipleShortcodesRegex)
+  globalMatches.forEach((line) => {
+    const innerContent = line.match(localShortcodesRegex)
+    if (innerContent && innerContent[ 2 ] === 'vc_row') {
+      const row = cook.get({ tag: 'row' })
+      elementsStorage.trigger('add', row.toJS(), false, {addColumn: false})
+      if (innerContent[ 5 ]) {
+        parse(innerContent[ 5 ], row.get('id'))
+      }
+    } else if (innerContent[ 2 ] === 'vc_column') {
+      const column = cook.get({ tag: 'column', parent: parent })
+      elementsStorage.trigger('add', column.toJS(), false)
+      if (innerContent[ 5 ]) {
+        parse(innerContent[ 5 ], column.get('id'))
+      }
+    } else {
+      const shortcode = cook.get({ tag: 'shortcode', parent: parent, shortcode: line })
+      elementsStorage.trigger('add', shortcode.toJS(), false)
+    }
+  })
+}
 
 export default (content) => {
-  let textElement = cook.get({ tag: 'textBlock', output: utils.wpAutoP(content, '__VCVID__') })
-  if (textElement) {
-    elementsStorage.trigger('add', textElement.toJS())
-  }
+  parse(content)
+  // let localMatches = globalMatches[0].match(localShortcodesRegex)
+  // let subContent = localMatches[5]
+  /*
+    const textElement = cook.get({ tag: 'textBlock', output: utils.wpAutoP(content, '__VCVID__') })
+    if (textElement) {
+      elementsStorage.trigger('add', textElement.toJS())
+    }
+  */
 }
