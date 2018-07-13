5a862710690dc14e7cfd657eb8838b38bd187587
Pavel I <pavel@wpbakery.com>
2018-07-13 15:34:03 +0300

    Add design options css class migration

--- public/editor/stores/wordpressData/lib/parseAttributes.js
+++ public/editor/stores/wordpressData/lib/parseAttributes.js
@@ -17,3 +17,15 @@ export const parseRowAttributes = (attrs) => {
   }
   return data
 }
+
+export const parseGeneralAttributes = (attrs) => {
+  const data = {}
+  if (attrs.css) {
+    // design options
+    let cssData = attrs.css.split(/\s*(\.[^\{]+)\s*\{\s*([^\}]+)\s*\}\s*/g)
+    if (cssData && cssData[ 1 ]) {
+      data.customClass = cssData[ 1 ].replace('.', '')
+    }
+  }
+  return data
+}
--- public/editor/stores/wordpressData/lib/wrapExistingContent.js
+++ public/editor/stores/wordpressData/lib/wrapExistingContent.js
@@ -1,6 +1,6 @@
 /* global wp */
 import { getStorage, getService } from 'vc-cake'
-import {parseRowAttributes} from './parseAttributes'
+import { parseRowAttributes, parseGeneralAttributes } from './parseAttributes'
 
 const utils = getService('utils')
 const cook = getService('cook')
@@ -13,26 +13,29 @@ const parse = (multipleShortcodesRegex, content, parent = false) => {
     const innerContent = line.match(localShortcodesRegex)
     const shortcodeTag = innerContent[ 2 ]
     const subInnerContent = innerContent[ 5 ]
+    const attr = wp.shortcode.attrs(innerContent[ 3 ]).named
+    const generalElementAttributes = parseGeneralAttributes(attr)
     if (innerContent && shortcodeTag === 'vc_row') {
-      const attr = wp.shortcode.attrs(innerContent[ 3 ]).named
-      const rowAttributes = parseRowAttributes(attr)
+      const rowAttributes = Object.assign({}, generalElementAttributes, parseRowAttributes(attr))
       const rowElement = cook.get(rowAttributes)
       elementsStorage.trigger('add', rowElement.toJS(), false, { addColumn: false })
       if (subInnerContent) {
         parse(multipleShortcodesRegex, subInnerContent, rowElement.get('id'))
       }
     } else if (shortcodeTag === 'vc_column') {
-      const attr = wp.shortcode.attrs(innerContent[ 3 ]).named
-      const columnElement = cook.get({ tag: 'column', parent: parent, size: attr.width || 'auto' })
+      const columnAttributes = Object.assign({}, generalElementAttributes, { tag: 'column', parent: parent, size: attr.width || 'auto' })
+      const columnElement = cook.get(columnAttributes)
       elementsStorage.trigger('add', columnElement.toJS(), false)
       if (subInnerContent) {
         parse(multipleShortcodesRegex, subInnerContent, columnElement.get('id'))
       }
     } else if (shortcodeTag === 'vc_column_text') {
-      const textElement = cook.get({ tag: 'textBlock', output: utils.wpAutoP(subInnerContent, '__VCVID__') })
+      const textElementAttributes = Object.assign({}, generalElementAttributes, { tag: 'textBlock', output: utils.wpAutoP(subInnerContent, '__VCVID__') })
+      const textElement = cook.get(textElementAttributes)
       elementsStorage.trigger('add', textElement.toJS())
     } else {
-      const shortcodeElement = cook.get({ tag: 'shortcode', parent: parent, shortcode: line })
+      const shortcodeElementAttributes = Object.assign({}, generalElementAttributes, { tag: 'shortcode', parent: parent, shortcode: line })
+      const shortcodeElement = cook.get(shortcodeElementAttributes)
       elementsStorage.trigger('add', shortcodeElement.toJS(), false)
     }
   })
