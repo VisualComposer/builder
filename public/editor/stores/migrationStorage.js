571e43d5f0ab847a53a27d987a586f615c233452
Janis <mcjohn@inbox.lv>
2018-08-02 10:57:14 +0300

    Remove row logic #756293290727783

--- public/editor/stores/migrationStorage.js
+++ public/editor/stores/migrationStorage.js
@@ -5,43 +5,8 @@ addStorage('migration', (storage) => {
   const utils = getService('utils')
   const elementsStorage = getStorage('elements')
 
-  // vc_row migration
-  storage.on('migrateElement', (elementData) => {
-    console.log('migrate row')
-    if (elementData.tag === 'vc_row') {
-      const parseRowAttributes = (attrs) => {
-        const data = { tag: 'row', rowWidth: 'boxed' }
-        attrs = Object.assign({
-          full_width: '',
-          full_height: ''
-        }, attrs)
-        if (attrs.full_width === 'stretch_row') {
-          data.rowWidth = 'stretchedRow'
-        } else if (attrs.full_width === 'stretch_row_content') {
-          data.rowWidth = 'stretchedRowAndColumn'
-        } else if (attrs.full_width === 'stretch_row_content_no_spaces') {
-          data.rowWidth = 'stretchedRowAndColumn'
-          data.removeSpaces = true
-        }
-        if (attrs.full_height) {
-          data.fullHeight = true
-        }
-        return data
-      }
-
-      const rowAttributes = Object.assign({}, parseRowAttributes(elementData._attrs))
-      const rowElement = cook.get(rowAttributes)
-      elementsStorage.trigger('add', rowElement.toJS(), false)
-      if (elementData._subInnerContent) {
-        elementData._parse(elementData._multipleShortcodesRegex, elementData._subInnerContent, rowElement.get('id'))
-      }
-      elementData._migrated = true
-    }
-  })
-
   // vc_column Migration
   storage.on('migrateElement', (elementData) => {
-    console.log('migrate column')
     if (elementData.tag === 'vc_column') {
       const columnAttributes = Object.assign({}, { tag: 'column', parent: elementData._parent, size: elementData._attrs.width || 'auto' })
       const columnElement = cook.get(columnAttributes)
@@ -55,7 +20,6 @@ addStorage('migration', (storage) => {
 
   // vc_column_text Migration
   storage.on('migrateElement', (elementData) => {
-    console.log('migrate text')
     if (elementData.tag === 'vc_column_text') {
       const textElementAttributes = Object.assign({}, { tag: 'textBlock', output: utils.wpAutoP(elementData._subInnerContent, '__VCVID__'), parent: elementData._parent })
       const textElement = cook.get(textElementAttributes)
--- public/editor/stores/wordpressData/lib/parseAttributes.js
+++ public/editor/stores/wordpressData/lib/parseAttributes.js
@@ -1,28 +1,8 @@
-export const parseRowAttributes = (attrs) => {
-  const data = { tag: 'row', rowWidth: 'boxed' }
-  attrs = Object.assign({
-    full_width: '',
-    full_height: ''
-  }, attrs)
-  if (attrs.full_width === 'stretch_row') {
-    data.rowWidth = 'stretchedRow'
-  } else if (attrs.full_width === 'stretch_row_content') {
-    data.rowWidth = 'stretchedRowAndColumn'
-  } else if (attrs.full_width === 'stretch_row_content_no_spaces') {
-    data.rowWidth = 'stretchedRowAndColumn'
-    data.removeSpaces = true
-  }
-  if (attrs.full_height) {
-    data.fullHeight = true
-  }
-  return data
-}
-
 export const parseGeneralAttributes = (attrs) => {
   const data = {}
   if (attrs.css) {
     // design options
-    let cssData = attrs.css.split(/\s*(\.[^\{]+)\s*\{\s*([^\}]+)\s*\}\s*/g)
+    let cssData = attrs.css.split(/\s*(\.[^{]+)\s*\{\s*([^}]+)\s*\}\s*/g)
     if (cssData && cssData[ 1 ]) {
       data.customClass = cssData[ 1 ].replace('.', '')
     }
