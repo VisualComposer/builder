ea53b337924bc4bb247b90b77591023191d6ffbd
Pavel I <pavel@wpbakery.com>
2018-08-03 14:19:29 +0300

    Add Design Options parsing #766364782189406

--- public/editor/stores/wordpressData/lib/parseAttributes.js
+++ public/editor/stores/wordpressData/lib/parseAttributes.js
@@ -6,6 +6,18 @@ export const parseGeneralAttributes = (attrs) => {
     if (cssData && cssData[ 1 ]) {
       data.customClass = cssData[ 1 ].replace('.', '')
     }
+
+    let spl = cssData[ 2 ].split(';')
+    let css = {}
+    spl.forEach((i) => {
+      if (i.length) {
+        let innerSplit = i.split(/:\s/)
+        let key = innerSplit[ 0 ].trim()
+        let val = innerSplit[ 1 ].replace('!important', '').trim()
+        css[ key ] = val
+      }
+    })
+    console.log('TODO: migrate DO', css)
   }
   return data
 }
