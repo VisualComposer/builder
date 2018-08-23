3a5eae8a40980a7e31fcbc40db7ce89a3198cd9c
Pavel I <pavel@wpbakery.com>
2018-08-23 14:28:30 +0300

    Add migration notice component #772574703814384 #778931453174829

--- public/editor/stores/migrationStorage.js
+++ public/editor/stores/migrationStorage.js
@@ -1,9 +1,30 @@
-import { addStorage, getStorage, getService } from 'vc-cake'
+import { addStorage, getStorage, getService, env } from 'vc-cake'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PopupComponent from 'public/resources/components/migrationNotice/popupComponent'
 
 addStorage('migration', (storage) => {
   const cook = getService('cook')
   const utils = getService('utils')
   const elementsStorage = getStorage('elements')
+
+  if (env('FT_MIGRATION_NOTICE')) {
+    storage.on('migrateContent', (contentData) => {
+      // If no addon installed show popup with offer to install addon
+      let iframeContent = document.getElementById('vcv-layout-iframe-content')
+
+      const removePopup = () => {
+        ReactDOM.unmountComponentAtNode(iframeContent)
+      }
+      const addPopup = () => {
+        ReactDOM.render(
+          <PopupComponent close={removePopup} />,
+          iframeContent
+        )
+      }
+      addPopup()
+    })
+  }
 
   storage.on('migrateContent', (contentData) => {
     window.setTimeout(() => {
---
+++ public/resources/components/migrationNotice/popupComponent.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+export default class PopupComponent extends React.Component {
+  closePopup (e) {
+    this.props.close()
+  }
+
+  backToWordpress (e) {
+    window.location.href = window.vcvPostData.backendEditorUrl
+  }
+
+  render () {
+    return <div style={{
+      position: 'fixed',
+      background: 'red',
+      width: '100%',
+      height: '100%',
+      zIndex: '1050',
+      top: '0',
+      left: '0',
+      opacity: '0.4'
+    }}>
+      <div>
+        <p>WPBakery Page Builder Layout Detected</p>
+
+        <p>In order to proceed with the Website Builder, you will need to migrate your layout. Basic elements of the page builder will be replaced with the elements of the website builder, the rest of the elements will be replaced with the Shortcode Element.
+
+          Migration may result in style changes, we highly recommend to create a backup copy of your site before the migration. Upon save, you will be asked to confirm or revert migration results.</p>
+        <label><input type='checkbox' /> I understand and accept the process of migration</label>
+        <button onClick={this.closePopup.bind(this)}>Start migration</button>
+        <button onClick={this.backToWordpress.bind(this)}>Back to wordpress</button>
+      </div>
+    </div>
+  }
+}
