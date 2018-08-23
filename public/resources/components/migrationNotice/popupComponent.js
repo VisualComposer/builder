c5bae36eeb279ec9ca827ed7479162f3a8cb0faa
Pavel I <pavel@wpbakery.com>
2018-08-23 14:33:54 +0300

    Fix codestyle

--- public/resources/components/migrationNotice/popupComponent.js
+++ public/resources/components/migrationNotice/popupComponent.js
@@ -24,7 +24,6 @@ export default class PopupComponent extends React.Component {
         <p>WPBakery Page Builder Layout Detected</p>
 
         <p>In order to proceed with the Website Builder, you will need to migrate your layout. Basic elements of the page builder will be replaced with the elements of the website builder, the rest of the elements will be replaced with the Shortcode Element.
-
           Migration may result in style changes, we highly recommend to create a backup copy of your site before the migration. Upon save, you will be asked to confirm or revert migration results.</p>
         <label><input type='checkbox' /> I understand and accept the process of migration</label>
         <button onClick={this.closePopup.bind(this)}>Start migration</button>
