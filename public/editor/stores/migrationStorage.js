ff1cd1bfd29569105bdee9daddd6b0d5adaaeb9c
Pavel I <pavel@wpbakery.com>
2018-09-04 17:46:13 +0300

    Fix order for elements #804139457321624

--- public/editor/stores/migrationStorage.js
+++ public/editor/stores/migrationStorage.js
@@ -31,8 +31,35 @@ addStorage('migration', (storage) => {
         if (textElement) {
           elementsStorage.trigger('add', textElement.toJS())
         }
+      } else {
+        let elements = storage.state('elements').get()
+        let elementsArray = []
+        for (let key in elements) {
+          if (elements.hasOwnProperty(key)) {
+            elementsArray.push(elements[ key ])
+          }
+        }
+        elementsArray.sort((first, second) => first.element.order > second.element.order)
+        elementsArray.forEach((element) => {
+          elementsStorage.trigger('add', element.element, element.wrap, element.options)
+        })
       }
     }, 15)
     // Timeout needed to be last in the call-stack
+  })
+
+  storage.on('add', (element, wrap = true, options = {}) => {
+    let elements = storage.state('elements').get() || {}
+    elements[ element.id ] = { element: element, wrap: wrap, options: options }
+    storage.state('elements').set(elements)
+  })
+
+  storage.on('update', (id, element) => {
+    let elements = storage.state('elements').get() || {}
+    if (!elements[ id ]) {
+      console.warn('Update called for wrong element', element)
+    }
+    elements[ id ].element = element
+    storage.state('elements').set(elements)
   })
 })
