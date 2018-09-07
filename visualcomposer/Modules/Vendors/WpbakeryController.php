a6af397de485a84ef271b703a70b0890b6c06f93
amit.sukapure <amit.sukapure89@gmail.com>
2018-09-07 15:35:51 +0530

    disabled page builder after migration #804020516286075

---
+++ visualcomposer/Modules/Vendors/WpbakeryController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace VisualComposer\Modules\Vendors;
+
+if (!defined('ABSPATH')) {
+    header('Status: 403 Forbidden');
+    header('HTTP/1.1 403 Forbidden');
+    exit;
+}
+
+use VisualComposer\Framework\Container;
+use VisualComposer\Framework\Illuminate\Support\Module;
+use VisualComposer\Helpers\Traits\WpFiltersActions;
+
+class WpbakeryController extends Container implements Module
+{
+    use WpFiltersActions;
+
+    public function __construct()
+    {
+        $this->wpAddAction('init', 'initialize');
+    }
+
+    protected function initialize()
+    {
+        if (!defined('WPB_VC_VERSION')) {
+            return;
+        }
+
+        $this->wpAddFilter(
+            'vc_is_valid_post_type_be',
+            'disableWpbakery'
+        );
+
+        $this->wpAddFilter(
+            'page_row_actions',
+            'hideWpbakeryActions'
+        );
+
+        $this->wpAddFilter(
+            'post_row_actions',
+            'hideWpbakeryActions'
+        );
+    }
+
+    protected function disableWpbakery($isValid)
+    {
+        $sourceId = get_the_id();
+        $postContent = get_post_meta($sourceId, VCV_PREFIX . 'pageContent', true);
+        if (!empty($postContent)) {
+            return false;
+        }
+
+        return $isValid;
+    }
+
+    protected function hideWpbakeryActions($actions)
+    {
+        $post = get_post();
+        $sourceId = $post->ID;
+        $postContent = get_post_meta($sourceId, VCV_PREFIX . 'pageContent', true);
+        if (!empty($postContent)) {
+            unset($actions['edit_vc']);
+        }
+
+        return $actions;
+    }
+}
