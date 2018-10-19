2b1bfea41b358f7943bfd42e0285c31f42bb08e2
Amit Sukapure <amit.sukapure89@gmail.com>
2018-10-19 17:36:47 +0530

    Version sent update #850919456246401

--- visualcomposer/Modules/License/Pages/Upgrade.php
+++ visualcomposer/Modules/License/Pages/Upgrade.php
@@ -166,7 +166,7 @@ class Upgrade extends Container implements Module
                 '&token=' . rawurlencode($licenseHelper->newKeyToken()) .
                 '&url=' . VCV_PLUGIN_URL .
                 '&domain=' . get_site_url() .
-                '&plugin-version=' . VCV_VERSION
+                '&vcv-version=' . VCV_VERSION
             );
         } else {
             wp_redirect(
@@ -181,7 +181,7 @@ class Upgrade extends Container implements Module
                 '&domain=' . get_site_url() .
                 '&agreement=' . $agreement .
                 '&category=' . rawurlencode($category) .
-                '&plugin-version=' . VCV_VERSION
+                '&vcv-version=' . VCV_VERSION
             );
         }
 
