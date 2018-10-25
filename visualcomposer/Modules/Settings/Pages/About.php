228658ef5661c76f83e6dba75480c5c4f9a3a6f1
Amit Sukapure <amit.sukapure89@gmail.com>
2018-10-25 21:20:19 +0530

    VCV_UTM cannot redefine #861255835493545

--- visualcomposer/Modules/Settings/Pages/About.php
+++ visualcomposer/Modules/Settings/Pages/About.php
@@ -38,7 +38,7 @@ class About extends Container implements Module
     /**
      * @var string
      */
-    protected $templatePath = 'account/partials/activation-layout';
+    protected $templatePath = 'account/partials/activation-go-premium';
 
     /**
      * About constructor.
