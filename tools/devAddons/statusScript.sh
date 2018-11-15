d88873292847e90b870fc42e8637e91191af531b
Pavel Ivanov <reactoranime@gmail.com>
2018-11-15 10:27:21 +0200

    Remove unused imports #888056030130826

---
+++ tools/devAddons/statusScript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "My script is running."
+
+declare -a arr=(
+'themeEditor'
+'globalTemplate'
+'exportImport'
+'wpbMigration'
+'templateWidget'
+)
+
+EXECDIR=`pwd`
+
+for i in "${arr[@]}"
+do
+   if cd $EXECDIR/devAddons/$i; then cd $EXECDIR/devAddons/$i && git status; fi
+done
+
+echo "Done!"
