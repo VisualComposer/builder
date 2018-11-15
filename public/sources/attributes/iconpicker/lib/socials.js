0ae352ea085252b4e73389739a1d46815b74088f
Janis K <janis.k@visualcomposer.com>
2018-11-15 08:27:31 +0200

    Edit social fonts #898674367489796

--- public/sources/attributes/iconpicker/lib/socials.js
+++ public/sources/attributes/iconpicker/lib/socials.js
@@ -1,4 +1,17 @@
 module.exports = [
+  { title: 'RSS', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-rss' },
+  { title: 'Facebook', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-facebook' },
+  { title: 'Twitter', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-twitter' },
+  { title: 'Google', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-google' },
+  { title: 'Linkedin', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-linkedin' },
+  { title: 'Youtube', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-youtube' },
+  { title: 'Vimeo', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-vimeo' },
+  { title: 'Pinterest', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-pinterest' },
+  { title: 'Instagram', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-instagram' },
+  { title: 'Flickr', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-flickr' },
+  { title: 'Dribbble', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-dribbble' },
+  { title: 'Tumblr', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-tumblr' },
   { title: 'Bahence', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-behance' },
-  { title: 'Dribbble', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-dribbble' }
+  { title: 'Github', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-github' },
+  { title: 'Snapchat', id: 'vcv-ui-icon-socials vcv-ui-icon-socials-snapchat' }
 ]
