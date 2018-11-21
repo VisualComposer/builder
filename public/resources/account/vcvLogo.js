0a04506d9d96d7babb8e247fb6c40e9a2844d9ab
Janis K <janis.k@visualcomposer.com>
2018-11-21 12:50:00 +0200

    Change logo in activation #913766615286990

--- public/resources/account/vcvLogo.js
+++ public/resources/account/vcvLogo.js
@@ -3,25 +3,20 @@ import React from 'react'
 const VCVLogo = () => {
   return (
     <div className='vcv-logo'>
-      <svg width='60px' height='62px' viewBox='0 0 36 37' version='1.1'
+      <svg width='80px' height='58px' viewBox='0 0 963 696' version='1.1'
         xmlns='http://www.w3.org/2000/svg'>
         <g stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
-          <g id='01-Intro-Free' transform='translate(-683.000000, -185.000000)'>
-            <g id='VC-Logo' transform='translate(683.000000, 185.000000)'>
-              <polygon id='Fill-1' fill='#257CA0'
-                points='17.982 21.662 17.989 37 8.999 31.837 8.999 21.499' />
-              <polyline id='Fill-5' fill='#74953D'
-                points='17.71 5.977 26.694 6.139 26.708 21.494 17.71 21.315 17.71 5.977' />
-              <polyline id='Fill-4' fill='#2CA2CF'
-                points='26.708 21.494 17.982 26.656 8.999 21.498 17.72 16.315 26.708 21.494' />
-              <polyline id='Fill-6' fill='#9AC753'
-                points='35.42 5.972 26.694 11.135 17.71 5.977 26.432 0.793 35.42 5.972' />
-              <polygon id='Fill-8' fill='#A77E2D'
-                points='8.984 6.145 8.998 21.499 0 16.32 0 5.98' />
-              <polyline id='Fill-9' fill='#F2AE3B'
-                points='17.71 5.977 8.984 11.139 0 5.98 8.722 0.799 17.71 5.977' />
-            </g>
+          <polygon fill='#0584B5' points='0.6 139.7 0.6 417.5 241.2 556.4 241.2 278.6' />
+          <g>
+            <polygon fill='#09AEEF' points='241.2 0.8 0.6 139.7 241.2 278.6 481.8 139.7' />
+            <polygon fill='#AED13B' points='722.4 0.8 481.8 139.7 722.4 278.6 963 139.7' />
           </g>
+          <g transform='translate(241.000000, 139.000000)'>
+            <polygon fill='#B9880C' points='240.8 0.7 240.8 278.5 481.4 417.4 481.4 139.6' />
+            <polygon fill='#B76800' points='0.2 139.6 0.2 417.4 240.8 278.5 240.8 0.7' />
+            <polygon fill='#FDB715' points='240.8 278.5 0.2 417.4 240.8 556.3 481.4 417.4' />
+          </g>
+          <polygon fill='#7F9E2E' points='722.4 278.6 722.4 556.4 963 417.5 963 139.7' />
         </g>
       </svg>
     </div>
