6b3af6fd5b8cc935919ab29992d3e802e5b27881
Deniss Kozickis <deniss.kozickis@gmail.com>
2018-11-28 16:23:53 +0200

    Run system checks on activation - #923676026863382

--- visualcomposer/Helpers/Status.php
+++ visualcomposer/Helpers/Status.php
@@ -16,7 +16,15 @@ class Status implements Helper
 
     protected $defaultMemoryLimit = 256; //In MB
 
-    protected $defaultFileUploadSize = 5; //In MB
+    protected $defaultFileUploadSize = 5;  //In MB
+
+    /** @var \VisualComposer\Helpers\Options */
+    private $optionsHelper;
+
+    public function __construct(Options $optionsHelper)
+    {
+        $this->optionsHelper = $optionsHelper;
+    }
 
     /**
      * @return int
@@ -55,31 +63,41 @@ class Status implements Helper
         return $this->checkVersion(VCV_REQUIRED_PHP_VERSION, PHP_VERSION);
     }
 
-    public function getStatusCssClass($status)
-    {
-        return $status ? 'good' : 'bad';
-    }
-
+    /**
+     * @return bool
+     */
     public function getWpVersionStatus()
     {
         return $this->checkVersion(VCV_REQUIRED_BLOG_VERSION, get_bloginfo('version'));
     }
 
+    /**
+     * @return string
+     */
     public function getVcvVersion()
     {
         return VCV_VERSION;
     }
 
+    /*
+     *
+     */
     public function getWpDebugStatus()
     {
         return !WP_DEBUG;
     }
 
+    /**
+     * @return string
+     */
     public function getMemoryLimit()
     {
         return ini_get('memory_limit');
     }
 
+    /**
+     * @return bool
+     */
     public function getMemoryLimitStatus()
     {
         $memoryLimit = $this->getMemoryLimit();
@@ -90,48 +108,80 @@ class Status implements Helper
         return ($this->convertMbToBytes($memoryLimit) >= $this->defaultMemoryLimit * 1024 * 1024);
     }
 
+    /**
+     * @return int
+     */
     public function getMaxExecutionTime()
     {
         return (int)ini_get('max_execution_time');
     }
 
+    /**
+     * @return bool
+     */
     public function getTimeoutStatus()
     {
-        return $this->getMaxExecutionTime() >= $this->defaultExecutionTime;
+        $maxExecutionTime = $this->getMaxExecutionTime();
+        if ($maxExecutionTime === 0) {
+            return true;
+        }
+
+        return $maxExecutionTime >= $this->defaultExecutionTime;
     }
 
+    /**
+     * @return string
+     */
     public function getMaxUploadFileSize()
     {
         return ini_get('upload_max_filesize');
     }
 
+    /**
+     * @return bool
+     */
     public function getUploadMaxFileSizeStatus()
     {
-        $maxFileSize = $this->getMaxUploadFileSize();
-
-        return $this->convertMbToBytes($maxFileSize) >= $this->defaultFileUploadSize;
+        return $this->convertMbToBytes($this->getMaxUploadFileSize()) >= $this->defaultFileUploadSize;
     }
 
+    /**
+     * @return bool
+     */
     public function getUploadDirAccessStatus()
     {
         return is_writable(wp_upload_dir()['basedir']);
     }
 
+    /**
+     * @return bool
+     */
     public function getFileSystemStatus()
     {
         return !(defined('FS_METHOD') && FS_METHOD !== 'direct');
     }
 
+    /**
+     * @return bool
+     */
     public function getZipStatus()
     {
         return class_exists('ZipArchive') || class_exists('PclZip');
     }
 
+    /**
+     * @return bool
+     */
     public function getCurlStatus()
     {
         return extension_loaded('curl');
     }
 
+    /**
+     * @param $size
+     *
+     * @return float|int
+     */
     public function convertMbToBytes($size)
     {
         if (preg_match('/^(\d+)(.)$/', $size, $matches)) {
@@ -145,5 +195,43 @@ class Status implements Helper
         }
 
         return $size;
+    }
+
+    public function getSystemStatus()
+    {
+        $results = [
+            $this->getMemoryLimitStatus(),
+            $this->getFileSystemStatus(),
+            $this->getCurlStatus(),
+            $this->getPhpVersionStatus(),
+            $this->getTimeoutStatus(),
+            $this->getZipStatus(),
+            $this->getWpDebugStatus(),
+            $this->getWpVersionStatus(),
+            $this->getUploadDirAccessStatus(),
+            $this->getUploadMaxFileSizeStatus(),
+        ];
+
+        foreach ($results as $result) {
+            if ($result === false) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * @return void
+     */
+    public function checkSystemStatusAndSetFlag()
+    {
+        $systemStatus = $this->getSystemStatus();
+
+        if (!$systemStatus) {
+            $this->optionsHelper->set('systemCheckFailing', true);
+        } else {
+            $this->optionsHelper->delete('systemCheckFailing');
+        }
     }
 }
