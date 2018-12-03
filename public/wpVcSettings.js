104ad82877a82b921387befb0a5a59b2236287a6
Janis K <janis.k@visualcomposer.com>
2018-12-03 16:29:05 +0200

    Add tooltip position #921717122090352

--- public/wpVcSettings.js
+++ public/wpVcSettings.js
@@ -82,4 +82,42 @@ import base64 from 'base-64'
   }
 
   checkStatus()
+
+  const hoverTooltip = () => {
+    const $tooltipIcon = $('.vcv-help-tooltip-icon')
+
+    $tooltipIcon.mouseover((e) => {
+      const $currentTarget = $(e.currentTarget)
+      const container = $currentTarget.closest('.vcv-help')
+      const tooltip = $currentTarget.next('.vcv-help-tooltip')
+
+      container.addClass('vcv-help-tooltip--active')
+
+      if (!elementInViewport(tooltip[ 0 ])) {
+        tooltip.addClass('vcv-help-tooltip-position--top')
+      }
+    })
+
+    $tooltipIcon.mouseleave((e) => {
+      const $currentTarget = $(e.currentTarget)
+      const container = $currentTarget.closest('.vcv-help')
+      const tooltip = $currentTarget.next('.vcv-help-tooltip')
+
+      container.removeClass('vcv-help-tooltip--active')
+
+      tooltip.removeClass('vcv-help-tooltip-position--top')
+    })
+
+    const elementInViewport = (el) => {
+      const rect = el.getBoundingClientRect()
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      )
+    }
+  }
+
+  hoverTooltip()
 })(window.jQuery)
