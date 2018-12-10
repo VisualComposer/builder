e5fb6c08a4ab3e34aaa87775d446b7d4cf1911ff
Vjaceslav Vasenin <time2type@gmail.com>
2018-12-10 16:49:18 +0200

    Move cypress files to tests folder

--- tests/cypress/integration/sample_spec.js
+++ tests/cypress/integration/sample_spec.js
@@ -1,5 +1,5 @@
-describe('My First Test', function() {
-  it('Visits the Kitchen Sink', function() {
+describe('My First Test', function () {
+  it('Visits the Kitchen Sink', function () {
     cy.visit('https://example.cypress.io')
   })
 })
