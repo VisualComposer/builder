8fc6f78f32d9ff0804c89a9039ccb8f6ee154f42
Janis K <janis.k@visualcomposer.com>
2018-12-12 16:19:33 +0200

    Add icon for wpbakery tree view attribute #941987964708841

--- public/sources/attributes/wpbakeryEditForm/lib/treeViewItem.js
+++ public/sources/attributes/wpbakeryEditForm/lib/treeViewItem.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import vcCake from 'vc-cake'
 
 const utils = vcCake.getService('utils')
+const hubCategoriesService = vcCake.getService('hubCategories')
 
 export default class TreeViewItem extends React.Component {
   static propTypes = {
@@ -17,15 +18,18 @@ export default class TreeViewItem extends React.Component {
     let { tag, value, level, getContent } = this.props
     let space = 0.8
     let isRTL = utils.isRTL()
-    let defaultSpace = isRTL ? 2 : 1
+    let defaultSpace = isRTL ? 1.5 : 0.5
     const controlPadding = (space * this.props.level + defaultSpace) + 'rem'
     const controlStyle = isRTL ? { paddingRight: controlPadding } : { paddingLeft: controlPadding }
+    const publicPath = hubCategoriesService.getElementIcon('wpbakeryElement')
 
     return (
       <li className='vcv-ui-tree-layout-node-child'>
         <div className='vcv-ui-tree-layout-control' style={controlStyle}>
           <div className='vcv-ui-tree-layout-control-content'>
-            <i className='vcv-ui-tree-layout-control-icon'>icon</i>
+            <i className='vcv-ui-tree-layout-control-icon'>
+              <img src={publicPath} className='vcv-ui-icon' alt='' />
+            </i>
             <span className='vcv-ui-tree-layout-control-label'>{tag}</span>
             <div className='vcv-ui-tree-layout-control-actions'>
               <span className='vcv-ui-tree-layout-control-action'>
