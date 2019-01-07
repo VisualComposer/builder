6082020344b4df891c37480ba32eb49cdd25a3d6
Pavel I <pavel@wpbakery.com>
2019-01-07 12:17:22 +0200

    Hide only settings button #962734833649102

--- visualcomposer/Modules/Vendors/WpbakeryController.php
+++ visualcomposer/Modules/Vendors/WpbakeryController.php
@@ -182,6 +182,9 @@ class WpbakeryController extends Container implements Module
             }
 
             .vc_ui-panel-header-controls > :not([data-vc-ui-element="button-close"]) {
+                /*display: none !important;*/
+            }
+            .vc_ui-panel-header-controls > [data-vc-ui-element="settings-dropdown"] {
                 display: none !important;
             }
 
