2187fc82304776737992479e5a92a4df573915d5
Amit Sukapure <amit.sukapure89@gmail.com>
2019-02-14 17:23:39 +0530

    VC-75 wrap sections

--- visualcomposer/resources/views/settings/pages/index.php
+++ visualcomposer/resources/views/settings/pages/index.php
@@ -10,9 +10,9 @@ if (!defined('ABSPATH')) {
 ?>
 
 <form action="options.php"
-    method="post"
-    data-vcv-ui-element="settings-tab-<?php echo esc_attr($slug); ?>"
-    class="vcv-settings-tab-content vcv-settings-tab-content-active"
+        method="post"
+        data-vcv-ui-element="settings-tab-<?php echo esc_attr($slug); ?>"
+        class="vcv-settings-tab-content vcv-settings-tab-content-active"
     <?php
     // @codingStandardsIgnoreLine
     echo apply_filters('vc_setting-tab-form-' . esc_attr($slug), '');
@@ -20,9 +20,34 @@ if (!defined('ABSPATH')) {
 >
     <?php settings_fields($slug . '_' . $slug) ?>
 
-    <?php do_settings_sections($slug) ?>
-
     <?php
+
+    global $wp_settings_sections, $wp_settings_fields;
+
+    if (!isset($wp_settings_sections[ $slug ])) {
+        return;
+    }
+
+    foreach ((array)$wp_settings_sections[ $slug ] as $section) {
+        echo '<div class="' . $slug . '-section ' . $section['id'] . '">';
+        if ($section['title']) {
+            echo "<h2>{$section['title']}</h2>\n";
+        }
+
+        if ($section['callback']) {
+            call_user_func($section['callback'], $section);
+        }
+
+        if (!isset($wp_settings_fields) || !isset($wp_settings_fields[ $slug ])
+            || !isset($wp_settings_fields[ $slug ][ $section['id'] ])) {
+            continue;
+        }
+        echo '<table class="form-table">';
+        do_settings_fields($slug, $section['id']);
+        echo '</table>';
+        echo '</div>';
+    }
+
     $submitButtonAttributes = [];
     $submitButtonAttributes = apply_filters(
         'vcv:template:settings:settings-tab-submit-button-attributes',
@@ -38,7 +63,8 @@ if (!defined('ABSPATH')) {
 
     <?php submit_button(__('Save Changes', 'vcwb'), 'primary', 'submit_btn', true, $submitButtonAttributes) ?>
 
-    <input type="hidden" name="vcv_action" value="vcv_action-<?php echo esc_attr($slug); ?>"
-        id="vcv_settings-<?php echo esc_attr($slug); ?>-action" />
+    <input type="hidden" name="vcv_action" value="vcv_action-<?php echo esc_attr(
+        $slug
+    ); ?>" id="vcv_settings-<?php echo esc_attr($slug); ?>-action" />
 
 </form>
