c7ee000de723844bdc3be3ba0920e3c839829a71
Janis <mcjohn@inbox.lv>
2019-04-05 14:24:56 +0000

    Add permalink base files and changes

--- public/components/panels/settings/lib/pageSettings/component.js
+++ public/components/panels/settings/lib/pageSettings/component.js
@@ -3,11 +3,18 @@ import { env } from 'vc-cake'
 import TitleSettings from './settings/title'
 import LayoutTemplates from './settings/layoutTemplates'
 import TemplateLayoutSettings from './settings/templateLayout'
+import Permalink from 'public/components/permalink/permalink'
 
 export default class PageSettings extends React.Component {
   render () {
     const content = []
     content.push(<TitleSettings key={content.length} />)
+
+    if (!env('VCV_JS_THEME_EDITOR')) {
+      content.push(<div className='vcv-ui-form-group vcv-ui-form-group-style--inline vcv-ui-form-group--permalink' key={content.length}>
+        <Permalink />
+      </div>)
+    }
 
     if (!env('VCV_JS_THEME_EDITOR')) {
       content.push(<TemplateLayoutSettings key={content.length} />)
