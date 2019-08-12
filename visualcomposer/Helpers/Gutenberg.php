e0f976f89bbebdd745f0847b36d5597dcf2d4b59
Pavel I <pavel@visualcomposer.com>
2019-08-13 00:16:51 +0300

    Fix VC-569 Remove helper func

--- visualcomposer/Helpers/Gutenberg.php
+++ visualcomposer/Helpers/Gutenberg.php
@@ -12,25 +12,6 @@ use VisualComposer\Framework\Illuminate\Support\Helper;
 
 class Gutenberg implements Helper
 {
-    public function isGutenbergEnabled()
-    {
-        $optionsHelper = vchelper('Options');
-        $requestHelper = vchelper('Request');
-        $settings = $optionsHelper->get('settings', ['gutenberg-editor']);
-        $screen = get_current_screen();
-
-        $available = false;
-        if ((!empty($settings) && in_array('gutenberg-editor', $settings) || empty($settings))
-            && (method_exists($screen, 'is_block_editor')
-                && $screen->is_block_editor())
-            && !$requestHelper->exists('classic-editor')
-        ) {
-            $available = true;
-        }
-
-        return $available;
-    }
-
     public function isGutenbergAvailable()
     {
         $optionsHelper = vchelper('Options');
