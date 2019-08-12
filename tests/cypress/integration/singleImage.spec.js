fb912750f1b85ee83843c7d47ca3fa72bf4c3055
Nikita Hlopovs <nikita.h@visualcomposer.com>
2019-08-12 12:46:45 +0300

    Add fixture for singleImage test VC-521

--- tests/cypress/integration/singleImage.spec.js
+++ tests/cypress/integration/singleImage.spec.js
@@ -1,175 +1,96 @@
 /* global describe, it, cy */
 
 const ELEMENT_NAME = 'Single Image'
-const alignment = 'right'
-const animation = 'jello'
-const size = {
-  width: 250,
-  height: 200
-}
-const designOptions = {
-  margin: 10,
-  border: 3,
-  borderRadius: 5,
-  borderStyle: 'dotted',
-  padding: 30,
-  bgColor: {
-    hex: 'BF5C5C',
-    rgb: 'rgb(191, 92, 92)'
-  },
-  borderColor: {
-    hex: 'FFC000',
-    rgb: 'rgb(255, 192, 0)'
-  }
-}
-const customId = 'single-image-id'
-const customClass = 'single-image-class'
 
 describe(ELEMENT_NAME, function () {
   it('Adds element to the page, checks automatically added elements, checks attributes', function () {
-    cy.createPage()
-    cy.addElement(ELEMENT_NAME)
+    cy.fixture('../fixtures/singleImage.json').then((settings) => {
+      cy.createPage()
+      cy.addElement(ELEMENT_NAME)
 
-    cy.get('.vcv-ui-form-group-heading')
-      .contains('OnClick action')
-      .then(($field) => {
-        cy.wrap($field)
-          .next()
-          .select('lightbox')
-      })
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('OnClick action')
+        .then(($field) => {
+          cy.wrap($field)
+            .next()
+            .select(settings.onClickAction)
+        })
 
-    cy.get('.vcv-ui-form-group-heading')
-      .contains('Shape')
-      .then(($field) => {
-        cy.wrap($field)
-          .next('.vcv-ui-form-buttons-group')
-          .find('.vcv-ui-form-button[data-value="rounded"]')
-          .click()
-      })
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('Shape')
+        .then(($field) => {
+          cy.wrap($field)
+            .next('.vcv-ui-form-buttons-group')
+            .find(`.vcv-ui-form-button[data-value="${settings.shape}"]`)
+            .click()
+        })
 
-    cy.get('.vcv-ui-form-group-heading')
-      .contains('Size')
-      .then(($field) => {
-        cy.wrap($field)
-          .next()
-          .clear()
-          .type(`${size.width}x${size.height}`)
-      })
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('Size')
+        .then(($field) => {
+          cy.wrap($field)
+            .next()
+            .clear()
+            .type(`${settings.size.width}x${settings.size.height}`)
+        })
 
-    cy.get('.vcv-ui-form-group-heading')
-      .contains('Alignment')
-      .then(($field) => {
-        cy.wrap($field)
-          .next('.vcv-ui-form-buttons-group')
-          .find(`.vcv-ui-form-button[data-value="${alignment}"]`)
-          .click()
-      })
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('Alignment')
+        .then(($field) => {
+          cy.wrap($field)
+            .next('.vcv-ui-form-buttons-group')
+            .find(`.vcv-ui-form-button[data-value="${settings.alignment}"]`)
+            .click()
+        })
 
-    cy.get('.vcv-ui-form-group-heading')
-      .contains('Element ID')
-      .then(($field) => {
-        cy.wrap($field)
-          .next()
-          .type(customId)
-      })
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('Element ID')
+        .then(($field) => {
+          cy.wrap($field)
+            .next()
+            .type(settings.customId)
+        })
 
-    cy.get('.vcv-ui-form-group-heading')
-      .contains('Extra class name')
-      .then(($field) => {
-        cy.wrap($field)
-          .next()
-          .type(customClass)
-      })
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('Extra class name')
+        .then(($field) => {
+          cy.wrap($field)
+            .next()
+            .type(settings.customClass)
+        })
 
-    cy.get('.vcv-ui-form-switch-trigger-label')
-      .contains('Simple controls')
-      .click()
+      cy.setDO(settings.designOptions)
 
-    cy.get('.vcv-ui-form-input[name="margin"]')
-      .type(`${designOptions.margin}px`)
-    cy.get('.vcv-ui-form-input[name="borderWidth"]')
-      .type(`${designOptions.border}px`)
-    cy.get('.vcv-ui-form-input[name="padding"]')
-      .type(`${designOptions.padding}px`)
-    cy.get('.vcv-ui-form-input[name="borderRadius"]')
-      .type(`${designOptions.borderRadius}px`)
+      cy.savePage()
+      cy.viewPage()
 
-    cy.get('.vcv-ui-form-group-heading')
-      .contains('Background color')
-      .then(($field) => {
-        cy.wrap($field)
-          .next('div')
-          .find('.vcv-ui-color-picker-dropdown')
-          .click()
-        cy.get('.vcv-ui-color-picker-custom-color input[value="000000"]')
-          .clear()
-          .type(designOptions.bgColor.hex)
-        cy.wrap($field)
-          .next('div')
-          .find('.vcv-ui-color-picker-dropdown')
-          .click()
-      })
+      cy.get(`#${settings.customId}`)
+        .should('have.class', settings.customClass)
+        .should('have.css', 'text-align', settings.alignment)
 
-    cy.get('.vcv-ui-form-group-heading')
-      .contains('Border style')
-      .then(($field) => {
-        cy.wrap($field)
-          .next()
-          .select(designOptions.borderStyle)
-      })
+      cy.get('.vce-single-image-wrapper')
+        .should('have.attr', 'data-vce-animate', `vce-o-animate--${settings.designOptions.animation}`)
+        .and('have.attr', 'data-vcv-o-animated', 'true')
+        .should('have.css', 'animation-name', `vce-o-animate--${settings.designOptions.animation}`)
+        .and('have.css', 'background-color', settings.designOptions.backgroundColor.rgb)
+        .and('have.css', 'margin', settings.designOptions.margin)
+        .and('have.css', 'padding', settings.designOptions.padding)
+        .and('have.css', 'border-width', settings.designOptions.borderWidth)
+        .and('have.css', 'border-radius', settings.designOptions.borderRadius)
+        .and('have.css', 'border-style', settings.designOptions.borderStyle)
+        .and('have.css', 'border-color', settings.designOptions.borderColor.rgb)
 
-    cy.get('.vcv-ui-form-group-heading')
-      .contains('Border color')
-      .then(($field) => {
-        cy.wrap($field)
-          .next('div')
-          .find('.vcv-ui-color-picker-dropdown')
-          .click()
-        cy.get('.vcv-ui-color-picker-custom-color input[value="000000"]')
-          .clear()
-          .type(designOptions.borderColor.hex)
-        cy.wrap($field)
-          .next('div')
-          .find('.vcv-ui-color-picker-dropdown')
-          .click()
-      })
+      cy.get('.vce-single-image')
+        .should('have.attr', 'data-width', settings.size.width)
+        .and('have.attr', 'data-height', settings.size.height)
 
-    cy.get('.vcv-ui-form-group-heading')
-      .contains('Animate')
-      .then(($field) => {
-        cy.wrap($field)
-          .next()
-          .select(animation)
-      })
+      cy.get('.vce-single-image-inner').eq(0)
+        .should('have.class', 'vce-single-image--border-rounded')
+        .should('have.css', 'border-radius', '5px')
+        .click()
 
-    cy.savePage()
-    cy.viewPage()
-
-    cy.get(`#${customId}`)
-      .should('have.class', customClass)
-      .should('have.css', 'text-align', alignment)
-
-    cy.get('.vce-single-image-wrapper')
-      .should('have.attr', 'data-vce-animate', `vce-o-animate--${animation}`)
-      .and('have.attr', 'data-vcv-o-animated', 'true')
-      .should('have.css', 'animation-name', `vce-o-animate--${animation}`)
-      .and('have.css', 'background-color', designOptions.bgColor.rgb)
-      .and('have.css', 'margin', `${designOptions.margin}px`)
-      .and('have.css', 'padding', `${designOptions.padding}px`)
-      .and('have.css', 'border-width', `${designOptions.border}px`)
-      .and('have.css', 'border-radius', `${designOptions.borderRadius}px`)
-      .and('have.css', 'border-style', designOptions.borderStyle)
-      .and('have.css', 'border-color', designOptions.borderColor.rgb)
-
-    cy.get('.vce-single-image').should('have.attr', 'width', `${size.width}`)
-    cy.get('.vce-single-image').should('have.attr', 'height', `${size.height}`)
-
-    cy.get('.vce-single-image-inner').eq(0)
-      .should('have.class', 'vce-single-image--border-rounded')
-      .should('have.css', 'border-radius', '5px')
-      .click()
-
-    cy.get('.vce-lightbox')
-      .should('have.css', 'display', 'block')
+      cy.get('.vce-lightbox')
+        .should('have.css', 'display', 'block')
+    })
   })
 })
