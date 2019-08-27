047206d2556716ab57c388e37a482740a5cb7835
Pavel I <pavel@visualcomposer.com>
2019-08-27 23:06:55 +0300

    Disable hubElements remove on system reset VC-627

--- visualcomposer/Modules/Elements/Controller.php
+++ visualcomposer/Modules/Elements/Controller.php
@@ -10,23 +10,34 @@ if (!defined('ABSPATH')) {
 
 use VisualComposer\Framework\Container;
 use VisualComposer\Framework\Illuminate\Support\Module;
-use VisualComposer\Helpers\Options;
 use VisualComposer\Helpers\Traits\EventsFilters;
 use VisualComposer\Helpers\Traits\WpFiltersActions;
 
+/**
+ * Class Controller
+ * @package VisualComposer\Modules\Elements
+ */
 class Controller extends Container implements Module
 {
     use EventsFilters;
     use WpFiltersActions;
 
+    /**
+     * Controller constructor.
+     */
     public function __construct()
     {
-        $this->addEvent('vcv:system:factory:reset', 'unsetOptions');
         if (vcvenv('VCV_FT_DEFAULT_ELEMENTS_INSIDE_PLUGIN')) {
+            /** @see \VisualComposer\Modules\Elements\Controller::addDefaultElements */
             $this->wpAddAction('vcv:api', 'addDefaultElements');
         }
     }
 
+    /**
+     * Add built-in elements into system
+     *
+     * @param $api
+     */
     protected function addDefaultElements($api)
     {
         $elementsToRegister = vchelper('DefaultElements')->all();
@@ -38,13 +49,5 @@ class Controller extends Container implements Module
             $elementBaseUrl = $urlHelper->to('elements/' . $tag);
             $elementsApi->add($manifestPath, $elementBaseUrl);
         }
-    }
-
-    protected function unsetOptions(Options $optionsHelper)
-    {
-        $optionsHelper
-            ->delete('hubElements')
-            ->delete('hubCategories')
-            ->delete('hubGroups');
     }
 }
