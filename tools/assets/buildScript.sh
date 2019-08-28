41cf93e8a5c31314b1697bafd6944faf5e5d750e
Pavel I <pavel@visualcomposer.com>
2019-08-28 08:59:35 +0300

    Minify all assets files VC-629 and create buildScript.sh

---
+++ tools/assets/buildScript.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+echo "### Build Default Script v1.0 12.11.2018 ### $(date)"
+
+EXECDIR=`pwd`
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+declare -a arr=($(find ${EXECDIR}/public/sources/assetsLibrary -mindepth 1 -maxdepth 1 -type d))
+
+TOTAL=0
+CNT=0
+PARALLELS_COUNT=4
+for i in "${arr[@]}";
+do {
+  i=${i//[$'\t\r\n']}
+  TOTAL=$(($TOTAL+1))
+  CNT=$(($CNT+1))
+  if cd $i; then
+    cd $i
+    ../../../../node_modules/.bin/webpack --config webpack.config.babel.js -p --no-info & pid=$1
+  fi
+
+  PID_LIST+=" $pid";
+  if [ "$CNT" -gt "$PARALLELS_COUNT" ]; then
+    wait $PID_LIST
+    PID_LIST=""
+    echo "..."
+    CNT=0
+  fi
+} done
+
+trap "kill $PID_LIST" SIGINT
+
+wait $PID_LIST
+
+echo
+echo "All processes have completed: $TOTAL";
+
+echo "Done!"
