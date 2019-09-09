659c3b6a0b86bec9322b1abc3394c1c00739f557
Nikita Hlopovs <nikita.h@visualcomposer.com>
2019-09-09 13:50:20 +0300

    Add wpWidgetsDefault e2e test VC-528

---
+++ tests/cypress/integration/defaultWidget.spec.js
@@ -0,0 +1,94 @@
+/* global describe, it, cy */
+
+const ELEMENT_NAME = 'WordPress Default Widget'
+
+describe(ELEMENT_NAME, function () {
+  it('Adds element to the page, checks automatically added elements, checks attributes', function () {
+    cy.fixture('../fixtures/defaultWidget.json').then((settings) => {
+      cy.createPage()
+      cy.addElement(ELEMENT_NAME)
+
+      cy.window().then((window) => {
+        cy.route('POST', window.vcvAjaxUrl).as('getWidget')
+      })
+
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('Widget')
+        .then(($field) => {
+          cy.wrap($field)
+            .next()
+            .select(settings.widgetType)
+        })
+
+      cy.wait('@getWidget')
+
+      cy.get('#widget-form-1-text')
+        .then(($field) => {
+          cy.wrap($field)
+            .clear()
+            .type(settings.widgetText)
+        })
+
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('Enable custom widget HTML')
+        .then(($field) => {
+          cy.wrap($field)
+            .next('.vcv-ui-form-switch-container')
+            .find('.vcv-ui-form-switch')
+            .click()
+        })
+
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('Before Title html')
+
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('After Title html')
+
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('Before Widget html')
+
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('After Widget html')
+
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('Element ID')
+        .then(($field) => {
+          cy.wrap($field)
+            .next()
+            .type(settings.customId)
+        })
+
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('Extra class name')
+        .then(($field) => {
+          cy.wrap($field)
+            .next()
+            .type(settings.customClass)
+        })
+
+      cy.setDO(settings.designOptions)
+
+      cy.savePage()
+      cy.viewPage()
+
+      cy.get('.vce-widgets-container')
+        .should('have.class', settings.customClass)
+        .should('have.attr', 'id', settings.customId)
+
+      cy.get('.textwidget')
+        .find('p')
+        .contains(settings.widgetText)
+
+      cy.get('.vce vce-widgets-wrapper')
+        .should('have.css', 'border-radius', settings.designOptions.borderRadius)
+        .and('have.css', 'border-width', settings.designOptions.borderWidth)
+        .and('have.css', 'border-style', settings.designOptions.borderStyle)
+        .and('have.css', 'border-color', settings.designOptions.borderColor.rgb)
+        .and('have.css', 'padding', settings.designOptions.padding)
+        .and('have.css', 'background-color', settings.designOptions.backgroundColor.rgb)
+        .and('have.css', 'animation-name', `vce-o-animate--${settings.designOptions.animation}`)
+        .should('have.attr', 'data-vce-animate', `vce-o-animate--${settings.designOptions.animation}`)
+        .and('have.attr', 'data-vcv-o-animated', 'true')
+    })
+  })
+})
