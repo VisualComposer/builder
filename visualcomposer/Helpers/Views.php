a690f3cb48752673d4a3fec81ecab39ff3c4f5bf
Janis K <janis.k@visualcomposer.com>
2019-09-10 14:07:36 +0300

    Fix wp backend settings for VC styles and html VC-667

--- visualcomposer/Helpers/Views.php
+++ visualcomposer/Helpers/Views.php
@@ -70,7 +70,9 @@ class Views extends container implements Helper
         global $wp_settings_fields;
         $wpSettingsFields = $wp_settings_fields;
         // @codingStandardsIgnoreEnd
-        $class = isset($section['vcv-args']) && isset($section['vcv-args']['class']) ? ' ' . esc_attr($section['vcv-args']['class']) : '';
+        $class = isset($section['vcv-args']) && isset($section['vcv-args']['class']) ? ' ' . esc_attr(
+                $section['vcv-args']['class']
+            ) : '';
 
         echo '<div class="' . esc_attr($slug) . '-section ' . esc_attr($section['id']) . $class . '">';
         if ($section['title']) {
@@ -81,14 +83,11 @@ class Views extends container implements Helper
             call_user_func($section['callback'], $section);
         }
 
-        if (!isset($wpSettingsFields) || !isset($wpSettingsFields[ $slug ])
-            || !isset($wpSettingsFields[ $slug ][ $section['id'] ])) {
-            echo '</div>';
-            return;
+        if (isset($wpSettingsFields, $wpSettingsFields[ $slug ], $wpSettingsFields[ $slug ][ $section['id'] ])) {
+            echo '<table class="form-table">';
+            do_settings_fields($slug, $section['id']);
+            echo '</table>';
         }
-        echo '<table class="form-table">';
-        do_settings_fields($slug, $section['id']);
-        echo '</table>';
         if (isset($section['children']) && !empty($section['children'])) {
             ?>
             <div class="vcv-child-section">
