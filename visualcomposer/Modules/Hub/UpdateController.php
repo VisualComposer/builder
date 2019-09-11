c08de2fabfb502480ff3b2361bfb921ef733b96a
Pavel I <pavel@visualcomposer.com>
2019-09-11 13:20:22 +0300

    Revert Hub UpdatesController changes to fix hub download process VC-657

--- visualcomposer/Modules/Hub/UpdateController.php
+++ visualcomposer/Modules/Hub/UpdateController.php
@@ -10,6 +10,7 @@ if (!defined('ABSPATH')) {
 
 use VisualComposer\Framework\Container;
 use VisualComposer\Framework\Illuminate\Support\Module;
+use VisualComposer\Helpers\Hub\Update;
 use VisualComposer\Helpers\Options;
 use VisualComposer\Helpers\Traits\EventsFilters;
 use VisualComposer\Helpers\Traits\WpFiltersActions;
@@ -21,8 +22,35 @@ class UpdateController extends Container implements Module
 
     public function __construct()
     {
+        $this->addEvent('vcv:admin:inited vcv:system:activation:hook', 'checkForUpdate');
+        $this->wpAddAction('admin_menu', 'checkForUpdate', 9);
+        $this->addFilter('vcv:editors:frontend:render', 'checkForUpdate', -1);
+
         // Factory reset
         $this->addEvent('vcv:system:activation:hook vcv:system:factory:reset', 'unsetOptions', -1);
+    }
+
+    /**
+     * @param $response
+     * @param \VisualComposer\Helpers\Options $optionsHelper
+     * @param \VisualComposer\Helpers\Hub\Update $hubUpdateHelper
+     *
+     * @return mixed
+     * @throws \ReflectionException
+     */
+    protected function checkForUpdate($response, Options $optionsHelper, Update $hubUpdateHelper)
+    {
+        if ($optionsHelper->getTransient('lastBundleUpdate') < time()) {
+            $result = $hubUpdateHelper->checkVersion();
+            if (!vcIsBadResponse($result)) {
+                $optionsHelper->setTransient('lastBundleUpdate', time() + DAY_IN_SECONDS);
+            } else {
+                //if failed try one more time after one hour
+                $optionsHelper->setTransient('lastBundleUpdate', time() + 3600);
+            }
+        }
+
+        return $response;
     }
 
     protected function unsetOptions(Options $optionsHelper)
