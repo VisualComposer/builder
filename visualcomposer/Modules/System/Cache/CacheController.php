a3e82088e4a3e4d32fdadae4981e18ef32c9cb39
Edgars V <edgars.v@visualcomposer.com>
2019-09-11 12:08:43 +0300

    VC-668 Add cache controller, clear cache on settings save and reset

---
+++ visualcomposer/Modules/System/Cache/CacheController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace VisualComposer\Modules\System\Cache;
+
+if (!defined('ABSPATH')) {
+    header('Status: 403 Forbidden');
+    header('HTTP/1.1 403 Forbidden');
+    exit;
+}
+
+use VisualComposer\Framework\Illuminate\Support\Module;
+use VisualComposer\Helpers\Traits\EventsFilters;
+use VisualComposer\Helpers\Traits\WpFiltersActions;
+use VisualComposer\Modules\System\Ajax\Controller;
+
+class CacheController extends Controller implements Module
+{
+    use EventsFilters;
+    use WpFiltersActions;
+
+    public function __construct()
+    {
+        $this->addEvent('vcv:system:cache:clearCache', 'clearCache');
+        $this->wpAddAction(
+            'update_option_vcv-settings',
+            function () {
+                vcevent('vcv:system:cache:clearCache');
+            }
+        );
+        $this->addEvent('vcv:system:factory:reset', 'clearCache');
+    }
+
+    protected function clearCache()
+    {
+        try {
+            // WP Super Cache
+            if (function_exists('wp_cache_clear_cache')) {
+                wp_cache_clear_cache();
+            }
+
+            // W3 Total Cahce
+            if (function_exists('w3tc_pgcache_flush')) {
+                w3tc_pgcache_flush();
+            }
+
+            // Clear Litespeed cache
+            if (class_exists('LiteSpeed_Cache_API') && method_exists('LiteSpeed_Cache_API', 'purge_all')) {
+                \LiteSpeed_Cache_API::purge_all();
+            }
+
+            // Site ground
+            if (class_exists('SG_CachePress_Supercacher')
+                && method_exists(
+                    'SG_CachePress_Supercacher ',
+                    'purge_cache'
+                )) {
+                \SG_CachePress_Supercacher::purge_cache(true);
+            }
+
+            // Endurance Cache
+            if (class_exists('Endurance_Page_Cache') && method_exists('Endurance_Page_Cache', 'purge_all')) {
+                $e = new \Endurance_Page_Cache;
+                $e->purge_all();
+            }
+
+            // WP Fastest Cache
+            if (isset($GLOBALS['wp_fastest_cache'])
+                && method_exists(
+                    $GLOBALS['wp_fastest_cache'],
+                    'deleteCache'
+                )) {
+                $GLOBALS['wp_fastest_cache']->deleteCache(true);
+            }
+        } catch (\Exception $e) {
+            return;
+        }
+    }
+}
