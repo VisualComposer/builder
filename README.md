3a6a773d09cca5b43d830f070f03c6e13b8abfe9
Edgars V <edgars.v@visualcomposer.com>
2019-09-12 16:13:01 +0300

    VC-688 VC-658 add readme.txt update GPL license

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Visual Composer Website Builder
 [![CircleCI](https://circleci.com/gh/VisualComposer/builder/tree/master.svg?style=svg)](https://circleci.com/gh/VisualComposer/builder/tree/master)
-[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)
+[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0.html)
 
 Visual Composer Website Builder is a drag and drop editor for WordPress. With an easy-to-use interface, it allows you to design layouts you always wanted. Coupled with premium templates, blocks, elements, and extensions - Visual Composer is all you need to create a website.
 
@@ -19,4 +19,4 @@ Check the [list of the latest improvements](./CHANGELOG.md).
 Read our [contributing guide](./CONTRIBUTING.md) to learn about our development process. How to build your changes to Visual Composer.
 
 ## License
-Visual Composer is [GPL licensed](./license.txt).
+Visual Composer is [GPL licensed](http://www.gnu.org/licenses/gpl-3.0.html).
--- plugin-wordpress.php
+++ plugin-wordpress.php
@@ -1,17 +1,18 @@
 <?php
 /**
- * Plugin Name: Visual Composer Website Builder
- * Plugin URI: https://visualcomposer.com/?utm_medium=wp-dashboard&utm_source=plugins-page&utm_campaign=vcwb&utm_content=plugin-link
+ * Plugin Name: Visual Composer
+ * Plugin URI: https://visualcomposer.com/premium/?utm_medium=wp-dashboard&utm_source=plugins-page&utm_campaign=vcwb&utm_content=plugin-link
  * Description: Create your WordPress website with the fast and easy-to-use drag-and-drop builder for experts and beginners.
  * Version: dev
- * Author: Visual Composer Team
+ * Author: visualcomposer.com
  * Author URI: https://visualcomposer.com/?utm_medium=wp-dashboard&utm_source=plugins-page&utm_campaign=vcwb&utm_content=author-link
  * Copyright: (c) 2017 TechMill Ltd.
- * License: GNU General Public License v2.0
- * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Requires at least: 4.5
- * Tested up to: 4.9.4
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Requires at least: 4.6
+ * Tested up to: 5.2.3
  * Text Domain: vcwb
+ * Domain Path: /languages/
  */
 
 /**
