22349787b23455743008f3bc2cc5c1fbe236c757
Janis Konutis <mcjohn@inbox.lv>
2019-10-07 13:54:03 +0300

    Add shortcode cypress test VC-524

---
+++ tests/cypress/integration/shortcode.spec.js
@@ -0,0 +1,45 @@
+/* global describe, it, cy */
+
+const ELEMENT_NAME = 'Shortcode'
+
+describe(ELEMENT_NAME, function () {
+  it('Adds element to the page, checks automatically added elements, checks attributes', function () {
+    cy.fixture('../fixtures/shortcode.json').then((settings) => {
+      cy.createPage()
+      cy.addElement(ELEMENT_NAME)
+
+      cy.get('.vcv-ui-form-input').first()
+        .clear()
+        .type(settings.shortcode)
+
+      cy.wait(3000)
+
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('Element ID')
+        .then(($field) => {
+          cy.wrap($field)
+            .next()
+            .type(settings.customId)
+        })
+
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('Extra class name')
+        .then(($field) => {
+          cy.wrap($field)
+            .next()
+            .type(settings.customClass)
+        })
+
+
+
+      cy.savePage()
+      cy.viewPage()
+
+      cy.get(`#${settings.customId}`)
+        .should('have.class', settings.customClass)
+        .find('a')
+        .should('have.attr', 'href', settings.embedHref)
+
+    })
+  })
+})
