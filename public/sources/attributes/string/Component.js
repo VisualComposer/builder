952c0390ab761bc710f655ef231980a1107b941b
Janis Konutis <mcjohn@inbox.lv>
2019-10-24 20:45:20 +0300

    Add validation for input type

--- public/sources/attributes/string/Component.js
+++ public/sources/attributes/string/Component.js
@@ -9,23 +9,31 @@ export default class StringAttribute extends Attribute {
     fieldType: 'string'
   }
 
+  constructor (props) {
+    super(props)
+    this.input = React.createRef()
+  }
+
   render () {
     let { value } = this.state
     let { placeholder, options } = this.props
     if (!placeholder && this.props.options && this.props.options.placeholder) {
       placeholder = this.props.options.placeholder
     }
+    const inputType = options && options.inputType ? options.inputType : 'text'
 
     let fieldClassNames = classNames({
       'vcv-ui-form-input': true,
-      'vcv-ui-form-field-dynamic': env('VCV_JS_FT_DYNAMIC_FIELDS') && options && options.dynamicField
+      'vcv-ui-form-field-dynamic': env('VCV_JS_FT_DYNAMIC_FIELDS') && options && options.dynamicField,
+      'vcv-ui-form-input--error': options && options.inputType && this.input.current && !this.input.current.checkValidity()
     })
     let fieldComponent = <input
       className={fieldClassNames}
-      type='text'
+      type={inputType}
       onChange={this.handleChange}
       placeholder={placeholder}
       value={value}
+      ref={this.input}
     />
 
     return (
