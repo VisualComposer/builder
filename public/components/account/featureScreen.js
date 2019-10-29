953a332e3b24854dd67d38d1fdcb0e6c6229e5a1
Janis Konutis <mcjohn@inbox.lv>
2019-10-29 11:37:22 +0200

    Add screens for activation in wordpress panel VC-732

---
+++ public/components/account/featureScreen.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import VCVLogo from './vcvLogo'
+import VersionBox from './versionBox'
+
+export default class FeatureScreen extends React.Component {
+  static localizations = window.VCV_I18N && window.VCV_I18N()
+
+
+  constructor (props) {
+    super(props)
+    this.activationContent = React.createRef()
+
+    this.handleFreeClick = this.handleFreeClick.bind(this)
+  }
+
+  componentDidMount () {
+    setTimeout(() => {
+      this.activationContent.current && this.activationContent.current.classList.add('vcv-activation-content--active')
+    }, 200)
+  }
+
+
+
+  handleFreeClick () {
+    console.log('free')
+  }
+
+
+  render () {
+    return (
+      <div className='vcv-activation-content' ref={this.activationContent}>
+        <VCVLogo />
+        <VersionBox />
+        <p className='vcv-activation-heading'>
+          Get Access to the Visual Composer Hub
+        </p>
+
+        <div className='vcv-activation-box-container'>
+          <div className='vcv-activation-box'>
+            <h3 className='vcv-activation-box-heading'>Free License</h3>
+            <ul className='vcv-basic-list'>
+              <li className='vcv-basic-list-item'>Limited access to the Visual Composer Hub of elements, templates, and extensions</li>
+              <li className='vcv-basic-list-item vcv-basic-list-item--not-included'>Theme builder with Header, Footer, and Sidebar editor</li>
+              <li className='vcv-basic-list-item vcv-basic-list-item--not-included'>WooCommerce compatibility</li>
+              <li className='vcv-basic-list-item vcv-basic-list-item--not-included'>Premium support and updates</li>
+            </ul>
+            <div className='vcv-activation-button-container'>
+              <button type='button' onClick={this.handleFreeClick} className='vcv-activation-button vcv-activation-button--dark'>Activate free</button>
+            </div>
+          </div>
+          <div className='vcv-activation-box'>
+            <h3 className='vcv-activation-box-heading'>Premium License</h3>
+            <ul className='vcv-basic-list'>
+              <li className='vcv-basic-list-item'>Unlimited access to the Visual Composer Hub of elements, templates, and extensions</li>
+              <li className='vcv-basic-list-item'>Theme builder with Header, Footer, and Sidebar editor</li>
+              <li className='vcv-basic-list-item'>WooCommerce compatibility</li>
+              <li className='vcv-basic-list-item'>Premium support and updates</li>
+            </ul>
+            <div className='vcv-activation-button-container'>
+              <button type='button' onClick={this.props.setActiveScreen.bind(this, 'vcv-activate-premium')} className='vcv-activation-button'>Activate Premium</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
