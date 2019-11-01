c8a4bbab0da7fa164fd514cc4d33ec9ba3b398ca
Janis Konutis <mcjohn@inbox.lv>
2019-11-01 12:29:12 +0200

    Add localizations VC-732

--- public/components/account/activatePremiumScreen.js
+++ public/components/account/activatePremiumScreen.js
@@ -6,7 +6,7 @@ import { getResponse } from 'public/tools/response'
 
 const dataProcessor = getService('dataProcessor')
 
-export default class FeatureScreen extends React.Component {
+export default class ActivatePremiumScreen extends React.Component {
   static localizations = window.VCV_I18N && window.VCV_I18N()
 
   constructor (props) {
@@ -41,7 +41,6 @@ export default class FeatureScreen extends React.Component {
   handleActivateClick (type, e) {
     e.preventDefault()
     this.setState({
-      licenseValue: '',
       hasError: false,
       errorText: ''
     })
@@ -94,14 +93,16 @@ export default class FeatureScreen extends React.Component {
   render () {
     const { hasError, errorText, loading } = this.state
     let stagingButton = null
+
     if (window.VCV_STAGING_AVAILABLE && window.VCV_STAGING_AVAILABLE()) {
       let buttonClasses = 'vcv-activation-button vcv-activation-button--dark'
       if (loading === 'staging') {
         buttonClasses += ' vcv-activation-button--loading'
       }
+      const activateStagingText = ActivatePremiumScreen.localizations ? ActivatePremiumScreen.localizations.activateStaging : 'Activate Staging'
       stagingButton = <div className='vcv-activation-button-container'>
         <button className={buttonClasses} onClick={this.handleActivateClick.bind(this, 'staging')}>
-          Activate staging
+          {activateStagingText}
         </button>
       </div>
     }
@@ -128,25 +129,38 @@ export default class FeatureScreen extends React.Component {
       activationBoxClasses += ' vcv-activation--loading'
     }
 
+    const getPremiumFeaturesText1 = ActivatePremiumScreen.localizations ? ActivatePremiumScreen.localizations.getPremiumFeaturesText1 : 'Get Premium Elements, Templates,'
+    const getPremiumFeaturesText2 = ActivatePremiumScreen.localizations ? ActivatePremiumScreen.localizations.getPremiumFeaturesText2 : 'Extensions, and Support'
+    const whatYouWillGetText = ActivatePremiumScreen.localizations ? ActivatePremiumScreen.localizations.whatYouWillGet : 'What You Will Get?'
+    const limitedAccessToExtensionsText = ActivatePremiumScreen.localizations ? ActivatePremiumScreen.localizations.limitedAccessToExtensions : 'Limited access to the Visual Composer Hub of elements, templates, and extensions'
+    // const activateStagingText = ActivatePremiumScreen.localizations ? ActivatePremiumScreen.localizations.activateStaging : 'Activate Staging'
+
+    const themeBuilderWithHFSText = ActivatePremiumScreen.localizations ? ActivatePremiumScreen.localizations.themeBuilderWithHFS : 'Theme builder with Header, Footer, and Sidebar editor'
+    const wooCommerceCompatibilityText = ActivatePremiumScreen.localizations ? ActivatePremiumScreen.localizations.wooCommerceCompatibility : 'WooCommerce compatibility'
+    const premiumSupportAndUpdatesText = ActivatePremiumScreen.localizations ? ActivatePremiumScreen.localizations.premiumSupportAndUpdates : 'Premium support and updates'
+    const iWantToGoPremiumText = ActivatePremiumScreen.localizations ? ActivatePremiumScreen.localizations.iWantToGoPremium : 'I want to go premium'
+    const findSubscriptionLicenseAtText = ActivatePremiumScreen.localizations ? ActivatePremiumScreen.localizations.findSubscriptionLicenseAt : 'You can find your Visual Composer Premium subscription license key by accessing our Customer Portal at'
+    const activatePremiumText = ActivatePremiumScreen.localizations ? ActivatePremiumScreen.localizations.activatePremium : 'Activate Premium'
+
     return (
       <div className='vcv-activation-content' ref={this.activationContent}>
         <VCVLogo />
         <VersionBox />
         <p className='vcv-activation-heading'>
-          Get Premium Elements, Templates,<br />
-          Extensions, and Support
+          {getPremiumFeaturesText1}<br />
+          {getPremiumFeaturesText2}
         </p>
         <div className={activationBoxClasses}>
           <div className='vcv-activation-box'>
-            <h3 className='vcv-activation-box-heading'>What You Will Get?</h3>
+            <h3 className='vcv-activation-box-heading'>{whatYouWillGetText}</h3>
             <ul className='vcv-basic-list'>
-              <li className='vcv-basic-list-item'>Limited access to the Visual Composer Hub of elements, templates, and extensions</li>
-              <li className='vcv-basic-list-item'>Theme builder with Header, Footer, and Sidebar editor</li>
-              <li className='vcv-basic-list-item'>WooCommerce compatibility</li>
-              <li className='vcv-basic-list-item'>Premium support and updates</li>
+              <li className='vcv-basic-list-item'>{limitedAccessToExtensionsText}</li>
+              <li className='vcv-basic-list-item'>{themeBuilderWithHFSText}</li>
+              <li className='vcv-basic-list-item'>{wooCommerceCompatibilityText}</li>
+              <li className='vcv-basic-list-item'>{premiumSupportAndUpdatesText}</li>
             </ul>
             <div className='vcv-activation-button-container'>
-              <a href={window.VCV_PREMIUM_URL()} className='vcv-activation-button vcv-activation-button--dark'>I want to go premium</a>
+              <a href={window.VCV_PREMIUM_URL()} className='vcv-activation-button vcv-activation-button--dark'>{iWantToGoPremiumText}</a>
             </div>
           </div>
           <div className='vcv-activation-box'>
@@ -155,11 +169,11 @@ export default class FeatureScreen extends React.Component {
               <input placeholder='Enter your license key' className={inputClasses} required='' name='licenseKey' type='text' value={this.state.licenseValue} onChange={this.handleInputChange} maxLength='36' onFocus={this.handleInputFocus} />
             </div>
             <div className='vcv-activation-input-description'>
-              You can find your Visual Composer Premium subscription license key by accessing our Customer Portal at <a href="https://account.visualcomposer.com" className='vcv-activation-link'>account.visualcomposer.com</a>
+              {findSubscriptionLicenseAtText} <a href="https://account.visualcomposer.com" className='vcv-activation-link'>account.visualcomposer.com</a>
             </div>
             <div className='vcv-activation-button-container'>
               <button className={premiumButtonClasses} onClick={this.handleActivateClick.bind(this, 'premium')}>
-                Activate premium
+                {activatePremiumText}
               </button>
             </div>
             {stagingButton}
--- public/components/account/featureScreen.js
+++ public/components/account/featureScreen.js
@@ -5,7 +5,6 @@ import VersionBox from './versionBox'
 export default class FeatureScreen extends React.Component {
   static localizations = window.VCV_I18N && window.VCV_I18N()
 
-
   constructor (props) {
     super(props)
     this.activationContent = React.createRef()
@@ -19,37 +18,45 @@ export default class FeatureScreen extends React.Component {
 
   render () {
     const activateFreeLink = window.VCV_PREMIUM_URL()
+    const activatePremiumText = FeatureScreen.localizations ? FeatureScreen.localizations.activatePremium : 'Activate Premium'
+    const headingText = FeatureScreen.localizations ? FeatureScreen.localizations.getAccessToTheVisualComposerHub : 'Get Access to the Visual Composer Hub'
+    const freeText = FeatureScreen.localizations ? FeatureScreen.localizations.freeLicense : 'Free License'
+    const limitedText = FeatureScreen.localizations ? FeatureScreen.localizations.limitedAccessToExtensions : 'Limited access to the Visual Composer Hub of elements, templates, and extensions'
+    const themeBuilderWithHFSText = FeatureScreen.localizations ? FeatureScreen.localizations.themeBuilderWithHFS : 'Theme builder with Header, Footer, and Sidebar editor'
+    const wooCommerceCompatibilityText = FeatureScreen.localizations ? FeatureScreen.localizations.wooCommerceCompatibility : 'WooCommerce compatibility'
+    const premiumSupportAndUpdatesText = FeatureScreen.localizations ? FeatureScreen.localizations.premiumSupportAndUpdates : 'Premium support and updates'
+    const activateFreeText = FeatureScreen.localizations ? FeatureScreen.localizations.activateFree : 'Activate Free'
+    const premiumLicenseText = FeatureScreen.localizations ? FeatureScreen.localizations.premiumLicense : 'Premium License'
+    const unlimitedAccessToExtensionsText = FeatureScreen.localizations ? FeatureScreen.localizations.unlimitedAccessToExtensions : 'Unlimited access to the Visual Composer Hub of elements, templates, and extensions'
 
     return (
       <div className='vcv-activation-content' ref={this.activationContent}>
         <VCVLogo />
         <VersionBox />
-        <p className='vcv-activation-heading'>
-          Get Access to the Visual Composer Hub
-        </p>
+        <p className='vcv-activation-heading'>{headingText}</p>
         <div className='vcv-activation-box-container'>
           <div className='vcv-activation-box'>
-            <h3 className='vcv-activation-box-heading'>Free License</h3>
+            <h3 className='vcv-activation-box-heading'>{freeText}</h3>
             <ul className='vcv-basic-list'>
-              <li className='vcv-basic-list-item'>Limited access to the Visual Composer Hub of elements, templates, and extensions</li>
-              <li className='vcv-basic-list-item vcv-basic-list-item--not-included'>Theme builder with Header, Footer, and Sidebar editor</li>
-              <li className='vcv-basic-list-item vcv-basic-list-item--not-included'>WooCommerce compatibility</li>
-              <li className='vcv-basic-list-item vcv-basic-list-item--not-included'>Premium support and updates</li>
+              <li className='vcv-basic-list-item'>{limitedText}</li>
+              <li className='vcv-basic-list-item vcv-basic-list-item--not-included'>{themeBuilderWithHFSText}</li>
+              <li className='vcv-basic-list-item vcv-basic-list-item--not-included'>{wooCommerceCompatibilityText}</li>
+              <li className='vcv-basic-list-item vcv-basic-list-item--not-included'>{premiumSupportAndUpdatesText}</li>
             </ul>
             <div className='vcv-activation-button-container'>
-              <a href={activateFreeLink} className='vcv-activation-button vcv-activation-button--dark'>Activate free</a>
+              <a href={activateFreeLink} className='vcv-activation-button vcv-activation-button--dark'>{activateFreeText}</a>
             </div>
           </div>
           <div className='vcv-activation-box'>
-            <h3 className='vcv-activation-box-heading'>Premium License</h3>
+            <h3 className='vcv-activation-box-heading'>{premiumLicenseText}</h3>
             <ul className='vcv-basic-list'>
-              <li className='vcv-basic-list-item'>Unlimited access to the Visual Composer Hub of elements, templates, and extensions</li>
-              <li className='vcv-basic-list-item'>Theme builder with Header, Footer, and Sidebar editor</li>
-              <li className='vcv-basic-list-item'>WooCommerce compatibility</li>
-              <li className='vcv-basic-list-item'>Premium support and updates</li>
+              <li className='vcv-basic-list-item'>{unlimitedAccessToExtensionsText}</li>
+              <li className='vcv-basic-list-item'>{themeBuilderWithHFSText}</li>
+              <li className='vcv-basic-list-item'>{wooCommerceCompatibilityText}</li>
+              <li className='vcv-basic-list-item'>{premiumSupportAndUpdatesText}</li>
             </ul>
             <div className='vcv-activation-button-container'>
-              <button type='button' onClick={this.props.setActiveScreen.bind(this, 'vcv-go-premium')} className='vcv-activation-button'>Activate Premium</button>
+              <button type='button' onClick={this.props.setActiveScreen.bind(this, 'vcv-go-premium')} className='vcv-activation-button'>{activatePremiumText}</button>
             </div>
           </div>
         </div>
