40e5d64839efa32232bd640365faa2a270f57fb5
Çağdaş Dağ <cagdasdag81@gmail.com>
2019-11-07 16:55:54 +0300

    Codestyle fix

--- public/components/wpVcSettings/dropdownEditLink.js
+++ public/components/wpVcSettings/dropdownEditLink.js
@@ -1,32 +1,34 @@
 const $ = window.jQuery
 const editLinkClass = 'vcv-custom-page-templates-404-page-edit-link'
-const editLinkItem = '.' + editLinkClass
-const dropdownID = '#vcv-custom-page-templates-404-page'
+const dropdownItem = $('#vcv-custom-page-templates-404-page')
 
 const localizations = window.VCV_I18N && window.VCV_I18N()
 const editLinkText = localizations && localizations.edit ? localizations.edit : 'Edit'
 
 const changeEditLink = () => {
-  let selectedPageUrl = $(dropdownID).find('option:selected').attr('data-url')
-  let dropdownContainer = $(dropdownID).closest('.vcv-ui-form-group').parent('td')
+  const selectedPageUrl = dropdownItem.find('option:selected').attr('data-url')
+  const dropdownContainer = dropdownItem.closest('.vcv-ui-form-group').parent('td')
+  const editLinkItem = $('.' + editLinkClass)
 
-  if (!$(dropdownID).val()) {
-    $(editLinkItem).remove()
-  } else if ($(editLinkItem).length) {
-    $(editLinkItem).attr('href', selectedPageUrl)
+  if (dropdownItem.val()) {
+    if (editLinkItem.length) {
+      editLinkItem.attr('href', selectedPageUrl)
+    } else {
+      dropdownContainer.append(`<a href='${selectedPageUrl}' class='${editLinkClass}' target='_blank'>${editLinkText}</a>`)
+    }
   } else {
-    dropdownContainer.append(`<a href='${selectedPageUrl}' class='${editLinkClass}' target='_blank'>${editLinkText}</a>`)
+    editLinkItem.remove()
   }
 }
 
 export const dropdownEditLink = () => {
   // Initial Page Load
-  if ($(dropdownID + ' option:selected').length) {
+  if (dropdownItem.find('option:selected').length) {
     changeEditLink()
   }
 
   // Change Event
-  $(dropdownID).on('change', function () {
+  dropdownItem.on('change', function () {
     changeEditLink()
   })
 }
