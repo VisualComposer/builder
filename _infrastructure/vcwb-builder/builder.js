f556110ae60744ad12217ce8f442501233fd84fe
Vjaceslav Vasenin <time2type@gmail.com>
2019-11-08 13:56:10 +0200

    Add release builder VC-797

---
+++ _infrastructure/vcwb-builder/builder.js
@@ -0,0 +1,12 @@
+const Plugin = require('./controller/plugin')
+const program = require('commander')
+
+program.command('plugin')
+  .description('Build VCWB Wordpress plugin zip archive')
+  .option('-p, --path <s>', 'Path where to create zip file')
+  .option('-b, --bundleVersion <s>', 'Add version to bundle.')
+  .action((options) => {
+    const plugin = new Plugin(options.path, options.bundleVersion)
+    plugin.build()
+  })
+program.parse(process.argv)
