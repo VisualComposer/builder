0dae1f4224c5363fd5525edcce0fc9562c3ecf06
Pavel I <pavel@visualcomposer.com>
2019-12-09 16:19:24 +0200

    Disable update page if not activated VC-900
    Signed-off-by: Pavel I <pavel@visualcomposer.com>

---
+++ visualcomposer/Modules/Migrations/LicenseMigration.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace VisualComposer\Modules\Migrations;
+
+if (!defined('ABSPATH')) {
+    header('Status: 403 Forbidden');
+    header('HTTP/1.1 403 Forbidden');
+    exit;
+}
+
+use VisualComposer\Framework\Illuminate\Support\Module;
+use VisualComposer\Helpers\License;
+
+/**
+ * Class LicenseMigration
+ *
+ * This migration setup license type for licenses
+ *
+ * @package VisualComposer\Modules\Migrations
+ */
+class LicenseMigration extends MigrationsController implements Module
+{
+    protected $migrationId = 'licenseMigrationV24';
+
+    protected $migrationPriority = 11;
+
+    protected function run(License $licenseHelper)
+    {
+        $key = $licenseHelper->getKey();
+        if (!empty($key)) {
+            $type = $licenseHelper->getType();
+            if (empty($type)) {
+                // If no type then it is premium license
+                $licenseHelper->setType('premium');
+            }
+        }
+
+        return true;
+    }
+}
