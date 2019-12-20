1de3227c5daa1c5351e6e5bafabac061c360850a
Janis Konutis <mcjohn@inbox.lv>
2019-12-20 10:11:58 +0200

    Add event for setting initial value VC-929

--- public/editor/stores/fieldOptionsStorage.js
+++ public/editor/stores/fieldOptionsStorage.js
@@ -1,4 +1,16 @@
 import { addStorage } from 'vc-cake'
 addStorage('fieldOptions', (storage) => {
+  storage.on('setElementInitialValue', (elementTag, attrKey, value) => {
+    let elementValues = storage.state('elementInitialValue').get() || {}
 
+    if (elementValues[elementTag]) {
+      elementValues[elementTag][attrKey] = value
+    } else {
+      elementValues[elementTag] = {
+        [attrKey]: value
+      }
+    }
+
+    storage.state('elementInitialValue').set(elementValues)
+  })
 })
