59c5411987650c726bb2761946299ca8ddef500b
Nikita <nikita.h@visualcomposer.com>
2020-02-08 19:16:22 +0200

    Refactor basicButton e2e test

--- tests/cypress/integration/basicButton.spec.js
+++ tests/cypress/integration/basicButton.spec.js
@@ -8,139 +8,45 @@ describe(ELEMENT_NAME, function () {
       cy.createPage()
       cy.addElement(ELEMENT_NAME)
 
-      cy.get('.vcv-ui-form-group-heading')
-        .contains('Button text')
-        .then(($field) => {
-          cy.wrap($field)
-            .next()
-            .clear()
-            .type(settings.buttonText)
-        })
-
-      cy.get('.vcv-ui-form-link-button').click()
-      cy.get('.vcv-ui-modal .vcv-ui-form-group-heading')
-        .contains('URL')
-        .then(($field) => {
-          cy.wrap($field)
-            .next('.vcv-ui-editor-dropdown-input-container')
-            .find('input')
-            .type(settings.buttonLink)
-        })
-      cy.get('.vcv-ui-modal label[for="targetBlank-0-1"]').click()
-      cy.get('.vcv-ui-modal .vcv-ui-modal-action').click()
-      cy.get('.vcv-ui-form-group-heading')
-        .contains('Alignment')
-        .then(($field) => {
-          cy.wrap($field)
-            .next('.vcv-ui-form-buttons-group')
-            .find(`.vcv-ui-form-button[data-value="${settings.alignment}"]`)
-            .click()
-        })
-
-      cy.get('.vcv-ui-form-group-heading')
-        .contains('Shape')
-        .then(($field) => {
-          cy.wrap($field)
-            .next('.vcv-ui-form-buttons-group')
-            .find('.vcv-ui-form-button[data-value="rounded"]')
-            .click()
-        })
-
-      cy.get('.vcv-ui-form-group-heading')
-        .contains('Size')
-        .then(($field) => {
-          cy.wrap($field)
-            .next('.vcv-ui-form-buttons-group')
-            .find('.vcv-ui-form-button[data-value="small"]')
-            .click()
-        })
-
-      cy.get('.vcv-ui-form-group-heading')
-        .contains('Stretch button')
-        .then(($field) => {
-          cy.wrap($field)
-            .next('.vcv-ui-form-switch-container')
-            .find('.vcv-ui-form-switch')
-            .click()
-        })
-
-      cy.get('.vcv-ui-form-group-heading')
-        .contains('Title color')
-        .then(($field) => {
-          cy.wrap($field)
-            .next('div')
-            .find('.vcv-ui-color-picker-dropdown')
-            .click()
-          cy.get('.vcv-ui-color-picker-custom-color input[value="FFFFFF"]')
-            .clear()
-            .type(settings.titleColor.hex)
-          cy.wrap($field)
-            .next('div')
-            .find('.vcv-ui-color-picker-dropdown')
-            .click()
-        })
-
-      cy.get('.vcv-ui-form-group-heading')
-        .contains('Background color')
-        .then(($field) => {
-          cy.wrap($field)
-            .next('div')
-            .find('.vcv-ui-color-picker-dropdown')
-            .click()
-          cy.get('.vcv-ui-color-picker-custom-color input[value="557CBF"]')
-            .clear()
-            .type(settings.backgroundColor.hex)
-          cy.wrap($field)
-            .next('div')
-            .find('.vcv-ui-color-picker-dropdown')
-            .click()
-        })
-
-      cy.get('.vcv-ui-form-group-heading')
-        .contains('Element ID')
-        .then(($field) => {
-          cy.wrap($field)
-            .next()
-            .type(settings.customId)
-        })
-
-      cy.get('.vcv-ui-form-group-heading')
-        .contains('Extra class name')
-        .then(($field) => {
-          cy.wrap($field)
-            .next()
-            .type(settings.customClass)
-        })
-
-      cy.get('.vcv-ui-form-group-heading')
-        .contains('Animate')
-        .then(($field) => {
-          cy.wrap($field)
-            .next()
-            .select('bounce')
-        })
+      cy.setInput('Button text', settings.buttonText)
+      cy.setURL('Link selection', settings.buttonLink)
+      cy.setButtonGroup('Alignment', settings.alignment)
+      cy.setButtonGroup('Shape', settings.shape.name)
+      cy.setButtonGroup('Size', settings.size.name)
+      cy.setSwitch('Stretch button')
+      cy.setColor(settings.titleColor)
+      cy.setColor(settings.backgroundColor)
+      cy.setClassAndId(settings.customId, settings.customClass)
+      cy.setDO(settings.designOptions)
 
       cy.savePage()
       cy.viewPage()
 
-      cy.get('.vce-button--style-basic-container')
-        .should('have.class', settings.customClass)
+      cy.get(`.${settings.customClass}.vce-button--style-basic-container--align-${settings.alignment}`)
         .should('have.css', 'text-align', settings.alignment)
-
-      cy.get('.vce-button--style-basic-wrapper--stretched')
-        .should('have.css', 'width')
 
       cy.get(`#${settings.customId}`)
         .contains(settings.buttonText)
-        .should('have.css', 'border-radius', '5px')
-        .and('have.css', 'padding', '10px 30px')
-        .and('have.css', 'color', settings.titleColor.rgb)
-        .and('have.css', 'background-color', settings.backgroundColor.rgb)
-        .and('have.css', 'animation-name', 'vce-o-animate--bounce')
-        .should('have.attr', 'data-vce-animate', 'vce-o-animate--bounce')
+        .should('have.css', 'border-radius', settings.shape.cssValue)
+        .and('have.css', 'padding', settings.size.cssValue)
+        .and('have.css', 'color', settings.titleColor.valueRgb)
+        .and('have.css', 'background-color', settings.backgroundColor.valueRgb)
+        .and('have.css', 'animation-name', `vce-o-animate--${settings.designOptions.animation}`)
+        .should('have.attr', 'data-vce-animate', `vce-o-animate--${settings.designOptions.animation}`)
         .and('have.attr', 'data-vcv-o-animated', 'true')
-        .and('have.attr', 'href', `http://${settings.buttonLink}`)
+        .and('have.attr', 'href', `http://${settings.buttonLink.url}`)
         .and('have.attr', 'target', '_blank')
+
+      cy.get('.vce-button--style-basic-wrapper--stretched')
+        .then((element) => {
+          let contentContainerWidth
+          cy.window().then((window) => {
+            const contentContainer = window.document.querySelector( '.vce-row-container')
+            contentContainerWidth = window.getComputedStyle(contentContainer).width
+            cy.wrap(element)
+              .should('have.css', 'width', contentContainerWidth)
+          })
+        })
 
       if (Cypress.env('checkSnapshots')) {
         cy.wait(2000)
