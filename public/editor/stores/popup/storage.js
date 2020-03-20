0e1598549afff6118d3cd596fa26e69d993ecc3b
Janis <mcjohn@inbox.lv>
2020-03-20 10:39:46 +0200

    Update storage.js

--- public/editor/stores/popup/storage.js
+++ public/editor/stores/popup/storage.js
@@ -6,52 +6,6 @@ addStorage('popup', (storage) => {
   const cook = getService('cook')
   const dataProcessor = getService('dataProcessor')
 
-  const getPopupId = (attrValue) => {
-    if (attrValue && attrValue.url && attrValue.type === 'popup') {
-      const popupId = attrValue.url.split('#vcv-popup-')
-      if (popupId && popupId[1]) {
-        return popupId[1]
-      }
-      return null
-    }
-    return null
-  }
-
-  const parseUrlAttributes = (cookElement) => {
-    const urlAttrKeys = cookElement.filter((key, value, settings) => {
-      return settings.type === 'url'
-    })
-
-    urlAttrKeys.forEach((urlKey) => {
-      const urlValue = cookElement.get(urlKey)
-      const popupId = getPopupId(urlValue)
-      if (popupId) {
-        storage.trigger('addPopupHtml', popupId)
-      }
-    })
-
-    const attachImageAttrKeys = cookElement.filter((key, value, settings) => {
-      return settings.type === 'attachimage'
-    })
-
-    attachImageAttrKeys.forEach((attachImageKey) => {
-      const imageValue = cookElement.get(attachImageKey)
-      if (Array.isArray(imageValue)) {
-        imageValue.forEach((imgValue) => {
-          const popupId = getPopupId(imgValue.link)
-          if (popupId) {
-            storage.trigger('addPopupHtml', popupId)
-          }
-        })
-      } else if (typeof imageValue === 'object') {
-        const popupId = getPopupId(imageValue.link)
-        if (popupId) {
-          storage.trigger('addPopupHtml', popupId)
-        }
-      }
-    })
-  }
-
   storage.on('addPopupHtml', (id) => {
     storage.state('popupAddInProgress').set(true)
     const contentWindow = window.document.querySelector('.vcv-layout-iframe').contentWindow
