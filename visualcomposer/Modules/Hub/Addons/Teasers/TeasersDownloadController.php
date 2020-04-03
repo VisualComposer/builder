76edd7c6c632f3defc804696a213d09ed45f0411
Nikita <nikita.h@visualcomposer.com>
2020-04-03 13:40:23 +0300

    Add metaAddonImageUrl property to addon data VC-1067

--- visualcomposer/Modules/Hub/Addons/Teasers/TeasersDownloadController.php
+++ visualcomposer/Modules/Hub/Addons/Teasers/TeasersDownloadController.php
@@ -42,6 +42,7 @@ class TeasersDownloadController extends Container implements Module
                 'metaThumbnailUrl' => $addon['thumbnailUrl'],
                 'metaPreviewUrl' => $addon['previewUrl'],
                 'metaDescription' => $addon['description'],
+                'metaAddonImageUrl' => $addon['addonImageUrl'],
                 'type' => 'addon',
                 'update' => isset($addon['update']) ? $addon['update'] : false,
                 'allowDownload' => isset($addon['allowDownload']) ? $addon['allowDownload'] : false,
