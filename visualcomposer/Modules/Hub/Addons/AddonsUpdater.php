3e3b0aa05abf4e181044fb63a1bb87442ba03ef1
Nikita <nikita.h@visualcomposer.com>
2020-04-03 14:01:06 +0300

    Update addons data VC-1067

--- visualcomposer/Modules/Hub/Addons/AddonsUpdater.php
+++ visualcomposer/Modules/Hub/Addons/AddonsUpdater.php
@@ -11,6 +11,7 @@ if (!defined('ABSPATH')) {
 use VisualComposer\Framework\Container;
 use VisualComposer\Framework\Illuminate\Support\Module;
 use VisualComposer\Helpers\Differ;
+use VisualComposer\Helpers\Hub\Addons as HubAddons;
 use VisualComposer\Helpers\Traits\EventsFilters;
 
 class AddonsUpdater extends Container implements Module
@@ -21,8 +22,7 @@ class AddonsUpdater extends Container implements Module
     {
         $this->addFilter('vcv:hub:download:bundle vcv:hub:download:bundle:addon/*', 'updateAddons');
     }
-
-    protected function updateAddons($response, $payload)
+    protected function updateAddons($response, $payload, HubAddons $addonsHelper)
     {
         if (vcvenv('VCV_ENV_DEV_ADDONS')) {
             return ['status' => true];
@@ -64,6 +64,21 @@ class AddonsUpdater extends Container implements Module
                         $addonsKey . '/' . $addonsData['bundlePath']
                     );
                 }
+            }
+            if (isset($addonsData['settings']['metaThumbnailUrl'])) {
+                $addonsData['settings']['metaThumbnailUrl'] = $addonsHelper->getAddonUrl(
+                    $addonsData['settings']['metaThumbnailUrl']
+                );
+            }
+            if (isset($addonsData['settings']['metaPreviewUrl'])) {
+                $addonsData['settings']['metaPreviewUrl'] = $addonsHelper->getAddonUrl(
+                    $addonsData['settings']['metaPreviewUrl']
+                );
+            }
+            if (isset($addonsData['settings']['metaAddonImageUrl'])) {
+                $addonsData['settings']['metaAddonImageUrl'] = $addonsHelper->getAddonUrl(
+                    $addonsData['settings']['metaAddonImageUrl']
+                );
             }
             $response['addons'][] = $addonsData;
         }
