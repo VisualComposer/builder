7b2c8fcf62eb7e3f083207cc88988a9fd2df6add
Pavel I <pavel@visualcomposer.com>
2020-04-27 12:52:30 +0300

    VC-1201 try to locate wordpress dynamically by SCRIPT_FILENAME

--- tests/php-e2e-actions/init.php
+++ tests/php-e2e-actions/init.php
@@ -1,8 +1,16 @@
 <?php
 
 $abspath = getenv('WP_TESTS_ABSPATH');
+$pluginDir = dirname(dirname(dirname($_SERVER['SCRIPT_FILENAME'])));
+$pluginBase = basename($pluginDir);
+$wordpressDir = str_replace('wp-content/plugins/' . $pluginBase, '', $pluginDir);
+
 if (empty($abspath)) {
-    die('Unable to run php-e2e because ENV: WP_TESTS_ABSPATH empty');
+    if (file_exists(trim($wordpressDir, '\\/') . '/wp-config.php')) {
+        $abspath = $wordpressDir;
+    } else {
+        die('Unable to run php-e2e because ENV: WP_TESTS_ABSPATH empty');
+    }
 }
 if (isset($_GET['php-e2e'])) {
     define('VCV_E2E', true);
