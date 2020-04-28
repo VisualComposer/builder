e4e3a8094f084354b1497114dc04fc1f0cb09046
Nikita <nikita.h@visualcomposer.com>
2020-04-28 09:14:18 +0300

    Hide append control and frame if parent element is locked VC-1142

--- public/editor/modules/layout/lib/controlsIframe/framesHandler.js
+++ public/editor/modules/layout/lib/controlsIframe/framesHandler.js
@@ -50,16 +50,33 @@ export default class Frames {
       left -= iframePos.left
     }
     let isElementLocked = false
+    let isParentElementLocked = false
+    let isTopParent = false
     if (env('VCV_ADDON_ROLE_MANAGER_ENABLED') && !window.vcvManageOptions) {
       const id = element.dataset.vcvElement
       const cookElement = cook.getById(id)
+      const parentElement = cookElement.toJS().parent
+      let cookParent = false
+      if (parentElement) {
+        cookParent = cook.getById(parentElement)
+        if (cookParent.get('metaIsElementLocked')) {
+          isParentElementLocked = true
+        }
+      } else {
+        isTopParent = true
+      }
       if (cookElement.get('metaIsElementLocked')) {
         isElementLocked = true
       }
     }
     frame.classList.remove('vcv-ui-element-frame--locked')
+    frame.classList.remove('vcv-ui-element-frame--parent-locked')
     if (isElementLocked) {
-      frame.classList.add('vcv-ui-element-frame--locked')
+      if (isParentElementLocked || isTopParent) {
+        frame.classList.add('vcv-ui-element-frame--parent-locked')
+      } else {
+        frame.classList.add('vcv-ui-element-frame--locked')
+      }
     }
     const scrollTop = this.iframeWrapper && this.iframeWrapper.scrollTop ? this.iframeWrapper.scrollTop : 0
     const scrollLeft = this.iframeWrapper && this.iframeWrapper.scrollLeft ? this.iframeWrapper.scrollLeft : 0
@@ -74,6 +91,7 @@ export default class Frames {
    * @param data
    */
   show (data) {
+    console.log('data', data)
     const elements = data.path
     // add frames if current frames count is not enough
     while (elements.length > this.frames.length) {
