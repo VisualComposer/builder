4229fd603dcc1de8d3c918af1d837a78e42b0f06
Nikita <nikita.h@visualcomposer.com>
2020-05-07 16:11:07 +0300

    Add functionality for VC dashboard VC-1151

--- public/wordpressSettings.js
+++ public/wordpressSettings.js
@@ -6,6 +6,7 @@ import { hfSectionToggle } from './components/wpVcSettings/hfSectionToggle'
 import { dropdownEditLink } from './components/wpVcSettings/dropdownEditLink'
 import { themeTemplatesToggle } from './components/wpVcSettings/themeTemplatesToggle'
 import { deactivationFeedbackPopup } from './components/deactivationFeedbackPopup/deactivationFeedbackPopup'
+import { dashboard } from './components/wpVcSettings/dashboard'
 
 (() => {
   // TODO: Refactor this, and call this methods only on required pages
@@ -16,4 +17,5 @@ import { deactivationFeedbackPopup } from './components/deactivationFeedbackPopu
   dropdownEditLink()
   themeTemplatesToggle()
   deactivationFeedbackPopup()
+  dashboard()
 })(window.jQuery)
