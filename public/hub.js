069456ff889d5fd42b8e476aef4c60af80fffd97
Janis Konutis <mcjohn@inbox.lv>
2020-05-08 14:28:06 +0300

    Add notifications for hub VC-1189

--- public/hub.js
+++ public/hub.js
@@ -10,6 +10,7 @@ import 'public/sources/less/bootstrap/init.less'
 import 'public/sources/css/wordpress.less'
 
 import HubContainer from './components/panels/hub/hubContainer'
+import Notifications from './components/notifications/notifications'
 
 export const setupCake = () => {
   vcCake.env('platform', 'wordpress').start(() => {
@@ -29,7 +30,7 @@ export const setupCake = () => {
     // require('./editor/stores/history/historyStorage')
     // require('./editor/stores/settingsStorage')
     // require('./editor/stores/attributes/attributesStorage')
-    // require('./editor/stores/notifications/storage')
+    require('./editor/stores/notifications/storage')
     // require('./editor/stores/wordpressData/wordpressDataStorage')
     require('./editor/stores/elements/elementSettings')
     // require('./editor/stores/popup/storage')
@@ -51,6 +52,11 @@ export const setupCake = () => {
       ReactDOM.render(
         <HubContainer parent={{}} hideScrollbar={hideScrollbar} />,
         document.querySelector('#vcv-hub')
+      )
+
+      ReactDOM.render(
+        <Notifications />,
+        document.querySelector('#vcv-wp-admin-notifications')
       )
     })
   })
