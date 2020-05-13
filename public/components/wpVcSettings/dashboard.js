88922a883a2fc4ea3f6d0f28fe743df8f4d14af3
Nikita <nikita.h@visualcomposer.com>
2020-05-13 10:51:05 +0300

    Add dashboard sidebar menu click functionality VC-1151

--- public/components/wpVcSettings/dashboard.js
+++ public/components/wpVcSettings/dashboard.js
@@ -1,13 +1,35 @@
 export const dashboard = () => {
-  const dashboardNavigationToggle = document.querySelector('.vcv-dashboard-nav-toggle')
-  const dashboardNavigationMenu = document.querySelector('.vcv-dashboard-sidebar-navigation-container')
+  const navigationToggle = document.querySelector('.vcv-dashboard-nav-toggle')
+  const navigationMenu = document.querySelector('.vcv-dashboard-sidebar-navigation-container')
+  const submenuLinks = Array.from(document.querySelectorAll('.vcv-dashboard-sidebar-navigation-menu--submenu .vcv-dashboard-sidebar-navigation-link'))
+  const sections = Array.from(document.querySelectorAll('.vcv-dashboards-section-content'))
 
   const handleNavigationToggle = () => {
-    dashboardNavigationMenu.classList.toggle('vcv-is-navigation-visible')
-    const ariaExpandedAttr = dashboardNavigationToggle.getAttribute('aria-expanded')
+    navigationMenu.classList.toggle('vcv-is-navigation-visible')
+    const ariaExpandedAttr = navigationMenu.getAttribute('aria-expanded')
     const newAriaExpandedAttr = ariaExpandedAttr === 'true' ? 'false' : 'true'
-    dashboardNavigationToggle.setAttribute('aria-expanded', newAriaExpandedAttr)
+    navigationToggle.setAttribute('aria-expanded', newAriaExpandedAttr)
   }
 
-  dashboardNavigationToggle.addEventListener('click', handleNavigationToggle)
+  const handleSubmenuLinkClick = (e) => {
+    const href = e.target.getAttribute('href').slice(1)
+
+    submenuLinks.forEach(link => {
+      if (link.classList.contains('vcv-dashboard-sidebar-navigation-link--active')) {
+        link.classList.remove('vcv-dashboard-sidebar-navigation-link--active')
+      }
+    })
+    sections.forEach(section => {
+      if (section.classList.contains('vcv-dashboards-section-content--active')) {
+        section.classList.remove('vcv-dashboards-section-content--active')
+      }
+      if (section.dataset.section === href) {
+        section.classList.add('vcv-dashboards-section-content--active')
+      }
+    })
+    e.target.classList.add('vcv-dashboard-sidebar-navigation-link--active')
+  }
+
+  submenuLinks.forEach(link => link.addEventListener('click', handleSubmenuLinkClick))
+  navigationToggle.addEventListener('click', handleNavigationToggle)
 }
