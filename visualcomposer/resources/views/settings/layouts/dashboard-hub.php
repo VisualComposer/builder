c75cf25c86c426cf87da63d40e0a04e807e59825
Çağdaş Dağ <cagdasdag81@gmail.com>
2020-05-13 17:30:11 +0300

    Markup changed and dashboard improvements VC-1151

--- visualcomposer/resources/views/settings/layouts/dashboard-hub.php
+++ visualcomposer/resources/views/settings/layouts/dashboard-hub.php
@@ -12,8 +12,20 @@ if (isset($_GET['page'])) {
 }
 $activeClass = $activeTab === $slug ? 'vcv-dashboards-section-content--active' : '';
 ?>
-<div class="vcv-dashboards-section-content <?php echo $activeClass ?>" data-section="<?php echo $slug ?>">
+
+<div class="vcv-dashboards-section-content hub <?php echo $activeClass ?>" data-section="<?php echo $slug ?>">
     <?php
     echo $content;
     ?>
 </div>
+
+<?php
+$extraOutput = vcfilter('vcv:frontend:hub:extraOutput', []);
+if (is_array($extraOutput)) {
+    foreach ($extraOutput as $output) {
+        // @codingStandardsIgnoreLine
+        echo $output;
+    }
+    unset($output);
+}
+?>
