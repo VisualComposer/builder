3a3cb410d64917a80b0819aeb955be9bef5b3373
alper <alper@alpers-MacBook-Pro.local>
2020-05-21 13:18:56 +0300

    Add e2e test for preview VC-1258

---
+++ tests/cypress/integration/preview.spec.js
@@ -0,0 +1,45 @@
+/* global describe, it, cy */
+
+describe('test preview is working fine', function () {
+  it('Uses php-e2e-actions to load page with custom script localizations', function () {
+    cy.createPage()
+    cy.addElement('Text Block')
+    cy.setTinyMce({
+      title: 'Content',
+      text: 'Test for Preview'
+    })
+
+    cy.setClassAndId('preview-id', 'preview-class')
+
+    cy.setDoColor('Background color', {
+      color: {
+        "hex": "B93030",
+        "rgb": "rgb(185, 48, 48)"
+      },
+      initialColor: '000000'
+    })
+
+
+    cy.window().then((win) => {
+      cy.route('POST', win.vcvAdminAjaxUrl).as('previewRequest')
+    })
+    cy.get('.vcv-ui-navbar-dropdown-trigger[title="Menu"]').click()
+    cy.get('.vcv-ui-navbar-control[title="Preview"]').should('be.visible').click()
+    cy.wait('@previewRequest')
+
+    // Check visual composer editor
+    cy.get('@previewRequest').should((response) => {
+      const previewPageUrl = response.response.body.postData.previewUrl
+      cy.visit(previewPageUrl)
+    })
+
+    cy.get('#preview-id.preview-class')
+      .then((element) => {
+        const textContent = element.text()
+
+        expect(textContent).to.equal('Test for Preview')
+        cy.get('#preview-id.preview-class .vce-text-block-wrapper').should('have.css', 'background-color', 'rgb(185, 48, 48)')
+      })
+
+  })
+})
