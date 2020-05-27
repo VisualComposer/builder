8b8e97f1870d71c5c42cd1a4dbdc8549efde65ce
Nikita <nikita.h@visualcomposer.com>
2020-05-27 16:49:13 +0300

    Add folder for Electron browser tests VC-1279

---
+++ tests/cypressElectron/cypress/integration/basicButton.spec.js
@@ -0,0 +1,58 @@
+/* global describe, it, cy, Cypress */
+
+const ELEMENT_NAME = 'Basic Button'
+
+describe(ELEMENT_NAME, function () {
+  it('Adds element to the page, checks automatically added elements, checks attributes', function () {
+    cy.fixture('../fixtures/basicButton.json').then((settings) => {
+      cy.createPage()
+      cy.addElement(ELEMENT_NAME)
+
+      cy.setInput('Button text', settings.buttonText)
+      cy.setURL('Link selection', settings.buttonLink)
+      cy.setButtonGroup('Alignment', settings.alignment)
+      cy.setButtonGroup('Shape', settings.shape.name)
+      cy.setButtonGroup('Size', settings.size.name)
+      cy.setSwitch('Stretch button')
+      cy.setColor(settings.titleColor)
+      cy.setColor(settings.backgroundColor)
+      cy.setClassAndId(settings.customId, settings.customClass)
+      cy.setDO(settings.designOptions)
+
+      cy.savePage()
+      cy.viewPage()
+
+      cy.wait(1000)
+      cy.get(`.${settings.customClass}.vce-button--style-basic-container--align-${settings.alignment}`)
+        .should('have.css', 'text-align', settings.alignment)
+
+      cy.get(`#${settings.customId}`)
+        .contains(settings.buttonText)
+        .should('have.css', 'border-radius', settings.shape.cssValue)
+        .and('have.css', 'padding', settings.size.cssValue)
+        .and('have.css', 'color', settings.titleColor.valueRgb)
+        .and('have.css', 'background-color', settings.backgroundColor.valueRgb)
+        .and('have.css', 'animation-name', `vce-o-animate--${settings.designOptions.animation}`)
+        .should('have.attr', 'data-vce-animate', `vce-o-animate--${settings.designOptions.animation}`)
+        .and('have.attr', 'data-vcv-o-animated', 'true')
+        .and('have.attr', 'href', `http://${settings.buttonLink.url}`)
+        .and('have.attr', 'target', '_blank')
+
+      cy.get('.vce-button--style-basic-wrapper--stretched')
+        .then((element) => {
+          let contentContainerWidth
+          cy.window().then((window) => {
+            const contentContainer = window.document.querySelector( '.vce-row-container')
+            contentContainerWidth = window.getComputedStyle(contentContainer).width
+            cy.wrap(element)
+              .should('have.css', 'width', contentContainerWidth)
+          })
+        })
+
+      if (Cypress.env('checkSnapshots')) {
+        cy.wait(2000)
+        cy.get(`#${settings.customId}`).matchImageSnapshot({ blur: 2 })
+      }
+    })
+  })
+})
---
+++ tests/cypressElectron/cypress/integration/defaultWidget.spec.js
@@ -0,0 +1,59 @@
+/* global describe, it, cy */
+
+const ELEMENT_NAME = 'WordPress Default Widget'
+
+describe(ELEMENT_NAME, function () {
+  it('Adds element to the page, checks automatically added elements, checks attributes', function () {
+    cy.fixture('../fixtures/defaultWidget.json').then((settings) => {
+      cy.createPage()
+      cy.addElement(ELEMENT_NAME)
+
+      // 1. Set attributes and DO
+      cy.setSwitch('Enable custom widget HTML')
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('Before Title html')
+      cy.get('.vcv-ui-form-group-heading')
+        .contains('After Title html')
+      cy.setCodeMirror(settings.beforeWidgetHTML)
+      cy.setCodeMirror(settings.afterWidgetHTML)
+      cy.setClassAndId(settings.customId, settings.customClass)
+      cy.setDO(settings.designOptions)
+
+      // 2. Set widget and widget content
+      cy.window().then((window) => {
+        cy.route('POST', window.vcvAjaxUrl).as('getWidget')
+      })
+      cy.setSelect('Widget', settings.widgetType)
+      cy.wait('@getWidget')
+      cy.get('#widget-form-1-text')
+        .then(($field) => {
+          cy.wrap($field)
+            .clear()
+            .type(settings.widgetText)
+        })
+
+      cy.savePage()
+      cy.viewPage()
+
+      cy.get(`.${settings.customClass}`)
+        .should('have.attr', 'id', settings.customId)
+
+      cy.get('.vce-widgets-wrapper')
+        .should('have.css', 'border-radius', settings.designOptions.borderRadius)
+        .and('have.css', 'border-width', settings.designOptions.borderWidth)
+        .and('have.css', 'border-style', settings.designOptions.borderStyle)
+        .and('have.css', 'border-color', settings.designOptions.borderColor.rgb)
+        .and('have.css', 'padding', settings.designOptions.padding)
+        .and('have.css', 'background-color', settings.designOptions.backgroundColor.rgb)
+        .and('have.css', 'animation-name', `vce-o-animate--${settings.designOptions.animation}`)
+        .should('have.attr', 'data-vce-animate', `vce-o-animate--${settings.designOptions.animation}`)
+        .and('have.attr', 'data-vcv-o-animated', 'true')
+
+      cy.wait(200)
+      cy.contains('.textwidget p', settings.widgetText)
+
+      cy.contains(settings.beforeWidgetHTML.selector, settings.beforeWidgetHTML.textString)
+      cy.contains(settings.afterWidgetHTML.selector, settings.afterWidgetHTML.textString)
+    })
+  })
+})
