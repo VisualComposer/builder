1ebf54a11b225b607745da8e47724d98c1982d6d
Pavel I <pavel@visualcomposer.com>
2020-06-12 13:44:07 +0300

    VC-1308 fix new page source-css

--- public/editor/modules/wordpressWorkspace/module.js
+++ public/editor/modules/wordpressWorkspace/module.js
@@ -105,6 +105,12 @@ add('wordpressWorkspace', (api) => {
     const removeOverlay = () => {
       iframeContent.querySelector('.vcv-loading-overlay') && iframeContent.querySelector('.vcv-loading-overlay').remove()
       document.querySelector('.vcv-layout-bar-header') && document.querySelector('.vcv-layout-bar-header').classList.remove('vcv-layout-bar-header--loading')
+
+      // Remove Current Post Source-CSS to avoid cascading issues
+      const sourceCss = env('iframe').document.querySelector('link[id*="assets:source:main:styles"][href$="-' + window.vcvSourceID + '"]')
+      if (sourceCss) {
+        sourceCss.remove()
+      }
     }
     let documentElements
     let isBlank = true
