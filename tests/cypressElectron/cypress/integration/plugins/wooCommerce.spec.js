e444c1a79e28b0c88ca0268368db10cfe360a4b3
Nikita <nikita.h@visualcomposer.com>
2020-06-19 09:49:14 +0300

    Add Woo Commerce e2e test VC-1236

---
+++ tests/cypressElectron/cypress/integration/plugins/wooCommerce.spec.js
@@ -0,0 +1,27 @@
+/* global describe, it, cy, Cypress */
+
+describe('WooCommerce plugin', function () {
+  it('Enables Product post type, creates a Product page, adds content, check WP product page for WooCommerce plugin compliance.', function () {
+    cy.viewport(1200, 800)
+    cy.visit(`${Cypress.env('baseUrl')}wp-admin/post-new.php?post_type=product&vcv-action=frontend`)
+
+    cy.get('.vcv-start-blank-title-input')
+      .clear()
+      .type('VC Test Product')
+
+    cy.addElement('Single Image')
+    cy.addElement('Text Block')
+    cy.addElement('Basic Button')
+
+    cy.savePage()
+
+    cy.visit(`${Cypress.env('baseUrl')}shop`)
+
+    cy.contains('.woocommerce-loop-product__title', 'VC Test Product')
+      .click()
+
+    cy.get('.vce-text-block')
+    cy.get('.vce-single-image')
+    cy.get('.vce-button--style-basic')
+  })
+})
