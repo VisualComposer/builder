378a13cae0d2e29ac450a5b767aebee09365061d
Janis Konutis <mcjohn@inbox.lv>
2020-08-26 16:23:00 +0300

    Change error ID VC-1354

--- public/components/stockMedia/giphyContainer.js
+++ public/components/stockMedia/giphyContainer.js
@@ -15,7 +15,7 @@ export default class GiphyContainer extends React.Component {
       searchText: localizations ? localizations.discoverGifAnimationsText : 'Discover best GIF animations from Giphy',
       getMediaText: localizations ? localizations.getGiphiesText : 'Download and Add Free Animated Giphies to Your Site',
       getMediaWithPremiumText: localizations ? localizations.getGiphiesWithPremiumText : 'Download and Add Free Animated Giphies to Your Site With Visual Composer Premium',
-      noConnectionToStockMediaText: `${localizations.noConnectionToGiphy} #10088` || 'Could not connect to Giphy Server! #10088',
+      noConnectionToStockMediaText: `${localizations.noConnectionToGiphy} #10089` || 'Could not connect to Giphy Server! #10089',
       downloadText: localizations ? localizations.downloadAnimationsFromGiphy : 'Download animations from Giphy to your Media Library',
       unlockText: localizations ? localizations.activatePremiumToUnlockGiphy : 'Activate Premium to Unlock Giphy Integration',
       searchResultKey: localizations ? localizations.GifAnimations : 'GIF animations',
