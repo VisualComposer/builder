a89e4a7a555f9ef12fb4d2804baabb2d88b8123b
Nikita <nikita.h@visualcomposer.com>
2020-08-31 14:22:16 +0300

    Add Giphy tab in add media library VC-1353

---
+++ public/sources/attributes/attachimage/giphyMediaTab.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import GiphyContainer from 'public/components/stockMedia/giphyContainer'
+
+export default class GiphyMediaTab extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      scrolledToBottom: false,
+      scrollTop: 0
+    }
+
+    this.handleMediaScroll = this.handleMediaScroll.bind(this)
+  }
+
+  componentDidMount () {
+    window.setTimeout(() => {
+      const mediaFrame = document.querySelector('.media-frame-content')
+      if (mediaFrame) {
+        mediaFrame.addEventListener('scroll', this.handleMediaScroll)
+      }
+    }, 100)
+  }
+
+  componentWillUnmount () {
+    const mediaFrame = document.querySelector('.media-frame-content')
+    if (mediaFrame) {
+      mediaFrame.removeEventListener('scroll', this.handleMediaScroll)
+    }
+  }
+
+  handleMediaScroll (e) {
+    const el = e.currentTarget
+    const clientRect = el.getBoundingClientRect()
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight
+    const scrolledToBottom = (el.scrollTop + clientRect.height + (windowHeight / 2)) >= el.scrollHeight
+    this.setState({
+      scrollTop: el.scrollTop,
+      scrolledToBottom: scrolledToBottom
+    })
+  }
+
+  render () {
+    return <GiphyContainer scrolledToBottom={this.state.scrolledToBottom} scrollTop={this.state.scrollTop} />
+  }
+}
