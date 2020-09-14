7448b47446f9bc71f9dcd24bf1f596a235d67331
Janis Konutis <mcjohn@inbox.lv>
2020-09-14 16:05:00 +0300

    Refactor feedback popup VC-1424

--- public/components/popup/popups/votePopup.js
+++ public/components/popup/popups/votePopup.js
@@ -1,39 +1,60 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import PopupInner from '../popupInner'
+import { getService, getStorage } from 'vc-cake'
 
-export default class VoteContainer extends React.Component {
-  static propTypes = {
-    onVote: PropTypes.func,
-    onClose: PropTypes.func
+const dataProcessor = getService('dataProcessor')
+const editorPopupStorage = getStorage('editorPopup')
+
+export default class VotePopup extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.handlePrimaryButtonClick = this.handlePrimaryButtonClick.bind(this)
   }
 
-  getVote = () => {
-    const checkedInput = document.querySelector('.vcv-feedback-vote input:checked')
+  handlePrimaryButtonClick () {
+    const checkedInput = document.querySelector('input.vcv-feedback-vote-control:checked')
     if (!checkedInput) {
       return
     }
-    return checkedInput.value
+
+    dataProcessor.appAdminServerRequest({
+      'vcv-action': 'license:feedback:submit:adminNonce',
+      'vcv-feedback': checkedInput.value
+    })
+
+    // Set vote value in storage so we can use it in the review popup
+    const popupState = editorPopupStorage.state('popups').get()
+    if (popupState.votePopup) {
+      popupState.votePopup.voteValue = checkedInput.value
+    }
+    editorPopupStorage.state('popups').set(popupState)
+
+    // Show review popup
+    const visibilityTimeout = setTimeout(() => {
+      editorPopupStorage.trigger('showPopup', 'reviewPopup')
+      window.clearTimeout(visibilityTimeout)
+    }, 2000)
+
+    this.props.onPrimaryButtonClick()
   }
 
   render () {
     const localizations = window.VCV_I18N && window.VCV_I18N()
     const headingText = localizations ? localizations.feedbackVoteHeadingText : 'How disappointed would you be if this product no longer existed tomorrow?'
-    const closeButtonText = localizations ? localizations.close : 'Close'
     const buttonText = localizations ? localizations.feedbackVoteButtonText : 'Submit Your Feedback'
     const veryDisappointed = localizations ? localizations.veryDisappointed : 'Very disappointed'
     const somewhatDisappointed = localizations ? localizations.somewhatDisappointed : 'Somewhat disappointed'
     const disappointed = localizations ? localizations.disappointed : 'Not disappointed (it really isn’t that useful)'
 
     return (
-      <div className='vcv-feedback-vote'>
-        <header className='vcv-feedback-vote-header'>
-          <h2 className='vcv-feedback-vote-heading'>{headingText}</h2>
-          <button
-            className='vcv-feedback-vote-close vcv-ui-icon vcv-ui-icon-close-thin'
-            aria-label={closeButtonText}
-            onClick={this.props.onClose}
-          />
-        </header>
+      <PopupInner
+        {...this.props}
+        headingText={headingText}
+        buttonText={buttonText}
+        onPrimaryButtonClick={this.handlePrimaryButtonClick}
+        popupName='votePopup'
+      >
         <div className='vcv-feedback-vote-control-wrapper'>
           <input type='radio' id='vcv-feedback-vote-very-disappointed' className='vcv-feedback-vote-control' name='vcv-feedback' value='1' />
           <label htmlFor='vcv-feedback-vote-very-disappointed' className='vcv-feedback-vote-control-label'>
@@ -52,13 +73,7 @@ export default class VoteContainer extends React.Component {
             {disappointed}
           </label>
         </div>
-        <a
-          className='vcv-feedback-vote-btn'
-          onClick={() => { this.props.onVote(this.getVote()) }}
-        >
-          {buttonText}
-        </a>
-      </div>
+      </PopupInner>
     )
   }
 }
