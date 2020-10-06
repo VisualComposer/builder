ed9fe5593da45e95ceeb4c6b6fb5954aea2bf4b6
Janis Konutis <mcjohn@inbox.lv>
2020-10-06 09:13:56 +0300

    Fix divider scale VC-1497

--- public/sources/assetsLibrary/divider/webpack.config.babel.js
+++ public/sources/assetsLibrary/divider/webpack.config.babel.js
@@ -1,37 +1,40 @@
 import path from 'path'
-import ExtractTextPlugin from 'extract-text-webpack-plugin'
 import webpack from 'webpack'
-import TerserPlugin from 'terser-webpack-plugin'
-
-module.exports = {
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
+import TerserWebpackPlugin from 'terser-webpack-plugin'
+import OptimizeCSSAssetsPlugin from 'optimize-css-assets-webpack-plugin'
+export default Object.assign({}, {
   mode: 'production',
   entry: {
     divider: [ './src/css/styles.less' ]
   },
   output: {
-    path: path.resolve(__dirname, 'dist/'), // Assets dist path
-    publicPath: '.', // Used to generate URL's
-    filename: '[name].bundle.js', // Main bundle file
-    chunkFilename: '[id].js'
+    path: path.resolve(process.cwd(), 'dist/'),
+    // Assets dist path
+    publicPath: '.',
+    // Used to generate URL's
+    filename: '[name].bundle.js',
+    // Main bundle file
+    chunkFilename: '[name].bundle.js',
+    jsonpFunction: 'vcvWebpackJsonp4x'
   },
   node: {
-    'fs': 'empty'
+    fs: 'empty'
   },
   optimization: {
     minimize: true,
     runtimeChunk: false,
-    namedChunks: true, // MUST BE true even for production
-    namedModules: true, // MUST BE true even for production
-    minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          safari10: true
-        }
-      })
-    ]
+    namedModules: true,
+    minimizer: [new TerserWebpackPlugin({
+      terserOptions: {
+        safari10: true
+      }
+    }), new OptimizeCSSAssetsPlugin()]
   },
   plugins: [
-    new ExtractTextPlugin('[name].bundle.css'),
+    new MiniCssExtractPlugin({
+      filename: '[name].bundle.css'
+    }),
     new webpack.NamedModulesPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
@@ -41,42 +44,63 @@ module.exports = {
   ],
   module: {
     rules: [
-      { parser: { amd: false } },
+      {
+        parser: {
+          amd: false
+        }
+      },
       {
         test: /\.mjs$/,
         include: /node_modules/,
         type: 'javascript/auto'
       },
       {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [ 'css-loader', {
-            loader: 'postcss-loader',
-            options: {
-              plugins: () => [ require('autoprefixer')() ]
-            }
-          }, 'less-loader' ]
-        })
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader'
+        },
+        exclude: /node_modules/
       },
       {
-        test: /\.less$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [ 'css-loader', {
+        test: /\.css|\.less$/,
+        exclude: [/styles\.css/, /editor\.css/],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+          {
             loader: 'postcss-loader',
             options: {
-              plugins: () => [ require('autoprefixer')() ]
+              plugins: function plugins () {
+                return [require('autoprefixer')()];
+              }
             }
-          }, 'less-loader' ]
-        })
+          },
+          'less-loader'
+        ]
       },
-      // use ! to chain loaders./
-      { test: /\.(png|jpe?g|gif)$/, use: 'url-loader?limit=10000&name=/images/[name].[ext]?[hash]' }, // inline base64 URLs for <=8k images, direct URLs for the rest.
-      { test: /\.woff(2)?(\?.+)?$/, use: 'url-loader?limit=10000&mimetype=application/font-woff&name=/fonts/[name].[ext]?[hash]' },
-      { test: /\.(ttf|eot|svg)(\?.+)?$/, use: 'file-loader?name=/fonts/[name].[ext]?[hash]' },
-      { test: /\.raw(\?v=\d+\.\d+\.\d+)?$/, use: 'raw-loader' }
-      // { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery&$=jquery' }
+      {
+        test: /\.svg/,
+        use: {
+          loader: 'svg-url-loader',
+          options: {}
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: 'url-loader?limit=10000&name=/images/[name].[ext]?[hash]'
+      }, // inline base64 URLs for <=8k images, direct URLs for the rest.
+      {
+        test: /\.woff(2)?(\?.+)?$/,
+        use: 'url-loader?limit=10000&mimetype=application/font-woff&name=/fonts/[name].[ext]?[hash]'
+      }, {
+        test: /\.(ttf|eot)(\?.+)?$/,
+        use: 'file-loader?name=/fonts/[name].[ext]?[hash]'
+      }, {
+        test: /\.raw(\?v=\d+\.\d+\.\d+)?$/,
+        use: 'raw-loader' // { test: /bootstrap\/js\//, loader: 'imports?jQuery=jquery&$=jquery' }
+      }
     ]
   }
-}
+})
