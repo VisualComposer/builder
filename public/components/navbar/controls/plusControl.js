b0790d8cd05b03fcca518ff7ab53e349e2b1b281
Janis Konutis <mcjohn@inbox.lv>
2020-10-08 15:04:57 +0300

    Add localizations VC-1416

--- public/components/navbar/controls/plusControl.js
+++ public/components/navbar/controls/plusControl.js
@@ -50,7 +50,7 @@ export default class PlusControl extends NavbarContent {
     })
 
     return (
-      <span className={controlClass} title='Add Content' onClick={this.handleClickAddContent}>
+      <span className={controlClass} title={name} onClick={this.handleClickAddContent}>
         <span className='vcv-ui-navbar-control-content'>
           <i className='vcv-ui-navbar-control-icon vcv-ui-icon vcv-ui-icon-add' />
           <span>{name}</span>
