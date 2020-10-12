66e3de2238479d1ef52ddecce0290bf7a06c141b
Nikita <nikita.h@visualcomposer.com>
2020-10-12 17:00:05 +0300

    Fix JS codestyle VC-1519

--- public/components/panels/addElement/addElementPanel.js
+++ public/components/panels/addElement/addElementPanel.js
@@ -12,11 +12,13 @@ export default class AddElementPanel extends React.Component {
   render () {
     let childrenOutput = this.props.children
     if (!childrenOutput) {
-      childrenOutput = (<Categories
-        parent={this.props.options.element ? this.props.options.element : {}}
-        searchValue={this.props.searchValue}
-        applyFirstElement={this.props.applyFirstElement}
-      />)
+      childrenOutput = (
+        <Categories
+          parent={this.props.options.element ? this.props.options.element : {}}
+          searchValue={this.props.searchValue}
+          applyFirstElement={this.props.applyFirstElement}
+        />
+      )
     }
 
     return (
