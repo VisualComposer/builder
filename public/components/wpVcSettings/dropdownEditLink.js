8488124e4bc19ee1badb76a92f350b3e2516bc52
Filips Masolovs <filips.masolovs@gmail.com>
2020-12-01 10:50:14 +0200

    Expand all actionContainers as default VC-1573

--- public/components/wpVcSettings/dropdownEditLink.js
+++ public/components/wpVcSettings/dropdownEditLink.js
@@ -8,14 +8,13 @@ const changeEditLink = (item) => {
   const selectedPageUrl = item.querySelector('option:checked').getAttribute('data-url')
   const linkTitle = item.closest('.vcv-ui-form-group').getAttribute('data-title')
   const actionsContainer = item.closest('.vcv-ui-form-group').closest('td').querySelector('.vcv-custom-page-templates-edit-link')
+  actionsContainer.closest('tr').classList.add('vcv-field-expand')
   actionsContainer.innerHTML = null
 
   if (item.value) {
     actionsContainer.insertAdjacentHTML('beforeend', editLinkText.replace('{editLink}', selectedPageUrl).replace('{linkTitle}', linkTitle).replace('{createLink}', 'post-new.php?post_type=vcv_headers&vcv-action=frontend&vcv-editor-type=vcv_headers'))
-    actionsContainer.closest('tr').classList.add('vcv-field-expand')
   } else {
     actionsContainer.insertAdjacentHTML('beforeend', createLinkText.replace('{createLink}', selectedPageUrl).replace('{linkTitle}', linkTitle))
-    actionsContainer.closest('tr').classList.add('vcv-field-expand')
   }
 }
 
