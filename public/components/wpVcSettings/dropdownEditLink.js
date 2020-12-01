ddb6d61526644ff894d87eaa46cb86135cece7cc
Filips Masolovs <filips.masolovs@gmail.com>
2020-12-01 09:49:26 +0200

    Updated code to toggle between two different action states VC-1573

--- public/components/wpVcSettings/dropdownEditLink.js
+++ public/components/wpVcSettings/dropdownEditLink.js
@@ -1,38 +1,33 @@
 import { getService } from 'vc-cake'
 const dataManager = getService('dataManager')
 const localizations = dataManager.get('localizations')
-const editLinkText = localizations && localizations.editThemeTemplate ? localizations.editThemeTemplate : '<div class="vcv-custom-page-templates-edit-link"><a href="{editLink}" target="_blank">Edit</a> this {editLinkTitle} or <a href="{createLink}" target="_blank">create</a> a new one.</div>'
+const editLinkText = localizations && localizations.editThemeTemplate ? localizations.editThemeTemplate : '<a href="{editLink}" target="_blank">Edit</a> this {linkTitle} or <a href="{createLink}" target="_blank">create</a> a new one.'
+const createLinkText = localizations && localizations.editThemeTemplate ? localizations.createThemeTemplate : '<a href="{createLink}" target="_blank">Create</a> a new {linkTitle}.'
 
 const changeEditLink = (item) => {
   const selectedPageUrl = item.querySelector('option:checked').getAttribute('data-url')
-  const editLinkTitle = item.closest('.vcv-ui-form-group').getAttribute('data-title')
-  const dropdownContainer = item.closest('.vcv-ui-form-group').closest('td')
-  const editLinkItem = dropdownContainer.querySelector('.vcv-custom-page-templates-edit-link')
+  const linkTitle = item.closest('.vcv-ui-form-group').getAttribute('data-title')
+  const actionsContainer = item.closest('.vcv-ui-form-group').closest('td').querySelector('.vcv-custom-page-templates-edit-link')
+  actionsContainer.innerHTML = null
 
   if (item.value) {
-    if (editLinkItem != null) {
-      editLinkItem.querySelector('a').setAttribute('href', selectedPageUrl)
-    } else {
-      dropdownContainer.insertAdjacentHTML('beforeend', editLinkText.replace('{editLink}', selectedPageUrl).replace('{editLinkTitle}', editLinkTitle).replace('{createLink}', 'post-new.php?post_type=vcv_headers&vcv-action=frontend&vcv-editor-type=vcv_headers'))
-      dropdownContainer.closest('tr').classList.add('vcv-field-expand')
-    }
+    actionsContainer.insertAdjacentHTML('beforeend', editLinkText.replace('{editLink}', selectedPageUrl).replace('{linkTitle}', linkTitle).replace('{createLink}', 'post-new.php?post_type=vcv_headers&vcv-action=frontend&vcv-editor-type=vcv_headers'))
+    actionsContainer.closest('tr').classList.add('vcv-field-expand')
   } else {
-    editLinkItem.remove()
-    dropdownContainer.closest('tr').classList.remove('vcv-field-expand')
+    actionsContainer.insertAdjacentHTML('beforeend', createLinkText.replace('{createLink}', selectedPageUrl).replace('{linkTitle}', linkTitle))
+    actionsContainer.closest('tr').classList.add('vcv-field-expand')
   }
 }
 
 export const dropdownEditLink = () => {
   const templateDropdowns = document.querySelectorAll('select.vcv-edit-link-selector')
+  const actionsContainer = '<div class="vcv-custom-page-templates-edit-link"></div>'
 
-  for (var i = 0, len = templateDropdowns.length; i < len; i++) {
-    var item = templateDropdowns[i]
-    console.log(item)
-
-    // Initial Page Load
-    if (item.querySelector('option:checked') && item.querySelector('option:checked').value !== '') {
-      changeEditLink(item)
-    }
+  for (let i = 0, len = templateDropdowns.length; i < len; i++) {
+    const item = templateDropdowns[i]
+    const dropdownContainer = item.closest('.vcv-ui-form-group').closest('td')
+    dropdownContainer.insertAdjacentHTML('beforeend', actionsContainer)
+    changeEditLink(item)
 
     item.addEventListener('change', function (item) {
       changeEditLink(item.target)
