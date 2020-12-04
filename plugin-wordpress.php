03ec7784dfeacd0922778453816cb9270beec67e
Pavel I <pavel@visualcomposer.com>
2020-12-04 15:11:21 +0200

    VC-1633 add plugin slug check and hardcode assets folder location

--- plugin-wordpress.php
+++ plugin-wordpress.php
@@ -1,18 +1,13 @@
 <?php
 /**
  * Plugin Name: Visual Composer
- * Plugin URI: https://visualcomposer.com/premium/?utm_source=vcwb&utm_medium=wp-plugins&utm_campaign=vcbrand&utm_content=text
- * Description: Create your WordPress website with the fast and easy-to-use drag-and-drop builder for experts and beginners.
- * Version: dev
- * Author: visualcomposer.com
- * Author URI: https://visualcomposer.com/?utm_source=vcwb&utm_medium=wp-plugins&utm_campaign=vcbrand&utm_content=text
- * Copyright: (c) 2017 TechMill Ltd.
- * License: GPLv3
- * License URI: http://www.gnu.org/licenses/gpl-3.0.html
- * Requires at least: 4.6
- * Tested up to: 5.2.3
- * Text Domain: visualcomposer
- * Domain Path: /languages/
+ * Plugin URI:
+ * https://visualcomposer.com/premium/?utm_source=vcwb&utm_medium=wp-plugins&utm_campaign=vcbrand&utm_content=text
+ * Description: Create your WordPress website with the fast and easy-to-use drag-and-drop builder for experts and
+ * beginners. Version: dev Author: visualcomposer.com Author URI:
+ * https://visualcomposer.com/?utm_source=vcwb&utm_medium=wp-plugins&utm_campaign=vcbrand&utm_content=text Copyright:
+ * (c) 2017 TechMill Ltd. License: GPLv3 License URI: http://www.gnu.org/licenses/gpl-3.0.html Requires at least: 4.6
+ * Tested up to: 5.2.3 Text Domain: visualcomposer Domain Path: /languages/
  */
 
 /**
@@ -64,10 +59,10 @@ define('VCV_PLUGIN_BASE_NAME', plugin_basename(__FILE__));
  */
 define('VCV_PLUGIN_FULL_PATH', __FILE__);
 /**
- * Plugin directory name: 'vc-five'
+ * Plugin directory name: 'visualcomposer'
  */
 define('VCV_PLUGIN_DIRNAME', basename(dirname(VCV_PLUGIN_BASE_NAME)));
-define('VCV_PLUGIN_ASSETS_DIRNAME', VCV_PLUGIN_DIRNAME . '-assets');
+define('VCV_PLUGIN_ASSETS_DIRNAME', 'visualcomposer-assets');
 /**
  * Plugin core prefix for options/meta and etc.
  */
