79990ab7611102bc536aca067288cc6fe310bdbb
Pavel I <pavel@visualcomposer.com>
2020-12-29 19:48:36 +0200

    VC-1629 fix missing service

--- public/base.js
+++ public/base.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-webpack-loader-syntax */
 import vcCake from 'vc-cake'
 import './variables'
+import './editor/services/dataManager/service'
 import './editor/services/dataProcessor/service'
 
 // const $ = require('expose-loader?$!jquery')
