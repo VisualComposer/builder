0ca0dfea70d73de41e5dbde9ce4f36fc9831d1be
Nikita <nikita.h@visualcomposer.com>
2021-01-11 09:26:01 +0200

    Add Enzyme check to jest unit test VC-1611

--- tests/jest/assetsStorage.test.js
+++ tests/jest/assetsStorage.test.js
@@ -19,6 +19,15 @@ import '../../elements/row/row/index'
 import '../../elements/column/column/index'
 import '../../elements/basicButton/basicButton/index'
 
+
+// EnzymeJS
+import React from 'react'
+import { shallow, configure, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
+import ButtonElement from '../../elements/basicButton/basicButton/component'
+
+configure({ adapter: new Adapter() });
+
 jest.useFakeTimers()
 
 describe('Test assetsStorage', () => {
@@ -29,6 +38,7 @@ describe('Test assetsStorage', () => {
 
   const id = '12345678'
   const buttonColor = 'rgb(11, 90, 224)'
+  const buttonText = 'Hello World'
   let assetsOptions = {}
 
   vcCake.env('VCV_DEBUG', true)
@@ -37,10 +47,16 @@ describe('Test assetsStorage', () => {
       elementsStorage.trigger('add', { tag: 'basicButton', id: id })
       const basicButton = documentManager.get(id)
       expect(basicButton.id).toBe(id)
+      const buttonWrapper = mount(<ButtonElement atts={{...basicButton}} id='123456789' />)
+      const button = buttonWrapper.find('.vce-button--style-basic')
+      expect(buttonWrapper.exists()).toBe(true)
+      expect(buttonWrapper.prop('atts').color).toBe('#fff')
+      expect(button.text()).toBe('Apply Now')
     })
     test('ElementsStorage update Basic Button color', () => {
       const basicButton = cook.get(documentManager.get(id))
       basicButton.set('color', buttonColor)
+      basicButton.set('buttonText', buttonText)
       const basicButtonData = basicButton.toJS()
       elementsStorage.trigger('update', id, basicButtonData)
       elementsStorage.on(`element:${id}`, (data, source, options) => {
@@ -49,13 +65,18 @@ describe('Test assetsStorage', () => {
       jest.runAllTimers()
       const element = documentManager.get(id)
       expect(element.color).toBe(buttonColor)
+      const buttonWrapper = mount(<ButtonElement atts={{...element}} id='123456789' />)
+      const button = buttonWrapper.find('.vce-button--style-basic')
+      expect(buttonWrapper.exists()).toBe(true)
+      expect(buttonWrapper.prop('atts').color).toBe(buttonColor)
+      expect(button.text()).toBe(buttonText)
     })
     test('Assets Storage update Basic Button color assets', () => {
       // const options = {
       //   changedAttribute: 'color'
       // }
       assetsStorage.trigger('updateElement', id, assetsOptions)
-      console.log('assetsStorage jobs',assetsStorage.state('jobs').get())
+      // console.log('assetsStorage jobs',assetsStorage.state('jobs').get())
     })
   })
 })
