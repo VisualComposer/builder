eba28d26854a284d67ae43475d87697d4722d89e
Pavel I <pavel@visualcomposer.com>
2021-01-15 16:51:10 +0200

    VC-1610 Create enqueue filter to skip enqueue, use this filter for WooCOmmerce Square plugin wp_enqueue_scripts handler to avoid duplication

---
+++ visualcomposer/Modules/Vendors/WooCommerceSquareController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace VisualComposer\Modules\Vendors;
+
+if (!defined('ABSPATH')) {
+    header('Status: 403 Forbidden');
+    header('HTTP/1.1 403 Forbidden');
+    exit;
+}
+
+use VisualComposer\Framework\Container;
+use VisualComposer\Framework\Illuminate\Support\Module;
+use VisualComposer\Helpers\Traits\EventsFilters;
+
+class WooCommerceSquareController extends Container implements Module
+{
+    use EventsFilters;
+
+    protected bool $callbackCalled = false;
+
+    public function __construct()
+    {
+        $this->addFilter(
+            'vcv:assets:enqueue:callback:skip',
+            function ($result, $payload) {
+                $closureInfo = $payload['closureInfo'];
+                if ($closureInfo instanceof \ReflectionMethod) {
+                    if (strpos($closureInfo->getDeclaringClass(), 'SV_WC_Payment_Gateway') !== false) {
+                        if (did_action('wp_head') && !$this->callbackCalled) {
+                            // Allow to call Enqueue Script for SV_WC_Payment_Gateway only after wp_head is done.
+                            // Also, make it call only ONCE
+                            $this->callbackCalled = true;
+
+                            return false;
+                        }
+
+                        return true;
+                    }
+                }
+
+                return $result;
+            }
+        );
+    }
+}
