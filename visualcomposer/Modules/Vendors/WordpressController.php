f9764d767d12e0582c54553b9395eda0d2c598e4
Alper <alper@Alpers-MacBook-Pro.local>
2021-01-26 13:30:47 +0300

    Add wp admin info text VC-1713

--- visualcomposer/Modules/Vendors/WordpressController.php
+++ visualcomposer/Modules/Vendors/WordpressController.php
@@ -19,6 +19,12 @@ class WordpressController extends Container implements Module
     public function __construct()
     {
         $this->addFilter('vcv:frontend:content:encode', 'fixWpEmbedShortcode');
+        if (
+            (isset($_GET['page']) && in_array('vcv', explode('-', $_GET['page']))) ||
+            (isset($_GET['post_type']) && in_array('vcv', explode('_', $_GET['post_type'])))
+        ) {
+            add_filter('admin_footer_text', array($this, 'adminFooterText'), 100000, 1);
+        }
     }
 
     protected function fixWpEmbedShortcode($content)
@@ -31,5 +37,18 @@ class WordpressController extends Container implements Module
         // @codingStandardsIgnoreEnd
 
         return $embedContent;
+    }
+
+    public function adminFooterText($current)
+    {
+        return sprintf(
+            __(
+                'Thank you for choosing Visual Composer Website Builder. <br>' .
+                'Like the plugin? %sRate us on WordPress.org%s',
+                'visualcomposer'
+            ),
+            '<a href="https://wordpress.org/support/plugin/visualcomposer/reviews/?filter=5" target="_blank" rel="noopener noreferrer">',
+            '</a>'
+        );
     }
 }
