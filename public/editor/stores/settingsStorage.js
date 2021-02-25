17ac487c8ab5b412433a4e4c93c80bcb92d65ca2
Janis Konutis <mcjohn@inbox.lv>
2021-02-25 11:33:30 +0200

    Move settings data to settings storage VC-1879

--- public/editor/stores/settingsStorage.js
+++ public/editor/stores/settingsStorage.js
@@ -1,5 +1,6 @@
 import { addStorage, getService } from 'vc-cake'
 import { getResponse } from 'public/tools/response'
+import innerAPI from 'public/components/api/innerAPI'
 
 addStorage('settings', (storage) => {
   const dataManager = getService('dataManager')
@@ -35,6 +36,45 @@ addStorage('settings', (storage) => {
       storage.state('footerTemplate').set(footerTemplates && footerTemplates.current)
     }
     storage.state('agreeHubTerms').set(dataManager.get('agreeHubTerms'))
+
+    innerAPI.addFilter('saveRequestData', (data) => {
+      const featuredImageState = storage.state('featuredImage').get()
+      // if storage state is empty, need to explicitly send a string, otherwise it won't be sent in a request
+      const featuredImageDataValue = featuredImageState && featuredImageState.urls && featuredImageState.urls.length ? featuredImageState : 'empty'
+      const customCss = storage.state('customCss').get() || ''
+      const globalCss = storage.state('globalCss').get() || ''
+
+      data['vcv-settings-source-local-head-js'] = storage.state('localJsHead').get() || ''
+      data['vcv-settings-source-local-footer-js'] = storage.state('localJsFooter').get() || ''
+      data['vcv-settings-global-head-js'] = storage.state('globalJsHead').get() || ''
+      data['vcv-settings-global-footer-js'] = storage.state('globalJsFooter').get() || ''
+      data['vcv-settings-parent-page'] = storage.state('parentPage').get() || ''
+      data['vcv-settings-tags'] = JSON.stringify(storage.state('tags').get() || '')
+      data['vcv-settings-excerpt'] = storage.state('excerpt').get() || ''
+      data['vcv-settings-comment-status'] = storage.state('commentStatus').get() || 'closed'
+      data['vcv-settings-ping-status'] = storage.state('pingStatus').get() || 'closed'
+      data['vcv-settings-author'] = storage.state('author').get() || ''
+      data['vcv-settings-categories'] = JSON.stringify(storage.state('categories').get() || '')
+      data['vcv-settings-featured-image'] = featuredImageDataValue
+      data['vcv-settings-source-custom-css'] = customCss
+      data['vcv-settings-global-css'] = globalCss
+      data['vcv-page-title'] = storage.state('pageTitle').get() || ''
+      data['vcv-page-title-disabled'] = storage.state('pageTitleDisabled').get() || ''
+      data['vcv-post-name'] = storage.state('postName').get() || ''
+      data['vcv-item-preview-disabled'] = storage.state('itemPreviewDisabled').get() || ''
+
+      const pageTemplateData = storage.state('pageTemplate').get()
+      if (pageTemplateData) {
+        if (pageTemplateData.stretchedContent) {
+          // Due to browsers converts Boolean TRUE to string "true"
+          pageTemplateData.stretchedContent = 1
+        } else {
+          pageTemplateData.stretchedContent = 0
+        }
+        data['vcv-page-template'] = pageTemplateData
+      }
+      return data
+    })
   })
 
   const dataProcessor = getService('dataProcessor')
