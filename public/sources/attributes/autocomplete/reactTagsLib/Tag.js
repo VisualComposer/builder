b93066e02b2d84465230cda1c4b1e40cada6919e
Filips Masolovs <filips.masolovs@gmail.com>
2021-03-02 12:43:39 +0200

    Update Autocomplete for row layout VC-1841

--- public/sources/attributes/autocomplete/reactTagsLib/Tag.js
+++ public/sources/attributes/autocomplete/reactTagsLib/Tag.js
@@ -1,12 +1,68 @@
 import React from 'react'
+import classNames from 'classnames'
 
-export default (props) => (
-  <button type='button' className={props.classNames.selectedTag}>
-    <span className={props.classNames.selectedTagName}>{props.tag.name}</span>
-    <i
-      className='vcv-ui-icon vcv-ui-icon-close-thin'
-      onClick={props.onDelete}
-      title={props.removeButtonText}
-    />
-  </button>
-)
+export default class Tag extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      isTagEditable: false
+    }
+
+    this.handleKeyDownPreventNewLine = this.handleKeyDownPreventNewLine.bind(this)
+    this.handleTagClick = this.handleTagClick.bind(this)
+    this.handleBlur = this.handleBlur.bind(this)
+  }
+
+  handleTagClick () {
+    this.setState({
+      isTagEditable: true
+    })
+  }
+
+  handleBlur (event) {
+    this.setState({
+      isTagEditable: false
+    })
+
+    this.props.onTagChange(event.currentTarget.innerHTML)
+  }
+
+  handleKeyDownPreventNewLine (event) {
+    if (event.key === 'Enter') {
+      event.preventDefault()
+      this.span && this.span.blur()
+      this.handleBlur(event)
+    }
+  }
+
+  render () {
+    const tagClasses = classNames({
+      [this.props.classNames.selectedTag]: true,
+      'vc-tags--selected-tag--editable': this.state.isTagEditable,
+      'vc-tags--tag-not-valid': !this.props.valid,
+    })
+
+    return (
+      <>
+        <div
+          className={tagClasses}
+        >
+        <span
+          contentEditable={this.props.isTagEditable && this.state.isTagEditable}
+          suppressContentEditableWarning
+          className={this.props.classNames.selectedTagName}
+          onClick={this.props.isTagEditable && this.handleTagClick}
+          onBlur={this.props.isTagEditable && this.handleBlur}
+          onKeyDown={this.props.isTagEditable && this.handleKeyDownPreventNewLine}
+        >{this.props.tag.name}</span>
+          <i
+            className='vcv-ui-icon vcv-ui-icon-close-thin'
+            onClick={this.props.onDelete}
+            title={this.props.removeButtonText}
+          />
+        </div>
+      </>
+    )
+  }
+}
