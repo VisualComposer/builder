45310c0a987559f6acfb2718efbe7081a6c5b14d
Çağdaş Dağ <cagdasdag81@gmail.com>
2021-03-03 11:44:15 +0300

    Contributors capabilities resetted for custom post types VC-1830

---
+++ visualcomposer/Modules/Migrations/CapabilityMigration.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace VisualComposer\Modules\Migrations;
+
+if (!defined('ABSPATH')) {
+    header('Status: 403 Forbidden');
+    header('HTTP/1.1 403 Forbidden');
+    exit;
+}
+
+use VisualComposer\Framework\Illuminate\Support\Module;
+
+class CapabilityMigration extends MigrationsController implements Module
+{
+    protected $migrationPriority = 1;
+
+    protected $migrationId = 'capabilityMigrationV36';
+
+    protected function run()
+    {
+        global $wp_roles;
+        $wpRoles = $wp_roles;
+        $optionsHelper = vchelper('Options');
+
+        $postTypes = [
+            'vcv_headers',
+            'vcv_footers',
+            'vcv_sidebars',
+            'vcv_templates',
+            'vcv_archives',
+            'vcv_popups',
+            'vcv_layouts',
+        ];
+        foreach ($postTypes as $postType) {
+            $optionsHelper->delete($postType . '-capabilities-set');
+            $wpRoles->remove_cap('contributor', 'read_' . $postType);
+            $wpRoles->remove_cap('contributor', 'edit_' . $postType);
+            $wpRoles->remove_cap('contributor', 'delete_' . $postType);
+            $wpRoles->remove_cap('contributor', 'edit_' . $postType . 's');
+            $wpRoles->remove_cap('contributor', 'delete_' . $postType . 's');
+        }
+
+        return true;
+    }
+}
