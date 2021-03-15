71de62419594598c17bff64c22c18a29c0d32f6d
Janis Konutis <mcjohn@inbox.lv>
2021-03-15 12:54:55 +0200

    Add API for editor window VC-1929

--- public/components/api/publicAPI.js
+++ public/components/api/publicAPI.js
@@ -1,6 +1,4 @@
 import ee from 'event-emitter'
-import innerAPI from './innerAPI'
-
 const MyEventEmitter = function () {}
 ee(MyEventEmitter.prototype)
 const apiEventEmitter = new MyEventEmitter()
@@ -21,33 +19,5 @@ export default {
   },
   ready (callback) {
     this.once('ready', callback)
-  },
-  mount (point, callback) {
-    const allowedPoints = ['panel:yoast']
-    if (allowedPoints.indexOf(point) < 0) {
-      console.warn('Mount point not allowed', point)
-      return
-    }
-    return innerAPI.mount(point, callback)
-  },
-  applyFilter (name, value, options) {
-    return innerAPI.applyFilter(name, value, options)
-  },
-  addFilter (name, callback) {
-    const allowedFilters = ['saveRequestData']
-    if (allowedFilters.indexOf(name) < 0) {
-      console.warn('Filter point not allowed', name)
-      return
-    }
-    return innerAPI.addFilter(name, callback)
-  },
-  dispatch (event, options) {
-    return innerAPI.dispatch(event, options)
-  },
-  subscribe (event, callback, once = false) {
-    return innerAPI.subscribe(event, callback, once)
-  },
-  filter (name, callback) {
-    return innerAPI.filter(name, callback)
   }
 }
