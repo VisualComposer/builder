5b8ee379765f02c516f0048a1aa70922c6ac07fa
Janis Konutis <mcjohn@inbox.lv>
2021-03-15 11:44:16 +0200

    Add list of allowed API points VC-1929

--- public/components/api/publicAPI.js
+++ public/components/api/publicAPI.js
@@ -23,12 +23,22 @@ export default {
     this.once('ready', callback)
   },
   mount (point, callback) {
+    const allowedPoints = ['panel:yoast']
+    if (allowedPoints.indexOf(point) < 0) {
+      console.warn('Mount point not allowed', point)
+      return
+    }
     return innerAPI.mount(point, callback)
   },
   applyFilter (name, value, options) {
     return innerAPI.applyFilter(name, value, options)
   },
   addFilter (name, callback) {
+    const allowedFilters = ['saveRequestData']
+    if (allowedFilters.indexOf(name) < 0) {
+      console.warn('Filter point not allowed', name)
+      return
+    }
     return innerAPI.addFilter(name, callback)
   },
   dispatch (event, options) {
