74822a8a48c7f714c5c7bef9b6470efddc944d69
Nikita <nikita.h@visualcomposer.com>
2021-03-19 10:48:18 +0200

    Replace line breaks with br tag for qtags editor VC-1957

--- public/sources/attributes/htmleditor/htmleditor.js
+++ public/sources/attributes/htmleditor/htmleditor.js
@@ -162,8 +162,8 @@ export default class HtmlEditorComponent extends React.Component {
   }
 
   handleChangeQtagsEditor (e) {
-    const field = e.target
-    this.props.setFieldValue(field.value)
+    const value = e.target.value.replace(/\n/g, '<br>')
+    this.props.setFieldValue(value)
   }
 
   skinChange (fieldKey, isDark) {
@@ -412,6 +412,10 @@ export default class HtmlEditorComponent extends React.Component {
           .replace(/__VCVID__/g, id)
           .replace(/%%content%%/g, this.encodeHTML(this.state.value))
 
+        // console.log('document.getElementById(\'vcv-wpeditor-template\').innerHTML', document.getElementById('vcv-wpeditor-template').innerHTML)
+        // console.log('this.state.value', this.state.value)
+        // console.log('this.encodeHTML(this.state.value)', this.encodeHTML(this.state.value))
+        // console.log('template', template)
         return (
           <>
             {this.getFieldComponent(template)}
