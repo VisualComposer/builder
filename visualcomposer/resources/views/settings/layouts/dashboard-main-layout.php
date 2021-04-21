b1e63e94e5bfd3ab97bd6112c9ba95d00e82f206
Filips Masolovs <filips.masolovs@gmail.com>
2021-04-21 13:58:12 +0300

    Added star icons to premium sections in dashboard VC-1891

--- visualcomposer/resources/views/settings/layouts/dashboard-main-layout.php
+++ visualcomposer/resources/views/settings/layouts/dashboard-main-layout.php
@@ -178,6 +178,24 @@ STYLE;
     color: #8E8F9F;
   }
 
+  .vcv-dashboard-sidebar-navigation-menu .vcv-dashboard-sidebar-navigation-menu-item {
+      margin-right: -12px;
+  }
+
+  .vcv-dashboard-sidebar-navigation-menu-item .vcv-available-in-premium.vcv-ui-icon-dashboard-star {
+    color: #6476BD;
+  }
+
+  .vcv-dashboard-sidebar-navigation-menu-item .vcv-available-in-premium.vcv-ui-icon-dashboard-star:hover {
+      color: #FFB718;
+  }
+
+  .vcv-dashboard-sidebar-navigation-menu-item .vcv-available-in-premium.vcv-ui-icon-dashboard-star::before {
+      font-size: 13px;
+      margin: 0;
+      padding: 0 0 0 8px;
+  }
+
   @-webkit-keyframes vcv-ui-wp-spinner-animation {
     from {
       -webkit-transform: translate(-50%, -50%) rotate(0deg);
@@ -279,6 +297,9 @@ STYLE;
                                                         href="<?php echo $subMenuLink ?>"
                                                         data-value="<?php echo esc_attr($tabKey) ?>">
                                                     <?php echo $tabTitle; ?>
+                                                    <?php if (isset($tab['premiumActionBundle'])) { ?>
+                                                        <span class="vcv-ui-icon-dashboard vcv-ui-icon-dashboard-star vcv-available-in-premium" title="This feature is available in Visual Composer Premium"/>
+                                                    <?php } ?>
                                                 </a>
                                             </li>
                                         <?php endforeach; ?>
