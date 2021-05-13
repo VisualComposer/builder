d01399c3ebc30603f4bcf99b1fd00f3c3bb4feb2
Nikita <nikita.h@visualcomposer.com>
2021-05-13 12:32:22 +0300

    Solve conflict

--- public/components/panels/hub/hubContainer.js
+++ public/components/panels/hub/hubContainer.js
@@ -46,6 +46,13 @@ export default class HubContainer extends React.Component {
       this.state.activeCategoryIndex = id
       this.state.bundleType = bundleType
     }
+    if (props.namespace === 'vcdashboard') {
+      const filterType = Object.keys(this.categories).filter(category => this.categories[category].visible)
+      this.state.filterType = filterType[0]
+      const firstCategory = filterType[0]
+      this.state.filterType = firstCategory
+      this.state.activeCategoryIndex = this.categories[firstCategory].index
+    }
     this.changeInput = this.changeInput.bind(this)
     this.addElement = this.addElement.bind(this)
     this.openEditForm = this.openEditForm.bind(this)
