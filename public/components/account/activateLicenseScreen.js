3645787fb61c197c88f3a22e73d18871a7c2e532
Janis Konutis <mcjohn@inbox.lv>
2021-07-21 10:11:37 +0300

    Add correct link in frontend VC-2071

--- public/components/account/activateLicenseScreen.js
+++ public/components/account/activateLicenseScreen.js
@@ -73,7 +73,7 @@ export default class ActivateLicenseScreen extends React.Component {
         console.warn(error)
       })
     } else {
-      this.setError(ActivateLicenseScreen.localizations ? ActivateLicenseScreen.localizations.noSuchLicenseFound : 'No such license found. Make sure it is correct or buy a new one <a class="vcv-activation-box-link" href="%s">here</a>.')
+      this.setError(ActivateLicenseScreen.localizations ? ActivateLicenseScreen.localizations.noSuchLicenseFound : `No such license found. Make sure it is correct or buy a new one <a class="vcv-activation-box-link" href="${dataManager.get('utm')['license-activation-purchase']}">here</a>.`)
     }
   }
 
