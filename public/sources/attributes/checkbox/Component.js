e294b8ca5b206eb49f732169829ca5013c720771
Pavel I <pavel@visualcomposer.com>
2021-08-05 13:04:08 +0300

    VC-2079 and VC-2078 toggleable checkbox component for post grid filters

--- public/sources/attributes/checkbox/Component.js
+++ public/sources/attributes/checkbox/Component.js
@@ -2,6 +2,10 @@ import React from 'react'
 import classNames from 'classnames'
 import Attribute from '../attribute'
 import Scrollbar from '../../../components/scrollbar/scrollbar'
+import { getResponse } from '../../../tools/response'
+import { getService } from 'vc-cake'
+
+const dataProcessor = getService('dataProcessor')
 
 export default class Checkbox extends Attribute {
   static defaultProps = {
@@ -10,17 +14,49 @@ export default class Checkbox extends Attribute {
 
   scrollbar = null
 
+  spinnerHtml = (
+    <span className='vcv-ui-wp-spinner' />
+  )
+
   constructor (props) {
     super(props)
     this.ref = React.createRef()
     this.handleScroll = this.handleScroll.bind(this)
     this.scrollBarMounted = this.scrollBarMounted.bind(this)
+    this.requestToServer = this.requestToServer.bind(this)
+    this.state.loading = props?.options?.action
+  }
+
+  updateState (props) {
+    return {
+      value: props.value,
+      values: this?.state?.values || this.getValues()
+    }
+  }
+
+  getValues () {
+    const { props } = this
+    let { values } = props.options || []
+    const { global } = props.options || {}
+    if (global && (!values || !values.length)) {
+      if (typeof window[global] === 'function') {
+        values = window[global]()
+      } else {
+        values = window[global] || []
+      }
+    }
+
+    return values
   }
 
   componentDidMount () {
+    if (this.props?.options?.action) {
+      // Initial Request to server
+      this.requestToServer()
+    }
     if (this.props.options && this.props.options.itemLimit) {
       const items = this.ref.current && this.ref.current.querySelectorAll('.vcv-ui-form-checkbox')
-      if (items.length > this.props.options.itemLimit) {
+      if (items && items.length > this.props.options.itemLimit) {
         let heightLimit = 0
         items.forEach((item, i) => {
           if (i < this.props.options.itemLimit) {
@@ -34,34 +70,48 @@ export default class Checkbox extends Attribute {
     }
   }
 
+  requestToServer () {
+    this.setState({
+      loading: true
+    })
+
+    dataProcessor.appAdminServerRequest({
+      'vcv-action': 'attribute:checkbox:render:adminNonce',
+      'vcv-tag': this.props?.elementAccessPoint?.tag || '',
+      'vcv-element': this.props.elementAccessPoint ? this.props.elementAccessPoint.cook().toJS() : {},
+      'vcv-field-key': this.props.fieldKey,
+      'vcv-field-value': this.state.value,
+      'vcv-field-action': this.props.options.action
+    }).then((requestData) => {
+      const response = getResponse(requestData)
+      const state = {
+        loading: false
+      }
+      if (response.status && response.results) {
+        state.values = response.results
+      }
+      this.setState(state)
+    }, (error) => {
+      console.warn('Failed to load checkboxes', error)
+      this.setState({
+        loading: false
+      })
+    })
+  }
+
   scrollBarMounted (scrollbar) {
     this.scrollbar = scrollbar
   }
 
   handleChange (event) {
     const value = event.target.value
-    var values = this.state.value
+    const values = this.state.value
     if (event.target.checked) {
       values.push(value)
     } else {
       values.splice(values.indexOf(value), 1)
     }
     this.setFieldValue(values)
-  }
-
-  getValues () {
-    const { props } = this
-    let { values } = props.options || {}
-    const { global } = props.options || {}
-    if (global && (!values || !values.length)) {
-      if (typeof window[global] === 'function') {
-        values = window[global]()
-      } else {
-        values = window[global] || []
-      }
-    }
-
-    return values
   }
 
   getCheckboxes (values, isChildren = false) {
@@ -97,11 +147,12 @@ export default class Checkbox extends Attribute {
       }
 
       if (this.props.options.listView && this.props.options.nesting) {
-        const initialValues = this.getValues()
+        const initialValues = this.state.values
         const children = initialValues.filter(value => value.parent === values[key].id)
         if (children.length && toRender) {
           const childCheckboxes = this.getCheckboxes(children, true)
-          optionElements.push(<div key={`${fieldKey}:${key}:children:${children.length}`} className='vcv-ui-form-checkbox-children'>{childCheckboxes}</div>)
+          optionElements.push(
+            <div key={`${fieldKey}:${key}:children:${children.length}`} className='vcv-ui-form-checkbox-children'>{childCheckboxes}</div>)
         }
       }
     }
@@ -113,7 +164,11 @@ export default class Checkbox extends Attribute {
   }
 
   render () {
-    const values = this.getValues()
+    if (this.props?.options?.action && this.state.loading) {
+      return this.spinnerHtml
+    }
+
+    const values = this.state.values
     const optionElements = this.getCheckboxes(values)
 
     let classNames = 'vcv-ui-form-checkboxes'
