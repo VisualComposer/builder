db3ec2b3a0d1b9203d1a7b5dac39c7b27c2aeb49
alperbuulut <bulut.alper2@gmail.com>
2021-08-06 15:03:11 +0300

    Remove unused params VC-2132

--- visualcomposer/Modules/Vendors/WpFormsController.php
+++ visualcomposer/Modules/Vendors/WpFormsController.php
@@ -10,7 +10,6 @@ if (!defined('ABSPATH')) {
 
 use VisualComposer\Framework\Container;
 use VisualComposer\Framework\Illuminate\Support\Module;
-use VisualComposer\Helpers\Frontend;
 use VisualComposer\Helpers\Request;
 use VisualComposer\Helpers\Traits\WpFiltersActions;
 
@@ -18,7 +17,7 @@ class WpFormsController extends Container implements Module
 {
     use WpFiltersActions;
 
-    public function __construct(Request $requestHelper, Frontend $frontendHelper)
+    public function __construct(Request $requestHelper)
     {
         if ($requestHelper->exists(VCV_ADMIN_AJAX_REQUEST) || $requestHelper->exists(VCV_AJAX_REQUEST)) {
             $this->wpAddFilter('wpforms_frontend_missing_assets_error_js_disable', '__return_true');
