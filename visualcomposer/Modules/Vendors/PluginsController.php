e2ac2445991178a40a2aa18791767e0ea605d812
alperbuulut <bulut.alper2@gmail.com>
2021-09-30 13:50:37 +0300

    Adjust incompatibility learnpress VC-2248

--- visualcomposer/Modules/Vendors/PluginsController.php
+++ visualcomposer/Modules/Vendors/PluginsController.php
@@ -24,6 +24,7 @@ class PluginsController extends Container implements Module
     {
         $this->wpAddAction('pre_current_active_plugins', 'enqueueJs');
         $this->wpAddFilter('plugin_row_meta', 'pluginRowMeta', 10, 2);
+        $this->addEvent('vcv:frontend:render', 'registerGlobalScreenId', 10);
 
         if (!$licenseHelper->isPremiumActivated()) {
             /** @see \VisualComposer\Modules\License\Pages\GoPremium::pluginsPageLink */
@@ -140,5 +141,18 @@ class PluginsController extends Container implements Module
         $links[] = $goPremiumLink;
 
         return $links;
+    }
+
+    /**
+     * Add global current screen id
+     */
+    protected function registerGlobalScreenId()
+    {
+        global $current_screen;
+        $sourceId = get_the_ID();
+
+        if (!$current_screen->id && $sourceId) {
+            $current_screen->id = $sourceId;
+        }
     }
 }
