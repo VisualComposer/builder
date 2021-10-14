12975028025e81022d2d7105cd13a15116582e87
OlegApanovich <dollar4444@gmail.com>
2021-10-14 16:22:49 +0300

    VC-2308 remove css gzip bundle

--- visualcomposer/resources/views/editor/frontend/frontend.php
+++ visualcomposer/resources/views/editor/frontend/frontend.php
@@ -37,7 +37,6 @@ wp_enqueue_media();
     <title><?php echo sprintf(__('Frontend editor: %s', 'visualcomposer'), get_the_title()); ?></title>
     <link rel="stylesheet"
             href="//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic&subset=latin,greek,greek-ext,cyrillic-ext,latin-ext,cyrillic">
-    <link rel="stylesheet" href="<?php echo get_site_url(null, '?vcv-style=wp'); ?>">
     <?php
     vcevent('vcv:frontend:render', ['sourceId' => $sourceId]);
     do_action('admin_enqueue_scripts', $hookSuffix);
