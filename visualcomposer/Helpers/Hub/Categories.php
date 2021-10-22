23805f7162413986ddc2b2e099edce202fd50f45
OlegApanovich <dollar4444@gmail.com>
2021-10-22 14:32:59 +0300

    VC-2282 Add category and tag layout elements

--- visualcomposer/Helpers/Hub/Categories.php
+++ visualcomposer/Helpers/Hub/Categories.php
@@ -247,6 +247,7 @@ class Categories implements Helper
                     'layoutPostDate',
                     'layoutPostAuthor',
                     'layoutPostAuthorBio',
+                    'layoutPostCategories',
                 ],
                 'icon' => $urlHelper->to('public/categories/icons/Section.svg'),
                 'iconDark' => $urlHelper->to('public/categories/iconsDark/Section.svg'),
