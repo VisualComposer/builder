9d251b8b045ee491d970b7f4b51df4266f0d9028
Pavel I <pavel@visualcomposer.com>
2021-11-10 13:45:20 +0200

    VC-2408 add notification message

--- visualcomposer/Helpers/Localizations.php
+++ visualcomposer/Helpers/Localizations.php
@@ -1826,6 +1826,10 @@ class Localizations extends Container implements Helper
                 'Archive template',
                 'visualcomposer'
             ),
+            'archiveGridElementMissingNotification' => __(
+                'It seems you don’t have a post grid element on your page with wpquery data source specified. Your archive page may not work properly. If you are using a custom grid element, feel free to ignore this message.',
+                'visualcomposer'
+            ),
         ];
 
         return vcfilter('vcv:helpers:localizations:i18n', $locale);
