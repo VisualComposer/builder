00930aa98ce4ac1b9f9a7c2d4101678f2439da06
OlegApanovich <dollar4444@gmail.com>
2021-11-23 13:08:50 +0200

    VC-2341 Improve replace element functionality for layout post list element

--- visualcomposer/Helpers/Hub/Categories.php
+++ visualcomposer/Helpers/Hub/Categories.php
@@ -250,6 +250,7 @@ class Categories implements Helper
                     'layoutPostCustomFields',
                     'layoutPostFeaturedImage',
                     'logoWidget',
+                    'layoutPostList',
                 ],
                 'icon' => $urlHelper->to('public/categories/icons/Section.svg'),
                 'iconDark' => $urlHelper->to('public/categories/iconsDark/Section.svg'),
