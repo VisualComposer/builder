760ed780d29ffd05da086954909eb65d34e2b73d
Pavel I <pavel@visualcomposer.com>
2021-12-21 16:28:15 +0200

    VC-2504 add filters for elements rendering on view page by backend side

---
+++ visualcomposer/Modules/Elements/ViewPageRender.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace VisualComposer\Modules\Elements;
+
+if (!defined('ABSPATH')) {
+    header('Status: 403 Forbidden');
+    header('HTTP/1.1 403 Forbidden');
+    exit;
+}
+
+use VisualComposer\Framework\Container;
+use VisualComposer\Framework\Illuminate\Support\Module;
+use VisualComposer\Helpers\Traits\WpFiltersActions;
+
+class ViewPageRender extends Container implements Module
+{
+    use WpFiltersActions;
+
+    public function __construct()
+    {
+        // Use WordPress 5.1 pre_render_block as it is more performance efficient
+        if (version_compare(get_bloginfo('version'), '5.1', '>=')) {
+            $this->wpAddFilter('pre_render_block', 'renderDynamicBlock');
+        } else {
+            $this->wpAddFilter('render_block', 'renderDynamicBlock');
+        }
+    }
+
+    /**
+     * Rendering dynamic view-page element render blocks.
+     *
+     * @param null $response
+     * @param array $block
+     *
+     * @return string
+     * @throws \ReflectionException|\VisualComposer\Framework\Illuminate\Container\BindingResolutionException
+     *
+     */
+    protected function renderDynamicBlock($response, $block)
+    {
+        if (!isset($block) || !is_array($block) || !array_key_exists('blockName', $block)) {
+            return $response;
+        }
+
+        $isDynamicViewPageRender = strpos($block['blockName'], 'vcwb-view-page-render-element') !== false;
+        if (!$isDynamicViewPageRender) {
+            return $response;
+        }
+
+        if (!empty($block['innerBlocks'])) {
+            $before = $block['innerContent'][0];
+            $after = $block['innerContent'][ count($block['innerContent']) - 1 ];
+            $content = '';
+            foreach ($block['innerBlocks'] as $innerBlock) {
+                $content .= render_block($innerBlock);
+            }
+        } else {
+            $before = $block['innerContent'][0];
+            $after = '';
+            $content = '';
+        }
+
+        $response = vcfilter('vcv:' . $block['blockName'], $before . $content . $after, [
+            'atts' => $block['attrs'],
+            'before' => $before,
+            'after' => $after,
+            'content' => $content,
+        ]);
+
+        return $response;
+    }
+}
