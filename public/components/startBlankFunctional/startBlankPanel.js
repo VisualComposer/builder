80a4fb606560356ffb299c5287a4293bff82d721
orkun <orkun.aybek@gmail.com>
2021-12-27 19:39:44 +0300

    VC-2301 Applied layout type logic for startBlankFunctional

--- public/components/startBlankFunctional/startBlankPanel.js
+++ public/components/startBlankFunctional/startBlankPanel.js
@@ -27,7 +27,9 @@ function StartBlankPanel (props) {
 
   const handleStartClick = () => {
     const editorType = dataManager.get('editorType')
-    const templateType = settingsStorage.state('templateType').get()
+    const localizations = dataManager.get('localizations')
+    const blankHeaderTitle = localizations.blankHeaderTitle ? localizations.blankHeaderTitle : 'Design your header here as a part of your layout. You can also download header templates from the Visual Composer Hub.'
+    const blankFooterTitle = localizations.blankFooterTitle ? localizations.blankFooterTitle : 'Design your footer here as a part of your layout. You can also download footer templates from the Visual Composer Hub.'
     if (editorType === 'popup') {
       const cookElement = cook.get({ tag: 'popupRoot' }).toJS()
       const rowElement = cook.get({ tag: 'row', parent: cookElement.id }).toJS()
@@ -37,13 +39,22 @@ function StartBlankPanel (props) {
       const iframe = document.getElementById('vcv-editor-iframe')
       iframeWindow = iframe && iframe.contentWindow && iframe.contentWindow.window
       iframeWindow.vcv && iframeWindow.vcv.on('ready', openEditForm)
-    } else if (editorType === 'vcv_layouts' && templateType === 'postTemplate') {
-      const layoutContentElement = cook.get({ tag: 'layoutContentArea' }).toJS()
-      const layoutHeaderElement = cook.get({ tag: 'layoutHeaderArea' }).toJS()
-      const layoutFooterElement = cook.get({ tag: 'layoutFooterArea' }).toJS()
-      elementsStorage.trigger('add', layoutHeaderElement)
-      elementsStorage.trigger('add', layoutContentElement)
-      elementsStorage.trigger('add', layoutFooterElement)
+    } else if (editorType === 'vcv_layouts') {
+      const layoutType = settingsStorage.state('layoutType').get()
+      let elementTag
+      if (layoutType === 'postTemplate') {
+        elementTag = 'layoutContentArea'
+      } else if (layoutType === 'archiveTemplate') {
+        elementTag = 'layoutPostList'
+      }
+
+      const commonDesignOptions = { device: { all: { boxModel: { marginTop: '50px', marginBottom: '50px' } } } }
+      const headerElement = cook.get({ tag: 'textBlock', designOptions: commonDesignOptions, output: `<p style="text-align:center;">${blankHeaderTitle}</p>` }).toJS()
+      const initialElement = cook.get({ tag: elementTag }).toJS()
+      const footerElement = cook.get({ tag: 'textBlock', designOptions: commonDesignOptions, output: `<p style="text-align:center;">${blankFooterTitle}</p>` }).toJS()
+      elementsStorage.trigger('add', headerElement)
+      elementsStorage.trigger('add', initialElement)
+      elementsStorage.trigger('add', footerElement)
     } else {
       const settings = {
         action: 'add',
