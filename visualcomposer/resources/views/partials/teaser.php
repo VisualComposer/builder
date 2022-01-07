6dcbcddf735f771627ed56074236ddd6daa86770
OlegApanovich <dollar4444@gmail.com>
2022-01-07 12:00:10 +0200

    VC-2474 improve rel link for a activate license button

--- visualcomposer/resources/views/partials/teaser.php
+++ visualcomposer/resources/views/partials/teaser.php
@@ -8,6 +8,7 @@ if (!defined('ABSPATH')) {
  * @var array[string#premiumTitle, string#premiumDescription, string#premiumButton] $page
  */
 
+$rel = empty($page['slug']) ? '' : '&vcv-ref=' . $page['slug'];
 ?>
 <div class="vcv-premium-teaser-inner">
     <div class="vcv-premium-teaser-image"></div>
@@ -23,7 +24,7 @@ if (!defined('ABSPATH')) {
         <?php else : ?>
             <a class="vcv-premium-teaser-btn" href="<?php echo $page['premiumUrl']; ?>" target="_blank" rel="noopener noreferrer"><?php esc_html_e('Go Premium', 'visualcomposer'); ?></a>
             <p class="vcv-premium-teaser-text">
-                <?php esc_html_e('Already have a Premium license?', 'visualcomposer'); ?> <a href="http://localhost:8888/wp-one/wp-admin/admin.php?page=vcv-activate-license&vcv-ref=elements-addelement-hub-editor" target="_blank" rel="noopener noreferrer"><?php esc_html_e('Activate here', 'visualcomposer'); ?></a>
+                <?php esc_html_e('Already have a Premium license?', 'visualcomposer'); ?> <a href="<?php echo set_url_scheme(admin_url('admin.php?page=vcv-activate-license' . $rel)); ?>" target="_blank" rel="noopener noreferrer"><?php esc_html_e('Activate here', 'visualcomposer'); ?></a>
             </p>
         <?php endif; ?>
     </div>
