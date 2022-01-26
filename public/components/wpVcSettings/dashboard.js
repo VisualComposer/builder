bb80992e14b7ada931f2421d774871111d5d1b6e
Janis Konutis <mcjohn@inbox.lv>
2022-01-26 10:44:30 +0200

    VC-2621 Add sticky sidebar

--- public/components/wpVcSettings/dashboard.js
+++ public/components/wpVcSettings/dashboard.js
@@ -1,5 +1,9 @@
 import { getService } from 'vc-cake'
 import importJS from '../../wordpressSettings'
+import ResizeSensorModule from 'resize-sensor'
+import StickySidebar from 'sticky-sidebar'
+
+window.ResizeSensor = ResizeSensorModule
 
 const dataManager = getService('dataManager')
 const localizations = dataManager.get('localizations')
@@ -26,16 +30,16 @@ export const dashboard = () => {
   const contentForms = Array.from(document.querySelectorAll('.vcv-settings-tab-content'))
   const dataCollectionTableWrapper = document.querySelector('.vcv-ui-settings-data-collection-table-wrapper')
   const dataCollectionTableButton = document.querySelector('#vcv-data-collection-table-button')
-  const setDashboardMenuStyles = () => {
-    const innerHeight = window.innerHeight - 30 - 32 - 30 - 32 // 2x paddings(30px) and logo(title) (32px) and admin-bar(32px)
-    if ((parseInt(window.getComputedStyle(navigationMenuTop).height) + parseInt(window.getComputedStyle(navigationMenuBottom).height)) > innerHeight) {
-      dashboardSidebar.style.height = 'auto'
-      navigationMenu.style.justifyContent = 'flex-start'
-    } else {
-      dashboardSidebar.style.height = '100%'
-      navigationMenu.style.justifyContent = 'space-between'
+
+  const sidebar = new StickySidebar('.vcv-dashboard-sidebar',
+    {
+      topSpacing: 32,
+      innerWrapperSelector: '.vcv-dashboard-sidebar-inner',
+      resizeSensor: true,
+      minWidth: 783
     }
-  }
+  )
+
   const handleNavigationToggle = () => {
     navigationMenuContainer.classList.toggle('vcv-is-navigation-visible')
     const ariaExpandedAttr = navigationToggle.getAttribute('aria-expanded')
@@ -139,25 +143,12 @@ export const dashboard = () => {
         handleSubmenuLinkClick(e)
       }
     }
-    setDashboardMenuStyles()
   }
 
   const handleContentFormChange = (e) => {
     formTouched = true
   }
-  // Implement throttle on window resize
-  const handleWindowResize = (func, duration) => {
-    let shouldWait = false
-    return function (...args) {
-      if (!shouldWait) {
-        func.apply(this, args)
-        shouldWait = true
-        setTimeout(function () {
-          shouldWait = false
-        }, duration)
-      }
-    }
-  }
+
   const handleDataCollectionTableToggle = () => {
     if (!dataCollectionTableWrapper) {
       return
@@ -177,12 +168,6 @@ export const dashboard = () => {
     form.addEventListener('change', handleContentFormChange)
   })
   navigationToggle.addEventListener('click', handleNavigationToggle)
-  window.addEventListener(
-    'resize',
-    handleWindowResize(() => {
-      setDashboardMenuStyles()
-    }, 250)
-  )
   window.onbeforeunload = () => {
     const isCodeEditorsTouched = dataManager.get('isCodeEditorsTouched')
     if (formTouched || isCodeEditorsTouched) {
