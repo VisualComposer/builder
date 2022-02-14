7550764bed19ff6d7f1f3f417943c496a0530d21
Kristaps <kristaps.kokins89@gmail.com>
2022-02-14 10:41:40 +0200

    VC-2388 rowResizer column prepend

--- public/components/columnResizer/rowSideResizer.js
+++ public/components/columnResizer/rowSideResizer.js
@@ -1,51 +1,62 @@
+import { getStorage, getService } from 'vc-cake'
 import React from 'react'
-// import vcCake from 'vc-cake'
-import { getStorage } from 'vc-cake'
 
 const RowSideResizer = (props) => {
-  // const documentService = vcCake.getService('document')
   const workspaceStorage = getStorage('workspace')
-  const [dragging, setDragging] = React.useState(false)
+  const elementsStorage = getStorage('elements')
+  const documentService = getService('document')
+  const layoutStorage = getStorage('layout')
+
   const [container, setContainer] = React.useState(undefined)
-  const [left, setLeft] = React.useState('auto')
-  const [right, setRight] = React.useState('auto')
+  const [dragging, setDragging] = React.useState(false)
   const [expanded, setExpanded] = React.useState(false)
+  const [right, setRight] = React.useState('auto')
+  const [left, setLeft] = React.useState('auto')
   const self = React.useRef(null)
 
   React.useEffect(() => setContainer(() => self.current.closest('.vce-row-container')), [])
 
   const handleMouseMove = (e) => {
     if (dragging) {
-      // const rowId = container.firstChild.dataset.vcvDndElement
-      // const rowService = documentService.get(rowId)
       const bounding = container.getBoundingClientRect()
       const row = self.current.closest('.vce-row')
-      /* const value = */
       props.left ? handleLeftBar(e, bounding, row) : handleRightBar(e, bounding, row)
-      // rowService.designOptionsAdvanced.attributeMixins['boxModelMixin:all'].variables.marginLeft.value = value
-      // rowService.designOptionsAdvanced.device.all.boxModel.marginLeft = value
-      // documentService.update(rowId, rowService)
     }
   }
 
   const handleLeftBar = (e, bounding, row) => {
     const distance = e.pageX - bounding.x
-    const marginLeft = distance > 0 ? `${Math.round(distance)}px` : 'auto'
-    row.style.marginLeft = marginLeft
-    setLeft(marginLeft)
+    const margin = /*distance > 0 ?*/ `${Math.round(distance)}px` /* : 'auto'*/
+    row.style.marginLeft = margin
+    setLeft(margin)
+    return margin
   }
 
   const handleRightBar = (e, bounding, row) => {
     const distance = bounding.x + bounding.width - e.pageX
-    const marginRight = distance > 0 ? `${Math.round(distance)}px` : 'auto'
-    row.style.marginRight = marginRight
-    setRight(marginRight)
+    const margin = /*distance > 0 ?*/ `${Math.round(distance)}px` /*: 'auto'*/
+    row.style.marginRight = margin
+    setRight(margin)
+    return margin
   }
 
   const addColumn = () => {
     if (!dragging) {
       const rowId = container.firstChild.dataset.vcvDndElement
-      workspaceStorage.trigger('add', rowId, 'column')
+      if (props.left) {
+        const row = self.current.closest('.vce-row-content')
+        const columns = row.getElementsByClassName('vce-col')
+        const firstColId = columns[0].dataset.vcvElement
+
+        workspaceStorage.trigger('add', rowId, 'column')
+
+        setTimeout(() => {
+          const newColId = columns[columns.length - 1].dataset.vcvElement
+          workspaceStorage.trigger('move', newColId, { action: 'before', related: firstColId })
+        }, 1);
+      } else {
+        workspaceStorage.trigger('add', rowId, 'column')
+      }
     }
   }
 
@@ -59,6 +70,22 @@ const RowSideResizer = (props) => {
     setExpanded(false)
   }
 
+  const handleMouseUp = () => {
+    // const row = self.current.closest('.vce-row')
+    // row.style.marginLeft = null
+    // row.style.marginRight = null
+
+    setDragging(false)
+    const id = container.firstChild.dataset.vcvDndElement
+    const element = documentService.get(id)
+    if (element.designOptionsAdvanced.device) {
+      const boxModel = element.designOptionsAdvanced.device.all.boxModel
+      boxModel.combined = false
+      boxModel[props.left ? 'marginLeft' : 'marginRight'] = props.left ? left : right
+      // elementsStorage.trigger('update', id, element, '', { changedAttributeType: 'designOption' })
+    }
+  }
+
   return (
     <div
       ref={self}
@@ -66,15 +93,15 @@ const RowSideResizer = (props) => {
       onMouseLeave={handleMouseLeave}
       onMouseMove={handleMouseMove}
       onMouseDown={() => setDragging(true)}
-      onMouseUp={() => setDragging(false)}
+      onMouseUp={handleMouseUp}
       className={`vce-row-resizer-handler vce-row-resizer-position-${props.left ? 'left' : 'right'} ${expanded ? 'expanded' : ''}`}
     >
       <div>
         {expanded &&
           <span onClick={addColumn}>
-            <i className='vcv-ui-navbar-control-icon vcv-ui-icon vcv-ui-icon-add' />
+            <i className="vcv-ui-navbar-control-icon vcv-ui-icon vcv-ui-icon-add" />
           </span>}
-        <i className='seperator' />
+        <i className="seperator" />
         {expanded && <span>{props.left ? left : right}</span>}
       </div>
     </div>
