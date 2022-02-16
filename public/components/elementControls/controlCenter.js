58668f5eae685951fa5585e37c9da96ab0d58b58
Kristaps <kristaps.kokins89@gmail.com>
2022-02-16 12:10:17 +0200

    VC-2388 element center controls outline on hover

--- public/components/elementControls/controlCenter.js
+++ public/components/elementControls/controlCenter.js
@@ -35,11 +35,11 @@ export default function ControlCenter (props) {
     const content = iframe.document.querySelector(`[data-vcv-element="${props.id}"]`)
 
     const container = document.querySelector('.vcv-layout-content')
-    const sidebar = document.querySelector('.vcv-layout-bar-content')
+    const sidebar = document.querySelector('.vcv-layout-bar')
     const border = document.createElement('div')
 
     border.setAttribute('class', 'vcv-ui-center-temp-outline-temp')
-    border.style.left = content.getBoundingClientRect().left + sidebar.clientWidth + 60 + 'px'
+    border.style.left = content.getBoundingClientRect().left + sidebar.clientWidth + 'px'
     border.style.top = content.getBoundingClientRect().top + 'px'
     border.style.width = content.clientWidth + 'px'
     border.style.height = content.offsetHeight + 'px'
