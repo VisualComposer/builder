625e67818037dccf50e761d159741dd308ba73af
Janis <mcjohn@inbox.lv>
2022-02-23 10:56:54 +0200

    Redesign element controls vc 2388 (#1894)
    * Redesign element controls wip VC-2388
    
    * Redesign controls wip VC-2388
    
    * Wip VC-2388
    
    * Change files to TSX VC-2388
    
    * Revert file VC-2388
    
    * Add interaction with outlines VC-2388
    
    * Update icon sizes VC-2388
    
    * Remove unnecessary action VC-2388

---
+++ public/components/elementControls/mainControl.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { getStorage, setData } from 'vc-cake'
+
+const layoutStorage = getStorage('layout')
+
+let clickState = 'mouseUp'
+let mouseDownTimeout: boolean | number | any = false
+
+interface Props {
+  title: string;
+  id: string;
+  icon: string
+}
+
+const MainControl: React.FC<Props> = ({title, id, icon}) => {
+  const startDrag = (e: React.MouseEvent) => {
+    layoutStorage.state('interactWithControls').set({
+      type: 'mouseLeave',
+      vcElementId: id
+    })
+    const layoutContent = document.querySelector('.vcv-layout-content') as HTMLElement
+    setData('draggingElement', { id: id, point: { x: e.clientX - layoutContent.offsetLeft, y: e.clientY - layoutContent.offsetTop } })
+    window.clearTimeout(mouseDownTimeout)
+    mouseDownTimeout = false
+  }
+
+  const handleMouseUp = (e: React.MouseEvent) => {
+    e && e.preventDefault()
+    clickState = 'mouseUp'
+    window.clearTimeout(mouseDownTimeout)
+    mouseDownTimeout = false
+  }
+
+  const handleMouseDown = (e: React.MouseEvent) => {
+    e && e.preventDefault()
+    clickState = 'mouseDown'
+    mouseDownTimeout = setTimeout(() => {
+      if (clickState === 'mouseDown') {
+        startDrag(e)
+      }
+    }, 200)
+  }
+
+  return (
+    <div
+      className='vcv-ui-outline-control-dropdown-trigger vcv-ui-outline-control'
+      title={title}
+      data-vcv-element-id={id}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+    >
+        <span className='vcv-ui-outline-control-content'>
+          <img className='vcv-ui-outline-control-icon' src={icon} alt={title} />
+        </span>
+    </div>
+  )
+}
+
+export default MainControl
