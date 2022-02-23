70bfc989d1ace83fe1efd3785080f7284a9a6d4b
Janis Konutis <mcjohn@inbox.lv>
2022-02-23 10:39:23 +0200

    Add interaction with outlines VC-2388

--- public/components/elementControls/centerControls.tsx
+++ public/components/elementControls/centerControls.tsx
@@ -1,8 +1,10 @@
 import React, { useRef, useState, useEffect } from 'react'
 import ControlDropdownInner from './controlDropdownInner'
 import { ControlHelpers } from './controlHelpers'
-import { getService } from 'vc-cake'
+import { getService, getStorage } from 'vc-cake'
 import MainControl from './mainControl'
+
+const layoutStorage = getStorage('layout')
 
 interface Props {
   id: string;
@@ -43,6 +45,22 @@ const CenterControls: React.FC<Props> = ({ id, containerPos, controlsListWidth }
     topPosition = 2
   }
 
+  const handleMouseEnter = () => {
+    layoutStorage.state('interactWithControls').set({
+      type: 'mouseEnter',
+      vcElementId: id
+    })
+    layoutStorage.state('userInteractWith').set(id)
+  }
+
+  const handleMouseLeave = () => {
+    layoutStorage.state('interactWithControls').set({
+      type: 'mouseLeave',
+      vcElementId: id
+    })
+    layoutStorage.state('userInteractWith').set(false)
+  }
+
   const styles = {
     top: `${topPosition}px`,
     left: `${containerPos.left}px`,
@@ -50,7 +68,12 @@ const CenterControls: React.FC<Props> = ({ id, containerPos, controlsListWidth }
   }
   const isCenterControls = true
   return (
-    <div className='vcv-ui-outline-controls-center' style={{ ...styles }} ref={outerRef}>
+    <div
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className='vcv-ui-outline-controls-center'
+      style={{...styles}}
+      ref={outerRef}>
       <div className='vcv-ui-outline-controls-center-inner' ref={innerRef}>
         <MainControl id={id} title={title} icon={icon} />
         <ControlDropdownInner elementId={id} isCenterControls={isCenterControls} />
