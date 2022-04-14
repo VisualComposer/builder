34c9c31ca10808a03a9bf247e28c7bc9f7f2ccff
orkunaybek <49758433+orkunaybek@users.noreply.github.com>
2022-04-14 13:22:49 +0300

    VC-2483 Fixed color options not updating after text edit (#1971)
    * VC-2483 Fixed color options not updating after text edit
    
    * VC-2483 Updated update rule for activites manager
    
    Co-authored-by: orkun <orkun.aybek@gmail.com>

--- public/components/panels/editForm/lib/activitiesManager.js
+++ public/components/panels/editForm/lib/activitiesManager.js
@@ -26,11 +26,13 @@ export default class ActivitiesManager extends React.Component {
   }
 
   shouldComponentUpdate (nextProps) {
-    this.mount = {}
-    this.stack = {}
-    this.mountStack = {}
-    this.initialStack = {}
-    this.listeners = this.initListeners(nextProps.elementAccessPoint.cook(), nextProps)
+    if (!lodash.isEqual(nextProps.id, this.props.id)) {
+      this.mount = {}
+      this.stack = {}
+      this.mountStack = {}
+      this.initialStack = {}
+      this.listeners = this.initListeners(nextProps.elementAccessPoint.cook(), nextProps)
+    }
     return true
   }
 
