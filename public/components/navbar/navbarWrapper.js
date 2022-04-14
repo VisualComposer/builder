7bb54fceee32289e64292f8095e3234b452a043e
Janis Konutis <mcjohn@inbox.lv>
2022-04-14 13:21:46 +0300

    Remove old drag functionality and fix disabled navbar VC-2679

--- public/components/navbar/navbarWrapper.js
+++ public/components/navbar/navbarWrapper.js
@@ -1,119 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import classNames from 'classnames'
 import { getStorage } from 'vc-cake'
 
 const workspaceStorage = getStorage('workspace')
 
-export default class NavbarWrapper extends React.Component {
-  state = {
-    showGuideline: false,
-    guidelinePosition: 'top',
-    isDragging: false,
-    isDisabled: true
-  }
+const NavbarWrapper = (props) => {
+  const [isDisabled, setDisabled] = useState(true)
 
-  constructor (props) {
-    super(props)
-    this.handleDisableChange = this.handleDisableChange.bind(this)
-  }
-
-  componentDidMount () {
-    document.addEventListener('vc.ui.navbar.drag-start', this.handleNavbarDragStart)
-    document.addEventListener('vc.ui.navbar.drag-end', this.handleNavbarDragEnd)
-    document.addEventListener('vc.ui.navbar.dragging', this.handleNavbarDragging)
-    workspaceStorage.state('navbarDisabled').onChange(this.handleDisableChange)
-  }
-
-  componentWillUnmount () {
-    document.removeEventListener('vc.ui.navbar.drag-start', this.handleNavbarDragStart)
-    document.removeEventListener('vc.ui.navbar.drag-end', this.handleNavbarDragEnd)
-    document.removeEventListener('vc.ui.navbar.dragging', this.handleNavbarDragging)
-    workspaceStorage.state('navbarDisabled').ignoreChange(this.handleDisableChange)
-  }
-
-  handleDisableChange (isDisabled) {
-    this.setState({ isDisabled })
-  }
-
-  handleNavbarDragStart = (e) => {
-    this.setState({
-      isDragging: true
-    })
-  }
-
-  handleNavbarDragEnd = (e) => {
-    this.setState({
-      isDragging: false
-    })
-  }
-
-  handleNavbarDragging = (e) => {
-    const { windowSize, navPosY, navPosX, navbarPosition } = e.eventData
-    const navSize = 60
-    const navSizeSide = 60 * 2
-    if (navbarPosition === 'detached') {
-      // if nav is on top
-      if (navPosY < navSize) {
-        this.setState({
-          showGuideline: true,
-          guidelinePosition: 'top'
-        })
-        return
-      }
-      // if nav is on bottom
-      if (windowSize.height - navSize < navPosY) {
-        this.setState({
-          showGuideline: true,
-          guidelinePosition: 'bottom'
-        })
-        return
-      }
-      // if nav is on left
-      if (navPosX < navSizeSide) {
-        this.setState({
-          showGuideline: true,
-          guidelinePosition: 'left'
-        })
-        return
-      }
-      // if nav is on right
-      if (windowSize.width - navSizeSide < navPosX) {
-        this.setState({
-          showGuideline: true,
-          guidelinePosition: 'right'
-        })
-        return
-      }
+  useEffect(() => {
+    workspaceStorage.state('navbarDisabled').onChange(handleDisableChange)
+    return () => {
+      workspaceStorage.state('navbarDisabled').ignoreChange(handleDisableChange)
     }
-    this.setState({
-      showGuideline: false
-    })
+  })
+
+  const handleDisableChange = (isDisabled) => {
+    setDisabled(isDisabled)
   }
 
-  render () {
-    const { guidelinePosition, isDragging, showGuideline } = this.state
-    let draggingContent = ''
-    if (isDragging) {
-      let guidelineClasses = ['vcv-ui-navbar-guideline', 'vcv-ui-navbar-guideline-' + guidelinePosition]
-      if (showGuideline) {
-        guidelineClasses.push('vcv-ui-navbar-guideline-is-visible')
-      }
-      guidelineClasses = guidelineClasses.join(' ')
-      draggingContent = (<div className={guidelineClasses} />)
-    }
+  const classes = classNames({
+    'vcv-layout-bar-header': true,
+    'vcv-layout-bar-header--loading': isDisabled
+  })
 
-    const classes = classNames({
-      'vcv-layout-bar-header': true,
-      'vcv-layout-bar-header--loading': this.state.isDisabled
-    })
-
-    return (
-      <div ref={this.props.wrapperRef} className={classes} id='vcv-editor-header'>
-        <div id='vc-navbar-container'>
-          {draggingContent}
-          {this.props.children}
-        </div>
+  return (
+    <div ref={props.wrapperRef} className={classes} id='vcv-editor-header'>
+      <div id='vc-navbar-container'>
+        {props.children}
       </div>
-    )
-  }
+    </div>
+  )
 }
+
+export default NavbarWrapper
