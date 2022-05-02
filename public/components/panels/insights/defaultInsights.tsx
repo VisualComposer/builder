8f4ea26d9f4c1223e4c36266f287772939bfcde6
Janis Konutis <mcjohn@inbox.lv>
2022-05-02 14:24:24 +0300

    Remove unused method VC-2701

--- public/components/panels/insights/defaultInsights.tsx
+++ public/components/panels/insights/defaultInsights.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import {connect} from 'react-redux'
 import {getService} from 'vc-cake'
 import InsightGroup from './insightGroup'
