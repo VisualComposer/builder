b9cf87620e0817746fca026ed28ab3b709ad1b1b
Nikita <nikita.h@visualcomposer.com>
2022-05-03 14:31:31 +0300

    Update JS codestyle VC-2545

--- public/components/uiHelpers/accordionPanel/accordionPanel.tsx
+++ public/components/uiHelpers/accordionPanel/accordionPanel.tsx
@@ -4,48 +4,49 @@ import classNames from 'classnames'
 import Tooltip from 'public/components/tooltip/tooltip'
 
 interface Props {
-    children: any,
-    sectionTitle: string,
-    tooltipText: string
+  children: any,
+  sectionTitle: string,
+  tooltipText: string
 }
 
 const AccordionPanel: React.FC<Props> = ({children, sectionTitle, tooltipText}) => {
-    const [isActive, setIsActive] = useState(true)
+  const [isActive, setIsActive] = useState(true)
 
   const handleClickToggleSection = (e: React.MouseEvent) => {
-    if (e.currentTarget === e.target || (e.target && e.target.classList && e.target.classList.contains('vcv-ui-edit-form-section-header-title'))) {
-        setIsActive(!isActive)
+    const target:any = e.target
+    if (e.currentTarget === target || (target && target.classList && target.classList.contains('vcv-ui-edit-form-section-header-title'))) {
+      setIsActive(!isActive)
     }
   }
 
-    const sectionClasses = classNames({
-        'vcv-ui-edit-form-section': true,
-        'vcv-ui-edit-form-section--opened': isActive,
-        'vcv-ui-edit-form-section--closed': !isActive
-    })
+  const sectionClasses = classNames({
+    'vcv-ui-edit-form-section': true,
+    'vcv-ui-edit-form-section--opened': isActive,
+    'vcv-ui-edit-form-section--closed': !isActive
+  })
 
-    let tooltip = null
-    if (tooltipText) {
-        tooltip = (
-            <Tooltip>
-                {tooltipText}
-            </Tooltip>
-        )
-    }
-
-    return (
-        <div className={sectionClasses}>
-            <div className='vcv-ui-edit-form-section-header' onClick={handleClickToggleSection}>
-          <span className='vcv-ui-edit-form-section-header-title'>
-            {sectionTitle}
-          </span>
-                {tooltip}
-            </div>
-            <div className='vcv-ui-edit-form-section-content'>
-                {children}
-            </div>
-        </div>
+  let tooltip = null
+  if (tooltipText) {
+    tooltip = (
+      <Tooltip>
+        {tooltipText}
+      </Tooltip>
     )
+  }
+
+  return (
+    <div className={sectionClasses}>
+      <div className='vcv-ui-edit-form-section-header' onClick={handleClickToggleSection}>
+        <span className='vcv-ui-edit-form-section-header-title'>
+          {sectionTitle}
+        </span>
+        {tooltip}
+      </div>
+      <div className='vcv-ui-edit-form-section-content'>
+        {children}
+      </div>
+    </div>
+  )
 }
 
 export default AccordionPanel
