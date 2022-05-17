492d98b06f7760153c1e1c4a9a4ed6ae939bf513
Janis Konutis <mcjohn@inbox.lv>
2022-05-17 13:20:46 +0300

    Update jest versions VC-2826

--- public/editor/services/utils/service.js
+++ public/editor/services/utils/service.js
@@ -1,5 +1,5 @@
 import { getService, addService, env } from 'vc-cake'
-import { deflate } from 'pako/lib/deflate'
+import { deflate } from 'pako'
 import base64 from 'base-64'
 
 // compute once, optimization for recalculate styles
