d9fa0cae54e19736d70814a587060513e5a00cc3
Nikita <nikita.h@visualcomposer.com>
2022-06-13 16:15:45 +0300

    Update blankIcon id attribute VC-2545

--- public/components/blankPageIntro/lib/layoutsData/blankIcon.tsx
+++ public/components/blankPageIntro/lib/layoutsData/blankIcon.tsx
@@ -6,7 +6,7 @@ const BlankIcon = () => {
       <g id='Blank-Page-Wizard' stroke='none' strokeWidth='1' fill='none' fillRule='evenodd'>
         <g id='blank-page-wizard' transform='translate(-409.000000, -193.000000)' stroke='#74788A' strokeWidth='3'>
           <g id='thumbnails' transform='translate(85.000000, 95.000000)'>
-            <g id='blan k-layout-hover' transform='translate(254.350943, 48.000000)'>
+            <g id='blank-layout-hover' transform='translate(254.350943, 48.000000)'>
               <g id='thumbnail'>
                 <path d='M71.5,51.5 L71.5,128.5 L148.5,128.5 L148.5,51.5 L71.5,51.5 Z' id='ico' />
               </g>
