ae9c7d8080f12374056bafdc5b0675752464028f
Janis Konutis <mcjohn@inbox.lv>
2022-06-14 10:41:41 +0300

    VC-2872 Remove console log

--- public/components/editorInit/setupCake.js
+++ public/components/editorInit/setupCake.js
@@ -2,7 +2,6 @@ import vcCake from 'vc-cake'
 export const setupCake = () => {
   const globalStoreInstance = require('../../editor/stores/globalStoreInstance')
   vcCake.env('globalStore', globalStoreInstance.default)
-  console.log('set global store')
   vcCake.env('platform', 'wordpress').start(() => {
     vcCake.env('editor', 'frontend')
     require('../../editor/stores/fieldOptionsStorage')
