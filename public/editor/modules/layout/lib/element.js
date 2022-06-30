20173a22d4f2c1d14e944ea663efcde31d777007
Nikita Hlopov <nikitahl@users.noreply.github.com>
2022-06-30 12:46:18 +0300

    VC-2894 Add debounce to element component update (#2091)
    * VC-2894 Add debounce to element component update
    
    * VC-2894 Fix JS codestyle

--- public/editor/modules/layout/lib/element.js
+++ public/editor/modules/layout/lib/element.js
@@ -3,7 +3,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ContentControls from 'public/components/layoutHelpers/contentControls/component'
 import ColumnResizer from 'public/components/columnResizer/columnResizer'
-import { isEqual, defer, cloneDeep } from 'lodash'
+import { isEqual, defer, cloneDeep, debounce } from 'lodash'
 import PropTypes from 'prop-types'
 import EmptyCommentElementWrapper from './emptyCommentElementWrapper.tsx'
 import ElementInner from './elementInner'
@@ -28,7 +28,7 @@ export default class Element extends React.Component {
 
   constructor (props) {
     super(props)
-    this.dataUpdate = this.dataUpdate.bind(this)
+    this.dataUpdate = debounce(this.dataUpdate.bind(this), 20)
     this.elementComponentTransformation = this.elementComponentTransformation.bind(this)
     this.getEditorProps = this.getEditorProps.bind(this)
     this.elementComponentRef = React.createRef()
