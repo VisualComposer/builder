add6c4d7ea29802c226377ba8c48fc756ffed91b
Nikita <nikita.h@visualcomposer.com>
2022-07-07 11:08:14 +0300

    Add cypress log to console package to debug pipeline VC-2545

--- tests/cypressConfig/cypress/plugins/index.js
+++ tests/cypressConfig/cypress/plugins/index.js
@@ -14,6 +14,8 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  require('cypress-log-to-output').install(on)
+
   config = config.env || config
   return config
 }
