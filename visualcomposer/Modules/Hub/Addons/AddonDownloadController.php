4f157f25726e96c6556fce26239e64aca9d1d8da
OlegApanovich <dollar4444@gmail.com>
2022-07-20 14:51:30 +0300

    VC-2697 code style commit

--- visualcomposer/Modules/Hub/Addons/AddonDownloadController.php
+++ visualcomposer/Modules/Hub/Addons/AddonDownloadController.php
@@ -67,8 +67,6 @@ class AddonDownloadController extends Container implements Module
         return $response;
     }
 
-
-
     protected function sendRequestJson($bundle, $token)
     {
         $hubBundleHelper = vchelper('HubBundle');
