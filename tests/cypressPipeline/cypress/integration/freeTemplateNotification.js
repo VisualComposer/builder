517fa8ddc508ac22fd1fe77bf6d43645927274be
Anna Tetere <anna.t@visualcomposer.com>
2022-07-21 10:18:09 +0300

    VC-2868 add step for notification

--- tests/cypressPipeline/cypress/integration/freeTemplateNotification.js
+++ tests/cypressPipeline/cypress/integration/freeTemplateNotification.js
@@ -6,12 +6,14 @@ describe('Free template notification', function () {
         cy.contains('.wp-menu-name', 'Visual Composer').click()
         cy.contains('.vcv-dashboard-sidebar-navigation-link', 'Visual Composer Hub').click()
 
+        cy.contains('.vcv-premium-teaser-btn', 'Yes, I agree').click()
+
         cy.contains('.vcv-ui-navigation-slider-button', 'Templates').click()
         cy.get('img[alt="Simple Blog Article"]').next().find('.vcv-ui-item-add').click()
-        cy.window().then((win) => {
-            cy.route('POST', win.vcvAdminAjaxUrl).as('loadContentRequest')
-          })
-          cy.wait('@loadContentRequest')
+        // cy.window().then((win) => {
+        //     cy.route('POST', win.vcvAdminAjaxUrl).as('loadContentRequest')
+        //   })
+        //   cy.wait('@loadContentRequest')
         
         cy.createPage()
 
