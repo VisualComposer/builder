cde86f28107e8abeb5b8259ef1e660feddb5e2ad
Anna Tetere <anna.t@visualcomposer.com>
2022-07-21 11:19:56 +0300

    VC-2868 add back step for notification

--- tests/cypressPipeline/cypress/integration/freeTemplateNotification.js
+++ tests/cypressPipeline/cypress/integration/freeTemplateNotification.js
@@ -6,7 +6,7 @@ describe('Free template notification', function () {
         cy.contains('.wp-menu-name', 'Visual Composer').click()
         cy.contains('.vcv-dashboard-sidebar-navigation-link', 'Visual Composer Hub').click()
 
-        // cy.contains('.vcv-premium-teaser-btn', 'Yes, I agree').click()
+        cy.contains('.vcv-premium-teaser-btn', 'Yes, I agree').click()
 
         cy.contains('.vcv-ui-navigation-slider-button', 'Templates').click()
         cy.get('img[alt="Simple Blog Article"]').next().find('.vcv-ui-item-add').click()
