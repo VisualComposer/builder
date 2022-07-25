59e0bbb67afd6c5f0f7be458e027a9add6d4d2d2
OlegApanovich <dollar4444@gmail.com>
2022-07-25 13:44:52 +0300

    VC-2925 fix pipline issue

--- visualcomposer/Modules/Settings/Ajax/SystemStatusController.php
+++ visualcomposer/Modules/Settings/Ajax/SystemStatusController.php
@@ -101,8 +101,9 @@ class SystemStatusController extends Container implements Module
         $charactersLength = strlen($characters);
         $randomString = '';
         for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[mt_rand(0, $charactersLength - 1)];
+            $randomString .= $characters[random_int(0, $charactersLength - 1)];
         }
+
         return $randomString;
     }
 }
