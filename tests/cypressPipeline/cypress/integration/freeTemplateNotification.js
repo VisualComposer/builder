573c3361eb3881bed34575ac9d270dad204dd0c2
Anna Tetere <anna.t@visualcomposer.com>
2022-07-26 12:55:12 +0300

    VC-2868 add wait before vsit page

--- tests/cypressPipeline/cypress/integration/freeTemplateNotification.js
+++ tests/cypressPipeline/cypress/integration/freeTemplateNotification.js
@@ -13,9 +13,17 @@ describe('Free template notification', function () {
         cy.contains('.vcv-ui-navigation-slider-button', 'Templates').click()
         cy.get('img[alt="Simple Blog Article"]').next().find('.vcv-ui-item-add').click()
 
-        cy.wait(1500)
+        /*
+        cy.get('.vcv-ui-wp-spinner-light').should("not.be.visible", { timeout: 10000000 }); 
+        */
         
-        cy.createPage()
+        cy.wait(20000).visit(Cypress.env('newPage'))
+        cy.window().then((win) => {
+          cy.route('POST', win.vcvAdminAjaxUrl).as('loadContentRequest')
+        })
+        cy.wait('@loadContentRequest')
+        cy.wait(1000)
+        cy.get('.blank-page-submit-button').click()
 
         cy.contains('.vcv-ui-navigation-slider-button', 'Templates').click()
         cy.get('img[alt="Simple Blog Article"]').should('exist')
