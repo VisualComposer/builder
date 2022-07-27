61d61d7b9fc6538821daf9c9039a6ebca773de6c
Nikita <nikita.h@visualcomposer.com>
2022-07-27 10:29:31 +0300

    VC-2868 Add template download request wait command

--- tests/cypressPipeline/cypress/integration/freeTemplateNotification.js
+++ tests/cypressPipeline/cypress/integration/freeTemplateNotification.js
@@ -3,24 +3,17 @@
 describe('Free template notification', function () {
     it('Check if notification shows up', function () {
 
-        cy.contains('.wp-menu-name', 'Visual Composer').click()
-        cy.contains('.vcv-dashboard-sidebar-navigation-link', 'Visual Composer Hub').click()
-
-       cy.contains('.vcv-premium-teaser-btn', 'Yes, I agree').click()
-
-        cy.wait(1500)
+        cy.visit('/wp-admin/admin.php?page=vcv-hub')
+        cy.wait(200)
 
         cy.contains('.vcv-ui-navigation-slider-button', 'Templates').click()
+        cy.window().then((win) => {
+          cy.route('POST', win.vcvAdminAjaxUrl).as('downloadRequest')
+        })
         cy.get('img[alt="Simple Blog Article"]').next().find('.vcv-ui-item-add').click()
-        //cy.window().then((win) => {
-        cy.route({
-            method: 'POST',
-            url:'http://localhost:8888/dev/wp-admin/admin-ajax.php?vcv-admin-ajax=1&action=vcv-admin-ajax',
-        }).as('downloadRequest')
-        //})
-        cy.wait('@downloadRequest')
-        
-        cy.wait(40000).visit(Cypress.env('newPage'))
+        cy.wait('@downloadRequest')
+
+        cy.visit(Cypress.env('newPage'))
         cy.window().then((win) => {
           cy.route('POST', win.vcvAdminAjaxUrl).as('loadContentRequest')
         })
@@ -31,7 +24,5 @@ describe('Free template notification', function () {
         cy.contains('.vcv-ui-navigation-slider-button', 'Templates').click()
         cy.get('img[alt="Simple Blog Article"]').should('exist')
 
-
-        
     }
     )})
