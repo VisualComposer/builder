9d09939e5fe52c593c120d0593389d4fffe2b93c
orkun <orkun.aybek@gmail.com>
2022-08-23 13:29:35 +0300

    VC-2736 Fixed layout width

--- visualcomposer/resources/views/editor/templates/blank-template.php
+++ visualcomposer/resources/views/editor/templates/blank-template.php
@@ -22,17 +22,17 @@ if (!defined('ABSPATH')) {
     <!-- Override the main container width styles -->
     <style>
         @media (min-width: 1200px) {
-            .vcv-content--boxed .entry-content > [data-vce-boxed-width="true"],
-            .vcv-content--boxed .vcv-layouts-html > [data-vce-boxed-width="true"],
-            .vcv-content--boxed .entry-content .vcv-layouts-html > [data-vce-boxed-width="true"],
-            .vcv-editor-theme-hf .vcv-layouts-html > [data-vce-boxed-width="true"],
-            .vcv-header > [data-vce-boxed-width="true"],
-            .vcv-footer > [data-vce-boxed-width="true"],
-            .vcv-content--boxed .entry-content > * > [data-vce-full-width="true"]:not([data-vce-stretch-content="true"]) > [data-vce-element-content="true"],
-            .vcv-content--boxed .vcv-layouts-html > * > [data-vce-full-width="true"]:not([data-vce-stretch-content="true"]) > [data-vce-element-content="true"],
-            .vcv-editor-theme-hf .vcv-layouts-html > * > [data-vce-full-width="true"]:not([data-vce-stretch-content="true"]) > [data-vce-element-content="true"],
-            .vcv-header > * > [data-vce-full-width="true"]:not([data-vce-stretch-content="true"]) > [data-vce-element-content="true"],
-            .vcv-footer > * > [data-vce-full-width="true"]:not([data-vce-stretch-content="true"]) > [data-vce-element-content="true"] {
+            div.vcv-content--boxed .entry-content > [data-vce-boxed-width="true"],
+            div.vcv-content--boxed .vcv-layouts-html > [data-vce-boxed-width="true"],
+            div.vcv-content--boxed .entry-content .vcv-layouts-html > [data-vce-boxed-width="true"],
+            div.vcv-editor-theme-hf .vcv-layouts-html > [data-vce-boxed-width="true"],
+            div.vcv-header > [data-vce-boxed-width="true"],
+            div.vcv-footer > [data-vce-boxed-width="true"],
+            div.vcv-content--boxed .entry-content > * > [data-vce-full-width="true"]:not([data-vce-stretch-content="true"]) > [data-vce-element-content="true"],
+            div.vcv-content--boxed .vcv-layouts-html > * > [data-vce-full-width="true"]:not([data-vce-stretch-content="true"]) > [data-vce-element-content="true"],
+            div.vcv-editor-theme-hf .vcv-layouts-html > * > [data-vce-full-width="true"]:not([data-vce-stretch-content="true"]) > [data-vce-element-content="true"],
+            div.vcv-header > * > [data-vce-full-width="true"]:not([data-vce-stretch-content="true"]) > [data-vce-element-content="true"],
+            div.vcv-footer > * > [data-vce-full-width="true"]:not([data-vce-stretch-content="true"]) > [data-vce-element-content="true"] {
                 max-width: <?php echo $customLayoutWidth . 'px' ?> !important;
             }
         }
