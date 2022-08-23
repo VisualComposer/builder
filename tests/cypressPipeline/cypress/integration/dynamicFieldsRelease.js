f0c72a451bec9515c2c436a7c98303902fd5a426
Anna Tetere <anna.t@visualcomposer.com>
2022-08-23 12:41:26 +0300

    VC-2937 add tests for current post and new post

---
+++ tests/cypressPipeline/cypress/integration/dynamicFieldsRelease.js
@@ -0,0 +1,72 @@
+/* global describe, it, cy */
+
+describe('Dynamic fields section automation for release checklist', function () {
+    it('Check dynamic fields in release checklist', function () {
+        cy.fixture('../fixtures/dynamicFieldsRelease.json').then((settings) => {
+        cy.visit('/wp-admin/post-new.php?vcv-action=frontend')
+        cy.contains('.blank-page-submit-button', 'Get Started').click()
+
+        cy.get('.vcv-ui-navbar-control-content').contains('Options').click({force: true})
+
+        cy.contains('.vcv-ui-form-group-heading', 'Title').next().clear().type(settings.title)
+        cy.contains('.vcv-ui-form-link', 'add a new category').click()
+
+        cy.contains('.vcv-ui-form-group-heading', 'Category').next().clear().type(settings.category)
+        cy.contains('button', 'Add New Category').click()
+
+        cy.contains('.vcv-ui-form-group-heading', 'Tags').next().find('.vc-tags--search-input').clear().type(settings.tag)
+
+        cy.contains('.vcv-ui-form-group-heading', 'Excerpt').next().clear().type(settings.excerpt)
+
+        cy.addElement('Basic Button', true)
+
+        //image??
+
+        cy.savePage()
+        cy.createPage()
+
+        cy.addElement('Basic Button')
+
+        cy.getIframe('#vcv-editor-iframe').find('vcvtitle').then(($title) => {
+            const pageTitle = $title.text()
+
+            cy.get('span[title="Insert dynamic content"]').first().click()
+
+            cy.get('header').contains('Dynamic Content').parent().parent().find('select').select('Post Title')
+            cy.get('.vcv-ui-modal-action[title="Save"]').click()
+
+            cy.getIframe('#vcv-editor-iframe').find('button').contains(pageTitle)
+
+            cy.contains('.vcv-ui-form-group-heading', 'Button text').parent().parent().find('span[title="Edit dynamic content"]').first().click()
+            cy.contains('label', 'Set custom post source').parent().click()
+
+            cy.get('.vcv-ui-tag-list-item-remove[title="Remove"]').click()
+            cy.contains('.vcv-ui-form-group-heading', 'Source').parent().parent().find('textarea').click({force:true}).clear().type(settings.title)
+            cy.get('.vcv-ui-suggest-box').children().first().click()
+            cy.wait(2000)
+            cy.get('.vcv-ui-modal-action[title="Save"]').click()
+
+            cy.getIframe('#vcv-editor-iframe').find('button').contains(settings.title)
+
+            cy.contains('.vcv-ui-form-group-heading', 'Button text').parent().parent().find('span[title="Edit dynamic content"]').first().click()
+            cy.get('header').contains('Dynamic Content').parent().parent().find('select').select('Post Excerpt')
+            cy.get('.vcv-ui-modal-action[title="Save"]').click()
+            cy.getIframe('#vcv-editor-iframe').find('button').contains(settings.excerpt)
+
+            cy.contains('.vcv-ui-form-group-heading', 'Button text').parent().parent().find('span[title="Edit dynamic content"]').first().click()
+            cy.get('header').contains('Dynamic Content').parent().parent().find('select').select('Post Tags')
+            cy.get('.vcv-ui-modal-action[title="Save"]').click()
+            cy.getIframe('#vcv-editor-iframe').find('button').contains(settings.tag)
+
+            cy.contains('.vcv-ui-form-group-heading', 'Button text').parent().parent().find('span[title="Edit dynamic content"]').first().click()
+            cy.get('header').contains('Dynamic Content').parent().parent().find('select').select('Post Categories')
+            cy.get('.vcv-ui-modal-action[title="Save"]').click()
+            cy.getIframe('#vcv-editor-iframe').find('button').contains(settings.category)
+
+          })
+
+
+
+        })
+    })
+})
