47601b98c10f7a8708ba7de9a6d6136eb40bf316
Janis <mcjohn@inbox.lv>
2022-09-20 15:12:40 +0300

    VC-2649 update cypress version (#2194)
    * VC-2649 update cypress version
    
    * VC-2649 update config
    
    * VC-2649 migrate cypress tests to v10

---
+++ tests/cypressElectron/cypress.config.js
@@ -0,0 +1,31 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  env: {
+    baseUrl: 'http://localhost/',
+    wpUserName: 'admin',
+    wpPassword: '12345',
+    dataPlugin: 'builder/plugin-wordpress.php',
+    newPage: '/wp-admin/post-new.php?post_type=page&vcv-action=frontend',
+    serverType: 'ci',
+    checkSnapshots: false,
+  },
+  video: true,
+  pageLoadTimeout: 80000,
+  execTimeout: 350000,
+  requestTimeout: 10000,
+  wpUserName: 'admin',
+  wpPassword: '12345',
+  dataPlugin: 'builder/plugin-wordpress.php',
+  newPage: '/wp-admin/post-new.php?post_type=page&vcv-action=frontend',
+  serverType: 'ci',
+  checkSnapshots: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost/',
+  },
+})
