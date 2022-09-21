018d073cc08b489c21597d2d8363d0ae26e26758
Nikita <nikita.h@visualcomposer.com>
2022-09-21 16:56:20 +0300

    VC-2371 Migrate rules manager to typescript

--- public/editor/services/rulesManager/service.ts
+++ public/editor/services/rulesManager/service.ts
@@ -1,18 +1,36 @@
 import vcCake from 'vc-cake'
 import { indexOf, get, isEqual } from 'lodash'
 
+interface Rule {
+  rule: string,
+  options: {
+    value: string
+  }
+}
+
+interface RulesObject {
+  [item:string]: Rule
+}
+
+interface ObjectValue {
+  [item:string]: string|boolean
+}
+
+type CheckFunction = () => boolean
+type RulePromise = Promise<boolean>
+
 const Rules = {
   true: () => {
     return true
   },
-  toggle: (value, options) => {
+  toggle: (value:boolean|ObjectValue, options:{key:string}) => {
     let checkValue = value
     if (options.key) {
       checkValue = get(value, options.key)
     }
     return !!checkValue
   },
-  minlength: (value, options) => {
+  minlength: (value:string|ObjectValue, options:{length:string, key:string}) => {
     const length = options.length
     let checkValue = value
     if (options.key) {
@@ -20,7 +38,7 @@ const Rules = {
     }
     return checkValue.length >= parseInt(length)
   },
-  maxlength: (value, options) => {
+  maxlength: (value:string|ObjectValue, options:{length:string, key:string}) => {
     const length = options.length
     let checkValue = value
     if (options.key) {
@@ -28,7 +46,7 @@ const Rules = {
     }
     return checkValue.length <= parseInt(length)
   },
-  range: (value, options) => {
+  range: (value:string|ObjectValue, options: {min: string, max: string, key: string}) => {
     const min = parseInt(options.min)
     const max = parseInt(options.max)
     let checkValue = value
@@ -37,37 +55,38 @@ const Rules = {
     }
     return min <= checkValue.length && checkValue.length <= max
   },
-  minvalue: (value, options) => {
+  minvalue: (value:string|ObjectValue, options:{min: string, key: string}) => {
     const min = parseFloat(options.min)
     let checkValue = value
     if (options.key) {
       checkValue = get(value, options.key)
     }
-    const fl = parseFloat(checkValue)
-    return !isNaN(fl) && fl >= min
+    const fl = typeof checkValue === 'string' && parseFloat(checkValue)
+
+    return typeof fl === 'number' && fl >= min
   },
-  maxvalue: (value, options) => {
+  maxvalue: (value:string|ObjectValue, options:{max: string, key: string}) => {
     const max = parseFloat(options.max)
     let checkValue = value
     if (options.key) {
       checkValue = get(value, options.key)
     }
-    const fl = parseFloat(checkValue)
+    const fl = typeof checkValue === 'string' && parseFloat(checkValue)
 
-    return !isNaN(fl) && fl <= max
+    return typeof fl === 'number' && fl <= max
   },
-  between: (value, options) => {
+  between: (value:string|ObjectValue, options:{min: string, max: string, key: string}) => {
     const min = parseFloat(options.min)
     const max = parseFloat(options.max)
     let checkValue = value
     if (options.key) {
       checkValue = get(value, options.key)
     }
-    const fl = parseFloat(checkValue)
+    const fl = typeof checkValue === 'string' && parseFloat(checkValue)
 
-    return !isNaN(fl) && min <= fl && fl <= max
+    return typeof fl === 'number' && min <= fl && fl <= max
   },
-  value: (value, options) => {
+  value: (value:string|ObjectValue, options:{key: string, value:string}) => {
     let checkValue = value
     if (options.key) {
       checkValue = get(value, options.key)
@@ -75,21 +94,21 @@ const Rules = {
 
     return isEqual(checkValue, options.value)
   },
-  valueIn: (value, options) => {
+  valueIn: (value:string|ObjectValue, options:{values:string[], key: string}) => {
     let checkValue = value
     if (options.key) {
       checkValue = get(value, options.key)
     }
     return indexOf(options.values, checkValue) > -1
   },
-  valueContains: (value, options) => {
+  valueContains: (value:string|ObjectValue, options:{key: string, value:string}) => {
     let checkValue = value
     if (options.key) {
       checkValue = get(value, options.key)
     }
-    return checkValue.indexOf(options.value) > -1
+    return (typeof checkValue === 'string' || Array.isArray(checkValue)) && checkValue.indexOf(options.value) > -1
   },
-  required: (value, options) => {
+  required: (value:string|ObjectValue, options:{key: string}) => {
     let checkValue = value
     if (options.key) {
       checkValue = get(value, options.key)
@@ -98,13 +117,14 @@ const Rules = {
   }
 }
 
-function createPromise (ruleData, value, fieldKey) {
+function createPromise (ruleData:Rule, value:string|boolean, fieldKey:string):Promise<boolean> {
   return new Promise((resolve, reject) => {
     if (!ruleData.rule) {
       ruleData.rule = 'true'
     }
     const ruleName = ruleData.rule.replace('!', '')
     const reversed = ruleData.rule[0] === '!'
+    // @ts-ignore accessing object property via bracket notation
     let result = Rules[ruleName](value, ruleData.options || {}, fieldKey)
     if (reversed) {
       result = !result
@@ -117,13 +137,14 @@ function createPromise (ruleData, value, fieldKey) {
   })
 }
 
-function createSync (ruleData, value) {
+function createSync (ruleData:Rule, value:string|boolean) {
   return () => {
     if (!ruleData.rule) {
       ruleData.rule = 'true'
     }
     const ruleName = ruleData.rule.replace('!', '')
     const reversed = ruleData.rule[0] === '!'
+    // @ts-ignore accessing object property via bracket notation
     let result = Rules[ruleName](value, ruleData.options || {})
     if (reversed) {
       result = !result
@@ -134,9 +155,10 @@ function createSync (ruleData, value) {
 }
 
 const RulesManagerAPI = {
-  check: (values, rules, resultCallback) => {
+  // disabling lint, because values is an element object with different properties
+  check: (values:any, rules:RulesObject, resultCallback:(status:boolean) => void) => { // eslint-disable-line
     const keys = Object.keys(rules || {})
-    const checks = []
+    const checks:RulePromise[] = []
     keys.forEach((key) => {
       const rulePromise = createPromise(rules[key], values[key], key)
       checks.push(rulePromise)
@@ -153,9 +175,10 @@ const RulesManagerAPI = {
         }
       )
   },
-  checkSync: (values, rules, resultCallback) => {
+  // disabling lint, because values is an element object with different properties
+  checkSync: (values:any, rules:RulesObject, resultCallback:(status:boolean) => void) => { // eslint-disable-line
     const keys = Object.keys(rules)
-    const checks = []
+    const checks:CheckFunction[] = []
     keys.forEach((key) => {
       const ruleFn = createSync(rules[key], values[key])
       checks.push(ruleFn)
