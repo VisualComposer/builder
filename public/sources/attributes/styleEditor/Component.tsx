5a9d1db2c2f0314ce0deb86c903892936b772f93
Nikita <nikita.h@visualcomposer.com>
2022-11-02 10:51:45 +0200

    VC-3008 Fix JS codestyle

--- public/sources/attributes/styleEditor/Component.tsx
+++ public/sources/attributes/styleEditor/Component.tsx
@@ -19,29 +19,30 @@ interface Props {
   fieldType: string
 }
 
-const StyleEditorAttribute = forwardRef((props: Props) => {
+// Disabling lint, because ref argument is required, but not used in this case
+const StyleEditorAttribute = forwardRef<HTMLTextAreaElement, Props>((props, ref) => { // eslint-disable-line
   const editorWrapper = useRef<HTMLTextAreaElement>(null)
   // @ts-ignore
   const codeEditor: CodeEditorFace = useRef(null)
 
-  const handleChange = () => {
-    // @ts-ignore
-    const val = codeEditor?.current?.getValue() || ''
-    const {
-      updater,
-      fieldKey,
-      fieldType
-    } = props
-    updater(fieldKey, { all: val }, null, fieldType)
-  }
-
   useEffect(() => {
     const defaultValue = '[element-id] {\n}'
     const editor = CodeEditor.getEditor(editorWrapper.current, 'css', props.value.all || defaultValue)
+    const handleChange = () => {
+      // @ts-ignore
+      const val = codeEditor?.current?.getValue() || ''
+      const {
+        updater,
+        fieldKey,
+        fieldType
+      } = props
+      updater(fieldKey, { all: val }, null, fieldType)
+    }
+
     editor.setSize('100%', '50vh')
     editor.on('change', handleChange)
     codeEditor.current = editor
-  }, [])
+  }, [props])
 
   useEffect(() => {
     codeEditor?.current?.refresh()
