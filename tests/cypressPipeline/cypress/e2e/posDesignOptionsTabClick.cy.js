a8f2fdb4755a5f83d45a0b19a927ea2b090cf67d
Anna Tetere <anna.t@visualcomposer.com>
2022-11-16 12:07:45 +0200

    VC-3026 create e2e test for post design options tab click

---
+++ tests/cypressPipeline/cypress/e2e/posDesignOptionsTabClick.cy.js
@@ -0,0 +1,16 @@
+describe('Post design option tab click', function () {
+    it('Check if design tab opens after click', function () {
+        cy.visit('/wp-admin/post-new.php?vcv-action=frontend')
+        cy.contains('.blank-page-submit-button', 'Get Started').click()
+
+        cy.get('.vcv-ui-navbar-control-content').contains('Options').click({ force: true })
+        cy.contains('.vcv-ui-navigation-slider-button', 'Design Options').click()
+
+        //checking if editor shows up
+        cy.getIframe('#vcv-editor-iframe').find('vcvtitle').contains('Visual Composer')
+        cy.getIframe('#vcv-editor-iframe').find('#vcv-editor')
+
+        //checking if design options tab shows up and has a content
+        cy.get('.advanced-design-options').contains('.vcv-ui-form-group-heading', 'Device type')
+    })
+})
