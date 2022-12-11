f2f98e3e1cfdbc24797e1d484f8fd5b053041179
OlegApanovich <dollar4444@gmail.com>
2022-12-11 23:46:54 +0200

    VC-2831 improve redirect after update

--- visualcomposer/Modules/Settings/SettingsController.php
+++ visualcomposer/Modules/Settings/SettingsController.php
@@ -148,6 +148,8 @@ class SettingsController extends Container implements Module
                 if ($tabsRegistry->get($page)) {
                     // Redirect if bundle update available
                     // Redirect only if slug !== vcv-settings (to allow reset)
+                    $optionsHelper = vchelper('Options');
+                    $optionsHelper->setTransient('redirect:update:url', $page, 60);
                     wp_safe_redirect(admin_url('admin.php?page=vcv-update'));
                     exit;
                 }
