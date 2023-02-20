acd0d480dc3fccd5175922e568f242669d6c18eb
orkun <orkun.aybek@gmail.com>
2023-02-20 17:11:26 +0300

    VC-3135 Updated hero section and feature section

--- elements/elementHeroSection/elementHeroSection/component.js
+++ elements/elementHeroSection/elementHeroSection/component.js
@@ -12,35 +12,32 @@ export default class HeroSectionElement extends vcvAPI.elementComponent {
   render () {
     const { id, atts, editor, children } = this.props
     const { description, align, customClass, metaCustomId, extraDataAttributes } = atts
-    const customProps = {}
-    const containerProps = this.getExtraDataAttributes(extraDataAttributes)
 
+    // Handle conditional classes
     const containerClasses = classNames({
       'vce-hero-section-container': true,
       'vce-hero-section-media--xs': true
     })
 
-    let elementClasses = classNames({
-      vce: true,
+    const elementClasses = classNames({
+      'vce': true,
       'vce-hero-section': true,
       'vce-hero-section--alignment-start': align === 'start',
-      'vce-hero-section--alignment-end': align === 'end'
+      'vce-hero-section--alignment-end': align === 'end',
+      [customClass]: typeof customClass === 'string' && customClass
     })
 
-    if (typeof customClass === 'string' && customClass) {
-      elementClasses = elementClasses.concat(' ' + customClass)
-    }
+    // Handle conditional props
+    const containerProps = this.getExtraDataAttributes(extraDataAttributes)
+    metaCustomId && (containerProps.id = metaCustomId)
 
-    if (metaCustomId) {
-      containerProps.id = metaCustomId
-    }
-
+    // Handle design options
     const doRest = this.applyDO('margin background border animation')
     const doPadding = this.applyDO('padding')
 
     return (
       <section className={containerClasses} {...editor} {...containerProps}>
-        <div className={elementClasses} id={'el-' + id} {...doRest} {...customProps}>
+        <div className={elementClasses} id={'el-' + id} {...doRest}>
           <div className="vce-hero-section--content" {...doPadding}>
             {description}
             {children}
