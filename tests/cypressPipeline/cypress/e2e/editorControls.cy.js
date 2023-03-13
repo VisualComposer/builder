1f03b5c17d48aed5d88c5a3833b3d27e593f5cfb
Nikita <nikita.h@visualcomposer.com>
2023-03-13 11:54:47 +0200

    VC-3197 Update editorControls e2e test

--- tests/cypressPipeline/cypress/e2e/editorControls.cy.js
+++ tests/cypressPipeline/cypress/e2e/editorControls.cy.js
@@ -11,15 +11,16 @@ describe('Editor controls', function () {
       cy.addElement(ELEMENT_NAME, true)
       cy.setInput('Button text', settings.buttonText)
       cy.setURL('Link selection', settings.buttonLink)
+      cy.contains('.vcv-ui-navigation-slider-button', 'Design').click()
       cy.setButtonGroup('Alignment', settings.alignment)
       cy.setButtonGroup('Shape', settings.shape.name)
       cy.setButtonGroup('Size', settings.size.name)
       cy.setSwitch('Stretch')
+      cy.contains('.vcv-ui-navigation-slider-button', 'Design').click()
       cy.setColor(settings.backgroundColor)
+      cy.setDO(settings.designOptions)
       cy.contains('.vcv-ui-navigation-slider-button', 'Advanced').click()
       cy.setClassAndId(settings.customId, settings.customClass)
-      cy.contains('.vcv-ui-navigation-slider-button', 'General').click()
-      cy.setDO(settings.designOptions)
 
       // 2. Open Templates panel
       cy.get('.vcv-ui-navbar-control[data-vcv-guide-helper="plus-control"').click()
