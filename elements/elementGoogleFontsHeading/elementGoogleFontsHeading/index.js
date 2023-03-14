fabc0cb123183e8d8338eba54babdaa1f69d38a6
orkun <orkun.aybek@gmail.com>
2023-03-14 17:28:15 +0300

    VC-3200 Updated google fonts heading component

---
+++ elements/elementGoogleFontsHeading/elementGoogleFontsHeading/index.js
@@ -0,0 +1,21 @@
+/* eslint-disable import/no-webpack-loader-syntax */
+import vcCake from 'vc-cake'
+import GoogleFontsHeadingElement from './component'
+
+const vcvAddElement = vcCake.getService('cook').add
+
+vcvAddElement(
+  require('./settings.json'),
+  // Component callback
+  function (component) {
+    //
+    component.add(GoogleFontsHeadingElement)
+  },
+  // css settings // css for element
+  {
+    css: require('raw-loader!./styles.css'),
+    editorCss: require('raw-loader!./editor.css')
+  },
+  // javascript callback
+  ''
+)
