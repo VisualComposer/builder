b59e0ddd21d6032c82f9a4f171d6ee28c7f3312b
OlegApanovich <dollar4444@gmail.com>
2023-04-11 14:40:01 +0300

    VC-3248 fix woocommerce square plugin compatibility

--- visualcomposer/Modules/Vendors/Plugins/WooCommerceSquareController.php
+++ visualcomposer/Modules/Vendors/Plugins/WooCommerceSquareController.php
@@ -43,24 +43,40 @@ class WooCommerceSquareController extends Container implements Module
 
         $this->addFilter(
             'vcv:assets:enqueue:callback:skip',
-            function ($result, $payload) {
-                $closureInfo = $payload['closureInfo'];
-                if ($closureInfo instanceof \ReflectionMethod) {
-                    if (
-                        !empty($closureInfo->getDeclaringClass()->getName()) &&
-                        strpos($closureInfo->getDeclaringClass()->getName(), 'SV_WC_Payment_Gateway') ||
-                        strpos($closureInfo->getDeclaringClass()->getName(), 'Square\Framework\PaymentGateway\Payment_Gateway')
-                    ) {
-                        return true;
-                    }
-                }
-
-                return $result;
-            }
+            'removePluginActionsForOurPostTypes',
+            10
         );
 
         add_action('wp_enqueue_scripts', [$this, 'removeDuplicateEnqueueForHfs'], 30, 1);
-        add_action('wp_enqueue_scripts', [$this, 'removeDuplicateEnqueueForEmptyGlobalHfs'], 30, 1);
+        add_action('wp_enqueue_scripts', [$this, 'removeDuplicateEnqueueForGlobalHf'], 30, 1);
+        add_action('wp_enqueue_scripts', [$this, 'removeEnqueueForGlobalBlockThemeHf'], 30, 1);
+        add_action('wp_enqueue_scripts', [$this, 'removeEnqueueForGlobalLayouts'], 30, 1);
+    }
+
+    /**
+     * Remove woocommerce square plugin actions for our post types.
+     *
+     * @param bool $result
+     * @param array $payload
+     *
+     * @return bool
+     */
+    public function removePluginActionsForOurPostTypes($result, $payload)
+    {
+        $closureInfo = $payload['closureInfo'];
+        if (! $closureInfo instanceof \ReflectionMethod) {
+            return $result;
+        }
+
+        if (
+            !empty($closureInfo->getDeclaringClass()->getName()) &&
+            strpos($closureInfo->getDeclaringClass()->getName(), 'SV_WC_Payment_Gateway') ||
+            strpos($closureInfo->getDeclaringClass()->getName(), 'Square\Framework\PaymentGateway\Payment_Gateway')
+        ) {
+            return true;
+        }
+
+        return $result;
     }
 
     /**
@@ -75,37 +91,97 @@ class WooCommerceSquareController extends Container implements Module
         ];
 
         if (in_array(get_post_type(), $hfsPostTypeList)) {
-            add_filter('wc_gateway_square_credit_card_is_available', function ($isAvailable) {
-
-                if (self::$cacheInvocation) {
-                    return false;
-                }
-
-                self::$cacheInvocation = true;
-
-                return $isAvailable;
-            }, 10, 1);
+            $this->activateRemovingAction();
         }
     }
 
-    public function removeDuplicateEnqueueForEmptyGlobalHfs()
+    /**
+     * Remove duplicate enqueue for our global header/footer.
+     */
+    public function removeDuplicateEnqueueForGlobalHf()
     {
         $optionsHelper = vchelper('Options');
 
         $globalHfSetting = $optionsHelper->get('headerFooterSettings');
+
+        if ($globalHfSetting !== 'allSite') {
+            return;
+        }
+
         $globalFooter = $optionsHelper->get('headerFooterSettingsAllFooter');
+        $globalHeader = $optionsHelper->get('headerFooterSettingsAllHeader');
 
-        if ($globalHfSetting === 'allSite' && empty($globalFooter)) {
-            add_filter('wc_gateway_square_credit_card_is_available', function ($isAvailable) {
+        if (empty($globalFooter) && empty($globalHeader)) {
+            $this->activateRemovingAction();
+        }
 
-                if (self::$cacheInvocation) {
-                    return false;
+        if (!empty($globalFooter) && empty($globalHeader)) {
+            $this->activateRemovingAction();
+        }
+    }
+
+    /**
+     * Fire up woocommerce square plugin credit card action.
+     */
+    public function activateRemovingAction()
+    {
+        add_filter('wc_gateway_square_credit_card_is_available', function ($isAvailable) {
+
+            if (self::$cacheInvocation) {
+                return false;
+            }
+
+            self::$cacheInvocation = true;
+
+            return $isAvailable;
+        }, 10, 1);
+    }
+
+    /**
+     * For a block theme we should process additional script removing action.
+     */
+    public function removeEnqueueForGlobalBlockThemeHf()
+    {
+        if (!function_exists('wp_is_block_theme') || !wp_is_block_theme()) {
+            return;
+        }
+
+        $this->activateSkipping();
+    }
+
+    /**
+     * For our layouts we should process additional script removing action.
+     */
+    public function removeEnqueueForGlobalLayouts()
+    {
+        if (get_post_type() !== 'vcv_layouts') {
+            return;
+        }
+
+        $this->activateSkipping();
+    }
+
+    /**
+     * Skipping plugin action.
+     */
+    public function activateSkipping()
+    {
+        $this->addFilter(
+            'vcv:assets:enqueue:callback:skip',
+            function ($result, $payload) {
+                $closureInfo = $payload['closureInfo'];
+
+                if ($closureInfo instanceof \ReflectionMethod) {
+                    if (
+                        !empty($closureInfo->name) &&
+                        $closureInfo->name == 'localize_printed_scripts'
+                    ) {
+                        return true;
+                    }
                 }
 
-                self::$cacheInvocation = true;
-
-                return $isAvailable;
-            }, 10, 1);
-        }
+                return $result;
+            }
+        );
     }
 }
