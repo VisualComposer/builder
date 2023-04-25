9eb5bde1ff38fe13f874a683c7da211c4a1ba503
Nikita <nikita.h@visualcomposer.com>
2023-04-25 10:56:35 +0300

    VC-3247 Update Hero Section background

--- elements/heroSection/heroSection/index.js
+++ elements/heroSection/heroSection/index.js
@@ -14,16 +14,6 @@ vcvAddElement(
   // css settings // css for element
   {
     css: require('raw-loader!./styles.css'),
-    editorCss: require('raw-loader!./editor.css'),
-    mixins: {
-      backgroundColor: {
-        mixin: require('raw-loader!./cssMixins/backgroundColor.pcss')
-      },
-      backgroundPosition: {
-        mixin: require('raw-loader!./cssMixins/backgroundPosition.pcss')
-      }
-    }
-  },
-  // javascript callback
-  ''
+    editorCss: require('raw-loader!./editor.css')
+  }
 )
