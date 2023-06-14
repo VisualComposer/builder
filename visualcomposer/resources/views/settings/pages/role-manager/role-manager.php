9555e9fef690d8fbb0410b38cf53288892b15c0a
Nikita <nikita.h@visualcomposer.com>
2023-06-14 12:24:31 +0300

    VC-3344 Update Role Manager notice padding

--- visualcomposer/resources/views/settings/pages/role-manager/role-manager.php
+++ visualcomposer/resources/views/settings/pages/role-manager/role-manager.php
@@ -164,7 +164,7 @@ left: -69px;
    <div class="vcv-dashboard-accordion-item-content">';
         // IF role doesn't have cap `unfiltered_html` show notice
         if ($roleObject && !$roleObject->has_cap('unfiltered_html')) {
-            echo '<div class="notice inline notice-warning update-nag" style="display:block !important;padding: 5px 12px;"><p>' . sprintf(
+            echo '<div class="notice inline notice-warning update-nag" style="display:inline-block !important;padding: 1px 12px;"><p>' . sprintf(
                 // translators: %1$s - role name, %2$s - link to help page, %3$s - capability, %4$s - </a>
                 __(
                     'To enable Visual Composer for %1$s, %2$senable ‘%3$s’ capability%4$s for the exact user.',
