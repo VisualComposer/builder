803020362b0f23bfddd315a03298136b01b35cc5
OlegApanovich <dollar4444@gmail.com>
2024-09-02 15:25:31 +0300

    VC-3461 fix issue with wpforms plugin and recaptcha

--- visualcomposer/Modules/Vendors/Plugins/WpFormsController.php
+++ visualcomposer/Modules/Vendors/Plugins/WpFormsController.php
@@ -16,7 +16,7 @@ use VisualComposer\Helpers\Traits\WpFiltersActions;
 /**
  * Backward compatibility with "WPForms" plugin.
  *
- * @see https://wpbakery.com/
+ * @see https://wordpress.org/plugins/wpforms-lite
  */
 class WpFormsController extends Container implements Module
 {
@@ -24,8 +24,47 @@ class WpFormsController extends Container implements Module
 
     public function __construct(Request $requestHelper)
     {
+        $this->wpAddAction('plugins_loaded', 'initialize');
+    }
+
+    /**
+     * Plugin compatibility hooks initialization.
+     */
+    protected function initialize()
+    {
+        if (! defined( 'WPFORMS_VERSION' )) {
+            return;
+        }
+
+        $requestHelper = vchelper('Request');
         if ($requestHelper->exists(VCV_ADMIN_AJAX_REQUEST) || $requestHelper->exists(VCV_AJAX_REQUEST)) {
             $this->wpAddFilter('wpforms_frontend_missing_assets_error_js_disable', '__return_true');
         }
+
+        add_filter('wpforms_frontend_recaptcha_disable', [$this, 'disableReCaptcha'], 10, 1);
+    }
+
+    /**
+     * Disable WPForms reCAPTCHA for specific plugin post types.
+     *
+     * @param bool $result
+     *
+     * @return bool
+     */
+    public function disableReCaptcha($result) {
+        global $post;
+
+        if (! $post) {
+            return $result;
+        }
+
+        $postTypeHelper = vchelper('PostType');
+        $editor_post_type = $postTypeHelper->getEditorTypeByPostTypeDependecy($post->post_type);
+
+        if ($editor_post_type !== 'default') {
+            return true;
+        }
+
+        return $result;
     }
 }
